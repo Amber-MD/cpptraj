75ffb8634173eee090f52e83adfa5c080cb7b7d3
Daniel R. Roe <daniel.r.roe@gmail.com>
2011-01-12 11:25:52 -0500

    Add basic test for rmsd calcuations with and without mass weighting.

---
+++ test/Test_RMSD/RunTest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+# Clean
+CleanFiles rms.in rmsd.dat rms.mass.in rmsd.mass.dat
+
+CheckNetcdf
+TOP="../ChainA-tip3p.parm7"
+
+# Test 1
+cat > rms.in <<EOF
+trajin ../run0.nc
+rms first :10-260 out rmsd.dat
+EOF
+INPUT="rms.in"
+RunCpptraj "RMSD Test."
+DoTest rmsd.dat.save rmsd.dat
+CheckTest
+
+# Test 2
+cat > rms.mass.in <<EOF
+trajin ../run0.nc
+rms first :10-260 out rmsd.mass.dat mass
+EOF
+INPUT="rms.mass.in"
+RunCpptraj "RMSD test with mass weighting."
+DoTest rmsd.mass.dat.save rmsd.mass.dat
+CheckTest
+
+EndTest
+
+exit 0
