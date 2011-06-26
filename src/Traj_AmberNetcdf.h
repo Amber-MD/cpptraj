606808fe862578ad41caa2bb38fd042599a8c1a2
Daniel R. Roe <daniel.r.roe@gmail.com>
2011-06-26 08:45:50 -0400

    DRR - Cpptraj: Amber netcdf, new style

---
+++ src/Traj_AmberNetcdf.h
@@ -0,0 +1,45 @@
+#ifndef INC_TRAJ_AMBERNETCDF_H
+#define INC_TRAJ_AMBERNETCDF_H
+#ifdef BINTRAJ
+#include "TrajectoryIO.h"
+/// Class: AmberNetcdf
+/// TrajectoryIO class for reading and writing Amber Netcdf format 
+/// trajectories. 
+class AmberNetcdf : public TrajectoryIO {
+    int ncid;
+    int frameDID;
+    int ncframe;
+    int atomDID;
+    int ncatom; 
+    float *Coord;
+    int coordVID;
+    int cellAngleVID;
+    int cellLengthVID;
+
+    int spatialDID;
+    int labelDID;
+    int cell_spatialDID;
+    int cell_angularDID;
+    int spatialVID;
+    int timeVID;
+    int cell_spatialVID;
+    int cell_angularVID;
+    int TempVID;
+
+    // Inherited functions
+    int setupRead(int);
+    int setupWrite(int);
+    int openTraj();
+    void closeTraj();
+    int readFrame(int,double*,double*,double*);
+    int writeFrame(int,double*,double*,double);
+    void info();
+
+  public:
+    AmberNetcdf();
+    ~AmberNetcdf();
+    // AmberNetcdf-specific functions
+    void SetRemdTraj();
+};
+#endif
+#endif
