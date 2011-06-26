516d4a987e8234b9cf10c03f11141152c07538ab
Daniel R. Roe <daniel.r.roe@gmail.com>
2011-06-26 16:10:55 -0400

    DRR - Cpptraj: Make virtual version of setupRead return correct error val of -1

--- src/TrajectoryIO.h
+++ src/TrajectoryIO.h
@@ -46,7 +46,7 @@ class TrajectoryIO {
     virtual ~TrajectoryIO(); // virtual since this class is inherited.
 
     // Inherited functions 
-    virtual int setupRead(int) { return 1; }
+    virtual int setupRead(int) { return -1; }
     virtual int setupWrite(int) { return 1; }
     virtual int openTraj() { return 1; }
     virtual int readFrame(int,double*,double*,double*) { return 1; }
