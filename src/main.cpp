40ab5db0577d86d8f14430a1cb225a59bad0950e
Daniel R. Roe <daniel.r.roe@gmail.com>
2011-10-14 09:04:44 -0400

    DRR - Cpptraj: Update version number.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <cstdio>
 #include <cstdlib> // atoi
 #ifndef CPPTRAJ_VERSION_STRING
-#define CPPTRAJ_VERSION_STRING "V2.2.2b"
+#define CPPTRAJ_VERSION_STRING "V2.2.3b"
 #endif
 
 /* Usage()
