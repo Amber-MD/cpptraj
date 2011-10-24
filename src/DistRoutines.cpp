79454801bc3010ae9f1197a573ee251cfb9f2ced
Daniel R. Roe <daniel.r.roe@gmail.com>
2011-10-24 08:42:29 -0400

    DRR - Cpptraj: Vectorize imaged distance calc loop.

--- src/DistRoutines.cpp
+++ src/DistRoutines.cpp
@@ -2,8 +2,7 @@
 #include "DistRoutines.h"
 #include "vectormath.h" // DEGRAD
 
-/*
- * Frame::ClosestImage()
+/* Frame::ClosestImage()
  * Given two coordinates A and B, determine the unit XYZ vector that points 
  * towards the closest image of B to A.
  * It is assumed the coordinates are already relative to the box center.
@@ -54,8 +53,7 @@ void Frame::ClosestImage(double *A, double *B, int *ixyz) {
 }
 */
 
-/*
- * MinImageNonOrtho2()
+/* MinImageNonOrtho2()
  * Given two sets of coordinates and reciprocal space information based on
  * the current non-orthorhombic box, return the shortest imaged distance^2
  * between the coordinates.
@@ -97,8 +95,7 @@ double MinImageNonOrtho2(double *Coord1, double *Coord2, double *box, bool origi
   return min;
 }
 
-/*
- * Frame::DIST2_ImageNonOrtho()
+/* Frame::DIST2_ImageNonOrtho()
  * Given two coordinates and reciprocal space information based on 
  * the current non-orthorhombic box, return the shortest imaged distance^2 
  * between the coordinates.
@@ -119,8 +116,7 @@ double DIST2_ImageNonOrtho(double *a1, double *a2, double *ucell, double *recip)
   return DIST2_ImageNonOrtho(f, f2, -1.0, ixyz, ucell);
 }
 
-/*
- * DIST2_ImageNonOrtho()
+/* DIST2_ImageNonOrtho()
  * Given two coordinate sets in reciprocal space, return the minimum imaged
  * distance^2 between them.
  * If minIn is > 0.0 it is considered a possible minimum distance.
@@ -128,14 +124,12 @@ double DIST2_ImageNonOrtho(double *a1, double *a2, double *ucell, double *recip)
  * space will be placed in ixyz.
  */
 double DIST2_ImageNonOrtho(double *f, double *f2, double minIn, int *ixyz, double *ucell) { 
-// double closest2
-  double fx, fy, fz, f2x, f2y, f2z;
+  //double closest2
+  double fx, fy, fz, f2x, f2y, f2z, X_factor, Y_factor, Z_factor;
+  double fxm1, fxp1, fym1, fyp1, fzm1, fzp1;
   double x,y,z,D,min;
-  int ix,iy,iz;
-  // DEBUG
 
-  /*
-   *  NON-ORTHORHOMBIC CASE: find shortest distance in periodic reference
+  /*  NON-ORTHORHOMBIC CASE: find shortest distance in periodic reference
    *  This is a brute force check requiring up to 26 distance evaluations.
    *  It has been adapted to be smarter by returning the first distance that
    *  is shorter than the minimum possible distance between images.
@@ -149,10 +143,49 @@ double DIST2_ImageNonOrtho(double *f, double *f2, double minIn, int *ixyz, doubl
   f2y = f2[1] - floor(f2[1]);
   f2z = f2[2] - floor(f2[2]);
 
+  // Precompute some factors
+  X_factor = (f2x*ucell[0] + f2y*ucell[3] + f2z*ucell[6]);
+  Y_factor = (f2x*ucell[1] + f2y*ucell[4] + f2z*ucell[7]);
+  Z_factor = (f2x*ucell[2] + f2y*ucell[5] + f2z*ucell[8]);
+
+  fxm1 = fx - 1; fxp1 = fx + 1;
+  fym1 = fy - 1; fyp1 = fy + 1;
+  fzm1 = fz - 1; fzp1 = fz + 1;
+
+  double fxm1u0 = fxm1 * ucell[0];
+  double fxu0   = fx   * ucell[0];
+  double fxp1u0 = fxp1 * ucell[0];
+  double fxm1u1 = fxm1 * ucell[1];
+  double fxu1   = fx   * ucell[1];
+  double fxp1u1 = fxp1 * ucell[1];
+  double fxm1u2 = fxm1 * ucell[2];
+  double fxu2   = fx   * ucell[2];
+  double fxp1u2 = fxp1 * ucell[2];
+
+  double fym1u3 = fym1 * ucell[3];
+  double fyu3   = fy   * ucell[3];
+  double fyp1u3 = fyp1 * ucell[3];
+  double fym1u4 = fym1 * ucell[4];
+  double fyu4   = fy   * ucell[4];
+  double fyp1u4 = fyp1 * ucell[4];
+  double fym1u5 = fym1 * ucell[5];
+  double fyu5   = fy   * ucell[5];
+  double fyp1u5 = fyp1 * ucell[5];
+
+  double fzm1u6 = fzm1 * ucell[6];
+  double fzu6   = fz   * ucell[6];
+  double fzp1u6 = fzp1 * ucell[6];
+  double fzm1u7 = fzm1 * ucell[7];
+  double fzu7   = fz   * ucell[7];
+  double fzp1u7 = fzp1 * ucell[7];
+  double fzm1u8 = fzm1 * ucell[8];
+  double fzu8   = fz   * ucell[8];
+  double fzp1u8 = fzp1 * ucell[8];
+
   // Calc ix iy iz = 0 case
-  x = (fx*ucell[0] + fy*ucell[3] + fz*ucell[6]) - (f2x*ucell[0] + f2y*ucell[3] + f2z*ucell[6]);
-  y = (fx*ucell[1] + fy*ucell[4] + fz*ucell[7]) - (f2x*ucell[1] + f2y*ucell[4] + f2z*ucell[7]);
-  z = (fx*ucell[2] + fy*ucell[5] + fz*ucell[8]) - (f2x*ucell[2] + f2y*ucell[5] + f2z*ucell[8]);
+  x = (fxu0 + fyu3 + fzu6) - X_factor;
+  y = (fxu1 + fyu4 + fzu7) - Y_factor;
+  z = (fxu2 + fyu5 + fzu8) - Z_factor;
   // DEBUG
   //fprintf(stdout,"DEBUG: a2: fx  fy  fz  = %lf %lf %lf\n",fx,fy,fz);
   //fprintf(stdout,"DEBUG: a1: f2x f2y f2z = %lf %lf %lf\n",f2x,f2y,f2z);
@@ -164,6 +197,166 @@ double DIST2_ImageNonOrtho(double *f, double *f2, double minIn, int *ixyz, doubl
   ixyz[1] = 0;
   ixyz[2] = 0;
 
+  // -1 -1 -1
+  x = (fxm1u0 + fym1u3 + fzm1u6) - X_factor;
+  y = (fxm1u1 + fym1u4 + fzm1u7) - Y_factor;
+  z = (fxm1u2 + fym1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] = -1; }
+  // -1 -1  0
+  x = (fxm1u0 + fym1u3 + fzu6  ) - X_factor;
+  y = (fxm1u1 + fym1u4 + fzu7  ) - Y_factor;
+  z = (fxm1u2 + fym1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] =  0; }
+  // -1 -1 +1
+  x = (fxm1u0 + fym1u3 + fzp1u6) - X_factor;
+  y = (fxm1u1 + fym1u4 + fzp1u7) - Y_factor;
+  z = (fxm1u2 + fym1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] =  1; }
+  // -1  0 -1
+  x = (fxm1u0 + fyu3   + fzm1u6) - X_factor;
+  y = (fxm1u1 + fyu4   + fzm1u7) - Y_factor;
+  z = (fxm1u2 + fyu5   + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] = -1; }
+  // -1  0  0
+  x = (fxm1u0 + fyu3   + fzu6  ) - X_factor;
+  y = (fxm1u1 + fyu4   + fzu7  ) - Y_factor;
+  z = (fxm1u2 + fyu5   + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] =  0; }
+  // -1  0 +1
+  x = (fxm1u0 + fyu3   + fzp1u6) - X_factor;
+  y = (fxm1u1 + fyu4   + fzp1u7) - Y_factor;
+  z = (fxm1u2 + fyu5   + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] =  1; }
+  // -1 +1 -1
+  x = (fxm1u0 + fyp1u3 + fzm1u6) - X_factor;
+  y = (fxm1u1 + fyp1u4 + fzm1u7) - Y_factor;
+  z = (fxm1u2 + fyp1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] = -1; }
+  // -1 +1  0
+  x = (fxm1u0 + fyp1u3 + fzu6  ) - X_factor;
+  y = (fxm1u1 + fyp1u4 + fzu7  ) - Y_factor;
+  z = (fxm1u2 + fyp1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] =  0; }
+  // -1 +1 +1
+  x = (fxm1u0 + fyp1u3 + fzp1u6) - X_factor;
+  y = (fxm1u1 + fyp1u4 + fzp1u7) - Y_factor;
+  z = (fxm1u2 + fyp1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] =  1; }
+
+  //  0 -1 -1
+  x = (fxu0   + fym1u3 + fzm1u6) - X_factor;
+  y = (fxu1   + fym1u4 + fzm1u7) - Y_factor;
+  z = (fxu2   + fym1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] = -1; }
+  //  0 -1  0
+  x = (fxu0   + fym1u3 + fzu6  ) - X_factor;
+  y = (fxu1   + fym1u4 + fzu7  ) - Y_factor;
+  z = (fxu2   + fym1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] =  0; }
+  //  0 -1 +1
+  x = (fxu0   + fym1u3 + fzp1u6) - X_factor;
+  y = (fxu1   + fym1u4 + fzp1u7) - Y_factor;
+  z = (fxu2   + fym1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] =  1; }
+  //  0  0 -1
+  x = (fxu0   + fyu3   + fzm1u6) - X_factor;
+  y = (fxu1   + fyu4   + fzm1u7) - Y_factor;
+  z = (fxu2   + fyu5   + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  0; ixyz[2] = -1; }
+  //  0  0  0
+  //  0  0 +1
+  x = (fxu0   + fyu3   + fzp1u6) - X_factor;
+  y = (fxu1   + fyu4   + fzp1u7) - Y_factor;
+  z = (fxu2   + fyu5   + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  0; ixyz[2] =  1; }
+  //  0 +1 -1
+  x = (fxu0   + fyp1u3 + fzm1u6) - X_factor;
+  y = (fxu1   + fyp1u4 + fzm1u7) - Y_factor;
+  z = (fxu2   + fyp1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] = -1; }
+  //  0 +1  0
+  x = (fxu0   + fyp1u3 + fzu6  ) - X_factor;
+  y = (fxu1   + fyp1u4 + fzu7  ) - Y_factor;
+  z = (fxu2   + fyp1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] =  0; }
+  //  0 +1 +1
+  x = (fxu0   + fyp1u3 + fzp1u6) - X_factor;
+  y = (fxu1   + fyp1u4 + fzp1u7) - Y_factor;
+  z = (fxu2   + fyp1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] =  1; }
+
+  // +1 -1 -1
+  x = (fxp1u0 + fym1u3 + fzm1u6) - X_factor;
+  y = (fxp1u1 + fym1u4 + fzm1u7) - Y_factor;
+  z = (fxp1u2 + fym1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] = -1; }
+  // +1 -1  0
+  x = (fxp1u0 + fym1u3 + fzu6  ) - X_factor;
+  y = (fxp1u1 + fym1u4 + fzu7  ) - Y_factor;
+  z = (fxp1u2 + fym1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] =  0; }
+  // +1 -1 +1
+  x = (fxp1u0 + fym1u3 + fzp1u6) - X_factor;
+  y = (fxp1u1 + fym1u4 + fzp1u7) - Y_factor;
+  z = (fxp1u2 + fym1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] =  1; }
+  // +1  0 -1
+  x = (fxp1u0 + fyu3   + fzm1u6) - X_factor;
+  y = (fxp1u1 + fyu4   + fzm1u7) - Y_factor;
+  z = (fxp1u2 + fyu5   + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] = -1; }
+  // +1  0  0
+  x = (fxp1u0 + fyu3   + fzu6  ) - X_factor;
+  y = (fxp1u1 + fyu4   + fzu7  ) - Y_factor;
+  z = (fxp1u2 + fyu5   + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] =  0; }
+  // +1  0 +1
+  x = (fxp1u0 + fyu3   + fzp1u6) - X_factor;
+  y = (fxp1u1 + fyu4   + fzp1u7) - Y_factor;
+  z = (fxp1u2 + fyu5   + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] =  1; }
+  // +1 +1 -1
+  x = (fxp1u0 + fyp1u3 + fzm1u6) - X_factor;
+  y = (fxp1u1 + fyp1u4 + fzm1u7) - Y_factor;
+  z = (fxp1u2 + fyp1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] = -1; }
+  // +1 +1  0
+  x = (fxp1u0 + fyp1u3 + fzu6  ) - X_factor;
+  y = (fxp1u1 + fyp1u4 + fzu7  ) - Y_factor;
+  z = (fxp1u2 + fyp1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] =  0; }
+  // +1 +1 +1
+  x = (fxp1u0 + fyp1u3 + fzp1u6) - X_factor;
+  y = (fxp1u1 + fyp1u4 + fzp1u7) - Y_factor;
+  z = (fxp1u2 + fyp1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] =  1; }
+
   //if (closest2 != 0.0 && min < closest2) return (min);
 //  this->ClosestImage(a1, a2, ixyz);
 //  fprintf(stdout,"DEBUG: Predict  = %2i %2i %2i\n",ixyz[0],ixyz[1],ixyz[2]);
@@ -172,38 +365,7 @@ double DIST2_ImageNonOrtho(double *f, double *f2, double minIn, int *ixyz, doubl
 //  iy = ixyz[1];
 //  iz = ixyz[2];
 
-  for (ix = -1; ix <= 1; ix++) {
-    for (iy = -1; iy <= 1; iy++) {
-      for (iz = -1; iz <= 1; iz++) {
-
-        if (! (ix == 0 && iy == 0 && iz == 0) ) {
-          x = ((fx+ix)*ucell[0] + (fy+iy)*ucell[3] + (fz+iz)*ucell[6]) - 
-              (f2x*ucell[0] + f2y*ucell[3] + f2z*ucell[6]);
-          y = ((fx+ix)*ucell[1] + (fy+iy)*ucell[4] + (fz+iz)*ucell[7]) - 
-              (f2x*ucell[1] + f2y*ucell[4] + f2z*ucell[7]);
-          z = ((fx+ix)*ucell[2] + (fy+iy)*ucell[5] + (fz+iz)*ucell[8]) - 
-              (f2x*ucell[2] + f2y*ucell[5] + f2z*ucell[8]);
-          D = (x*x) + (y*y) + (z*z);
-
-          //if (debug > 3) 
-          //  printf("DISTANCE + %2i*X %2i*Y %2i*Z unit cells is %8.3f\n", ix, iy, iz, sqrt(D));
-          
-          if (D < min) {
-            min = D;
-            ixyz[0] = ix;
-            ixyz[1] = iy;
-            ixyz[2] = iz;
-            // DEBUG
-            //if (closest2 != 0.0 && min < closest2)
-            //  return(min);
-          }
-        }
-
-      }
-    }
-  }
-
-  //D = sqrt(min);
+//D = sqrt(min);
 //  fprintf(stdout,"DEBUG: MinDist  = %2i %2i %2i = %8.3f\n", ixmin, iymin, izmin, D);
 //  printf("---------------------------------------------------------------\n");
   return(min);
