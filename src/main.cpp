1a5c302e06bbf49b3e6b405c3407dc28099c40d5
Daniel R. Roe <daniel.r.roe@gmail.com>
2011-11-09 13:17:27 -0500

    DRR - Cpptraj: Update version to reflect addition of ptraj actions/analyze routines

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <cstdio>
 #include <cstdlib> // atoi
 #ifndef CPPTRAJ_VERSION_STRING
-#define CPPTRAJ_VERSION_STRING "V2.2.8b"
+#define CPPTRAJ_VERSION_STRING "V2.3.0b"
 #endif
 
 // Usage()
