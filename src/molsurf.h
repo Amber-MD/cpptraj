b412c1060505204da600741b00954d05eaeb131c
Daniel R. Roe <daniel.r.roe@gmail.com>
2011-11-10 13:39:14 -0500

    DRR - Cpptraj: Begin adaption of molsurf routine for inclusion in cpptraj

---
+++ src/molsurf.h
@@ -0,0 +1,9 @@
+#ifndef INC_MOLSURF_H
+#define INC_MOLSURF_H
+
+#define REAL_T	double
+#include "Name.h" // NAME
+
+REAL_T molsurf(REAL_T, REAL_T *, int, NAME *, NAME *, int *, REAL_T *, REAL_T *);
+
+#endif
