628f6c1dc29d45414142efd78b34b308b98f5e8d
Daniel R. Roe <daniel.r.roe@gmail.com>
2012-01-26 13:38:11 -0500

    DRR - Cpptraj: Inlining test for surf

--- src/Action_Surf.cpp
+++ src/Action_Surf.cpp
@@ -19,13 +19,9 @@ Surf::~Surf() {
   if (distances!=NULL) free(distances);
 }
 
-/* Surf::init()
- * Expected call: surf <name> <mask1> [out filename]
- * Dataset name will be the last arg checked for. Check order is:
- *    1) Keywords
- *    2) Masks
- *    3) Dataset name
- */
+// Surf::init()
+/** Expected call: surf <name> <mask1> [out filename]
+  */
 int Surf::init() {
   char *mask1;
   char *surfFile;
@@ -48,10 +44,10 @@ int Surf::init() {
   return 0;
 }
 
-/* Surf::setup()
- * Set surf up for this parmtop. Get masks etc.
- * currentParm is set in Action::Setup
- */
+// Surf::setup()
+/** Set LCPO surface area calc parameters for this parmtop if not already set. 
+  * Get the mask, and check that the atoms in mask belong to solute. 
+  */
 int Surf::setup() {
   int matrixSize, i;
 
@@ -85,13 +81,12 @@ int Surf::setup() {
   return 0;  
 }
 
-/* 
- * CalcIndex()
- * Given a row and column value for a half square matrix minus diagonal 
- * calculate the position in a 1D array.
- * NOTE: Is all the extra math in index calc worth it? Just have simple
- * square matrix?
- */
+// CalcIndex()
+/** Given a row and column value for a half square matrix minus diagonal 
+  * calculate the position in a 1D array.
+  * NOTE: Is all the extra math in index calc worth it? Just have simple
+  * square matrix?
+  */
 static int CalcIndex(int iIn, int jIn, int natom) {
   int i, j, i1;
 
@@ -107,14 +102,11 @@ static int CalcIndex(int iIn, int jIn, int natom) {
   return ( ( (natom * i) - ((i1 * i) / 2) ) + j - i1 );
 }
 
-/* Surf::CalcLCPO()
- * Return the LCPO surface area for the given atom. LCPO method from:
- *   J. Weiser, P.S. Shenkin, and W.C. Still,
- *   "Approximate atomic surfaces from linear combinations of pairwise
- *   overlaps (LCPO)", J. Comp. Chem. 20:217 (1999).
- * Adapted from gbsa=1 method in SANDER, egb.f and gbsa.h
- */
-double Surf::CalcLCPO(int atomi, std::vector<int> ineighbor) {
+// Surf::CalcLCPO()
+/** Return the LCPO surface area for the given atom. 
+  * Adapted from gbsa=1 method in SANDER, egb.f and gbsa.h
+  */
+double Surf::CalcLCPO(int atomi, std::vector<int>  ineighbor) {
   double sumaij, sumajk, sumajk_2, sumaijajk;
   double tmpaij, vdw2dif, tmpajk;
   double aij, ajk;
@@ -257,8 +249,82 @@ int Surf::action() {
       }
     }
     // Calculate surface area
+    // -------------------------------------------------------------------------
+#ifdef SURF_USEINLINE
+    // Calculate LCPO surface area Ai for atomi:
+    // Ai = P1*S1 + P2*Sum(Aij) + P3*Sum(Ajk) + P4*Sum(Aij * Sum(Ajk))
+    double sumaij = 0.0;
+    double sumajk = 0.0;
+    double sumaijajk = 0.0;
+
+    // DEBUG - print neighbor list
+    //mprintf("SURF: Neighbors for atom %i:",atomi);
+    //for (std::vector<int>::iterator jt = ineighbor.begin(); jt != ineighbor.end(); jt++) {
+    //  mprintf(" %i",*jt);
+    //}
+    //mprintf("\n");
+
+    // Calculate surface area of atom i
+    double vdwi = currentParm->SurfaceInfo[atomi].vdwradii;
+    double vdwi2 = vdwi * vdwi;
+    double Si = vdwi2 * FOURPI;
+
+    // Loop over all neighbors of atomi (j)
+    // NOTE: Factor through the 2 in aij and ajk?
+    for (std::vector<int>::iterator jt = ineighbor.begin(); 
+                                    jt != ineighbor.end(); 
+                                    jt++) 
+    {
+      //printf("i,j %i %i\n",atomi + 1,(*jt)+1);
+      double vdwj = currentParm->SurfaceInfo[*jt].vdwradii;
+      double vdwj2 = vdwj * vdwj;
+      distIndex = CalcIndex(atomi, *jt, soluteAtoms);
+      double dij = distances[distIndex];
+      double tmpaij = vdwi - (dij * 0.5) - ( (vdwi2 - vdwj2)/(2.0 * dij) );
+      double aij = 2.0 * PI * vdwi * tmpaij;
+      sumaij += aij;
+
+      // Find which neighbors of atom i (j and k) are themselves neighbors
+      double sumajk_2 = 0.0;
+      for (std::vector<int>::iterator kt = ineighbor.begin(); 
+                                      kt != ineighbor.end(); 
+                                      kt++) 
+      {
+        if ( (*kt) == (*jt) ) continue;
+        //printf("i,j,k %i %i %i\n",atomi + 1,(*jt)+1,(*kt)+1);
+        double vdwk = currentParm->SurfaceInfo[*kt].vdwradii;
+        distIndex = CalcIndex(*jt, *kt, soluteAtoms);
+        double djk = distances[distIndex];
+        //printf("%4s%6i%6i%12.8lf\n","DJK ",(*jt)+1,(*kt)+1,djk);
+        //printf("%6s%6.2lf%6.2lf\n","AVD ",vdwj,vdwk);
+        if ( (vdwj + vdwk) > djk ) {
+          double vdw2dif = vdwj2 - (vdwk * vdwk);
+          double tmpajk = (2.0*vdwj) - djk - (vdw2dif / djk);
+          double ajk = PI*vdwj*tmpajk;
+          //tmpajk = vdwj - (djk *0.5) - ( (vdwj2 - (vdwk * vdwk))/(2.0 * djk) );
+          //ajk = 2.0 * PI * vdwi * tmpajk;
+          //printf("%4s%6i%6i%12.8lf%12.8lf%12.8lf\n","AJK ",(*jt)+1,(*kt)+1,ajk,vdw2dif,tmpajk);
+          sumajk += ajk;
+          sumajk_2 += ajk;
+        }
+      } // END loop over neighbor-neighbor pairs of atom i (kt)
+
+      sumaijajk += (aij * sumajk_2);
+
+      // DEBUG
+      //printf("%4s%20.8lf %20.8lf %20.8lf\n","AJK ",aij,sumajk,sumaijajk);
+
+    } // END Loop over neighbors of atom i (jt)
+    SA += ( (currentParm->SurfaceInfo[atomi].P1 * Si       ) +
+            (currentParm->SurfaceInfo[atomi].P2 * sumaij   ) +
+            (currentParm->SurfaceInfo[atomi].P3 * sumajk   ) +
+            (currentParm->SurfaceInfo[atomi].P4 * sumaijajk) 
+          );
+#else
     SA+=CalcLCPO(atomi,ineighbor);
-  }  
+#endif
+    // -------------------------------------------------------------------------
+  } // END Loop over atoms in mask (atomi) 
 #ifdef _OPENMP
 } // END pragma omp parallel
 #endif
--- src/Action_Surf.h
+++ src/Action_Surf.h
@@ -1,16 +1,21 @@
 #ifndef INC_ACTION_SURF_H
 #define INC_ACTION_SURF_H
-// Surf
 #include "Action.h"
 #include <vector>
-
+// Class: Surf
+/// Calculate LCPO surface area.
+/** LCPO method from:
+  * -  J. Weiser, P.S. Shenkin, and W.C. Still,
+  *    "Approximate atomic surfaces from linear combinations of pairwise
+  *    overlaps (LCPO)", J. Comp. Chem. 20:217 (1999).
+  */
 class Surf: public Action {
     DataSet *surf;
     AtomMask Mask1;
     double *distances;
     int soluteAtoms;
 
-    double CalcLCPO(int,std::vector<int>);
+    double CalcLCPO(int,std::vector<int> );
   public:
     Surf();
     ~Surf();
