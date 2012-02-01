3475977df1375ad7e6eb5628222789dcb460caac
Daniel R. Roe <daniel.r.roe@gmail.com>
2012-02-01 12:29:52 -0500

    DRR - Cpptraj: Rework byte swapping code

--- src/Traj_CharmmDcd.cpp
+++ src/Traj_CharmmDcd.cpp
@@ -83,6 +83,7 @@ void CharmmDcd::closeTraj() {
   tfile->CloseFile();
 }
 
+// ---------- Byte Swapping Routines -------------------------------------------
 /*inline void endian_swap(unsigned int& x)
 {
     x = (x>>24) | 
@@ -101,47 +102,59 @@ void CharmmDcd::closeTraj() {
         ((x>>40) & 0x000000000000FF00) |
         (x<<56);
 }*/
+/// Perform byte swaps on 4-byte segments.
+/** \param x starting memory address of consecutive 4-byte segments to swap
+  * \param nswap number of segments to swap
+  */
+static void endian_swap(void *x, long nswap) {
+  int *Xi;
 
-// FROM VMD 1.8.7 --------------------------------------------------------------
-/* Only works with aligned 4-byte quantities, will cause a bus error */
-/* on some platforms if used on unaligned data.                      */
-static void endian_swap(void *v, long ndata) {
-  int *data = (int *) v;
-  int *N;
-  for (long i=0; i<ndata; i++) {
-    N = data + i;
-    *N=( 
-        ((*N>>24)&0xFF) | 
-        ((*N&0xFF)<<24) |
-        ((*N>>8)&0xFF00) | 
-        ((*N&0xFF00)<<8)
-       );
+  int *addr = (int *) x;
+  for (long i=0; i<nswap; i++) {
+    Xi = addr + i;
+    *Xi=( 
+         ((*Xi>>24) & 0xFF)   | 
+         ((*Xi&0xFF)   << 24) |
+         ((*Xi>>8)  & 0xFF00) | 
+         ((*Xi&0xFF00) << 8)
+        );
   }
 }
-static void endian_swap8(void *v, long ndata) {
-  /* Use int* internally to prevent bugs caused by some compilers */
-  /* and hardware that would potentially load data into an FP reg */
-  /* and hose everything, such as the old "jmemcpy()" bug in NAMD */
-  int *data = (int *) v;
-  long i;
-  int *N;
-  int t0, t1;
+/// Perform byte swaps on 8-byte segments.
+/** Break up the total swap into two 4-byte swaps.
+  * \param x starting memory address of consecutive 8-byte segments to swap
+  * \param nswap number of segments to swap
+  */
+static void endian_swap8(void *x, long nswap) {
+  int *Xi;
+  int x0, x1;
 
-  for (i=0; i<ndata; i++) {
-    N = data + (i<<1);
-    t0 = N[0];
-    t0=(((t0>>24)&0xff) | ((t0&0xff)<<24) |
-        ((t0>>8)&0xff00) | ((t0&0xff00)<<8));
-
-    t1 = N[1];
-    t1=(((t1>>24)&0xff) | ((t1&0xff)<<24) |
-        ((t1>>8)&0xff00) | ((t1&0xff00)<<8));
-
-    N[0] = t1;
-    N[1] = t0;
+  int *addr = (int *) x;
+  for (long i=0; i<nswap; i++) {
+    Xi = addr + (i<<1);
+    // Perform swap on first 4 bytes
+    x0 = Xi[0];
+    x0=(
+        ((x0>>24) & 0xFF)   | 
+        ((x0&0xFF)   << 24) |
+        ((x0>>8)  & 0xFF00) | 
+        ((x0&0xFF00) << 8)
+       );
+    // Perform swap on second 4 bytes
+    x1 = Xi[1];
+    x1=(
+        ((x1>>24) & 0xFF)   | 
+        ((x1&0xFF)   << 24) |
+        ((x1>>8)  & 0xFF00) | 
+        ((x1&0xFF00) << 8)
+       );
+    // Re-assemble swapped bytes
+    Xi[0] = x1;
+    Xi[1] = x0;
   }
 } 
 // -----------------------------------------------------------------------------
+
 // CharmmDcd::ReadBlock()
 /** Read readByte number of bytes, convert to integer. If expected
   * is not -1, check that the integer matches expected.
