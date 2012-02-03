f712429beda27d643d73b14184e2b717a8b61019
Daniel R. Roe <daniel.r.roe@gmail.com>
2012-02-03 15:40:16 -0500

    DRR - Cpptraj: Dont use class for now

--- src/Integrate.h
+++ src/Integrate.h
@@ -1,12 +1,13 @@
 #ifndef INC_INTEGRATE_H
 #define INC_INTEGRATE_H
+//#include <vector>
 // Class: Interpolate
-class Interpolate {
+/*class Interpolate {
     std::vector<double> b;
     std::vector<double> c;
     std::vector<double> d;
-  public:
-    void cubicSpline_coeff(double *, double *, int);
-    int cubicSpline_eval(double*,double*,int,double*,double*,int);
-};
+  public:*/
+    void cubicSpline_coeff(double *, double *, int, double*, double*, double*);
+    int cubicSpline_eval(double*,double*,int,double*,double*,double*,double*,double*,int);
+//};
 #endif
