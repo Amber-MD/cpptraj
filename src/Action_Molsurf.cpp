9696b0dfa3dc839f5cfa86b7381283e305b6e5c3
Daniel R. Roe <daniel.r.roe@gmail.com>
2012-02-20 10:51:06 -0500

    DRR - Cpptraj: Remove molsurf dependency on malloc

--- src/Action_Molsurf.cpp
+++ src/Action_Molsurf.cpp
@@ -1,6 +1,5 @@
 // Molsurf
-#include <cstdlib> // Using malloc since interfacing with C code
-#include <cstring> 
+#include <cstring> // strcpy, memset
 #include "Action_Molsurf.h"
 #include "CpptrajStdio.h"
 
@@ -39,31 +38,31 @@ Molsurf::Molsurf() {
 // DESTRUCTOR
 Molsurf::~Molsurf() {
   ClearMemory();
-  if (atom!=NULL) free(atom);
+  if (atom!=NULL) delete[] atom;
 }
 
 // MolSurf::ClearMemory()
 /// Clear mem used by molsurf data structures
 void Molsurf::ClearMemory() {
-  if (upper_neighbors!=NULL) free (upper_neighbors);
-  if (neighbors!=NULL) free (neighbors);
-  if (probelist!=NULL) free (probelist);
-  if (toruslist!=NULL) free (toruslist);
-  if (convex_circle_list!=NULL) free (convex_circle_list);
-  if (concave_circle_list!=NULL) free (concave_circle_list);
-  if (concave_face!=NULL) free (concave_face);
-  if (convex_face!=NULL) free (convex_face);
-  if (saddle_face!=NULL) free (saddle_face);
-  if (cone_face!=NULL) free (cone_face);
-  if (broken_concave_face!=NULL) free (broken_concave_face);
-  if (concave_cycle!=NULL) free (concave_cycle);
-  if (cyclelist!=NULL) free (cyclelist);
-  if (vertexlist!=NULL) free (vertexlist);
-  if (concave_edge_list!=NULL) free (concave_edge_list);
-  if (convex_edge_list!=NULL) free (convex_edge_list);
-  if (low_torus!=NULL) free (low_torus);
-  if (cusp_edge!=NULL) free (cusp_edge);
-  if (cusp_pair!=NULL) free (cusp_pair);
+  if (upper_neighbors!=NULL) delete[] upper_neighbors;
+  if (neighbors!=NULL) delete[] neighbors;
+  if (probelist!=NULL) delete[] probelist;
+  if (toruslist!=NULL) delete[] toruslist;
+  if (convex_circle_list!=NULL) delete[] convex_circle_list;
+  if (concave_circle_list!=NULL) delete[] concave_circle_list;
+  if (concave_face!=NULL) delete[] concave_face;
+  if (convex_face!=NULL) delete[] convex_face;
+  if (saddle_face!=NULL) delete[] saddle_face;
+  if (cone_face!=NULL) delete[] cone_face;
+  if (broken_concave_face!=NULL) delete[] broken_concave_face;
+  if (concave_cycle!=NULL) delete[] concave_cycle;
+  if (cyclelist!=NULL) delete[] cyclelist;
+  if (vertexlist!=NULL) delete[] vertexlist;
+  if (concave_edge_list!=NULL) delete[] concave_edge_list;
+  if (convex_edge_list!=NULL) delete[] convex_edge_list;
+  if (low_torus!=NULL) delete[] low_torus;
+  if (cusp_edge!=NULL) delete[] cusp_edge;
+  if (cusp_pair!=NULL) delete[] cusp_pair;
 }
 
 // Molsurf::AllocateMemory()
@@ -72,102 +71,26 @@ int Molsurf::AllocateMemory() {
   int error_status = 0;
   int natm_sel = Mask1.Nselected;
   // ---------- Allocate Memory For molsurf routines --------------------
-  if ((upper_neighbors = (NEIGHBOR_TORUS *) malloc (
-                NUM_NEIGHBOR * natm_sel * sizeof (NEIGHBOR_TORUS))) == NULL) {
-        mprinterr("Unable to allocate space for upper_neighbors\n");
-        error_status++;
-  }
-  if ((neighbors = (NEIGHBOR *) malloc (
-                  NUM_NEIGHBOR * natm_sel * sizeof (NEIGHBOR))) == NULL) {
-        mprinterr("Unable to allocate space for neighbors\n");
-        error_status++;
-  }
-  if ((probelist = (PROBE *) malloc (
-                NUM_PROBE * natm_sel * sizeof (PROBE))) == NULL) {
-        mprinterr("Unable to allocate space for probelist\n");
-        error_status++;
-  }
-  if ((toruslist = (TORUS *) malloc (
-                NUM_TORUS * natm_sel * sizeof (TORUS))) == NULL) {
-        mprinterr("Unable to allocate space for toruslist\n");
-        error_status++;
-  }
-  if ((convex_circle_list = (CIRCLE *) malloc (
-                NUM_CIRCLE * natm_sel * sizeof (CIRCLE))) == NULL) {
-        mprinterr("Unable to allocate space for convex_circle_list\n");
-        error_status++;
-  }
-  if ((concave_circle_list = (CIRCLE *) malloc (
-                NUM_CIRCLE * natm_sel * sizeof (CIRCLE))) == NULL) {
-        mprinterr("Unable to allocate space for concave_circle_list\n");
-        error_status++;
-  }
-  if ((concave_face = (CONCAVE_FACE *) malloc (
-                NUM_FACE * natm_sel * sizeof (CONCAVE_FACE))) == NULL) {
-        mprinterr("Unable to allocate space for concave_face\n");
-        error_status++;
-  }
-  if ((convex_face = (CONVEX_FACE *) malloc (
-           NUM_FACE * natm_sel * sizeof (CONVEX_FACE))) == NULL) {
-        mprinterr("Unable to allocate space for convex_face\n");
-        error_status++;
-  }
-  if ((saddle_face = (SADDLE_FACE *) malloc (
-           NUM_FACE * natm_sel * sizeof (SADDLE_FACE))) == NULL) {
-        mprinterr("Unable to allocate space for saddle_face\n");
-        error_status++;
-  }
-  if ((cone_face = (CONE_FACE *) malloc (
-                 NUM_FACE * natm_sel * sizeof (CONE_FACE))) == NULL) {
-        mprinterr("Unable to allocate space for cone_face\n");
-        error_status++;
-  }
-  if ((broken_concave_face = (BROKEN_CONCAVE_FACE *) malloc (
-                   NUM_FACE * natm_sel * sizeof (BROKEN_CONCAVE_FACE))) == NULL) {
-        mprinterr("Unable to allocate space for broken_concave_face\n");
-        error_status++;
-  }
-  if ((concave_cycle = (CONCAVE_CYCLE *) malloc (
-                         NUM_CYCLE * natm_sel * sizeof (CONCAVE_CYCLE))) == NULL) {
-        mprinterr("Unable to allocate space for concave_cycle\n");
-        error_status++;
-  }
-  if ((cyclelist = (CYCLE *) malloc (
-                         NUM_CYCLE * natm_sel * sizeof (CYCLE))) == NULL) {
-        mprinterr("Unable to allocate space for cyclelist\n");
-        error_status++;
-  }
-  if ((vertexlist = (VERTEX *) malloc (
-                NUM_VERTEX * natm_sel * sizeof (VERTEX))) == NULL) {
-        mprinterr("Unable to allocate space for vertexlist\n");
-        error_status++;
-  }
-  if ((concave_edge_list = (EDGE *) malloc (
-          NUM_EDGE * natm_sel * sizeof (EDGE))) == NULL) {
-        mprinterr("Unable to allocate space for concave_edge_list\n");
-        error_status++;
-  }
-  if ((convex_edge_list = (EDGE *) malloc (
-          NUM_EDGE * natm_sel * sizeof (EDGE))) == NULL) {
-        mprinterr("Unable to allocate space for convex_edge_list\n");
-        error_status++;
-  }
-  if ((low_torus = (LOW_TORUS *) malloc (
-                 NUM_TORUS * natm_sel * sizeof (LOW_TORUS))) == NULL) {
-        mprinterr("Unable to allocate space for low_torus\n");
-        error_status++;
-  }
-  // NOTE: cusp_edge must be initialized. Handled in action before 1st call 
-  if ((cusp_edge = (CUSP_EDGE *) malloc (
-                 NUM_EDGE * natm_sel * sizeof (CUSP_EDGE))) == NULL) {
-        mprinterr("Unable to allocate space for cusp_edge\n");
-        error_status++;
-  }
-  if ((cusp_pair = (CUSP_PAIR *) malloc (
-                 NUM_CUSP * natm_sel * sizeof (CUSP_PAIR))) == NULL) {
-        mprinterr("Unable to allocate space for cusp_pair\n");
-        error_status++;
-  }
+  upper_neighbors = new NEIGHBOR_TORUS[ NUM_NEIGHBOR * natm_sel ];
+  neighbors = new NEIGHBOR [ NUM_NEIGHBOR * natm_sel ]; 
+  probelist = new PROBE[ NUM_PROBE * natm_sel ];
+  toruslist = new TORUS[ NUM_TORUS * natm_sel ];
+  convex_circle_list = new CIRCLE[ NUM_CIRCLE * natm_sel ];
+  concave_circle_list = new CIRCLE[ NUM_CIRCLE * natm_sel ];
+  concave_face = new CONCAVE_FACE[ NUM_FACE * natm_sel ];
+  convex_face = new CONVEX_FACE[ NUM_FACE * natm_sel ];
+  saddle_face = new SADDLE_FACE[ NUM_FACE * natm_sel ];
+  cone_face = new CONE_FACE[ NUM_FACE * natm_sel ];  
+  broken_concave_face = new BROKEN_CONCAVE_FACE[ NUM_FACE * natm_sel ];
+  concave_cycle = new CONCAVE_CYCLE[ NUM_CYCLE * natm_sel ];
+  cyclelist = new CYCLE[ NUM_CYCLE * natm_sel ];
+  vertexlist = new VERTEX[ NUM_VERTEX * natm_sel ];
+  concave_edge_list = new EDGE[ NUM_EDGE * natm_sel ];
+  convex_edge_list = new EDGE[ NUM_EDGE * natm_sel ];
+  low_torus = new LOW_TORUS[ NUM_TORUS * natm_sel ];
+  // NOTE: cusp_edge must be initialized. Handled in action before 1st call
+  cusp_edge = new CUSP_EDGE[ NUM_EDGE * natm_sel ];
+  cusp_pair = new CUSP_PAIR[ NUM_CUSP * natm_sel ]; 
   // ------------------------------------------------------------
   return error_status;
 }
@@ -222,8 +145,8 @@ int Molsurf::setup() {
 
   // The ATOM structure is how molsurf organizes atomic data. Allocate
   // here and fill in parm info. Coords will be filled in during action. 
-  if (atom!=NULL) free(atom);
-  atom = (ATOM*) malloc(Mask1.Nselected * sizeof(ATOM));
+  if (atom!=NULL) delete[] atom;
+  atom = new ATOM[ Mask1.Nselected ];
   if (atom==NULL) {
     mprinterr("Error: Molsurf::Setup Could not allocate memory for ATOMs.\n");
     return 1;
