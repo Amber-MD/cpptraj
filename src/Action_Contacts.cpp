c0d9351b99c747f15c36f42da48eb7dc1d04c434
Daniel R. Roe <Daniel.r.roe@gmail.com>
2012-05-02 13:10:45 -0400

    DRR - Cpptraj: Fix up contacts byResidue printout.

--- src/Action_Contacts.cpp
+++ src/Action_Contacts.cpp
@@ -5,7 +5,6 @@
 Action_Contacts::Action_Contacts() :
   byResidue_(false),
   distance_(49.0), // 7.0^2
-  RefParm_(0),
   first_(false)
 {
   // Do not allow reference traj
@@ -15,13 +14,12 @@ Action_Contacts::Action_Contacts() :
 // DESTRUCTOR
 Action_Contacts::~Action_Contacts() {
   outfile_.CloseFile();
+  if (byResidue_)
+    outfile2_.CloseFile();
 }
 
 /** Set up native contacts based on reference structure. */
-int Action_Contacts::SetupContacts(Frame *refframe) {
-  if (RefParm_ == 0) return 1;
-  // Set up atom mask for reference frame
-  if (RefParm_->SetupIntegerMask(Mask_, *refframe)) return 1;
+int Action_Contacts::SetupContacts(Frame* refframe, Topology* refparm) {
   // Determine which pairs of atoms satisfy cutoff, build contact list
   AtomMask::const_iterator atom1end = Mask_.end() - 1;
   for (AtomMask::const_iterator atom1 = Mask_.begin();
@@ -32,18 +30,18 @@ int Action_Contacts::SetupContacts(Frame *refframe) {
     {
       double d2 = refframe->DIST2(*atom1, *atom2);
       if (d2 < distance_)
-        contactlist_.push_back( std::pair<int,int>(*atom1, *atom2) );
+        nativecontacts_.insert( contactType(*atom1, *atom2) );
     }
   }
   // DEBUG - Print contacts
-  mprintf("\tSetup %zu contacts:\n", contactlist_.size());
-  for (std::vector< std::pair<int,int> >::iterator contact = contactlist_.begin();
-                                                   contact != contactlist_.end(); ++contact)
+  mprintf("\tSetup %zu native contacts:\n", nativecontacts_.size());
+  for (contactListType::iterator contact = nativecontacts_.begin();
+                                 contact != nativecontacts_.end(); ++contact)
   {
     int a1 = (*contact).first;
     int a2 = (*contact).second;
-    mprintf("\t\tAtom %i[%s] to %i[%s]\n", a1+1, (*RefParm_)[a1].c_str(),
-            a2+1, (*RefParm_)[a2].c_str());
+    mprintf("\t\tAtom %i[%s] to %i[%s]\n", a1+1, (*refparm)[a1].c_str(),
+            a2+1, (*refparm)[a2].c_str());
   }
   return 0;
 }
@@ -73,6 +71,18 @@ int Action_Contacts::init() {
     return 1;
   if (outfile_.OpenFile())
     return 1;
+  if (byResidue_) {
+    if (outfilename==0) {
+      mprinterr("Error: Contacts 'byresidue' requires output filename.\n");
+      return 1;
+    }
+    std::string file2name( outfilename );
+    file2name += ".native";
+    if (outfile2_.SetupWrite( file2name.c_str(), debug ))
+      return 1;
+    if (outfile2_.OpenFile())
+      return 1;
+  }
 
   // Get Mask
   char* mask0 = actionArgs.getNextMask();
@@ -86,6 +96,10 @@ int Action_Contacts::init() {
   // 'RefTrans' to be NULL.
   //if (RefInit(true, false, Mask_.MaskString(), actionArgs, FL, PFL, NULL)!=0)
   //  return 1;
+  if (!first_ && referenceName==0 && refindex==-1) {
+    mprintf("\tNo reference structure specified. Defaulting to first.\n");
+    first_ = true;
+  }
 
   if (!first_) {
     // Attempt to get the reference index by name/tag
@@ -99,13 +113,23 @@ int Action_Contacts::init() {
       return 1;
     }
     // Get reference parm for frame
-    RefParm_ = FL->GetFrameParm(refindex);
-    if (RefParm_==0) {
+    Topology* RefParm = FL->GetFrameParm(refindex);
+    if (RefParm==0) {
       mprinterr("Error: Could not get parm for frame %s\n", FL->FrameName(refindex));
       return 1;
     }
+    // Set up atom mask for reference frame
+    if (RefParm->SetupIntegerMask(Mask_, *RefFrame)) return 1;
     // Set up reference contacts 
-    SetupContacts(RefFrame);
+    SetupContacts(RefFrame, RefParm);
+  }
+
+  // Output file header
+  if (!byResidue_) {
+    outfile_.Printf("#time\tContacts\tnative Contacts ");
+    if (!first_)
+      outfile_.Printf("(number of natives: %zu)", nativecontacts_.size());
+    outfile_.Printf("\n");
   }
 
   mprintf("    CONTACTS: [%s] Calculating current contacts and comparing results to",
@@ -127,16 +151,100 @@ int Action_Contacts::init() {
 }
 
 int Action_Contacts::setup() {
-  if (first_) 
-    RefParm_ = currentParm;
+  //if (first_) 
+  //  RefParm_ = currentParm;
+  // Set up atom mask 
+  if (currentParm->SetupIntegerMask(Mask_)) return 1;
+
+  residueContacts_.reserve( currentParm->Nres() );
+  residueNative_.reserve( currentParm->Nres() );
+
+  activeResidues_.clear();
+  for (AtomMask::const_iterator atom = Mask_.begin();
+                                atom != Mask_.end(); ++atom)
+  {
+    int resnum = (*currentParm)[*atom].ResNum();
+    activeResidues_.insert( resnum );
+  }
 
   return 0;
 }
 
 int Action_Contacts::action() {
   if (first_) {
-    SetupContacts( currentFrame );
+    SetupContacts( currentFrame, currentParm );
     first_ = false;
+  }
+
+  // Determine how many contacts and how many native contacts are present.
+  //contactListType::iterator native = nativecontacts_.begin();
+  residueContacts_.assign( currentParm->Nres(), 0 );
+  residueNative_.assign( currentParm->Nres(), 0 );
+
+  // Determine which pairs of atoms satisfy cutoff
+  int numcontacts = 0;
+  int numnative = 0;
+  AtomMask::const_iterator atom1end = Mask_.end() - 1;
+  for (AtomMask::const_iterator atom1 = Mask_.begin();
+                                atom1 != atom1end; ++atom1)
+  {
+    int res1 = (*currentParm)[*atom1].ResNum();
+    for (AtomMask::const_iterator atom2 = atom1 + 1;
+                                  atom2 != Mask_.end(); ++atom2)
+    {
+      double d2 = currentFrame->DIST2(*atom1, *atom2);
+      // Contact?
+      if (d2 < distance_) {
+        ++numcontacts;
+        int res2 = (*currentParm)[*atom2].ResNum();
+        mprintf("CONTACT: %i res %i to %i res %i [%i]",*atom1,res1,*atom2,res2,numcontacts);
+        ++residueContacts_[res1];
+        ++residueContacts_[res2];
+        // Is this a native contact?
+        contactListType::iterator nativebegin = nativecontacts_.lower_bound( *atom1 );
+        if (nativebegin != nativecontacts_.end()) {
+          contactListType::iterator nativeend = nativecontacts_.upper_bound( *atom1 );
+          for (contactListType::iterator native = nativebegin; native!=nativeend; ++native) {
+            if ( *atom2 == (*native).second ) {
+              ++numnative;
+              mprintf(" NATIVE [%i]",numnative);
+              ++residueNative_[res1];
+              ++residueNative_[res2];
+              break;
+            }
+          }
+        }
+        mprintf("\n");
+      }
+/*
+      if (native != nativecontacts_.end()) {
+        if ( *atom1 == (*native).first && *atom2 == (*native).second ) {
+          if (d2 < distance_)
+            ++numnative;
+          ++native;
+        }
+      }
+*/
+    }
+  }
+
+  // The total # of contacts is multiplied by 2 since each contact
+  // is bidirectional, i.e. atom1->atom2 and atom2->atom1 each
+  // count as a separate contact.
+  if (!byResidue_) {
+    outfile_.Printf("%10.2lf\t%i\t%i\n", (double)frameNum+OUTPUTFRAMESHIFT,
+                    numcontacts*2, numnative*2);
+  } else {
+    outfile_.Printf("%10.2lf", (double)frameNum+OUTPUTFRAMESHIFT);
+    outfile2_.Printf("%10.2lf", (double)frameNum+OUTPUTFRAMESHIFT);
+    for (std::set<int>::iterator res = activeResidues_.begin();
+                                 res != activeResidues_.end(); ++res)
+    {
+      outfile_.Printf("\t%i", residueContacts_[ *res ]);
+      outfile2_.Printf("\t%i", residueNative_[ *res ]);
+    }
+    outfile_.Printf("\t%i\n", numcontacts*2);
+    outfile2_.Printf("\t%i\n", numnative*2);
   }
 
   return 0;
--- src/Action_Contacts.h
+++ src/Action_Contacts.h
@@ -1,5 +1,7 @@
 #ifndef INC_ACTION_CONTACTS_H
 #define INC_ACTION_CONTACTS_H
+#include <map>
+#include <set>
 #include "Action.h"
 //#include "ActionReference.h"
 // TODO: Use with ActionReference?
@@ -11,15 +13,22 @@ class Action_Contacts : public Action {
     AtomMask Mask_;
     bool byResidue_;
     double distance_;
-    Topology* RefParm_;
+    //Topology* RefParm_;
     bool first_;
     CpptrajFile outfile_;
-    std::vector< std::pair<int,int> > contactlist_;
+    CpptrajFile outfile2_;
+    typedef std::pair<int,int> contactType;
+    //typedef std::vector< contactType > contactListType;
+    typedef std::multimap<int,int> contactListType;
+    contactListType nativecontacts_;
+    std::vector<int> residueContacts_;
+    std::vector<int> residueNative_;
+    std::set<int> activeResidues_;
 
     int init();
     int setup();
     int action();
 
-    int SetupContacts(Frame*);
+    int SetupContacts(Frame*, Topology*);
 };
 #endif
