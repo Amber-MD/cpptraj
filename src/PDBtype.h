a9915ac2a74f0e18618db185b122340d927f384b
Daniel R. Roe <Daniel.r.roe@gmail.com>
2012-05-16 14:03:44 -0400

    DRR - Cpptraj: Remove obsolete header stuff.

--- src/PDBtype.h
+++ src/PDBtype.h
@@ -35,21 +35,4 @@ class PDBtype {
     char buffer_[BUF_SIZE_];
     double XYZ_[3];
 };
-/*
-bool isPDBkeyword(char *);
-bool isPDBatomKeyword(char *);
-char *pdb_title(char *);
-int pdb_atom(char *);
-NameType pdb_name(char *);
-int pdb_resname(char *, char*);
-char pdb_chain(char *);
-int pdb_resnum(char *);
-int pdb_xyz(char *, double *);
-double pdb_occ(char *);
-double pdb_Bfactor(char *);
-char *pdb_lastChar(char *);
-char *pdb_elt(char *);
-char *pdb_charge(char*);
-int pdb_write_ATOM(char *, PDB_RECTYPE, int, char *, char *, char, int,
-                    double, double, double, float, float, char *,bool);*/
 #endif
