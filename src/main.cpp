391124267bcf9762590e18b3fc5e12e6a3f9e2c9
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-08-09 13:45:53 -0600

    DRR - Cpptraj: Update internal version string (for BINPOS addition)

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <cstdlib> // atoi
 #ifndef CPPTRAJ_VERSION_STRING
 #define CPPTRAJ_VERSION_STRING "V13.6b"
-#define CPPTRAJ_INTERNAL_VERSION "V3.4.1b"
+#define CPPTRAJ_INTERNAL_VERSION "V3.4.2b"
 #endif
 
 // Usage()
