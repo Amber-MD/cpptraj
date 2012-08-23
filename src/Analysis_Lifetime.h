20f4241e266dc5abcb13e769d16531df0df82332
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-08-23 10:22:09 -0600

    DRR - Cpptraj: Separate out lifetime analysis into two routines as a test - may create an averaging class to generalize the procedure for datasets. Not yet enabled.

--- src/Analysis_Lifetime.h
+++ src/Analysis_Lifetime.h
@@ -19,6 +19,8 @@ class Analysis_Lifetime : public Analysis {
     bool averageonly_;
     bool cumulative_;
 
+    //void CalcAvgOnly( DataSet&, DataSet& );
+    //void CalcLifetime( DataSet&, DataSet&, DataSet&, DataSet&);
     void PrintListToFile(DataFileList *, std::vector<DataSet*>&, std::string const&);
 };
 #endif
