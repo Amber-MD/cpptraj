617df4820a032dc0a2fce3b4e543b141b077fdba
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-10-15 12:12:40 -0600

    DRR - Cpptraj: Re-enable analysis modes fluct; results are slightly different from ptraj due to round off, original ptraj had 2 * PI defined only to 4 decimal places.

--- src/Analysis_Modes.cpp
+++ src/Analysis_Modes.cpp
@@ -1,5 +1,8 @@
+#include <cmath> // tanh, sqrt
+#include <cstring> // memset
 #include "Analysis_Modes.h"
 #include "CpptrajStdio.h"
+#include "Constants.h" // TWOPI
 
 // CONSTRUCTOR
 Analysis_Modes::Analysis_Modes() :
@@ -9,9 +12,19 @@ Analysis_Modes::Analysis_Modes() :
   bose_(false),
   factor_(0),
   modinfo_(0),
-  source_(ModesInfo::MS_UNKNOWN),
   results_(0)
 {}
+
+/// hc/2kT in cm, with T=300K; use for quantum Bose statistics)
+const double Analysis_Modes::CONSQ = 2.39805E-3;
+const double Analysis_Modes::TKBC2 = 0.46105E-34;
+/// Avogadros number
+const double Analysis_Modes::AVO   = 6.023E23;
+const double Analysis_Modes::CNST  = TKBC2 * AVO;
+// cm to angstroms
+const double Analysis_Modes::CMTOA = 1.000E8;
+const double Analysis_Modes::CONT  = CMTOA / TWOPI;
+// NOTE: Original TWOPI was 6.2832, results in small roundoff diffs from ptraj
 
 /// Strings describing modes calculation types.
 const char Analysis_Modes::analysisTypeString[3][22] = {
@@ -22,16 +35,22 @@ const char Analysis_Modes::analysisTypeString[3][22] = {
 
 // DESTRUCTOR
 Analysis_Modes::~Analysis_Modes() {
-  // Only delete ModesInfo if it was read in from a file.
-  if (modinfo_ != 0 && source_==ModesInfo::MS_FILE)
-    delete modinfo_;
   if (results_!=0)
     delete[] results_;
 }
 
+void Analysis_Modes::CheckDeprecated(std::string& modesname, const char* key) {
+  std::string arg = analyzeArgs_.GetStringKey( key );
+  if (!arg.empty()) {
+    mprintf("Warning: analyze modes: Argument %s is deprecated, use 'name <modes>' instead.\n",
+            key);
+    if (modesname.empty()) modesname = arg;
+  }
+}
+
 // Analysis_Modes::Setup()
 /** analyze modes fluct|displ|corr
-  *                            stack <stackname> | file <filename> 
+  *                            name <modesname> 
   *                            [beg <beg>] [end <end>] 
   *                            [bose] [factor <factor>]
   *                            [out <outfile>] [maskp <mask1> <mask2> [...]]
@@ -63,36 +82,39 @@ int Analysis_Modes::Setup(DataSetList* DSLin) {
   }
 
   // Get beg, end, factor, bose
-  beg_ = analyzeArgs_.getKeyInt("beg",7);
-  end_ = analyzeArgs_.getKeyInt("end", 50);
   bose_ = analyzeArgs_.hasKey("bose");
   factor_ = analyzeArgs_.getKeyDouble("factor",1.0);
 
-  // Get modes
-  std::string modesfile = analyzeArgs_.GetStringKey("file");
-  std::string modesname = analyzeArgs_.GetStringKey("stack");
-  if (!modesfile.empty()) {
-    modinfo_ = new ModesInfo();
-    if (modinfo_->ReadEvecFile( modesfile, beg_, end_ )) return 1;
-    if (modinfo_->Nvect() != (end_ - beg_ + 1)) {
-      mprintf("Warning: analyze modes: # read evecs is %i, # requested is %i\n",
-              modinfo_->Nvect(), (end_ - beg_ + 1));
-    }
-  } else if (!modesname.empty()) {
-    modinfo_ = (ModesInfo*)DSLin->FindSetOfType( modesname, DataSet::MODES );
-    if (modinfo_ == 0) {
-      mprinterr("Error: analyze modes: modes with name %s not found.\n", modesname.c_str());
+  // Get modes name
+  std::string modesfile = analyzeArgs_.GetStringKey("name");
+  if (modesfile.empty()) {
+    // Check for deprecated args
+    CheckDeprecated(modesfile, "file");
+    CheckDeprecated(modesfile, "stack");
+    if (modesfile.empty()) {
+      mprinterr("Error: analyze modes: No 'modes <name>' arg given.\n");
       return 1;
     }
-  } else {
-    mprinterr("Error: analyze modes: Source 'stack' or 'file' not specified.\n");
+  }
+  beg_ = analyzeArgs_.getKeyInt("beg",7) - 1; // Args start at 1
+  end_ = analyzeArgs_.getKeyInt("end", 50);
+  // Check if modes name exists on the stack
+  modinfo_ = (DataSet_Modes*)DSLin->FindSetOfType( modesfile, DataSet::MODES );
+  if (modinfo_ == 0) {
+    // If not on stack, check for file.
+    if ( fileExists(modesfile.c_str()) ) {
+      modinfo_ = (DataSet_Modes*)DSLin->AddSet( DataSet::MODES, modesfile, "Modes" );
+      if (modinfo_->ReadEvecFile( modesfile, beg_+1, end_ )) return 1;
+    }
+  }
+  if (modinfo_ == 0) {
+    mprinterr("Error: analyze modes: Modes %s DataSet/file not found.\n");
     return 1;
   }
-  source_ = modinfo_->Source();
 
   // Check modes type
-  if (modinfo_->Mtype()!=ModesInfo::MT_COVAR && 
-      modinfo_->Mtype()!=ModesInfo::MT_MWCOVAR)
+  if (modinfo_->Type() != DataSet_Matrix::COVAR && 
+      modinfo_->Type() != DataSet_Matrix::MWCOVAR)
   {
     mprinterr("Error: analyze modes: evecs must be of type COVAR or MWCOVAR.\n");
     return 1;
@@ -137,7 +159,7 @@ int Analysis_Modes::Setup(DataSetList* DSLin) {
 
   // Status
   mprintf("    ANALYZE MODES: Calculating %s using modes %i to %i from %s\n",
-          analysisTypeString, beg_, end_, modinfo_->Legend().c_str());
+          analysisTypeString, beg_+1, end_, modinfo_->Legend().c_str());
   mprintf("\tResults are written to");
   if (filename_.empty())
     mprintf(" STDOUT\n");
@@ -151,8 +173,8 @@ int Analysis_Modes::Setup(DataSetList* DSLin) {
     mprintf("\tFactor for displacement: %lf\n", factor_);
   if (type_ == CORR) {
     mprintf("\tUsing the following atom pairs:");
-    for (ModesInfo::modestack_it apair = atompairStack_.begin();
-                                 apair != atompairStack_.end(); ++apair)
+    for (modestack_it apair = atompairStack_.begin();
+                      apair != atompairStack_.end(); ++apair)
       mprintf(" (%i,%i)", (*apair).first+1, (*apair).second+1 );
     mprintf("\n");
   }
@@ -166,45 +188,81 @@ int Analysis_Modes::Analyze() {
   // ----- FLUCT PRINT -----
   if (type_ == FLUCT) {
     // Calc rms atomic fluctuations
-    results_ = modinfo_->CalcRMSfluct(beg_, end_, bose_);
-    if (results_ == 0) return 1;
+    int natoms = modinfo_->NavgCrd() / 3; // Possible because COVAR/MWCOVAR
+    results_ = new double[ natoms * 4 ];
+    memset(results_, 0, natoms * 4 * sizeof(double));
+    double* Ri = results_;
+    for (int vi = 0; vi < natoms; ++vi) {
+      double sumx = 0.0;
+      double sumy = 0.0;
+      double sumz = 0.0;
+      // Loop over all eigenvector elements for this atom
+      const double* Vec = modinfo_->Eigenvector(beg_) + vi*3;
+      for (int mode = beg_; mode < end_; ++mode) {
+        double frq = modinfo_->Eigenvalue(mode);
+        if (frq >= 0.5) {
+          // Don't use eigenvectors associated with zero or negative eigenvalues
+          double distx = Vec[0] * Vec[0];
+          double disty = Vec[1] * Vec[1];
+          double distz = Vec[2] * Vec[2];
+          double fi = 1.0 / (frq*frq);
+          if (bose_) {
+            double argq = CONSQ * frq;
+            fi *= (argq / tanh(argq));
+          }
+          sumx += distx * fi;
+          sumy += disty * fi;
+          sumz += distz * fi;
+        }
+        Vec += modinfo_->VectorSize();
+      }
+      sumx *= CNST;
+      sumy *= CNST;
+      sumz *= CNST;
+      Ri[0] = sqrt(sumx) * CONT;
+      Ri[1] = sqrt(sumy) * CONT;
+      Ri[2] = sqrt(sumz) * CONT;
+      Ri[3] = sqrt(sumx + sumy + sumz) * CONT;
+      Ri += 4;
+    }
+    // Output
     outfile.OpenWrite( filename_ );
     outfile.Printf("Analysis of modes: RMS FLUCTUATIONS\n");
     outfile.Printf("%10s %10s %10s %10s %10s\n", "Atom no.", "rmsX", "rmsY", "rmsZ", "rms");
     int anum = 1;
-    for (int i4 = 0; i4 < modinfo_->Navgelem()*4/3; i4+=4) 
+    for (int i4 = 0; i4 < modinfo_->NavgCrd()*4/3; i4+=4) 
       outfile.Printf("%10i %10.3f %10.3f %10.3f %10.3f\n", anum++, results_[i4], 
                      results_[i4+1], results_[i4+2], results_[i4+3]); 
   // ----- DISPLACE PRINT -----
   } else if (type_ == DISPLACE) {
     // Calc displacement of coordinates along normal mode directions
-    results_ = modinfo_->CalcDisplacement(beg_, end_, bose_, factor_);
+    //results_ = modinfo_->CalcDisplacement(beg_, end_, bose_, factor_);
     if (results_ == 0) return 1;
     outfile.OpenWrite( filename_ );
     outfile.Printf("Analysis of modes: DISPLACEMENT\n");
     outfile.Printf("%10s %10s %10s %10s\n", "Atom no.", "displX", "displY", "displZ");
     int anum = 1;
-    for (int i3 = 0; i3 < modinfo_->Navgelem(); i3 += 3)
+    for (int i3 = 0; i3 < modinfo_->NavgCrd(); i3 += 3)
       outfile.Printf("%10i %10.3f %10.3f %10.3f\n", anum++, results_[i3], 
                      results_[i3+1], results_[i3+2]);
   // ----- CORR PRINT -----
   } else if (type_ == CORR) {
     // Calc dipole-dipole correlation functions
-    results_ = modinfo_->CalcDipoleCorr( beg_, end_, bose_, atompairStack_);
+    //results_ = modinfo_->CalcDipoleCorr( beg_, end_, bose_, atompairStack_); // TODO
     if (results_==0) return 1;
     outfile.OpenWrite( filename_ );
     outfile.Printf("Analysis of modes: CORRELATION FUNCTIONS\n");
     outfile.Printf("%10s %10s %10s %10s %10s %10s\n", "Atom1", "Atom2", "Mode", 
                    "Freq", "1-S^2", "P2(cum)");
     int ncnt = 0;
-    for (ModesInfo::modestack_it apair = atompairStack_.begin();
-                                 apair != atompairStack_.end(); ++apair)
+    for (modestack_it apair = atompairStack_.begin();
+                      apair != atompairStack_.end(); ++apair)
     {
       outfile.Printf("%10i %10i\n", (*apair).first+1, (*apair).second+1);
       double val = 1.0;
-      for (int i = 0; i < modinfo_->Nvect(); ++i) {
+      for (int i = 0; i < modinfo_->Nmodes(); ++i) {
         if (i+1 >= beg_ && i+1 <= end_) { // TODO: Pre-calc the shift
-          double frq = modinfo_->Freq(i);
+          double frq = modinfo_->Eigenvalue(i);
           if (frq >= 0.5) {
             val += results_[ncnt];
             outfile.Printf("%10s %10s %10i %10.5f %10.5f %10.5f\n",
