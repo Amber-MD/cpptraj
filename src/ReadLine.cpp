1e9d25f54bda81bd1eee4bb198b579ab3d70fbd4
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-10-26 09:29:41 -0600

    DRR - Cpptraj: Make interactive input recognize line continutation via backlash and '#' comments.

--- src/ReadLine.cpp
+++ src/ReadLine.cpp
@@ -1,18 +1,40 @@
 #include <cstdio> // required for readline
 #include <cstdlib> // free
+#include <cstring>
 #define READLINE_LIBRARY
 #include <readline.h>
 #include <history.h>
 #include "ReadLine.h"
 
+/** Get next input line with readline. Lines terminated with a backslash
+  * will be concatenated. Comments will be ignored.
+  */
 int ReadLine::GetInput() {
+  input_.clear();
   char* line = readline("> ");
-  if (line) {
-    if (*line) add_history(line);
-  } else
-    return 1;
-  input_.assign( line );
-  free(line);
+  if (line == 0) return 1; // EOF
+  input_ += line;
+  // Backslash requests a continutation
+  size_t end = strlen( line );
+  while (end > 1 && line[end - 1] == '\\') {
+    // Remove that backlash
+    size_t bs_pos = input_.find_last_of('\\');
+    input_.erase( bs_pos, 1 );
+    free( line );
+    line = readline("");
+    if (line == 0) break;
+    input_ += line;
+    end = strlen( line );
+  }
+  // Find '#' not preceded by blackslash; indicates comment
+  end = input_.find_first_of('#');
+  if (end != std::string::npos) {
+    if (end == 0 || (end > 0 && input_[end-1] != '\\'))
+      input_.erase( input_.begin() + end, input_.end() );
+  }
+  // Add line to history
+  if (!input_.empty()) add_history(input_.c_str());
+  if (line != 0) free( line );
   return 0;
 }
   
