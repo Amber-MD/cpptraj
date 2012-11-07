db1c4ac11c62e5f8e6104062fc1130df9173041d
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-11-07 11:26:47 -0700

    DRR - Cpptraj: PDBfile is now a proper CpptrajFile which can be used to independently read/write PDB files.

--- src/PDBfile.cpp
+++ src/PDBfile.cpp
@@ -1,30 +1,279 @@
+#include <cstdlib> // atoi, atof
+#include <cstring> //strncmp
 #include "PDBfile.h"
 
-PDBfile::PDBfile() :
-  anum_(1)
-{}
+/// PDB record types
+// NOTE: Must correspond with PDB_RECTYPE
+const char PDBfile::PDB_RECNAME[3][7] = {
+  "ATOM  ", "HETATM", "TER   "/*, "END   ", "HEADER", "TITLE ", "COMPND",
+  "AUTHOR", "CRYST1", "REMARK", "MODEL ", "JRNL  ", "SEQRES", ""*/
+};
 
-PDBfile::~PDBfile() {
-  CloseFile();
+// PDBfile::IsPDBkeyword()
+bool PDBfile::IsPDBkeyword(const char* recname) {
+  if (strncmp(recname,"HEADER",6)==0) return true;
+  if (strncmp(recname,"TITLE ",6)==0) return true;
+  if (strncmp(recname,"COMPND",6)==0) return true;
+  if (strncmp(recname,"AUTHOR",6)==0) return true;
+  if (strncmp(recname,"ATOM  ",6)==0) return true;
+  if (strncmp(recname,"HETATM",6)==0) return true;
+  if (strncmp(recname,"CRYST1",6)==0) return true;
+  if (strncmp(recname,"REMARK",6)==0) return true;
+  if (strncmp(recname,"MODEL ",6)==0) return true;
+  if (strncmp(recname,"JRNL  ",6)==0) return true;
+  if (strncmp(recname,"SEQRES",6)==0) return true;
+  return false;
 }
 
-int PDBfile::OpenPDB(std::string const& nameIn) {
-  if (OpenWrite( nameIn )) return 1;
-  return 0;
+// PDBfile::ID_PDB()
+bool PDBfile::ID_PDB(CpptrajFile& fileIn) {
+  // NOTE: ASSUME FILE SET UP FOR READ
+  if (fileIn.OpenFile()) return false;
+  std::string nextLine = fileIn.GetLine();
+  if (IsPDBkeyword( nextLine.c_str() )) return true;
+  nextLine = fileIn.GetLine();
+  if (IsPDBkeyword( nextLine.c_str() )) return true;
+  fileIn.CloseFile();
+  return false;
 }
 
-int PDBfile::OpenPDB(const char* nameIn) {
-  if (OpenWrite( nameIn )) return 1;
-  return 0;
+// PDBfile::IsPDBatomKeyword()
+bool PDBfile::IsPDBatomKeyword() {
+  if (strncmp(linebuffer_,"ATOM  ",6)==0) return true;
+  if (strncmp(linebuffer_,"HETATM",6)==0) return true;
+  return false;
+}
+
+bool PDBfile::IsPDB_TER() {
+  if (linebuffer_[0]=='T' && linebuffer_[1]=='E' && linebuffer_[2]=='R')
+    return true;
+  return false;
+}
+
+bool PDBfile::IsPDB_END() {
+  if (linebuffer_[0]=='E' && linebuffer_[1]=='N' && linebuffer_[2]=='D')
+    return true;
+  return false;
+}
+
+// PDBfile::pdb_Atom()
+Atom PDBfile::pdb_Atom() {
+  // Atom number (6-11)
+  // Atom name (12-16)
+  char savechar = linebuffer_[16];
+  linebuffer_[16] = '\0';
+  NameType aname(linebuffer_+12);
+  // Replace asterisks with single quotes
+  aname.ReplaceAsterisk();
+  linebuffer_[16] = savechar;
+  // X coord (30-38)
+  // Y coord (38-46)
+  // Z coord (46-54)
+
+  return Atom(aname);
+}
+
+// PDBfile::pdb_Residue()
+Residue PDBfile::pdb_Residue() {
+  // Res name (16-20)
+  char savechar = linebuffer_[20];
+  linebuffer_[20] = '\0';
+  NameType resname(linebuffer_+16);
+  // Replace asterisks with single quotes
+  resname.ReplaceAsterisk();
+  linebuffer_[20] = savechar;
+  // Res num (22-27)
+  savechar = linebuffer_[27];
+  linebuffer_[27] = '\0';
+  int resnum = atoi( linebuffer_+22 );
+  linebuffer_[27] = savechar;
+  return Residue(resnum, resname);
+}
+
+// PDBfile::pdb_XYZ()
+// NOTE: Should check for NULL Xout
+void PDBfile::pdb_XYZ(double *Xout) {
+  // X coord (30-38)
+  char savechar = linebuffer_[38];
+  linebuffer_[38] = '\0';
+  Xout[0] = atof( linebuffer_+30 );
+  linebuffer_[38] = savechar;
+  // Y coord (38-46)
+  savechar = linebuffer_[46];
+  linebuffer_[46] = '\0';
+  Xout[1] = atof( linebuffer_+38 );
+  linebuffer_[46] = savechar;
+  // Z coord (46-54)
+  savechar = linebuffer_[54];
+  linebuffer_[54] = '\0';
+  Xout[2] = atof( linebuffer_+46 );
+  linebuffer_[54] = savechar;
 }
 
 void PDBfile::WriteHET(int res, double x, double y, double z) {
-  pdb_write_ATOM(IOptr(), PDBtype::PDBHET, anum_++, "XX", "XXX", ' ', res, x, y, z);
+  WriteRec(HETATM, anum_++, "XX", "XXX", ' ', res, x, y, z);
 }
 
-void PDBfile::WriteATOM(int res, double x, double y, double z, const char* resnameIn,
-                        double Occ)
+void PDBfile::WriteATOM(int res, double x, double y, double z, 
+                        const char* resnameIn, double Occ)
 {
-  pdb_write_ATOM(IOptr(), PDBtype::PDBATOM, anum_++, "XX", resnameIn, ' ',
-                 res, x, y, z, (float)Occ, 0, "", false);
+  WriteRec(ATOM, anum_++, "XX", resnameIn, ' ',
+           res, x, y, z, (float)Occ, 0, "", false);
 }
+
+void PDBfile::WriteTER(int anum, NameType const& resnameIn, char chain, int resnum)
+{
+  WriteRec(TER, anum, "", resnameIn, chain, resnum, 0, 0, 0, 0, 0, "", false);
+}
+
+void PDBfile::WriteRec(PDB_RECTYPE Record, int anum, NameType const& name,
+                       NameType const& resnameIn, char chain, int resnum,
+                       double X, double Y, double Z)
+{
+  WriteRec(Record, anum, name, resnameIn, chain, resnum, X, Y, Z, 0, 0, "", false);
+}
+
+/// Write out an ATOM or HETATM record
+/** \return the number of characters written */
+void PDBfile::WriteRec(PDB_RECTYPE Record, int anum, NameType const& name,
+                       NameType const& resnameIn, char chain, int resnum,
+                       double X, double Y, double Z,
+                       float Occ, float B, const char* End, bool highPrecision) 
+{
+  char resName[5], atomName[5];
+
+  resName[4]='\0';
+  atomName[4]='\0';
+  // Residue number in PDB format can only be 4 digits wide
+  while (resnum>9999) resnum-=9999;
+  // Atom number in PDB format can only be 5 digits wide
+  while (anum>99999) anum-=99999;
+  // Residue names in PDB format are 3 chars long starting at column 18. 
+  // However in Amber residues are 4 characters long, usually with a space
+  // at the end. If this is the case remove the space so that the residue name
+  // lines up properly.
+  resName[0] = resnameIn[0];
+  resName[1] = resnameIn[1];
+  resName[2] = resnameIn[2];
+  if (resnameIn[3]!=' ')
+    resName[3] = resnameIn[3];
+  else
+    resName[3] = '\0';
+  // Atom names in PDB format start from col 14 when <= 3 chars, 13 when 4 chars.
+  if (name[3]!=' ') { // 4 chars
+    atomName[0] = name[0];
+    atomName[1] = name[1];
+    atomName[2] = name[2];
+    atomName[3] = name[3];
+  } else {            // <= 3 chars
+    atomName[0] = ' ';
+    atomName[1] = name[0];
+    atomName[2] = name[1];
+    atomName[3] = name[2];
+  }
+
+  Printf("%-6s%5i %-4s%4s %c%4i",PDB_RECNAME[Record], anum, atomName, 
+               resName, chain, resnum);
+  if (Record == TER) 
+    Printf("\n");
+  else if (highPrecision)
+    Printf("    %8.3f%8.3f%8.3f%8.4f%8.4f%10s\n", X, Y, Z, Occ, B, End);
+  else
+    Printf("    %8.3f%8.3f%8.3f%6.2f%6.2f%14s\n", X, Y, Z, Occ, B, End);
+}
+ 
+/*/// PDB Record Chain ID
+char PDB::pdb_chainID(char *buffer) {
+  return buffer[21];
+}*/
+
+/*/// PDB Record Occupancy 
+double pdb_occ(char *buffer) {
+  char temp[7];
+
+  temp[0]=buffer[54];
+  temp[1]=buffer[55];
+  temp[2]=buffer[56];
+  temp[3]=buffer[57];
+  temp[4]=buffer[58];
+  temp[5]=buffer[59];
+  temp[6]='\0';
+  return atof(temp);
+}*/
+
+/// PDB Record B-factor
+/*double pdb_Bfactor(char *buffer) {
+  char temp[7];
+
+  temp[0]=buffer[60];
+  temp[1]=buffer[61];
+  temp[2]=buffer[62];
+  temp[3]=buffer[63];
+  temp[4]=buffer[64];
+  temp[5]=buffer[65];
+  temp[6]='\0';
+  return atof(temp);
+}*/
+
+/// 10 chars between Bfactor and element
+/*char *pdb_lastChar(char *buffer) {
+  char *E;
+
+  E=(char*) malloc(11*sizeof(char));
+  E[0]=buffer[66];
+  E[1]=buffer[67];
+  E[2]=buffer[68];
+  E[3]=buffer[69];
+  E[4]=buffer[70];
+  E[5]=buffer[71];
+  E[6]=buffer[72];
+  E[7]=buffer[73];
+  E[8]=buffer[74];
+  E[9]=buffer[75];
+  E[10]='\0';
+  return E;
+}*/
+
+/// Element. If blank, try to guess from the name
+/** Currently recognized: H C N O F Cl Br S P */
+/*char *pdb_elt(char *buffer) {
+  char *E,*ptr;
+  char name[5];
+
+  E=(char*) malloc(3*sizeof(char));
+  E[0]=buffer[76]; // Element
+  E[1]=buffer[77]; // Element
+  E[2]='\0';*/
+
+/*  if (E[0]==' ' && E[1]==' ') {
+    pdb_name(buffer,name);
+    // position ptr at first non-space character
+    ptr=name;
+    while (*ptr==' ' && *ptr!='\0') ptr++;
+    // if NULL something went wrong, abort
+    if (*ptr=='\0') return E;
+    E[0]=ptr[0];
+    // If C, check for L or l for chlorine
+    if (ptr[0]=='C') {
+      if (ptr[1]=='L' || ptr[1]=='l') E[1]='l';
+    }
+    // If B, check for R or r for bromine
+    if (ptr[0]=='B') {
+      if (ptr[1]=='R' || ptr[1]=='r') E[1]='r';
+    }
+  }*/
+
+/*  return E;
+}*/
+
+/// Charge. 
+/*char *pdb_charge(char *buffer) {
+  char *E;
+
+  E=(char*) malloc(3*sizeof(char));
+  E[0]=buffer[78]; // Charge
+  E[1]=buffer[79]; // Charge
+  E[2]='\0';
+  return E;
+}*/
+
--- src/PDBfile.h
+++ src/PDBfile.h
@@ -1,17 +1,49 @@
 #ifndef INC_PDBFILE_H
 #define INC_PDBFILE_H
+#include "Topology.h" // Atom, Residue
 #include "CpptrajFile.h"
-#include "PDBtype.h"
-class PDBfile : CpptrajFile, PDBtype {
+/// Used to access PDB files
+class PDBfile : public CpptrajFile {
   public:
-    PDBfile();
-    ~PDBfile();
-
-    int OpenPDB(std::string const&);
-    int OpenPDB(const char*);
+    PDBfile() : anum_(1) {}
+    // NOTE: PDB_RECNAME must correspond with this.
+    enum PDB_RECTYPE {ATOM=0, HETATM, TER/*, END, HEADER, TITLE, COMPND, AUTHOR,
+                      CRYST1, REMARK, MODEL, JRNL, SEQRES, BLANK*/};
+    /// \return true if the first 6 chars of buffer match a PDB keyword
+    static bool IsPDBkeyword(const char*);
+    /// Check if either of the first two lines contain valid PDB records.
+    static bool ID_PDB(CpptrajFile&);
+    /// \return true if current line has an ATOM/HETATM record.
+    bool IsPDBatomKeyword();
+    /// \return true if current line has TER keyword.
+    bool IsPDB_TER();
+    /// \return true if current line has END keyword.
+    bool IsPDB_END();
+    /// \return Atom based on current line.
+    Atom pdb_Atom();
+    /// \return Residue based on current line.
+    Residue pdb_Residue();
+    /// Set XYZ based on current line.
+    void pdb_XYZ(double*);
+    /// Write HETATM record using internal atom numbering
     void WriteHET(int, double, double, double);
+    /// Write ATOM record using internal atom numbering
     void WriteATOM(int, double, double, double, const char*, double);
+    /// Write TER record
+    void WriteTER(int, NameType const&, char, int);
+    /// Write PDB ATOM/HETATM record
+    void WriteRec(PDB_RECTYPE, int, NameType const&, NameType const&, char, int,
+                  double, double, double, float, float, const char *, bool);
+    /// Write PDB ATOM/HETATM record
+    void WriteRec(PDB_RECTYPE, int, NameType const&, NameType const&, char, int,
+                  double, double, double);
+/*    int pdb_atomNumber(char*);
+    NameType pdb_atomName(char*);
+    NameType pdb_resName(char*);
+    char pdb_chainID(char*);
+    int pdb_resNum(char*);*/
   private:
     int anum_;
+    static const char PDB_RECNAME[][7];
 };
 #endif
