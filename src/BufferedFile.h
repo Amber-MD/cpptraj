dbe5b43bc63698c5b2511afe1b318002adc1f6c7
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-11-07 10:28:48 -0700

    DRR - Cpptraj: Finish up BufferedFile, replacement for FileBuffer/FrameBuffer classes.

--- src/BufferedFile.h
+++ src/BufferedFile.h
@@ -7,15 +7,21 @@ class BufferedFile : public CpptrajFile {
     BufferedFile();
     ~BufferedFile();
 
-    size_t SetupFrameBuffer(int, int, int, int);
+    int SetupBuffer();
+    const char* BufferedLine();
+    int TokenizeLine(const char*);
+    const char* NextToken();
+
+    size_t SetupFrameBuffer(int, int, int);
+    size_t SetupFrameBuffer(int, int, int, size_t, int);
     size_t ResizeBuffer(int);
     int SeekToFrame(size_t);
     int ReadFrame();
     int WriteFrame();
     void GetDoubleAtPosition(double&,size_t,size_t);
-    void BufferBeginOffset();
     void BufferBegin();
     void BufferBeginAt(size_t);
+    void AdvanceBuffer(size_t);
     void BufferToDouble(double*,int);
     void DoubleToBuffer(const double*,int, const char*);
 
@@ -24,11 +30,21 @@ class BufferedFile : public CpptrajFile {
   private:
     static const size_t DEFAULT_BUFFERSIZE = 16384;
 
-    char* buffer_;
-    char* bufferPosition_;
+    size_t CalcFrameSize(int);
+
+    char* buffer_;         ///< Character buffer
+    char* bufferPosition_; ///< Position in buffer
     size_t frameSize_;     ///< Total size of frame to read.
     size_t offset_;        ///< User specified offset, used in seeking.
     int Ncols_;            ///< Number of columns, use to convert array to buffer.
     size_t eltWidth_;      ///< Width of each element in the frame.
+
+    /// Array of pointers to beginning of tokens in linebuffer
+    char* tokens_[BUF_SIZE];
+    char** tokenptr_;      ///< Position in tokens
+    char savechar_;        ///< Saved last char of current token
+    char* lineptr_;        ///< Position in linebuffer
+    char* endlinebuffer_;  ///< End position of linebuffer
+    char* endbuffer_;      ///< End position of buffer
 };
 #endif
