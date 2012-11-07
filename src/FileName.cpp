a6b3a9d90ee0cd85de2643bd23dd74daf602c3dc
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-11-06 21:15:36 -0700

    DRR - Cpptraj: Add FileName class

---
+++ src/FileName.cpp
@@ -0,0 +1,64 @@
+#include "FileName.h"
+
+void FileName::clear() {
+  fullPathName_.clear();
+  baseName_.clear();
+  extension_.clear();
+  compressExt_.clear();
+}
+
+int FileName::SetFileName(std::string const& nameIn, FileName::CompressStatus compressed) {
+  // NULL filename allowed for WRITE (indicates STDOUT)
+  if (nameIn.empty()) {
+    clear();
+    return 0;
+  }
+  // Assign filename with full path
+  fullPathName_.assign( nameIn );
+  // Get position of last occurence of '/' to determine base filename
+  size_t found = fullPathName_.find_last_of("/");
+  if (found == std::string::npos)
+    baseName_ = fullPathName_;
+  else
+    baseName_ = fullPathName_.substr(found+1);
+  // Get the filename extension
+  found = baseName_.find_last_of(".");
+  if (found == std::string::npos) {
+    extension_.clear();
+  } else {
+    extension_ = baseName_.substr(found);
+  }
+  bool searchExtension = (compressed == YES);
+  // If compression is unknown (e.g. on writes), see if the extension is one 
+  // of the 2 recognized compression extensions.
+  if (compressed == UNKNOWN) {
+    if ( extension_ == ".gz" || extension_ == ".bz2" )
+      searchExtension = true;
+  }
+  if ( searchExtension ) {
+    // If file is compressed, the extension just found should be the compression
+    // extension.
+    compressExt_ = extension_;
+    // Get everything before the compressed extension
+    std::string compressPrefix = baseName_.substr(0,found);
+    // See if there is another extension
+    found = compressPrefix.find_last_of(".");
+    if (found == std::string::npos) 
+      // No other extension
+      extension_.clear();
+    else 
+      extension_ = compressPrefix.substr(found);
+  } else
+    compressExt_.clear();
+  return 0;
+}
+
+int FileName::SetFileName( std::string const& nameIn ) {
+  return SetFileName( nameIn, UNKNOWN );
+}
+
+int FileName::SetFileName( std::string const& nameIn, bool isCompressed ) {
+  if (isCompressed)
+    return SetFileName( nameIn, YES );
+  return SetFileName( nameIn, NO );
+}
---
+++ src/FileName.h
@@ -0,0 +1,30 @@
+#ifndef INC_FILENAME_H
+#define INC_FILENAME_H
+#include <string>
+/// Class to hold file name, extension, etc.
+class FileName {
+  public:
+    FileName() {}
+    /// Only name is known;
+    int SetFileName(std::string const&);
+    /// Name and compressed status are known.
+    int SetFileName(std::string const&, bool);
+    /// Clear FileName
+    void clear();
+
+    const std::string& Full()     const { return fullPathName_;     }
+    const std::string& Base()     const { return baseName_;         }
+    const char* base()            const { return baseName_.c_str(); }
+    const std::string& Ext()      const { return extension_;        }
+    const std::string& Compress() const { return compressExt_;      }
+    bool empty()                  const { return fullPathName_.empty(); }
+  private:
+    enum CompressStatus { UNKNOWN=0, YES, NO };
+    std::string fullPathName_;
+    std::string baseName_;
+    std::string extension_;
+    std::string compressExt_;
+
+    int SetFileName(std::string const&, CompressStatus);
+};
+#endif
