180399fb1f49e98351c4b35ae461e0de5dfc73e8
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2012-12-04 11:35:02 -0700

    DRR - Cpptraj: Add ComplexArray class for dealing with complex #s.

---
+++ src/ComplexArray.cpp
@@ -0,0 +1,123 @@
+#include <cstring>
+#include "ComplexArray.h"
+
+ComplexArray::ComplexArray(int i) : 
+  ndata_(i*2), ncomplex_(i) 
+{ 
+  if (ndata_ > 0) 
+    data_ = new double[ ndata_ ]; 
+  else
+    data_ = 0;
+}
+
+ComplexArray::ComplexArray(ComplexArray const& rhs) :
+  ndata_(rhs.ndata_),
+  ncomplex_(rhs.ncomplex_)
+{
+  if (ndata_ > 0) 
+    data_ = new double[ ndata_ ];
+  else
+    data_ = 0;
+}
+
+ComplexArray& ComplexArray::operator=(ComplexArray const& rhs) {
+  if (&rhs == this) return *this;
+  if (data_!=0) delete[] data_;
+  data_ = 0;
+  ndata_ = rhs.ndata_;
+  ncomplex_ = rhs.ncomplex_;
+  if (ndata_ > 0) {
+    data_ = new double[ ndata_ ];
+    memcpy(data_, rhs.data_, ndata_ * sizeof(double));
+  }
+  return *this;
+}
+
+ComplexArray::~ComplexArray() { if (data_ != 0) delete[] data_; }
+
+void ComplexArray::Allocate(int i) {
+  ndata_ = i * 2;
+  ncomplex_ = i;
+  if (data_!=0) delete[] data_;
+  if (ndata_ > 0)
+    data_ = new double[ ndata_ ];
+  else
+    data_ = 0;
+}
+
+void ComplexArray::PadWithZero(int start) {
+  int startIdx = 2 * start;
+  memset(data_ + startIdx, 0, (ndata_ - startIdx) * sizeof(double));
+}
+
+void ComplexArray::Normalize(double norm) {
+  for (int i = 0; i < ndata_; ++i)
+    data_[i] *= norm;
+}
+
+void ComplexArray::SquareModulus() {
+  for (int i = 0; i < ndata_; i+=2) {
+    data_[i  ] = data_[i  ] * data_[i  ] + data_[i+1] * data_[i+1];
+    data_[i+1] = 0.0;
+  }
+}
+
+// TODO: Check size of rhs
+void ComplexArray::ComplexConjTimes(ComplexArray const& rhs) {
+  for (int i = 0; i < ndata_; i+=2) {
+    double dtmp = data_[i  ] * rhs.data_[i  ] + data_[i+1] * rhs.data_[i+1];
+    data_[i+1]  = data_[i  ] * rhs.data_[i+1] - data_[i+1] * rhs.data_[i  ];
+    data_[i  ]  = dtmp;
+  }
+}
+
+// -----------------------------------------------------------------------------
+CorrF_Direct::CorrF_Direct(int stepsIn) : nsteps_(stepsIn) { 
+  table_.assign(2*nsteps_, 0.0); 
+}
+
+void CorrF_Direct::Allocate(int stepsIn) {
+  nsteps_ = stepsIn;
+  table_.resize(2*nsteps_, 0.0);
+}
+
+void CorrF_Direct::AutoCorr(ComplexArray& data1) {
+  int ndata2 = data1.size();
+  for (int i = 0; i < ndata2; i++) {
+    double dsum = 0.0;
+    for (int j = i; j < ndata2; j++) {
+      int ind1 = 2 * j;
+      int ind2 = 2 * (j-i);
+      dsum += data1[ind1] * data1[ind2] + data1[ind1+1] * data1[ind2+1];
+    }
+    if (i < nsteps_) {
+      int ind1 = 2 * i;
+      table_[ind1  ] = dsum;
+      table_[ind1+1] = 0.0;
+    } else
+      break;
+  }
+  std::copy(table_.begin(), table_.end(), data1.CAptr());
+}
+
+void CorrF_Direct::CrossCorr(ComplexArray& data1, ComplexArray const& data2) {
+  if (data2.size() < data1.size()) return;
+  int ndata2 = data1.size();
+  for (int i = 0; i < ndata2; i++) {
+    double dsum = 0.0;
+    double dsumi = 0.0;
+    for (int j = i; j < ndata2; j++) {
+      int ind1 = 2 * j;
+      int ind2 = 2 * (j-i);
+      dsum  += data2[ind1] * data1[ind2  ] + data2[ind1+1] * data1[ind2+1];
+      dsumi += data2[ind1] * data1[ind2+1] - data2[ind1+1] * data1[ind2  ];
+    }
+    if(i < nsteps_) {
+      int ind1 = 2 * i;
+      table_[ind1  ] = dsum;
+      table_[ind1+1] = dsumi;
+    } else
+      break;
+  }
+  std::copy(table_.begin(), table_.end(), data1.CAptr());
+}
---
+++ src/ComplexArray.h
@@ -0,0 +1,49 @@
+#ifndef INC_COMPLEXARRAY_H
+#define INC_COMPLEXARRAY_H
+#include <vector>
+/// Array that will hold complex numbers.
+/** Implementation does not use STL vector so as to easily interface with
+  * fortran routines for e.g. calculating FFT; current standard does not
+  * directly allow access interal STL vector representation (i.e. double*).
+  */
+class ComplexArray {
+  public:
+    ComplexArray() : data_(0), ndata_(0), ncomplex_(0) {}
+    ComplexArray(int);
+    ComplexArray(ComplexArray const&);
+    ComplexArray& operator=(ComplexArray const&);
+    ~ComplexArray();
+    void Allocate(int);
+    void PadWithZero(int);
+    /// Multiply all entries in data by input 
+    void Normalize(double);
+    void SquareModulus();
+    /// Calculate [this]* x [rhs] where * denotes complex conjugate.
+    void ComplexConjTimes(ComplexArray const&);
+    double* CAptr()  { return data_;     }
+    int size() const { return ncomplex_; }
+    double& operator[](int idx)             { return data_[idx]; }
+    double const& operator[](int idx) const { return data_[idx]; }
+  private:
+    double* data_;
+    int ndata_;    ///< Actual size of data
+    int ncomplex_; ///< # of complex numbers in array (ndata / 2)
+};
+
+/// Used to directly calculate auto/cross-correlation for complex arrays.
+/** Calculates correlation functions using the "direct" approach
+  * (s. Comp. Sim. of Liquids, p.185)
+  * - the result is not yet normalized by (no_of_discrete_data - t)**-1 (!)
+  */
+class CorrF_Direct {
+  public:
+    CorrF_Direct() : nsteps_(0) {}
+    CorrF_Direct(int);
+    void Allocate(int);
+    void AutoCorr(ComplexArray&);
+    void CrossCorr(ComplexArray&, ComplexArray const&);
+  private:
+    int nsteps_;
+    std::vector<double> table_;
+};
+#endif
