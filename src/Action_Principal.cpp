6e13af503a76d4f96fd7f613a780ffb4ea42c966
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2013-02-20 15:38:13 -0700

    DRR - Cpptraj: Add ability to write eigenvectors/eigenvalues from principal command to file.

--- src/Action_Principal.cpp
+++ src/Action_Principal.cpp
@@ -10,7 +10,7 @@ Action_Principal::Action_Principal() :
 { }
 
 void Action_Principal::Help() {
-  mprintf("principal [<mask>] [dorotation] [mass]\n");
+  mprintf("principal [<mask>] [dorotation] [mass] [out <filename>]\n");
 }
 
 // Action_Principal::init()
@@ -21,11 +21,19 @@ Action::RetType Action_Principal::Init(ArgList& actionArgs, TopologyList* PFL, F
   // Keywords
   doRotation_ = actionArgs.hasKey("dorotation");
   useMass_ = actionArgs.hasKey("mass");
-
+  std::string filename = actionArgs.GetStringKey("out");
+  if (!doRotation_ && filename.empty()) {
+    mprinterr("Error: principal: At least one of 'dorotation' or 'out <filename>' must be specified.\n");
+    return Action::ERR;
+  }
   // Masks
   mask_.SetMaskString( actionArgs.GetMaskNext() );
 
-  mprintf("    PRINCIPAL");
+  mprintf("    PRINCIPAL:");
+  if (!filename.empty()) {
+    mprintf(" output eigenvectors/eigenvalues to %s,", filename.c_str());
+    if (outfile_.OpenWrite(filename)) return Action::ERR;
+  }
   if (doRotation_)
     mprintf(" with rotation by");
   else
@@ -58,14 +66,18 @@ Action::RetType Action_Principal::DoAction(int frameNum, Frame* currentFrame, Fr
   Vec3 Eval;
 
   currentFrame->CalculateInertia( mask_, Inertia );
-  //printMatrix_3x3("PRINCIPAL INERTIA", Inertia);
 
   // NOTE: Diagonalize_Sort_Chirality places sorted eigenvectors in rows.
   Inertia.Diagonalize_Sort_Chirality( Eval, debug_ );
-  if (debug_ > 2) {
-    Eval.Print("PRINCIPAL EIGENVALUES");
-    //TEMP.Print("GENERAL");
-    Inertia.Print("PRINCIPAL EIGENVECTORS (Rows)");
+  if (outfile_.IsOpen()) {
+    int fn = frameNum+1; 
+    outfile_.Printf("%i EIGENVALUES: %f %f %f\n%i EIGENVECTOR 0: %f %f %f\n%i EIGENVECTOR 1: %f %f %f\n%i EIGENVECTOR 2: %f %f %f\n", 
+      fn, Eval[0], Eval[1], Eval[2],
+      fn, Inertia[0], Inertia[1], Inertia[2],
+      fn, Inertia[3], Inertia[4], Inertia[5],
+      fn, Inertia[6], Inertia[7], Inertia[8]);
+    //Eval.Print("PRINCIPAL EIGENVALUES");
+    //Inertia.Print("PRINCIPAL EIGENVECTORS (Rows)");
   }
   
   // Rotate - since Evec is already transposed (eigenvectors
