8e2df52f04638eeb0c97bb811c04b1c71858c50c
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2013-04-16 08:19:58 -0600

    DRR - Cpptraj: Add 'amd' option to 'hist' for AMD reweighting.

--- src/Histogram.h
+++ src/Histogram.h
@@ -14,6 +14,7 @@ class Histogram : public DataSet {
     int AddDimension(Dimension&); 
     /// Bin given data. Dim must be the same as what the histogram has been set up for.
     int BinData(std::vector<double>&); 
+    int BinAMD(std::vector<double> const&, double); 
 
     void PrintBins(const char*, bool,bool);
 
