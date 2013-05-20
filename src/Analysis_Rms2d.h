27b257d3d0cc8e72c1cc660bac2b853f58d457fb
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2013-05-20 14:59:20 -0600

    DRR - Cpptraj: Make 2drms functions non-static members again since Cluster has its own 2drms calcs now.

--- src/Analysis_Rms2d.h
+++ src/Analysis_Rms2d.h
@@ -3,7 +3,7 @@
 #include "Analysis.h"
 #include "Trajin_Single.h"
 #include "DataSet_Coords.h"
-#include "TriangleMatrix.h"
+#include "DataSet_MatrixFlt.h"
 // Class: Analysis_Rms2d
 /// Calculate the RMSD between two sets of frames.
 /** Perform RMS calculation between each input frame and each other input 
@@ -13,28 +13,26 @@
 class Analysis_Rms2d: public Analysis {
   public:
     Analysis_Rms2d();
-
     static DispatchObject* Alloc() { return (DispatchObject*)new Analysis_Rms2d(); }
     static void Help();
-    static int Calc2drms(DataSet_Coords&, TriangleMatrix&,bool,bool,std::string const&);
-    static int CalcDME(DataSet_Coords&, TriangleMatrix&, std::string const&);
-
     Analysis::RetType Setup(ArgList&,DataSetList*,TopologyList*,DataFileList*,int);
     Analysis::RetType Analyze();
   private:
+    int Calc2drms();
+    int CalcDME();
+    int CalcRmsToTraj();
+    void CalcAutoCorr();
+
     enum ModeType { NORMAL = 0, REFTRAJ, DME };
     ModeType mode_;
     DataSet_Coords* coords_;   ///< Hold coords from input frames.
     bool nofit_;               ///< Do not perform rms fitting
     bool useMass_;
-    std::string maskexpr_;
+    AtomMask TgtMask_;         ///< Target atom mask
     AtomMask RefMask_;         ///< Reference Traj atom mask
     Trajin_Single RefTraj_;    ///< Reference trajectory, each frame used in turn
     Topology* RefParm_;        ///< Reference trajectory Parm
-    DataSet* rmsdataset_;
+    DataSet_MatrixFlt* rmsdataset_;
     DataSet* Ct_;
-
-    int CalcRmsToTraj();
-    void CalcAutoCorr(TriangleMatrix const&);
 };
 #endif
