ab1b7b8a849633ae9de014bbf84038baaabb105e
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2013-10-08 11:55:44 -0600

    DRR - Cpptraj: Add 'byatom', 'byres', 'bymol', and 'center' keywords to 'unwrap'; they function the same way they do in the 'image' command.

--- src/Action_Image.h
+++ src/Action_Image.h
@@ -3,19 +3,15 @@
 // Class: Action_Image
 /// Action to wrap coordinates back into primary box
 #include "Action.h"
+#include "ImageTypes.h"
 class Action_Image: public Action {
   public:
     Action_Image();
-
     static DispatchObject* Alloc() { return (DispatchObject*)new Action_Image(); }
     static void Help();
-
     ~Action_Image();
-    void Print() {}
   private:
-    enum ImageMode { BYMOL = 0, BYRES, BYATOM };
-    static const char* ImageModeString[];
-    ImageMode imageMode_;
+    Image::Mode imageMode_;
     /// Only atoms in Mask1 will be imaged
     AtomMask Mask1_;
     /// If defined, image w.r.t. the center of atoms in ComMask.
@@ -32,12 +28,12 @@ class Action_Image: public Action {
     TriclinicArg triclinic_;
     int debug_;
     /// Vector containing atom ranges to be imaged (first to last)
-    std::vector<int> imageList_; 
+    Image::PairType imageList_; 
 
     Action::RetType Init(ArgList&, TopologyList*, FrameList*, DataSetList*,
                           DataFileList*, int);
     Action::RetType Setup(Topology*, Topology**);
     Action::RetType DoAction(int, Frame*, Frame**);
-    void CheckRange(int,int);
+    void Print() {}
 };
 #endif
--- src/ImageRoutines.h
+++ src/ImageRoutines.h
@@ -1,14 +1,19 @@
 #ifndef INC_IMAGEROUTINES_H
 #define INC_IMAGEROUTINES_H
-#include "Frame.h"
-Vec3 SetupImageTruncoct( Frame const&, AtomMask*, bool, bool);
-void ImageNonortho(Frame&, bool, Vec3 const&, Matrix_3x3 const&, Matrix_3x3 const&,
-                   bool, bool, bool, std::vector<int> const&);
-Vec3 ImageNonortho(Vec3 const&, bool, bool,
-                   Matrix_3x3 const&, Matrix_3x3 const&, Vec3 const&, double);
-int SetupImageOrtho(Box const&, Vec3&, Vec3&, bool);
-void ImageOrtho(Frame&, Vec3 const&, Vec3 const&, bool, bool, std::vector<int> const&);
-Vec3 ImageOrtho(Vec3 const&, Vec3 const&, Vec3 const&, Box const&);
-void UnwrapNonortho( Frame&, Frame&, AtomMask const&, Matrix_3x3 const&, Matrix_3x3 const& );
-void UnwrapOrtho( Frame&, Frame&, AtomMask const& );
+#include "Topology.h"
+#include "ImageTypes.h"
+namespace Image {
+  PairType CreatePairList(Topology const&, Mode, AtomMask);
+  Vec3 SetupTruncoct( Frame const&, AtomMask*, bool, bool);
+  void Nonortho(Frame&, bool, Vec3 const&, Matrix_3x3 const&, Matrix_3x3 const&,
+                bool, bool, bool, PairType const&);
+  Vec3 Nonortho(Vec3 const&, bool, bool,
+                Matrix_3x3 const&, Matrix_3x3 const&, Vec3 const&, double);
+  int SetupOrtho(Box const&, Vec3&, Vec3&, bool);
+  void Ortho(Frame&, Vec3 const&, Vec3 const&, bool, bool, PairType const&);
+  Vec3 Ortho(Vec3 const&, Vec3 const&, Vec3 const&, Box const&);
+  void UnwrapNonortho( Frame&, Frame&, PairType const&, 
+                       Matrix_3x3 const&, Matrix_3x3 const&, bool, bool );
+  void UnwrapOrtho( Frame&, Frame&, PairType const&, bool, bool );
+}
 #endif
