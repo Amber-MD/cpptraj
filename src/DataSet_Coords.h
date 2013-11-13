7aee690a14603ffaf4143bc793ab21bc0f350de0
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2013-11-13 11:19:35 -0700

    DRR - Cpptraj: DataSet_Coords is now an interface.

---
+++ src/DataSet_Coords.h
@@ -0,0 +1,23 @@
+#ifndef INC_DATASET_COORDS_H
+#define INC_DATASET_COORDS_H
+#include "Topology.h"
+#include "DataSet_1D.h"
+// NOTE: Should this be a 4D DataSet?
+class DataSet_Coords : public DataSet_1D {
+  public:
+    DataSet_Coords() : DataSet_1D(COORDS, 8, 3), numBoxCrd_(0), numVel_(0) {}
+    virtual ~DataSet_Coords() {}
+    /// Allocate a frame that can be used to store COORDS 
+    Frame AllocateFrame() const;
+    virtual void AddFrame(Frame const&) = 0;
+    virtual void SetCRD(int, Frame const&) = 0;
+    virtual void GetFrame(int, Frame&) const = 0;
+    virtual void GetFrame(int, Frame&, AtomMask const&) const = 0;
+    void SetTopology(Topology const&);
+    inline Topology const& Top() const { return top_; }
+  protected:
+    Topology top_;                     ///< Topology corresponding to coordinates.
+    size_t numBoxCrd_;                 ///< Number of box coords (0 or 6).
+    size_t numVel_;                    ///< Number of velocities.
+};
+#endif
