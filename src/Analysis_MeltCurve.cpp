50d85260993a92c32c0ca48890b1329202648f5a
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2013-12-17 09:35:13 -0700

    DRR - Cpptraj: Remove unneeded class variable.

--- src/Analysis_MeltCurve.cpp
+++ src/Analysis_MeltCurve.cpp
@@ -1,20 +1,19 @@
 #include "Analysis_MeltCurve.h"
 #include "CpptrajStdio.h"
 
-Analysis_MeltCurve::Analysis_MeltCurve() : outfile_(0), mcurve_(0), cut_(0) {}
-
 void Analysis_MeltCurve::Help() {
-  mprintf("\t<dset0> [<dset1> ...] [out <outfile>] [name <outsetname>] cut <cut>\n");
-  mprintf("\tCalculate melting curve from input data sets assuming a simple 2-state\n");
-  mprintf("\ttransition model, using data below <cut>as 'folded' and data above <cut>\n");
-  mprintf("\tas 'unfolded'\n");
+  mprintf("\t<dset0> [<dset1> ...] [out <outfile>] [name <outsetname>] cut <cut>\n"
+          "\tCalculate melting curve from input data sets assuming a simple 2-state\n"
+          "\ttransition model, using data below <cut>as 'folded' and data above <cut>\n"
+          "\tas 'unfolded'\n");
 }
 
+// Analysis_MeltCurve::Setup()
 Analysis::RetType Analysis_MeltCurve::Setup(ArgList& analyzeArgs, DataSetList* datasetlist,
                             TopologyList* PFLin, DataFileList* DFLin, int debugIn)
 {
   std::string setname = analyzeArgs.GetStringKey("name");
-  outfile_ = DFLin->AddDataFile(analyzeArgs.GetStringKey("out"), analyzeArgs);
+  DataFile* outfile = DFLin->AddDataFile(analyzeArgs.GetStringKey("out"), analyzeArgs);
   cut_ = analyzeArgs.getKeyDouble("cut", -1.0);
   if (cut_ < 0.0) {
     mprinterr("Error: meltcurve: 'cut <cut>' must be specified and > 0.0\n");
@@ -30,21 +29,22 @@ Analysis::RetType Analysis_MeltCurve::Setup(ArgList& analyzeArgs, DataSetList* d
   mcurve_ = datasetlist->AddSet(DataSet::DOUBLE, setname, "Melt");
   if (mcurve_ == 0) return Analysis::ERR;
   // Add dataset to datafile
-  if (outfile_ != 0) outfile_->AddSet( mcurve_ );
+  if (outfile != 0) outfile->AddSet( mcurve_ );
   
   mprintf("    MELTCURVE: Calculating melting curve from %i data sets.\n",
           input_dsets_.size());
   mprintf("\tCut= %f", cut_);
   if (!setname.empty())
     mprintf("  Output set name: %s", setname.c_str());
-  if (outfile_ != 0)
-    mprintf("\tOutfile name: %s", outfile_->DataFilename().base());
+  if (outfile != 0)
+    mprintf("\tOutfile name: %s", outfile->DataFilename().base());
   mprintf("\n");
   for (Array1D::const_iterator set = input_dsets_.begin(); set != input_dsets_.end(); ++set)
     mprintf("\t%s\n", (*set)->Legend().c_str());
   return Analysis::OK;
 }
 
+// Analysis_MeltCurve::Analyze()
 Analysis::RetType Analysis_MeltCurve::Analyze() {
   int idx = 0;
   for (Array1D::const_iterator DS = input_dsets_.begin();
