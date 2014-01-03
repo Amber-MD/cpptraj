6f3165d9804d9f41357f77e63099d54707de9612
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-01-03 09:51:51 -0700

    DRR - Cpptraj: Document DataSet_Coords interface.

--- src/DataSet_Coords.h
+++ src/DataSet_Coords.h
@@ -3,16 +3,22 @@
 #include "Topology.h"
 #include "DataSet_1D.h"
 // NOTE: Should this be a 4D DataSet?
+/// Interface to COORDS data sets
 class DataSet_Coords : public DataSet_1D {
   public:
     DataSet_Coords() : DataSet_1D(COORDS, 8, 3), numBoxCrd_(0), numVel_(0) {}
     virtual ~DataSet_Coords() {}
-    /// Allocate a frame that can be used to store COORDS 
+    /// Allocate a Frame that can be used to store COORDS 
     Frame AllocateFrame() const;
+    /// Add given Frame to this COORDS
     virtual void AddFrame(Frame const&) = 0;
+    /// Set COORDS at specified position with Frame
     virtual void SetCRD(int, Frame const&) = 0;
+    /// Set given Frame with COORDS at specified position
     virtual void GetFrame(int, Frame&) const = 0;
+    /// Set given Frame with COORDS at specified position according to mask
     virtual void GetFrame(int, Frame&, AtomMask const&) const = 0;
+    /// Set main topology that will be associated with frames to/from this COORDS
     void SetTopology(Topology const&);
     inline Topology const& Top() const { return top_; }
   protected:
