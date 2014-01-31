377178cfc28102c67c75bad2105f47c4fd836996
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-01-31 09:33:30 -0700

    DRR - Cpptraj: Fix potential segfault in closest when not using 'first' and improve speed at the same time.

--- src/Action_Closest.h
+++ src/Action_Closest.h
@@ -7,44 +7,41 @@
 class Action_Closest: public Action, ImagedAction {
   public:
     Action_Closest();
-
     static DispatchObject* Alloc() { return (DispatchObject*)new Action_Closest(); }
     static void Help();
-
     ~Action_Closest();
-
-    void Print() {}
   private:
     Action::RetType Init(ArgList&, TopologyList*, FrameList*, DataSetList*,
                           DataFileList*, int);
     Action::RetType Setup(Topology*, Topology**);
     Action::RetType DoAction(int, Frame*, Frame**);
+    void Print() {}
 
-    DataFile *outFile_;
-    DataSet *framedata_;
-    DataSet *moldata_;
-    DataSet *distdata_;
-    DataSet *atomdata_;
-
-    int Nclosest_;          ///< Index into Closest datasets.
-    std::string prefix_;          ///< Output topology prefix.
-    int closestWaters_;     ///< # of waters to keep.
-    bool firstAtom_;        ///< If true just calc based on solvent first atom.
-    AtomMask stripMask_;    ///< Mask including solvent and kept waters.
+    DataFile *outFile_;     ///< Output file for data on closest molecules
+    DataSet *framedata_;    ///< Frame number for each closest molecule.
+    DataSet *moldata_;      ///< Mol# for each closest molecule.
+    DataSet *distdata_;     ///< Closest distance of each molecule.
+    DataSet *atomdata_;     ///< First atom of each closest molecule.
+    int Nclosest_;          ///< Index into Closest molecule DataSets.
+    std::string prefix_;    ///< Output topology prefix.
+    int closestWaters_;     ///< Closest # of molecules to keep.
+    bool firstAtom_;        ///< If true just calc based on molecule first atom.
+    AtomMask stripMask_;    ///< Mask including all solute and closest molecules.
     AtomMask distanceMask_; ///< Mask of atoms to calculate distance from solvent to.
-    Topology *newParm_;     ///< New topology with solute and kept waters.
-    int NsolventMolecules_; ///< # of solvent molecules in original topology.
+    Topology *newParm_;     ///< New topology with solute and closest molecules.
+    int NsolventMolecules_; ///< # of solvent molecules in SolventMols.
     int debug_;
     Frame newFrame_;        ///< New frame with solute and kept waters.
-    /// Hold atom #s of kept solvent in new frame.
-    std::vector<int> keptWaterAtomNum_;
-
+    typedef std::vector<int> Iarray;
+    /// Hold atom #s of kept solvent in new frame for placing into stripMask.
+    Iarray keptWaterAtomNum_;
     /** The moldist structure is used in order to preserve the original
       * solvent molecule numbers after sorting. */
     struct MolDist {
       int mol;        ///< Original solvent molecule number (starts from 1).
       double D;       ///< Closest distance of solvent molecule to atoms in distanceMask.
       AtomMask mask;  ///< Original topology solvent molecule atom mask.
+      Iarray solventAtoms; ///< Actual solvent atom #s to loop over.
     };
     /// Return true if the first molecule is closer than the second
     struct moldist_cmp {
