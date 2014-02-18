2b28371316e5919535cc3d6c141d56255c3724ff
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-02-18 08:30:51 -0700

    DRR - Cpptraj: Clean up some code formatting. Add spacing options to bounds, useful for setting up grid parameters.

--- src/Action_Angle.h
+++ src/Action_Angle.h
@@ -6,16 +6,14 @@
 class Action_Angle: public Action {
   public:
     Action_Angle();
-
     static DispatchObject* Alloc() { return (DispatchObject*)new Action_Angle(); }
     static void Help();
-
-    void Print() {}
   private:
     Action::RetType Init(ArgList&, TopologyList*, FrameList*, DataSetList*,
                           DataFileList*, int);
     Action::RetType Setup(Topology*, Topology**);
     Action::RetType DoAction(int, Frame*, Frame**);
+    void Print() {}
 
     DataSet *ang_;
     bool useMass_;
--- src/Action_Bounds.cpp
+++ src/Action_Bounds.cpp
@@ -6,7 +6,7 @@
 Action_Bounds::Action_Bounds() : ensembleNum_(-1) {}
 
 void Action_Bounds::Help() {
-  mprintf("\t[<mask>] [out <filename>]\n"
+  mprintf("\t[<mask>] [out <filename>] [dx <dx>] [dy <dy>] [dz <dz>]\n"
           "  Calcuate the max/min coordinates (X,Y,Z) of atoms in <mask>.\n");
 }
 
@@ -15,6 +15,9 @@ Action::RetType Action_Bounds::Init(ArgList& actionArgs, TopologyList* PFL, Fram
 {
   ensembleNum_ = DSL->EnsembleNum();
   outfilename_ = actionArgs.GetStringKey("out");
+  dxyz_[0] = actionArgs.getKeyDouble("dx", -1.0);
+  dxyz_[1] = actionArgs.getKeyDouble("dy", -1.0);
+  dxyz_[2] = actionArgs.getKeyDouble("dz", -1.0);
   mask_.SetMaskString( actionArgs.GetMaskNext() );
 
   min_[0] = DBL_MAX;
@@ -54,10 +57,16 @@ Action::RetType Action_Bounds::DoAction(int frameNum, Frame* currentFrame, Frame
   return Action::OK;
 }
 
+static const char cXYZ[3] = {'X', 'Y', 'Z'};
 void Action_Bounds::Print() {
   CpptrajFile outfile;
   if ( outfile.OpenEnsembleWrite( outfilename_, ensembleNum_ ) ) return;
-  outfile.Printf("%f < X < %f\n%f < Y < %f\n%f < Z < %f\n",
-                 min_[0], max_[0], min_[1], max_[1], min_[2], max_[2]);
+  for (int i = 0; i < 3; i++) {
+    outfile.Printf("%f < %c < %f", min_[i], cXYZ[i], max_[i]);
+    if (dxyz_[i] > 0.0)
+      outfile.Printf("\tCenter= %f  Bins=%f", (max_[i] + min_[i]) / 2.0,
+                     (max_[i] - min_[i]) / dxyz_[i]);
+    outfile.Printf("\n");
+  }
   outfile.CloseFile();
 }
