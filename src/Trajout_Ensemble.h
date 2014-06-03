b8bbed70b7946450007dee126a2a72ee5614911f
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-06-03 17:14:49 -0600

    DRR - Cpptraj: Create separate file for definition of FramePtrArray. Initial incarnation of the Trajout_Ensemble class for writing ensemble to single trajectory file.

---
+++ src/Trajout_Ensemble.h
@@ -0,0 +1,23 @@
+#ifndef INC_TRAJOUT_ENSEMBLE_H
+#define INC_TRAJOUT_ENSEMBLE_H
+#include "Trajout.h"
+/// Class for writing ensemble as single file.
+class Trajout_Ensemble : public Trajout {
+  public:
+    Trajout_Ensemble();
+    ~Trajout_Ensemble();
+    // ----- Inherited functions -----------------
+    inline int InitTrajWrite(std::string const&, ArgList const&, Topology*,
+                             TrajectoryFile::TrajFormatType);
+    void EndTraj();
+    int WriteSingle(int, Frame const&) { return 1; }
+    int WriteEnsemble(int,FramePtrArray const&);
+    int SetupTrajWrite(Topology*);
+    void PrintInfo(int) const;
+    void SetEnsembleInfo(int i) { ensembleSize_ = i; }
+    // -------------------------------------------
+  private:
+    TrajectoryIO* eio_;
+    int ensembleSize_;
+};
+#endif
