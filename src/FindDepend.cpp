330fe6268f9a1bb975671d1174036c3f1ff31765
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-06-09 13:16:32 -0600

    DRR - Cpptraj: Add pnetcdf to list of exceptions for header dependencies. Update dependencies.

--- src/FindDepend.cpp
+++ src/FindDepend.cpp
@@ -82,6 +82,7 @@ list<string> *PeakHeader(char *filename, int indent, bool includeStdHeaders) {
       if (strcmp(headername,"zlib.h")==0) continue;
       if (strcmp(headername,"bzlib.h")==0) continue;
       if (strcmp(headername,"netcdf.h")==0) continue;
+      if (strcmp(headername,"pnetcdf.h")==0) continue;
       if (strcmp(headername,"omp.h")==0) continue;
       if (strncmp(headername,"readline",8)==0) continue;
     }
