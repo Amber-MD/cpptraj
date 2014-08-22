0aa6ea21e5507ef03c6e52e357edf83ffdd46ade
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-08-22 09:48:31 -0600

    DRR - Cpptraj: Add ability to only calculate certain energy terms.

--- src/Action_Energy.h
+++ src/Action_Energy.h
@@ -14,12 +14,18 @@ class Action_Energy: public Action {
     Action::RetType Setup(Topology*, Topology**);
     Action::RetType DoAction(int, Frame*, Frame**);
     void Print();
-
-    enum Etype { BOND, ANGLE, DIHEDRAL, V14, Q14, VDW, ELEC, TOTAL};
-    std::vector<DataSet*> Energy_;
-    Topology* currentParm_;
-    AtomMask Mask1_;
-    AtomMask Imask_;
-    Energy_Amber ENE_;
+    /// Corresponds to data sets.
+    enum Etype { BOND = 0, ANGLE, DIHEDRAL, V14, Q14, VDW, ELEC, TOTAL};
+    /// Add energy data set of specified type.
+    int AddSet(Etype, DataSetList*, DataFile*, std::string const&);
+    /// Corresponds to calculations.
+    enum CalcType { BND, ANG, DIH, N14, NBD };
+    std::vector<DataSet*> Energy_; ///< Hold output data sets
+    std::vector<CalcType> Ecalcs_; ///< Hold which calcs to perform
+    typedef std::vector<CalcType>::const_iterator calc_it;
+    Topology* currentParm_;        ///< Hold current topology
+    AtomMask Mask1_;               ///< Char mask for all but NB calc
+    AtomMask Imask_;               ///< Int mask for NB calc
+    Energy_Amber ENE_;             ///< Energy calc class.
 };
 #endif
