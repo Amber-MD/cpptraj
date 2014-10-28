fafd162548d1223c6603d0ccde52cf9ee5c5969e
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-10-28 08:04:38 -0600

    DRR - Cpptraj: Separate assocated class definitions from main definition for easier reading.

--- src/Action_NMRrst.h
+++ src/Action_NMRrst.h
@@ -38,57 +38,14 @@ class Action_NMRrst: public Action {
 
     typedef std::vector<int> Iarray;
 
-    /// Potential NOE site.
-    class Site {
-      public:
-        Site() : resNum_(-1) {}
-        Site(int r, Iarray const& i) : resNum_(r), indices_(i), shortestCount_(i.size(), 0) {}
-        int ResNum()                   const { return resNum_;           }
-        unsigned int Nindices()        const { return indices_.size();   }
-        int Idx(unsigned int i)        const { return indices_[i];       }
-        int Count(unsigned int i)      const { return shortestCount_[i]; }
-        void Increment(int c)                { ++shortestCount_[c];      }
-//        int TotalCount()               const {
-//          int c = 0;
-//          for (Iarray::const_iterator it = shortestCount_.begin(); it != shortestCount_.end(); ++it)
-//            c += *it;
-//          return c;
-//        };
-      private:
-        int resNum_; ///< Site residue number.
-        Iarray indices_; ///< Site atom indices.
-        Iarray shortestCount_; ///< # times atom was part of shortest distance.
-    };
+    class Site;
     typedef std::vector<Site> SiteArray;
     SiteArray potentialSites_;
 
     /// Used to map NOEs to unique values, res1 always < res2. 
     typedef std::pair<int,int> Ptype;
 
-    /// NOE between two sites.
-    class NOEtype {
-      public:
-        NOEtype() : dist2_(0), belowCut_(false) {}
-        NOEtype(Site const& s1, Site const& s2, DataSet* d) :
-          site1_(s1), site2_(s2), dist2_(d), belowCut_(false) {}
-        Site const& Site1()    const { return site1_;    }
-        Site const& Site2()    const { return site2_;    }
-        bool CutoffSatisfied() const { return belowCut_; }
-        DataSet* Data()              { return dist2_;    }
-        void ResetData()             { dist2_ = 0;       }
-        void UpdateNOE(int i, double d, unsigned int c1, unsigned int c2, bool satisfyCut) {
-          float fval = (float)d;
-          dist2_->Add(i, &fval);
-          site1_.Increment( c1 );
-          site2_.Increment( c2 );
-          if (satisfyCut) belowCut_ = true;
-        }
-      private:
-        Site site1_; ///< First site, lower resNum
-        Site site2_; ///< Second site, higher resNum
-        DataSet* dist2_; ///< Distance^2 data.
-        bool belowCut_; ///< If true, cutoff was satisfied at least once.
-    };
+    class NOEtype;
     typedef std::vector<NOEtype> NOEtypeArray;
     NOEtypeArray noeArray_;
 //    typedef std::map<Ptype, NOEtype> NOEmap;
@@ -107,5 +64,51 @@ class Action_NMRrst: public Action {
     int nframes_; ///< Total # of frames.
     bool useMass_;
     bool findNOEs_;
+};
+// ----- Associated Classes ----------------------------------------------------
+/// Potential NOE site.
+class Action_NMRrst::Site {
+  public:
+    Site() : resNum_(-1) {}
+    Site(int r, Iarray const& i) : resNum_(r), indices_(i), shortestCount_(i.size(), 0) {}
+    int ResNum()                   const { return resNum_;           }
+    unsigned int Nindices()        const { return indices_.size();   }
+    int Idx(unsigned int i)        const { return indices_[i];       }
+    int Count(unsigned int i)      const { return shortestCount_[i]; }
+    void Increment(int c)                { ++shortestCount_[c];      }
+//    int TotalCount()               const {
+//      int c = 0;
+//      for (Iarray::const_iterator it = shortestCount_.begin(); it != shortestCount_.end(); ++it)
+//        c += *it;
+//      return c;
+//    };
+  private:
+    int resNum_; ///< Site residue number.
+    Iarray indices_; ///< Site atom indices.
+    Iarray shortestCount_; ///< # times atom was part of shortest distance.
+};
+/// NOE between two sites.
+class Action_NMRrst::NOEtype {
+  public:
+    NOEtype() : dist2_(0), belowCut_(false) {}
+    NOEtype(Site const& s1, Site const& s2, DataSet* d) :
+      site1_(s1), site2_(s2), dist2_(d), belowCut_(false) {}
+    Site const& Site1()    const { return site1_;    }
+    Site const& Site2()    const { return site2_;    }
+    bool CutoffSatisfied() const { return belowCut_; }
+    DataSet* Data()              { return dist2_;    }
+    void ResetData()             { dist2_ = 0;       }
+    void UpdateNOE(int i, double d, unsigned int c1, unsigned int c2, bool satisfyCut) {
+      float fval = (float)d;
+      dist2_->Add(i, &fval);
+      site1_.Increment( c1 );
+      site2_.Increment( c2 );
+      if (satisfyCut) belowCut_ = true;
+    }
+  private:
+    Site site1_; ///< First site, lower resNum
+    Site site2_; ///< Second site, higher resNum
+    DataSet* dist2_; ///< Distance^2 data.
+    bool belowCut_; ///< If true, cutoff was satisfied at least once.
 };
 #endif
