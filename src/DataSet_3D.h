8a1f323ac60c63e266e137428e4c01c17da571b2
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2014-11-19 14:06:07 -0700

    DRR - Cpptraj: Some initial code for non-orthogonal gridding.

--- src/DataSet_3D.h
+++ src/DataSet_3D.h
@@ -2,7 +2,7 @@
 #define INC_DATASET_3D_H
 #include "DataSet.h"
 #include "CpptrajFile.h"
-#include "Vec3.h"
+#include "Matrix_3x3.h"
 /// Interface for 3D DataSets.
 // FIXME: Use DataSet Dims?
 class DataSet_3D : public DataSet {
@@ -33,6 +33,7 @@ class DataSet_3D : public DataSet {
     int Allocate_X_C_D(Vec3 const&,Vec3 const&,Vec3 const&);
     /// Convert X, Y, and Z coords to index.
     inline bool CalcBins(double,double,double,int&,int&,int&) const;
+    inline bool CalcNonOrthoBins(double, double, double, int&, int&, int&) const;
     inline double DX() const { return dx_; }
     inline double DY() const { return dy_; }
     inline double DZ() const { return dz_; }
@@ -59,7 +60,9 @@ class DataSet_3D : public DataSet {
     double oz_; ///< Grid Z origin (minimum).
     double mx_; ///< Grid X maximum.
     double my_; ///< Grid Y maximum.
-    double mz_; ///< Grid Z maximum. 
+    double mz_; ///< Grid Z maximum.
+    Matrix_3x3 ucell_; ///< Unit cell axes.
+    Matrix_3x3 recip_; ///< Convert to fractional coords. 
 };
 // ----- INLINE FUNCTIONS ------------------------------------------------------
 // DataSet_3D::CalcBins()
@@ -92,5 +95,22 @@ Vec3 DataSet_3D::BinCenter(int i, int j, int k) {
   return Vec3( (double)i*dx_ + ox_ + 0.5*dx_,
                (double)j*dy_ + oy_ + 0.5*dy_,
                (double)k*dz_ + oz_ + 0.5*dz_ );
+}
+// DataSet_3d::CalcNonOrthoBins()
+bool DataSet_3D::CalcNonOrthoBins(double x, double y, double z,
+                                  int& i, int& j, int& k) const
+{
+  Vec3 frac = recip_ * Vec3(x, y, z);
+  if (frac[0] >= 0.0 && frac[0] < 1.0) {
+    if (frac[1] >= 0.0 && frac[1] < 1.0) {
+      if (frac[2] >= 0.0 && frac[2]) {
+        i = (int)(frac[0] * (double)NX());
+        j = (int)(frac[1] * (double)NY());
+        k = (int)(frac[2] * (double)NZ());
+        return true;
+      }
+    }
+  }
+  return false;
 }
 #endif
