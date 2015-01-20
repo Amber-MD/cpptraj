9dd0dc16c934b38f862e16bcc690f238c41b7e33
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-01-20 16:18:57 -0700

    DRR - Cpptraj: All relevant data for Dpeaks stored in new Cpoint class. Prototype heuristic for choosing cluster centers based on running avg of density vs distance.

--- src/Cluster_DPeaks.cpp
+++ src/Cluster_DPeaks.cpp
@@ -1,5 +1,8 @@
+#include <cmath> // fabs
+#include <algorithm> // sort
 #include "Cluster_DPeaks.h"
 #include "CpptrajStdio.h"
+#include "DataSet_Mesh.h"
 
 Cluster_DPeaks::Cluster_DPeaks() : epsilon_(-1.0) {}
 
@@ -22,56 +25,49 @@ void Cluster_DPeaks::ClusteringInfo() {
 }
 
 int Cluster_DPeaks::Cluster() {
-  Iarray FramesToCluster;
-  Iarray local_density; // For each point, # other points within epsilon
-  Darray distances; // For each point, minimum distance to point with higher density
+  Carray Points; // Frames to cluster.
   // First determine which frames are being clustered.
   for (int frame = 0; frame < (int)FrameDistances_.Nframes(); ++frame)
     if (!FrameDistances_.IgnoringRow( frame ))
-      FramesToCluster.push_back( frame );
+      Points.push_back( Cpoint(frame) );
   // Sanity check.
-  if (FramesToCluster.size() < 2) {
+  if (Points.size() < 2) {
     mprinterr("Error: Only 1 frame in initial clustering.\n");
     return 1;
   }
   // For each point, determine how many others are within epsilon
-  local_density.reserve( FramesToCluster.size() );
-  for (Iarray::const_iterator point0 = FramesToCluster.begin();
-                              point0 != FramesToCluster.end(); ++point0)
+  for (Carray::iterator point0 = Points.begin();
+                        point0 != Points.end(); ++point0)
   {
     int density = 0;
-    for (Iarray::const_iterator point1 = FramesToCluster.begin();
-                                point1 != FramesToCluster.end(); ++point1)
+    for (Carray::const_iterator point1 = Points.begin();
+                                point1 != Points.end(); ++point1)
     {
       if (point0 != point1) {
-        if ( FrameDistances_.GetFdist(*point0, *point1) < epsilon_ )
-          ++density;
+        if ( FrameDistances_.GetFdist(point0->Fnum(), point1->Fnum()) < epsilon_ )
+          density++;
       }
     }
-    local_density.push_back( density );
+    point0->SetDensity( density );
   }
   // For each point, find the closest point that has higher density.
-  distances.reserve( FramesToCluster.size() );
-  Iarray::const_iterator p0_density = local_density.begin();
-  for (Iarray::const_iterator point0 = FramesToCluster.begin();
-                              point0 != FramesToCluster.end();
-                            ++point0, ++p0_density)
+  for (Carray::iterator point0 = Points.begin();
+                        point0 != Points.end(); ++point0)
   {
     double min_dist = -1.0;
     double max_dist = -1.0;
-    Iarray::const_iterator p1_density = local_density.begin();
-    for (Iarray::const_iterator point1 = FramesToCluster.begin();
-                                point1 != FramesToCluster.end();
-                              ++point1, ++p1_density)
+    for (Carray::const_iterator point1 = Points.begin();
+                                point1 != Points.end(); ++point1)
     {
       if (point0 != point1) {
-        max_dist = std::max(max_dist, FrameDistances_.GetFdist(*point0, *point1)); 
-        if (*p1_density > *p0_density)
+        double dist1_2 = FrameDistances_.GetFdist(point0->Fnum(), point1->Fnum());
+        max_dist = std::max(max_dist, dist1_2); 
+        if (point1->Density() > point0->Density())
         {
           if (min_dist < 0.0)
-            min_dist = FrameDistances_.GetFdist(*point0, *point1);
+            min_dist = dist1_2;
           else
-            min_dist = std::min(min_dist, FrameDistances_.GetFdist(*point0, *point1));
+            min_dist = std::min(min_dist, dist1_2);
         }
       }
     }
@@ -79,18 +75,88 @@ int Cluster_DPeaks::Cluster() {
     // i.e. this point has the highest density. Assign it the maximum observed
     // distance.
     if (min_dist < 0.0)
-      distances.push_back( max_dist );
+      point0->SetDist( max_dist );
     else
-      distances.push_back( min_dist );
+      point0->SetDist( min_dist );
   }
+  // Sort by density
+  std::sort( Points.begin(), Points.end() );
   // DEBUG - Plot density vs distance for each point.
   CpptrajFile output;
   output.OpenWrite("dpeaks.dat");
-  for (unsigned int p = 0; p != FramesToCluster.size(); ++p)
-    output.Printf("%i %g \"%i\"\n", local_density[p], distances[p], FramesToCluster[p]+1);
+  for (Carray::const_iterator point = Points.begin();
+                              point != Points.end(); ++point)
+    output.Printf("%i %g \"%i\"\n", point->Density(), point->Dist(), point->Fnum()+1);
   output.CloseFile();
   // Choose points for which the min distance to point with higher density is
   // anomalously high.
+  // Currently doing this by calculating the running average of density vs 
+  // distance, then choosing points with distance > twice the SD of the 
+  // running average.
+  // NOTE: Store in a mesh data set for now in case we want to spline etc later.
+  unsigned int avg_factor = 10;
+  unsigned int window_size = Points.size() / avg_factor;
+  mprintf("DBG:\tRunning avg window size is %u\n", window_size);
+  // FIXME: Handle case where window_size < frames
+  DataSet_Mesh runavg;
+  unsigned int ra_size = Points.size() - window_size + 1;
+  runavg.Allocate1D( ra_size );
+  mprintf("DBG:\tRunning avg set should be size %u\n", ra_size);
+  CpptrajFile raOut;
+  raOut.OpenWrite("runavg.dpeaks.dat");
+  double dwindow = (double)window_size;
+  double sumx = 0.0;
+  double sumy = 0.0;
+  for (unsigned int i = 0; i < window_size; i++) {
+    sumx += (double)Points[i].Density();
+    sumy += Points[i].Dist();
+  }
+  double avgy = sumy / dwindow;
+  runavg.AddXY( sumx / dwindow, avgy );
+  raOut.Printf("%g %g\n", sumx / dwindow, avgy );
+  for (unsigned int i = 1; i < ra_size; i++) {
+    unsigned int nextwin = i + window_size - 1;
+    unsigned int prevwin = i - 1;
+    sumx = (double)Points[nextwin].Density() - (double)Points[prevwin].Density() + sumx;
+    sumy =         Points[nextwin].Dist()    -         Points[prevwin].Dist()    + sumy;
+    avgy = sumy / dwindow;
+    runavg.AddXY( sumx / dwindow, avgy );
+    raOut.Printf("%g %g\n", sumx / dwindow, avgy );
+  }
+  raOut.CloseFile();
+  mprintf("DBG:\tRunning avg set is size %zu\n", runavg.Size());
+  double ra_sd;
+  double ra_avg = runavg.Avg( ra_sd );
+  // Double stdev
+  ra_sd *= 2.0;
+  mprintf("DBG:\tAvg of running avg set is %g, sd*2.0 is %g\n", ra_avg, ra_sd);
+  // For each point, what is the closest running avgd point?
+  CpptrajFile raDelta;
+  raDelta.OpenWrite("radelta.dat");
+  raDelta.Printf("%-10s %10s %10s\n", "#Frame", "RnAvgPos", "Delta");
+  unsigned int ra_position = 0;
+  unsigned int ra_end = Points.size() - 1;
+  for (Carray::const_iterator point = Points.begin();
+                              point != Points.end(); ++point)
+  {
+    if (ra_position != ra_end) {
+      // Is the next running avgd point closer to this point?
+      while (ra_position != ra_end) {
+        double dens  = (double)point->Density();
+        double diff0 = fabs( dens - runavg.X(ra_position  ) );
+        double diff1 = fabs( dens - runavg.X(ra_position+1) );
+        if (diff1 < diff0)
+          ++ra_position; // Next running avg position is closer for this point.
+        else
+          break; // This position is closer.
+      }
+    }
+    double delta = point->Dist() - runavg.Y(ra_position);
+    raDelta.Printf("%-10i %10u %10g", point->Fnum()+1, ra_position, delta);
+    if (delta > ra_sd) raDelta.Printf(" POTENTIAL CLUSTER");
+    raDelta.Printf("\n");
+  }
+  raDelta.CloseFile();
   return 0;
 }
 
--- src/Cluster_DPeaks.h
+++ src/Cluster_DPeaks.h
@@ -12,7 +12,27 @@ class Cluster_DPeaks : public ClusterList {
     void ClusterResults(CpptrajFile&) const;
   private:
     double epsilon_;
-    typedef std::vector<int> Iarray;
-    typedef std::vector<double> Darray;
+    class Cpoint {
+      public:
+        Cpoint() : dist_(-1.0), density_(0), fnum_(-1) {}
+        Cpoint(int f) : dist_(-1.0), density_(0), fnum_(f) {}
+        /// Used to sort Carray
+        bool operator<(Cpoint const& second) const {
+          if (density_ == second.density_)
+            return (fnum_ < second.fnum_);
+          else
+            return (density_ < second.density_);
+        }
+        double Dist() const { return dist_; }
+        int Density() const { return density_; }
+        int Fnum()    const { return fnum_; }
+        void SetDensity(int d) { density_ = d; }
+        void SetDist(double d) { dist_ = d; }
+      private:
+        double dist_; ///< minimum distance to point with higher density
+        int density_; ///< # other points within epsilon
+        int fnum_;    ///< Frame number.
+    };
+    typedef std::vector<Cpoint> Carray;
 };
 #endif
