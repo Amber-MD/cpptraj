e59c11b4cf5c0ddd24846653ba0f8f8720085e45
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-03-05 09:39:23 -0700

    DRR - Cpptraj: Fix # frames in crdout

--- src/Command.cpp
+++ src/Command.cpp
@@ -749,6 +749,7 @@ Command::RetType CrdOut(CpptrajState& State, ArgList& argIn, Command::AllocType 
   if (State.Debug() > 0) mprintf("\tDBG: Frames %i to %i, offset %i\n", start+1, stop, offset);
   Trajout_Single outtraj;
   Topology* currentParm = (Topology*)&(CRD->Top()); // TODO: Fix cast
+  currentParm->SetNframes( CRD->Size() ); // FIXME: This is a hack to get correct # frames.
   if (outtraj.InitTrajWrite( setname, argIn, currentParm, TrajectoryFile::UNKNOWN_TRAJ)) {
     mprinterr("Error: crdout: Could not set up output trajectory.\n");
     return Command::C_ERR;
