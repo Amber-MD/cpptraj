0cbe76cb616880269b702ced913715f9e5a3f2a9
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-04-16 16:18:25 -0600

    DRR - Cpptraj: Wrap torsion/pucker values during processing rather than after so that commands like 'filter' access the expected data.

--- src/Action_Dihedral.cpp
+++ src/Action_Dihedral.cpp
@@ -8,8 +8,8 @@
 // CONSTRUCTOR
 Action_Dihedral::Action_Dihedral() :
   dih_(0),
-  useMass_(false),
-  range360_(false)
+  minTorsion_(-180.0),
+  useMass_(false)
 { } 
 
 void Action_Dihedral::Help() {
@@ -19,13 +19,16 @@ void Action_Dihedral::Help() {
           "  Calculate dihedral angle for atoms in masks 1-4.\n");
 }
 
-// Action_Dihedral::init()
+// Action_Dihedral::Init()
 Action::RetType Action_Dihedral::Init(ArgList& actionArgs, TopologyList* PFL, DataSetList* DSL, DataFileList* DFL, int debugIn)
 {
   // Get keywords
   DataFile* outfile = DFL->AddDataFile( actionArgs.GetStringKey("out"), actionArgs );
   useMass_ = actionArgs.hasKey("mass");
-  range360_ = actionArgs.hasKey("range360");
+  if (actionArgs.hasKey("range360"))
+    minTorsion_ = 0.0;
+  else
+    minTorsion_ = -180.0;
   int dsidx = actionArgs.getKeyInt("idx", -1);
   DataSet::scalarType stype = DataSet::UNDEFINED;
   std::string stypename = actionArgs.GetStringKey("type");
@@ -63,7 +66,7 @@ Action::RetType Action_Dihedral::Init(ArgList& actionArgs, TopologyList* PFL, Da
           M2_.MaskString(), M3_.MaskString(), M4_.MaskString());
   if (useMass_)
     mprintf("              Using center of mass of atoms in masks.\n");
-  if (range360_)
+  if (minTorsion_ > -180.0)
     mprintf("              Output range is 0 to 360 degrees.\n");
   else
     mprintf("              Output range is -180 to 180 degrees.\n");
@@ -71,7 +74,7 @@ Action::RetType Action_Dihedral::Init(ArgList& actionArgs, TopologyList* PFL, Da
   return Action::OK;
 }
 
-// Action_Dihedral::setup
+// Action_Dihedral::Setup
 Action::RetType Action_Dihedral::Setup(Topology* currentParm, Topology** parmAddress) {
   if (currentParm->SetupIntegerMask(M1_)) return Action::ERR;
   if (currentParm->SetupIntegerMask(M2_)) return Action::ERR;
@@ -91,7 +94,7 @@ Action::RetType Action_Dihedral::Setup(Topology* currentParm, Topology** parmAdd
   return Action::OK;  
 }
 
-// Action_Dihedral::action()
+// Action_Dihedral::DoAction()
 Action::RetType Action_Dihedral::DoAction(int frameNum, Frame* currentFrame, Frame** frameAddress) {
   Vec3 a1, a2, a3, a4;
 
@@ -110,14 +113,10 @@ Action::RetType Action_Dihedral::DoAction(int frameNum, Frame* currentFrame, Fra
 
   torsion *= Constants::RADDEG;
 
+  if (torsion < minTorsion_)
+    torsion += 360.0; 
+
   dih_->Add(frameNum, &torsion);
 
-  //fprintf(outfile,"%10i %10.4lf\n",frameNum,D);
-  
   return Action::OK;
-} 
-
-void Action_Dihedral::Print() {
-  if (range360_)
-    ((DataSet_double*)dih_)->ShiftTorsions(0.0, 0.0);
 }
