84905c1f6749cb2b6ef26af3c181db611669b10d
Daniel R. Roe <daniel.r.roe@gmail.com>
2015-05-08 14:21:07 -0600

    DRR - Cpptraj: Allow multiple vectors to be written to a single vector pseudo trajectory.

--- src/DataIO_VecTraj.cpp
+++ src/DataIO_VecTraj.cpp
@@ -21,14 +21,31 @@ int DataIO_VecTraj::processWriteArgs(ArgList& argIn) {
 
 int DataIO_VecTraj::WriteData(std::string const& fname, DataSetList const& SetList) {
   if (SetList.empty()) return 1;
-  if (SetList.size() > 1)
-    mprintf("Warning: Multiple sets not yet supported for Evecs write.\n");
-  DataSet_Vector const& Vec = static_cast<DataSet_Vector const&>( *(*(SetList.begin())) );
-  // Create pseudo-topology.
+  // Create pseudo-topology for all vectors.
   Topology pseudo;
-  pseudo.AddTopAtom(Atom("OXYZ", ' ', 0), 1, "VEC", 0);
-  pseudo.AddTopAtom(Atom("VXYZ", ' ', 0), 1, "VEC", 0);
-  pseudo.AddBond(0, 1);
+  int nres = 1;
+  int natom = 0;
+  int vec_size = -1;
+  for (DataSetList::const_iterator set = SetList.begin(); set != SetList.end(); ++set) {
+    if ((*set)->Type() != DataSet::VECTOR)
+      mprintf("Warning: Set '%s' is not a vector, skipping.\n", (*set)->legend());
+    else {
+      DataSet_Vector const& Vec = static_cast<DataSet_Vector const&>( *(*set) );
+      if (vec_size == -1)
+        vec_size = (int)Vec.Size();
+      else if (vec_size != (int)Vec.Size()) {
+        mprinterr("Error: Vector '%s' size %zu != first vector size %zu.\n"
+                  "Error:   All vectors must have same size.\n",
+                  Vec.legend(), Vec.Size(), vec_size);
+        return 1;
+      }
+      pseudo.AddTopAtom(Atom("OXYZ", ' ', 0), nres, "VEC", 0);
+      pseudo.AddTopAtom(Atom("VXYZ", ' ', 0), nres, "VEC", 0);
+      pseudo.AddBond(natom, natom+1);
+      natom += 2;
+      ++nres;
+    }
+  }
   pseudo.CommonSetup(false);
   if (!parmoutName_.empty()) {
     ParmFile pfile;
@@ -39,12 +56,17 @@ int DataIO_VecTraj::WriteData(std::string const& fname, DataSetList const& SetLi
   }
   Trajout_Single out;
   if (out.InitTrajWrite(fname, ArgList(), &pseudo, trajoutFmt_) == 0) {
-    Frame outFrame(2);
-    for (unsigned int i = 0; i != Vec.Size(); ++i) {
+    Frame outFrame(pseudo.Natom());
+    for (int i = 0; i != vec_size; ++i) {
       outFrame.ClearAtoms();
-      Vec3 const& ovec = Vec.OXYZ(i);
-      outFrame.AddVec3( ovec );
-      outFrame.AddVec3( Vec[i] + ovec );
+      for (DataSetList::const_iterator set = SetList.begin(); set != SetList.end(); ++set) {
+        if ((*set)->Type() == DataSet::VECTOR) {
+          DataSet_Vector const& Vec = static_cast<DataSet_Vector const&>( *(*set) );
+          Vec3 const& ovec = Vec.OXYZ(i);
+          outFrame.AddVec3( ovec );
+          outFrame.AddVec3( Vec[i] + ovec );
+        }
+      }
       if (out.WriteFrame(i, &pseudo, outFrame)) return 1;
     }
     out.EndTraj();
