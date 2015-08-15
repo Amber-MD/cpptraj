c3deaca72a43c258c96c3ffe515d23f604aa716a
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-08-15 07:55:46 -0600

    DRR - Cpptraj: Finish setting up prototype AssociatedData, NOE.

--- src/AssociatedData.h
+++ src/AssociatedData.h
@@ -1,5 +1,6 @@
 #ifndef INC_ASSOCIATEDDATA_H
 #define INC_ASSOCIATEDDATA_H
+#include "ArgList.h"
 class AssociatedData {
   public:
     enum AssociatedType { NOE = 0 };
@@ -13,16 +14,18 @@ class AssociatedData {
 /// For Analysis_Statistics DISTANCE NOE
 class AssociatedData_NOE : public AssociatedData {
   public:
-    AssociatedData_NOE() : AssociatedData(NOE), bound_(0.0), boundh_(0.0), rexp_(-1.0) {}
-    void SetNOE(double b, double bh, double r) { bound_=b; boundh_=bh; rexp_=r;}
-    double NOE_bound()  const { return bound_;  }
-    double NOE_boundH() const { return boundh_; }
+    AssociatedData_NOE() : AssociatedData(NOE), l_bound_(0.0), u_bound_(0.0), rexp_(-1.0) {}
+    static const char* HelpText;
+    int NOE_Args(ArgList&);
+    //void SetNOE(double l, double u, double r) { l_bound_=l; u_bound_=u; rexp_=r;}
+    double NOE_bound()  const { return l_bound_;  }
+    double NOE_boundH() const { return u_bound_; }
     double NOE_rexp()   const { return rexp_;   }
 
     AssociatedData* Copy() const { return new AssociatedData_NOE(*this); }
   private:
-    double bound_; ///< Lower bound
-    double boundh_; ///< Upper bound
-    double rexp_; ///< Expected distance
+    double l_bound_; ///< Lower bound
+    double u_bound_; ///< Upper bound
+    double rexp_;    ///< Expected distance
 };
 #endif
