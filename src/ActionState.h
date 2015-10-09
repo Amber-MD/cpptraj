bb9ec839b84057a0e9b310258024afd485173cb6
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-10-09 15:16:57 -0600

    DRR - Cpptraj: Add code doc to ActionState

--- src/ActionState.h
+++ src/ActionState.h
@@ -1,6 +1,12 @@
 #ifndef INC_ACTIONSTATE_H
 #define INC_ACTIONSTATE_H
 #include "DataFileList.h"
+/*! \file ActionState.h
+    \brief Classes used to wrap arguments passed to Actions.
+ */
+/** The ActionInit class is used to pass in the master DataSetList and
+  * DataFileList.
+  */
 class ActionInit {
   public:
     ActionInit(DataSetList& dslIn, DataFileList& dflIn) :
@@ -10,15 +16,18 @@ class ActionInit {
     DataSetList* DslPtr()           { return dsl_;  }
     DataFileList& DFL()             { return *dfl_; }
     DataFileList const& DFL() const { return *dfl_; }
+    /// Can be used by Actions that want to set up DataSets in Setup/DoAction/Print.
     DataSetList* DSL_Ptr()          { return dsl_;  }
   private:
     DataSetList* dsl_;
     DataFileList* dfl_;
 };
-
+/* The ActionSetup class is used to pass in the current Topology, CoordinateInfo,
+ * and expected number of frames associated with the current Topology.
+ */
 class ActionSetup {
   public:
-    // NOTE: Blank constructor/Set for use during ensemble
+    // NOTE: Blank constructor/Set() for use during ensemble
     ActionSetup() : top_(0), cInfo_(0), nFrames_(0) {}
     void Set(Topology* p, CoordinateInfo const& c, int n) {
       top_ = p;
@@ -28,7 +37,10 @@ class ActionSetup {
     ActionSetup(Topology* topIn, CoordinateInfo const& cInfoIn, int nIn) :
       top_(topIn), cInfo_((CoordinateInfo*)&cInfoIn), nFrames_(nIn) {}
     Topology const& Top()             const { return *top_;    }
-    // NOTE: Used to set up output trajectories and for certain Actions (DoAction).
+    /// Current Topology pointer.
+    /** Used to set up output trajectories and for certain Actions so that
+      * current Topology can be used in other functions (e.g. DoAction).
+      */
     Topology* TopAddress()                  { return top_;     }
     CoordinateInfo const& CoordInfo() const { return *cInfo_;  }
     int Nframes()                     const { return nFrames_; }
@@ -39,13 +51,14 @@ class ActionSetup {
     CoordinateInfo* cInfo_;
     int nFrames_;
 };
-
+/** The ActionFrame class is used to pass in the current Frame. */
 class ActionFrame {
   public:
     ActionFrame() : frm_(0) {}
     ActionFrame(Frame* fIn) : frm_(fIn) {}
     Frame const& Frm()  const { return *frm_; }
     Frame& ModifyFrm()        { return *frm_; }
+    // NOTE: Used during ensemble.
     Frame* FramePtr()         { return frm_;  }
     void SetFrame( Frame* f ) { frm_ = f;     }
   private:
