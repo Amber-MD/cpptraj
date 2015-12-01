c9b3a644bf8ab77add20089322cb001a258d0a0a
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-12-01 13:15:51 -0700

    DRR - Cpptraj: Move rst command to its own file

---
+++ src/Cmd_GenerateAmberRst.h
@@ -0,0 +1,6 @@
+#ifndef INC_CMD_GENERATEAMBERRST_H
+#define INC_CMD_GENERATEAMBERRST_H
+#include "Cmd.h"
+void Help_GenerateAmberRst();
+Cmd::RetType GenerateAmberRst(CpptrajState&, ArgList&, Cmd::AllocType);
+#endif
