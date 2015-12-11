2e43313edca95a8afd25c026bb6601df10125214
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-12-11 10:33:27 -0700

    DRR - Cpptraj: Add commands for Topology loading and info printing

--- src/Command.cpp
+++ src/Command.cpp
@@ -31,6 +31,7 @@
 #include "Exec_Traj.h"
 // ----- TOPOLOGY --------------------------------------------------------------
 #include "Exec_CompareTop.h"
+#include "Exec_Top.h"
 // ----- ACTION ----------------------------------------------------------------
 #include "Action_Angle.h"
 #include "Action_Distance.h"
@@ -162,7 +163,21 @@ void Command::Init() {
   Command::AddCmd( new Exec_Trajin(),    Cmd::EXE, 1, "trajin" );
   Command::AddCmd( new Exec_Trajout(),   Cmd::EXE, 1, "trajout" );
   // TOPOLOGY COMMANDS
-  Command::AddCmd( new Exec_CompareTop(),   Cmd::EXE, 1, "comparetop" );
+  Command::AddCmd( new Exec_AngleInfo(),   Cmd::EXE, 3, "angles", "angleinfo", "printangles" );
+  Command::AddCmd( new Exec_AtomInfo(),    Cmd::EXE, 3, "atoms", "atominfo", "printatoms" );
+  Command::AddCmd( new Exec_BondInfo(),    Cmd::EXE, 3, "bonds", "bondinfo", "printbonds" );
+  Command::AddCmd( new Exec_ChargeInfo(),  Cmd::EXE, 1, "charge" );
+  Command::AddCmd( new Exec_CompareTop(),  Cmd::EXE, 1, "comparetop" );
+  Command::AddCmd( new Exec_DihedralInfo(),Cmd::EXE, 3,"dihedrals","dihedralinfo","printdihedrals");
+  Command::AddCmd( new Exec_MassInfo(),    Cmd::EXE, 1, "mass" );
+  Command::AddCmd( new Exec_MolInfo(),     Cmd::EXE, 1, "molinfo" );
+  Command::AddCmd( new Exec_LoadParm(),    Cmd::EXE, 1, "parm" );
+  //Command::AddCmd( new Exec_ParmBox(),    Cmd::EXE, 1, "parmbox" );
+  Command::AddCmd( new Exec_ParmInfo(),    Cmd::EXE, 1, "parminfo" );
+  //Command::AddCmd( new Exec_ParmSolvent(),Cmd::EXE, 1, "solvent" );
+  //Command::AddCmd( new Exec_ParmStrip(),    Cmd::EXE, 1, "parmstrip" );
+  //Command::AddCmd( new Exec_ParmWrite(),    Cmd::EXE, 1, "parmwrite" );
+  //Command::AddCmd( new Exec_ScaleDihedralK(),    Cmd::EXE, 1, "scaledihedralk" );
   // ACTION
   Command::AddCmd( new Action_Angle(),         Cmd::ACT, 1, "angle" );
   Command::AddCmd( new Action_AreaPerMol(),    Cmd::ACT, 1, "areapermol" );
