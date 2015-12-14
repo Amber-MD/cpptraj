9c80bac42dd870b043658d2db0c6833a3d15a4c5
Daniel R. Roe <daniel.r.roe@gmail.com>
2015-12-14 11:31:54 -0700

    DRR - Cpptraj: Add cluster random sieve test

---
+++ test/Test_Cluster_RandomSieve/RunTest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles cluster.in random.out random.info.dat random.summary.dat \
+           random.crd.c? random.cpop.agr random.rep.*.pdb CpptrajPairDist
+
+PREFIX="random"
+INPUT="-i cluster.in"
+
+Ctest() {
+  cat > cluster.in <<EOF
+parm ../tz2.parm7
+trajin ../tz2.nc
+cluster crd1 @CA clusters 5 rms out $PREFIX.out sieve 5 random sieveseed 1 \
+        summary $PREFIX.summary.dat info $PREFIX.info.dat \
+        clusterout $PREFIX.crd cpopvtime $PREFIX.cpop.agr \
+        repout $PREFIX.rep repfmt pdb savepairdist loadpairdist
+EOF
+  RunCpptraj "Cluster test with random sieve ($1)."
+  DoTest random.info.dat.save random.info.dat
+  DoTest random.summary.dat.save random.summary.dat
+}
+
+Ctest "Save Pairwise Distances"
+Ctest "Load Pairwise Distances"
+
+EndTest
+exit 0
