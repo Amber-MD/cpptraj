fe46d9677d84c053c01cc8811eb452a8038d3402
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-12-22 10:05:00 -0700

    DRR - Cpptraj: Add integrate test

---
+++ test/Test_Integrate/RunTest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles int.in curve.dat
+
+INPUT="-i curve.in"
+
+cat > curve.in <<EOF
+readdata ../Test_Corr/corr.dat.save name Corr
+runanalysis integrate Corr out curve.dat name Int_Corr
+EOF
+RunCpptraj "Integration test."
+DoTest curve.dat.save curve.dat
+
+EndTest
+exit 0
