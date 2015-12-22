a1e05ce30842989ee09b50adaf3be67af491da57
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-12-22 10:15:29 -0700

    DRR - Cpptraj: Add hbond series output and lifetime analysis test

--- test/Test_Hbond/RunTest.sh
+++ test/Test_Hbond/RunTest.sh
@@ -3,22 +3,36 @@
 . ../MasterTest.sh
 
 # Clean
-CleanFiles hbond.in nhb.dat avghb.dat solvhb.dat solvavg.dat
+CleanFiles hbond.in nhb.dat avghb.dat solvhb.dat solvavg.dat \
+           nbb.dat hbavg.dat solutehb.agr lifehb.gnu avg.lifehb.gnu max.lifehb.gnu \
+           crv.lifehb.gnu
 
-# Solute test 
+INPUT="-i hbond.in"
 CheckNetcdf
+
+# Solute-solute, series output, lifetime analysis
 cat > hbond.in <<EOF
 noprogress
 parm ../DPDP.parm7
 trajin ../DPDP.nc
 hbond HB out nhb.dat avgout avghb.dat
+hbond BB out nbb.dat @N,H,C,O series avgout hbavg.dat printatomnum
+run
+write solutehb.agr BB[solutehb]
+runanalysis lifetime BB[solutehb] out lifehb.gnu window 10
 EOF
-INPUT="-i hbond.in"
 RunCpptraj "Solute Hbond test."
 DoTest nhb.dat.save nhb.dat 
 DoTest avghb.dat.save avghb.dat
+DoTest nbb.dat.save nbb.dat
+DoTest hbavg.dat.save hbavg.dat
+DoTest solutehb.agr.save solutehb.agr
+DoTest lifehb.gnu.save lifehb.gnu
+DoTest avg.lifehb.gnu.save avg.lifehb.gnu
+DoTest max.lifehb.gnu.save max.lifehb.gnu
+DoTest crv.lifehb.gnu.save crv.lifehb.gnu
 
-# Solvent test
+# Solute-Solvent test
 cat > hbond.in <<EOF
 parm ../tz2.ortho.parm7
 trajin ../tz2.ortho.nc
@@ -28,8 +42,6 @@ EOF
 RunCpptraj "Solvent Hbond test."
 DoTest solvhb.dat.save solvhb.dat
 DoTest solvavg.dat.save solvavg.dat
-
-CheckTest
 
 EndTest
 
