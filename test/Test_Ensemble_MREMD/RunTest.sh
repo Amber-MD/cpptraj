7b0db91832ba8d5023ca3593558fd295d5878ea9
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-12-22 09:31:40 -0700

    DRR - Cpptraj: Add MREMD hbond test

--- test/Test_Ensemble_MREMD/RunTest.sh
+++ test/Test_Ensemble_MREMD/RunTest.sh
@@ -2,7 +2,8 @@
 
 . ../MasterTest.sh
 
-CleanFiles mremd.in Strip.sorted.crd.? rmsd.dat rmsd.dat.? all.rmsd.dat
+CleanFiles mremd.in Strip.sorted.crd.? rmsd.dat rmsd.dat.? all.rmsd.dat \
+           nhbond.dat hbavg.dat
 
 INPUT="-i mremd.in"
 
@@ -21,24 +22,28 @@ EOF
 }
 
 # Test M-REMD traj sort, rms calc
-RmsTest() {
+ActionsTest() {
   cat > mremd.in <<EOF
 noprogress
 parm rGACC.nowat.parm7
 ensemble rGACC.nowat.001
+hbond HB :1-4 solventdonor :Na+ solventacceptor :Na+ \
+      out nhbond.dat avgout hbavg.dat
 rms R1-4NoH first :1-4&!@H= mass out rmsd.dat
 EOF
-  RunCpptraj "M-REMD Rms calc test."
+  RunCpptraj "M-REMD actions test."
   if [[ -z $DO_PARALLEL ]] ; then
     DoTest rmsd.dat.save rmsd.dat
   else
     cat rmsd.dat.? > all.rmsd.dat
     DoTest all.rmsd.dat.save all.rmsd.dat
   fi
+  DoTest nhbond.dat.save nhbond.dat
+  DoTest hbavg.dat.save hbavg.dat
 }
 
 TrajSort
-RmsTest
+ActionsTest
 
 EndTest
 exit 0
