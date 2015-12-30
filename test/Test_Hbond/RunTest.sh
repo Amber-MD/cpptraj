89b0f4f820d956f8271a34e3b4fdf4a9c1ed58b9
Daniel R. Roe <daniel.r.roe@gmail.com>
2015-12-30 12:46:36 -0700

    DRR - Cpptraj: Put hbond tests in functions for easier debugging

--- test/Test_Hbond/RunTest.sh
+++ test/Test_Hbond/RunTest.sh
@@ -11,7 +11,8 @@ INPUT="-i hbond.in"
 CheckNetcdf
 
 # Solute-solute, series output, lifetime analysis
-cat > hbond.in <<EOF
+TestUU() {
+  cat > hbond.in <<EOF
 noprogress
 parm ../DPDP.parm7
 trajin ../DPDP.nc
@@ -21,50 +22,61 @@ run
 write solutehb.agr BB[solutehb]
 runanalysis lifetime BB[solutehb] out lifehb.gnu window 10
 EOF
-RunCpptraj "Solute Hbond test."
-DoTest nhb.dat.save nhb.dat 
-DoTest avghb.dat.save avghb.dat
-DoTest nbb.dat.save nbb.dat
-DoTest hbavg.dat.save hbavg.dat
-DoTest solutehb.agr.save solutehb.agr
-DoTest lifehb.gnu.save lifehb.gnu
-DoTest avg.lifehb.gnu.save avg.lifehb.gnu
-DoTest max.lifehb.gnu.save max.lifehb.gnu
-DoTest crv.lifehb.gnu.save crv.lifehb.gnu
+  RunCpptraj "Solute Hbond test."
+  DoTest nhb.dat.save nhb.dat 
+  DoTest avghb.dat.save avghb.dat
+  DoTest nbb.dat.save nbb.dat
+  DoTest hbavg.dat.save hbavg.dat
+  DoTest solutehb.agr.save solutehb.agr
+  DoTest lifehb.gnu.save lifehb.gnu
+  DoTest avg.lifehb.gnu.save avg.lifehb.gnu
+  DoTest max.lifehb.gnu.save max.lifehb.gnu
+  DoTest crv.lifehb.gnu.save crv.lifehb.gnu
+}
 
 # Solute-Solvent test
-cat > hbond.in <<EOF
+TestUV() {
+  cat > hbond.in <<EOF
 parm ../tz2.ortho.parm7
 trajin ../tz2.ortho.nc
 hbond hb out solvhb.dat avgout solvavg.dat :1-13 solventacceptor :WAT@O solventdonor :WAT \
       solvout solvavg.dat bridgeout solvavg.dat
 EOF
-RunCpptraj "Solvent Hbond test."
-DoTest solvhb.dat.save solvhb.dat
-DoTest solvavg.dat.save solvavg.dat
+  RunCpptraj "Solvent Hbond test."
+  DoTest solvhb.dat.save solvhb.dat
+  DoTest solvavg.dat.save solvavg.dat
+}
 
 # Imaged hbond test
-cat > hbond.in <<EOF
+TestImage() {
+  cat > hbond.in <<EOF
 parm strip.4lztSc_nowat.parm7
 trajin strip.4lztSc.rst7
 hbond donormask :3@ND2 acceptormask :1@O avgout hb1.dat image
 hbond donormask :2@ND2 acceptormask :4@O avgout hb2.dat image
 EOF
-RunCpptraj "Hbond with imaging"
-DoTest hb1.dat.save hb1.dat
-DoTest hb2.dat.save hb2.dat
+  RunCpptraj "Hbond with imaging"
+  DoTest hb1.dat.save hb1.dat
+  DoTest hb2.dat.save hb2.dat
+}
 
 # Nointramol test
-cat > hbond.in <<EOF
+TestNointramol() {
+  cat > hbond.in <<EOF
 parm ../FtuFabI.NAD.TCL.parm7
 trajin ../FtuFabI.NAD.TCL.nc
 hbond IntraMol out hbond.mol.dat
 hbond NoIntraMol out hbond.mol.dat nointramol avgout mol.avg.dat
 EOF
-RunCpptraj "Hbond, no intramolecular hydrogen bonds test."
-DoTest hbond.mol.dat.save hbond.mol.dat
-DoTest mol.avg.dat.save mol.avg.dat
+  RunCpptraj "Hbond, no intramolecular hydrogen bonds test."
+  DoTest hbond.mol.dat.save hbond.mol.dat
+  DoTest mol.avg.dat.save mol.avg.dat
+}
 
+TestUU
+TestUV
+TestImage
+TestNointramol
 EndTest
 
 exit 0
