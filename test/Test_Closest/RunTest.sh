4e82753c8f911da71eaa8bc2f6da76569a42d7a2
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-04 11:03:07 -0700

    DRR - Cpptraj: Use 'multi' keyword for pdb output so tests can be run in parallel

--- test/Test_Closest/RunTest.sh
+++ test/Test_Closest/RunTest.sh
@@ -3,36 +3,34 @@
 . ../MasterTest.sh
 
 # Clean
-CleanFiles closest.in first.Closest.pdb closestmols.dat \
-           closest.tz2.truncoct.parm7 all.Closest.pdb closest10.center2_4.crd
+CleanFiles closest.in first.Closest.pdb.1 closestmols.dat \
+           closest.tz2.truncoct.parm7 all.Closest.pdb.1 closest10.center2_4.crd
 
 INPUT="-i closest.in"
 CheckNetcdf
-if [[ -z $DO_PARALLEL ]] ; then
-  # Test 1 - Closest, first solvent atom only
-  cat > closest.in <<EOF
+# Test 1 - Closest, first solvent atom only
+cat > closest.in <<EOF
 noprogress
 parm ../tz2.truncoct.parm7
 trajin ../tz2.truncoct.nc 1 1
 closest 10 :1-13 first closestout closestmols.dat name CL outprefix closest
-trajout first.Closest.pdb pdb nobox 
+trajout first.Closest.pdb pdb nobox multi
 EOF
-  RunCpptraj "Closest command test using first solvent atom."
-  DoTest first.Closest.pdb.save first.Closest.pdb
-  DoTest closestmols.dat.save closestmols.dat
-  # Tell diff to ignore the VERSION line
-  DoTest closest.tz2.truncoct.parm7.save closest.tz2.truncoct.parm7 -I %VERSION
+RunCpptraj "Closest command test using first solvent atom."
+DoTest first.Closest.pdb.save first.Closest.pdb.1
+DoTest closestmols.dat.save closestmols.dat
+# Tell diff to ignore the VERSION line
+DoTest closest.tz2.truncoct.parm7.save closest.tz2.truncoct.parm7 -I %VERSION
 
-  # Test 2 - Closest, all solvent atoms
-  cat > closest.in <<EOF
+# Test 2 - Closest, all solvent atoms
+cat > closest.in <<EOF
 parm ../tz2.truncoct.parm7
 trajin ../tz2.truncoct.nc 1 1
 closest 10 :1-13
-trajout all.Closest.pdb pdb nobox
+trajout all.Closest.pdb pdb nobox multi
 EOF
-  RunCpptraj "Closest command test using all solvent atoms."
-  DoTest all.Closest.pdb.save all.Closest.pdb 
-fi
+RunCpptraj "Closest command test using all solvent atoms."
+DoTest all.Closest.pdb.save all.Closest.pdb.1
 
 # Test 3 - Closest atoms to mask center
 cat > closest.in <<EOF
