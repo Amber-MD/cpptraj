181bb2bc6d7237e9031a3adc1526561373077088
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-07 10:28:08 -0700

    DRR - Cpptraj: Add data set output to principal test

--- test/Test_Principal/RunTest.sh
+++ test/Test_Principal/RunTest.sh
@@ -3,7 +3,7 @@
 # Test of principal 
 . ../MasterTest.sh
 
-CleanFiles principal.in Ctest.pdb principal.dat Ctest.crd
+CleanFiles principal.in Ctest.pdb principal.dat Ctest.crd eigen.dat
 
 TOP="../Test_IRED/1IEE_A_prot.prmtop"
 INPUT="principal.in"
@@ -11,12 +11,15 @@ INPUT="principal.in"
 cat > principal.in <<EOF
 noprogress
 trajin ../Test_IRED/1IEE_A_test.mdcrd 1 10
-principal * dorotation mass out principal.dat
+principal * dorotation mass out principal.dat name All
 trajout Ctest.crd
+run
+writedata eigen.dat All[eval] All[evec]
 EOF
 RunCpptraj "Principal Test"
 DoTest Ctest.crd.save Ctest.crd
 DoTest principal.dat.save principal.dat
+DoTest eigen.dat eigen.dat.save
 
 EndTest
 
