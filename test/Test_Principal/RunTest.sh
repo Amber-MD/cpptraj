019ee5b8455e07c468da3a3b63c9a9c3c663e9e0
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-07 11:19:46 -0700

    DRR - Cpptraj: Modify test for parallel ('principal out' not supported).

--- test/Test_Principal/RunTest.sh
+++ test/Test_Principal/RunTest.sh
@@ -8,7 +8,8 @@ CleanFiles principal.in Ctest.pdb principal.dat Ctest.crd eigen.dat
 TOP="../Test_IRED/1IEE_A_prot.prmtop"
 INPUT="principal.in"
 
-cat > principal.in <<EOF
+if [[ -z $DO_PARALLEL ]] ; then
+  cat > principal.in <<EOF
 noprogress
 trajin ../Test_IRED/1IEE_A_test.mdcrd 1 10
 principal * dorotation mass out principal.dat name All
@@ -16,10 +17,24 @@ trajout Ctest.crd
 run
 writedata eigen.dat All[eval] All[evec]
 EOF
-RunCpptraj "Principal Test"
-DoTest Ctest.crd.save Ctest.crd
-DoTest principal.dat.save principal.dat
-DoTest eigen.dat eigen.dat.save
+  RunCpptraj "Principal Test"
+  DoTest Ctest.crd.save Ctest.crd
+  DoTest principal.dat.save principal.dat
+  DoTest eigen.dat eigen.dat.save
+else
+  # 'out' not supported in parallel
+  cat > principal.in <<EOF
+noprogress
+trajin ../Test_IRED/1IEE_A_test.mdcrd 1 10
+principal * dorotation mass name All
+trajout Ctest.crd
+run
+writedata eigen.dat All[eval] All[evec]
+EOF
+  RunCpptraj "Principal Test"
+  DoTest Ctest.crd.save Ctest.crd
+  DoTest eigen.dat eigen.dat.save
+fi
 
 EndTest
 
