af05917aac4d52892ccd1780c16b7ccfabacc97f
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-07 09:51:07 -0700

    DRR - Cpptraj: Use Amber ASCII crd instead of pdb for principal test; uses less space and amenable to parallel testing.

--- test/Test_Principal/RunTest.sh
+++ test/Test_Principal/RunTest.sh
@@ -3,9 +3,7 @@
 # Test of principal 
 . ../MasterTest.sh
 
-CleanFiles principal.in Ctest.pdb principal.dat
-
-#CheckPtrajAnalyze
+CleanFiles principal.in Ctest.pdb principal.dat Ctest.crd
 
 TOP="../Test_IRED/1IEE_A_prot.prmtop"
 INPUT="principal.in"
@@ -14,13 +12,12 @@ cat > principal.in <<EOF
 noprogress
 trajin ../Test_IRED/1IEE_A_test.mdcrd 1 10
 principal * dorotation mass out principal.dat
-trajout Ctest.pdb pdb
+trajout Ctest.crd
 EOF
 RunCpptraj "Principal Test"
-DoTest Ctest.pdb.save Ctest.pdb
+DoTest Ctest.crd.save Ctest.crd
 DoTest principal.dat.save principal.dat
 
-CheckTest
 EndTest
 
 exit 0
