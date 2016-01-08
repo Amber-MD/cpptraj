b2552e8b7768d70001272db900bf707d328cebe1
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-08 14:31:52 -0700

    DRR - Cpptraj: Add another test suitable for atommap in parallel, disable the one that is not when DO_PARALLEL set.

--- test/Test_AtomMap/RunTest.sh
+++ test/Test_AtomMap/RunTest.sh
@@ -3,10 +3,13 @@
 . ../MasterTest.sh
 
 # Clean
-CleanFiles atommap.in initial.mol2 atommap.dat reordered.pdb reordered.mol2 fit.mol2 rmsd.dat 
+CleanFiles atommap.in initial.mol2 atommap.dat reordered.pdb reordered.mol2 \
+           fit.mol2 rmsd.dat map.chm_to_amb.dat mapped.pdb.? rmsout.dat
 
-# Test 1
-cat > atommap.in <<EOF
+INPUT="-i atommap.in"
+if [[ -z $DO_PARALLEL ]] ; then
+  # Test 1
+  cat > atommap.in <<EOF
 noprogress
 parm xtallig.mol2
 reference xtallig.mol2
@@ -25,16 +28,16 @@ outtraj reordered.mol2 mol2
 rms 1g9v refindex 1 out rmsd.dat
 trajout fit.mol2 mol2
 EOF
-INPUT="-i atommap.in"
-RunCpptraj "AtomMap Test"
+  RunCpptraj "AtomMap Test"
+  DoTest initial.mol2.save initial.mol2
+  DoTest atommap.dat.save atommap.dat
+  DoTest reordered.pdb.save reordered.pdb
+  DoTest reordered.mol2.save reordered.mol2
+  DoTest rmsd.dat.save rmsd.dat
+  DoTest fit.mol2.save fit.mol2
+fi
 
-DoTest initial.mol2.save initial.mol2
-DoTest atommap.dat.save atommap.dat
-DoTest reordered.pdb.save reordered.pdb
-DoTest reordered.mol2.save reordered.mol2
-DoTest rmsd.dat.save rmsd.dat
-DoTest fit.mol2.save fit.mol2
-
+# Test 2
 cat > atommap.in <<EOF
 parm xtallig.mol2
 reference xtallig.mol2
@@ -48,6 +51,19 @@ trajin xtallig.mol2
 EOF
 RunCpptraj "AtomMap with 'rmsfit'"
 DoTest rmsd.dat.save rmsout.dat
+
+# Test 3
+cat > atommap.in <<EOF
+parm cg-amb.topo
+reference cg-amb.crds
+parm cg-chm.topo
+reference cg-chm.crds parmindex 1
+atommap cg-amb.crds cg-chm.crds mapout map.chm_to_amb.dat
+trajin cg.crd
+trajout mapped.pdb pdb multi
+EOF
+RunCpptraj "Atom map charmm->amber atom order"
+DoTest map.chm_to_amb.dat.save map.chm_to_amb.dat
 
 EndTest
 
