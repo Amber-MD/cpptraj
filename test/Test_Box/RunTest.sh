fa9086414b0b4147432b1c9d87cac109008fd73d
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-13 13:55:20 -0700

    DRR - Cpptraj: Modify box test so it processes more frames and writes out stripped coordinates. This both saves some space and lets the test work in parallel.

--- test/Test_Box/RunTest.sh
+++ test/Test_Box/RunTest.sh
@@ -2,38 +2,33 @@
 
 . ../MasterTest.sh
 
-CleanFiles box.in addbox.rst7 modX.rst7
+CleanFiles box.in addbox.rst7 addbox.rst7.? addbox.rst7.10 \
+                  modX.rst7   modX.rst7.?   modX.rst7.10
 
 INPUT="-i box.in"
 cat > box.in <<EOF
 parm ../tz2.parm7
-trajin ../tz2.rst7
+trajin ../tz2.nc 1 10 
+strip !(:1)
 box x 42.428  y 42.428  z 42.428 alpha 109.471 beta 109.471 gamma 109.471
-trajout addbox.rst7 restart novelocity time0 1.0
+trajout addbox.rst7
 go
 EOF
 RunCpptraj "Box Test (Add box info)"
-DoTest addbox.rst7.save addbox.rst7
+DoTest addbox.rst7.1.save addbox.rst7.1
+DoTest addbox.rst7.10.save addbox.rst7.10
 
 cat > box.in <<EOF
 parm ../tz2.truncoct.parm7
-trajin ../tz2.truncoct.nc 1 1
+trajin ../tz2.truncoct.nc 1 10
+strip !(:1)
 box x 45.0
-trajout modX.rst7 restart time0 1.0
+trajout modX.rst7
 go
 EOF
 RunCpptraj "Box test (Modify box length)"
-DoTest modX.rst7.save modX.rst7
-
-#cat > box.in <<EOF
-#parm ../tz2.parm7
-#reference ../tz2.rst7
-#trajin ../tz2.pdb 
-#box x 42.428  y 42.428  z 42.428 alpha 90.0 beta 90.0 gamma 90.90
-##image
-#unwrap reference
-#EOF
-#RunCpptraj "Box test with image"
+DoTest modX.rst7.1.save modX.rst7.1
+DoTest modX.rst7.10.save modX.rst7.10
 
 EndTest
 
