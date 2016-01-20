548e7a0e1fad55ad342b17d35e0143669e25d7c9
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-20 15:10:57 -0700

    DRR - Cpptraj: Fix for multiple threads

--- test/Test_Image/RunTest.sh
+++ test/Test_Image/RunTest.sh
@@ -5,8 +5,9 @@
 # Clean
 CleanFiles image.in ortho.dat nonortho.dat image.crd image2.crd image3.crd image4.crd
 
-# Test 1 - orthorhombic imaged distance
+INPUT="-i image.in"
 CheckNetcdf
+# Test 1 - orthorhombic imaged distance
 cat > image.in <<EOF
 noprogress
 parm ../tz2.ortho.parm7
@@ -15,8 +16,8 @@ distance image :507@O :499@O out ortho.dat
 distance noimage :507@O :499@O out ortho.dat noimage
 go
 EOF
-INPUT="-i image.in"
 RunCpptraj "Orthorhombic imaged distance test."
+DoTest ortho.dat.save ortho.dat
 
 # Test 2 - nonorthorhombic imaged distance
 cat > image.in <<EOF
@@ -27,15 +28,13 @@ distance image :1507@O :1676@O out nonortho.dat
 distance noimage :1507@O :1676@O out nonortho.dat noimage
 go
 EOF
-INPUT="-i image.in"
 RunCpptraj "Non-orthorhombic imaged distance test."
-
-DoTest ortho.dat.save ortho.dat
 DoTest nonortho.dat.save nonortho.dat
-CheckTest
 
-# Test 3 - Orthorhombic coordinate imaging 
-cat > image.in <<EOF
+MaxThreads 2 "Coordinate imaging tests."
+if [[ $? -eq 0 ]] ; then
+  # Test 3 - Orthorhombic coordinate imaging 
+  cat > image.in <<EOF
 noprogress
 parm ../tz2.ortho.parm7
 trajin ../tz2.ortho.nc 1 2
@@ -44,11 +43,11 @@ image origin center
 trajout image.crd 
 go
 EOF
-INPUT="-i image.in"
-RunCpptraj "Orthorhombic coordinate imaging test."
+  RunCpptraj "Orthorhombic coordinate imaging test."
+  DoTest image.crd.save image.crd
 
-# Test 4 - Nonorthorhombic coordinate imaging
-cat > image.in <<EOF
+  # Test 4 - Nonorthorhombic coordinate imaging
+  cat > image.in <<EOF
 noprogress
 parm ../tz2.truncoct.parm7
 trajin ../tz2.truncoct.nc 1 2
@@ -57,11 +56,11 @@ image center triclinic
 trajout image2.crd
 go
 EOF
-INPUT="-i image.in"
-RunCpptraj "Nonorthorhombic coordinate imaging test."
+  RunCpptraj "Nonorthorhombic coordinate imaging test."
+  DoTest image2.crd.save image2.crd
 
-# Test - Nonorthorhombic coordinate imaging with familiar
-cat > image.in <<EOF
+  # Test - Nonorthorhombic coordinate imaging with familiar
+  cat > image.in <<EOF
 noprogress
 parm ../tz2.truncoct.parm7
 trajin ../tz2.truncoct.nc 1 2
@@ -70,10 +69,10 @@ image origin center familiar
 trajout image3.crd
 go
 EOF
-INPUT="-i image.in"
-RunCpptraj "Nonorthorhombic coordinate imaging test with familiar."
+  RunCpptraj "Nonorthorhombic coordinate imaging test with familiar."
+  DoTest image3.crd.save image3.crd
 
-# Test - Nonorthorhombic coordinate imaging test with familiar and COM
+  # Test - Nonorthorhombic coordinate imaging test with familiar and COM
 cat > image.in <<EOF
 noprogress
 parm ../tz2.truncoct.parm7
@@ -83,15 +82,9 @@ image center familiar com :6
 trajout image4.crd
 go
 EOF
-INPUT="-i image.in"
-RunCpptraj "Nonorthorhombic coordinate imaging test with familiar and COM."
-
-DoTest image.crd.save image.crd
-DoTest image2.crd.save image2.crd
-DoTest image3.crd.save image3.crd
-DoTest image4.crd.save image4.crd
-CheckTest
-
+  RunCpptraj "Nonorthorhombic coordinate imaging test with familiar and COM."
+  DoTest image4.crd.save image4.crd
+fi
 EndTest
 
 exit 0
