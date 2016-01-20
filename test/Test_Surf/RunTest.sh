d9f0665b3dceffdd90be432b440f70ca495d3ae0
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-20 15:25:20 -0700

    DRR - Cpptraj: Fix CleanFiles rule and for # threads

--- test/Test_Surf/RunTest.sh
+++ test/Test_Surf/RunTest.sh
@@ -3,20 +3,19 @@
 . ../MasterTest.sh
 
 # Clean
-CleanFiles cpptraj.in surf.dat tsurf.dat
+CleanFiles cpptraj.in surf.dat tsurf.dat ral.surf.dat
 
-# Test 1
 CheckNetcdf
+INPUT="-i cpptraj.in"
+# Test 1
 cat > cpptraj.in <<EOF
 noprogress
 trajin ../DPDP.nc
 surf All out surf.dat
 EOF
-INPUT="-i cpptraj.in"
 TOP="../DPDP.parm7"
 RunCpptraj "Surface calculation test."
 DoTest surf.dat.save surf.dat
-CheckTest
 
 # Test 2
 cat > cpptraj.in <<EOF
@@ -24,23 +23,22 @@ noprogress
 trajin ../DPDP.nc
 surf R1-12 out tsurf.dat :1-12
 EOF
-INPUT="-i cpptraj.in"
 TOP="../DPDP.parm7"
 RunCpptraj "Partial Surface calculation test."
 DoTest tsurf.dat.save tsurf.dat
-CheckTest
 
 # Test 3
-INPUT="-i cpptraj.in"
-TOP=""
-cat > cpptraj.in <<EOF
+MaxThreads 1 "LCPO test with GAFF atom types."
+if [[ $? -eq 0 ]] ; then
+  TOP=""
+  cat > cpptraj.in <<EOF
 parm RAL.sol.top
 trajin RAL.crd
 surf S0 out ral.surf.dat
 EOF
-RunCpptraj "LCPO test with GAFF atom types."
-DoTest ral.surf.dat.save ral.surf.dat
-
+  RunCpptraj "LCPO test with GAFF atom types."
+  DoTest ral.surf.dat.save ral.surf.dat
+fi
 
 EndTest
 
