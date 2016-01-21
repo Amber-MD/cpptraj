42383938f0f518a2073ee1fdc6c43193f6a8b5c4
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-21 10:40:13 -0700

    DRR - Cpptraj: Add test for radial center2, intrdf, rawrdf

--- test/Test_Radial/RunTest.sh
+++ test/Test_Radial/RunTest.sh
@@ -2,21 +2,24 @@
 
 . ../MasterTest.sh
 
-CleanFiles radial.in Radial.agr cRadial.agr
+CleanFiles radial.in Radial.agr cRadial.agr WatO-Trp4.agr WatO-Trp4.raw.agr
 CheckNetcdf
 cat > radial.in <<EOF
 parm ../tz2.truncoct.parm7
 trajin ../tz2.truncoct.nc 1 10
 
-radial Radial.agr 0.5 10.0 :5@CD :WAT@O 
-radial cRadial.agr 0.5 10.0 :5 :WAT@O center1
+radial Radial.agr    0.5 10.0 :5@CD  :WAT@O
+radial cRadial.agr   0.5 10.0 :5     :WAT@O center1
+radial WatO-Trp4.agr 0.5 10.0 :WAT@O :4&!@C,O,CA,HA,N,H center2 \
+       intrdf WatO-Trp4.raw.agr rawrdf WatO-Trp4.raw.agr
 EOF
 
 INPUT="-i radial.in"
 RunCpptraj "Radial Test"
 DoTest Radial.agr.save Radial.agr
 DoTest cRadial.agr.save cRadial.agr
-CheckTest
+DoTest WatO-Trp4.agr.save WatO-Trp4.agr
+DoTest WatO-Trp4.raw.agr.save WatO-Trp4.raw.agr
 
 EndTest
 
