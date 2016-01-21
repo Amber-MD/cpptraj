08417a0d59151a65d29130df2a14284aeab281ab
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-01-21 09:33:42 -0700

    DRR - Cpptraj: Use EnsembleNavigator as an analogue of DataSet_Coords_TRJ in RunParaEnsemble(), primarily to enable preloading.

--- src/CpptrajState.cpp
+++ src/CpptrajState.cpp
@@ -8,6 +8,7 @@
 #ifdef MPI
 # include "Parallel.h"
 # include "DataSet_Coords_TRJ.h"
+# include "EnsembleNavigator.h"
 # ifdef TIMER
 #   include "EnsembleIn.h"
 # endif
@@ -665,54 +666,17 @@ int CpptrajState::RunParaEnsemble() {
   FramePtrArray SortedFrames( 2 );
   // In parallel, each ensemble member is responsible for only 1 frame.
   FramePtrArray CurrentFrames( 1 );
-  // TODO Check ensemble size?
   // List state
   ListState();
   // Currently only let this happen if all trajectories share same topology.
-  Topology* FirstParm = 0;
-  TrajFrameIndex IDX;
-  int err = 0;
-  CoordinateInfo ensCoordInfo;
-  for (TrajinList::ensemble_it ens = trajinList_.ensemble_begin();
-                               ens != trajinList_.ensemble_end(); ++ens)
-  {
-    if (FirstParm == 0) {
-      FirstParm = (*ens)->Traj().Parm();
-      ensCoordInfo = (*ens)->EnsembleCoordInfo();
-    } else {
-      if (FirstParm != (*ens)->Traj().Parm()) {
-        err = 1;
-        break;
-      }
-      // Want to ensure frame is properly allocated for all ensembles, so
-      // e.g. if one ensemble has velocity info allocate for all.
-      if (ensCoordInfo.HasVel() != (*ens)->EnsembleCoordInfo().HasVel())
-        ensCoordInfo.SetVelocity( true );
-      if (ensCoordInfo.HasForce() != (*ens)->EnsembleCoordInfo().HasForce())
-        ensCoordInfo.SetForce( true );
-      // Sanity check
-      if (ensCoordInfo.ReplicaDimensions().Ndims() !=
-          (*ens)->EnsembleCoordInfo().ReplicaDimensions().Ndims())
-      {
-        mprinterr("Internal Error: Replica dimensions changed.\n");
-        err = 1;
-        break;
-      }
-    }
-    IDX.AddTraj( (*ens)->Traj().Counter().TotalReadFrames(),
-                 (*ens)->Traj().Counter().Start(),
-                 (*ens)->Traj().Counter().Offset() );
-  }
-  if (Parallel::World().CheckError( err )) {
-    mprinterr("Error: Trajectory parallelized 'ensemble' currently requires all\n"
-              "Error:   ensembles use the same topology file.\n");
-    return 1;
-  }
+  EnsembleNavigator NAV;
+  int err = NAV.AddEnsembles(trajinList_.ensemble_begin(), trajinList_.ensemble_end());
+  if (Parallel::World().CheckError( err )) return 1;
 
   // Divide frames among threads
   int my_start, my_stop, my_frames;
   std::vector<int> rank_frames = DivideFramesAmongThreads(my_start, my_stop, my_frames,
-                                                          IDX.MaxFrames(), TrajComm);
+                                                          NAV.IDX().MaxFrames(), TrajComm);
   // Ensure at least 1 frame per thread, otherwise some ranks could cause hangups.
   if (my_frames > 0)
     err = 0;
@@ -728,11 +692,11 @@ int CpptrajState::RunParaEnsemble() {
   DSL_.SetNewSetsNeedSync( true );
 
   // ----- SETUP PHASE ---------------------------
-  FirstParm->SetBoxFromTraj( ensCoordInfo.TrajBox() ); // FIXME necessary?
-  ActionSetup currentParm( FirstParm, ensCoordInfo, IDX.MaxFrames() );
+  NAV.FirstParm()->SetBoxFromTraj( NAV.EnsCoordInfo().TrajBox() ); // FIXME necessary?
+  ActionSetup currentParm( NAV.FirstParm(), NAV.EnsCoordInfo(), NAV.IDX().MaxFrames() );
   err = actionList_.SetupActions( currentParm, exitOnError_ );
   if (Parallel::World().CheckError( err )) {
-    mprinterr("Error: Could not set up actions for '%s'\n", FirstParm->c_str());
+    mprinterr("Error: Could not set up actions for '%s'\n", NAV.FirstParm()->c_str());
     return 1;
   }
 
@@ -751,32 +715,23 @@ int CpptrajState::RunParaEnsemble() {
   ProgressBar progress;
   if (showProgress_)
     progress.SetupProgress( my_frames );
-  FrameEnsemble.SetupFrames( FirstParm->Atoms(), ensCoordInfo );
+  FrameEnsemble.SetupFrames( NAV.FirstParm()->Atoms(), NAV.EnsCoordInfo() );
   int actionSet = 0; // Internal data frame
-  EnsembleIn* currentEns = 0;
   err = 0;
   for (int set = my_start; set != my_stop; set++, actionSet++) {
-    int internalIdx = IDX.FindIndex( set );
-    // If desired ensemble is different than current, open desired ensemble.
-    if (IDX.TrajHasChanged()) {
-      if (currentEns != 0) currentEns->EndEnsemble();
-      currentEns = trajinList_.EnsPtr( IDX.CurrentTrajNum() );
-      // NOTE: Need to check for reallocation? TODO better error check
-      if (currentEns->BeginEnsemble()) {
-        rprinterr("Error: Could not open ensemble %i '%s'\n", IDX.CurrentTrajNum(),
-                  currentEns->Traj().Filename().full());
-        err = 1;
-        break;
-      }
+    // Read the ensemble
+    err = NAV.GetEnsemble( set, FrameEnsemble, SortedFrames ); // TODO better error check
+    if (err != 0) { 
+      rprinterr("Error: Could not open ensemble %i '%s'\n", NAV.IDX().CurrentTrajNum(),
+                NAV.CurrentEns()->Traj().Filename().full());
+      break;
     }
-    // Read the frame
-    currentEns->ReadEnsemble( internalIdx, FrameEnsemble, SortedFrames );
-    if (!currentEns->BadEnsemble()) {
+    if (!NAV.CurrentEns()->BadEnsemble()) {
       // Since Frame can be modified by actions, save original and use currentFrame
       ActionFrame currentFrame( SortedFrames[0], set );
       if ( currentFrame.Frm().CheckCoordsInvalid() )
         rprintf("Warning: Ensemble member %i frame %i may be corrupt.\n",
-                EnsComm.Rank(), currentEns->Traj().Counter().PreviousFrameNumber()+1);
+                EnsComm.Rank(), NAV.CurrentEns()->Traj().Counter().PreviousFrameNumber()+1);
       // Perform Actions on Frame
       bool suppress_output = actionList_.DoActions(actionSet, currentFrame);
       if (!suppress_output) {
@@ -788,13 +743,13 @@ int CpptrajState::RunParaEnsemble() {
         }
       }
     } else {
-      rprinterr("Error: Could not read frame %i for ensemble.\n", internalIdx + 1);
+      rprinterr("Error: Could not read set %i for ensemble.\n", set + 1);
     }
     if (showProgress_) progress.Update( actionSet );
   } // END loop over frames
   frames_time.Stop();
   // Close the trajectory file
-  currentEns->EndEnsemble();
+  NAV.CurrentEns()->EndEnsemble();
   // Collect FPS stats from each rank. 
   std::vector<double> darray( TrajComm.Size() );
   double rank_fps = (double)actionSet / frames_time.Total();
@@ -802,7 +757,7 @@ int CpptrajState::RunParaEnsemble() {
   for (int rank = 0; rank < TrajComm.Size(); rank++)
     mprintf("TIME: Rank %i throughput= %.4f frames / second.\n", rank, darray[rank]);
   mprintf("TIME: Avg. throughput= %.4f frames / second.\n",
-          (double)IDX.MaxFrames() / frames_time.Total());
+          (double)NAV.IDX().MaxFrames() / frames_time.Total());
 //# ifdef TIMER
 //  Ensemble::TimingData(trajin_time.Total());
 //# endif
@@ -814,7 +769,7 @@ int CpptrajState::RunParaEnsemble() {
   // Sync Actions to master thread
   actionList_.SyncActions();
   // Sync data sets to master thread
-  if (DSL_.SynchronizeData( IDX.MaxFrames(), rank_frames, TrajComm )) return 1;
+  if (DSL_.SynchronizeData( NAV.IDX().MaxFrames(), rank_frames, TrajComm )) return 1;
   time_sync.Stop();
   time_sync.WriteTiming(1, "Data set/actions sync");
   mprintf("\nACTION OUTPUT:\n");
