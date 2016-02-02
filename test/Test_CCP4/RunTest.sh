44d8f6eb554153b3458a40429b436ab9aeb45b06
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-02-02 13:28:53 -0700

    DRR - Cpptraj: Add CCP4 write test

--- test/Test_CCP4/RunTest.sh
+++ test/Test_CCP4/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles data.in fav8.dx
+CleanFiles data.in fav8.dx fav8.ccp4
 
 INPUT="-i data.in"
 
@@ -10,10 +10,12 @@ cat > data.in <<EOF
 readdata fav8.guv.O.1.ccp4 name MyCcp4
 list dataset
 writedata fav8.dx MyCcp4
+writedata fav8.ccp4 MyCcp4 title "Amber 3D-RISM CCP4 map volumetric data. Format revision A."
 quit
 EOF
-RunCpptraj "CCP4 -> DX conversion test."
+RunCpptraj "CCP4 read/write and DX conversion test."
 DoTest fav8.dx.save fav8.dx
+DoTest fav8.guv.O.1.ccp4 fav8.ccp4
 
 EndTest
 exit 0
