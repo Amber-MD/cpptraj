d6595e97cd40ff647c7a20ccc7b12a01e9d2042f
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-02-05 13:05:06 -0700

    DRR - Cpptraj: Protect test in parallel

--- test/Test_Rotate/RunTest.sh
+++ test/Test_Rotate/RunTest.sh
@@ -18,15 +18,17 @@ EOF
 RunCpptraj "Rotation of coords from matrices"
 DoTest fromMatrices.crd.save fromMatrices.crd
 
-cat > rotate.in <<EOF
+MaxThreads 1 "Rotate with defined axis."
+if [[ $? -eq 0 ]] ; then
+  cat > rotate.in <<EOF
 parm ../FtuFabI.NAD.TCL.parm7
 trajin ../FtuFabI.NAD.TCL.nc 1 1
 rotate :270 axis0 :270@C1,C2,C3,C4,C5,C6 axis1 :270@C7,C8,C9,C10,C11,C12 90.0
 strip :1-269
 trajout TCS.rotated.mol2
 EOF
-RunCpptraj "Rotate with defined axis."
-DoTest TCS.rotated.mol2.save TCS.rotated.mol2
-
+  RunCpptraj "Rotate with defined axis."
+  DoTest TCS.rotated.mol2.save TCS.rotated.mol2
+fi
 EndTest
 exit 0
