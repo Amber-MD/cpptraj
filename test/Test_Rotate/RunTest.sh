4229b2c58364e20e394f6eec8fbc963da8578193
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-02-05 13:01:35 -0700

    DRR - Cpptraj: Add rotate with user-defined axis test.

--- test/Test_Rotate/RunTest.sh
+++ test/Test_Rotate/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles rotate.in fromMatrices.crd
+CleanFiles rotate.in fromMatrices.crd TCS.rotated.mol2
 
 INPUT="-i rotate.in"
 cat > rotate.in <<EOF
@@ -17,6 +17,16 @@ trajout fromMatrices.crd
 EOF
 RunCpptraj "Rotation of coords from matrices"
 DoTest fromMatrices.crd.save fromMatrices.crd
+
+cat > rotate.in <<EOF
+parm ../FtuFabI.NAD.TCL.parm7
+trajin ../FtuFabI.NAD.TCL.nc 1 1
+rotate :270 axis0 :270@C1,C2,C3,C4,C5,C6 axis1 :270@C7,C8,C9,C10,C11,C12 90.0
+strip :1-269
+trajout TCS.rotated.mol2
+EOF
+RunCpptraj "Rotate with defined axis."
+DoTest TCS.rotated.mol2.save TCS.rotated.mol2
 
 EndTest
 exit 0
