da9741136404b53064f886e97e010b044a03e384
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-02-08 14:14:50 -0700

    DRR - Cpptraj: add esander GB test

--- test/Test_Esander/RunTest.sh
+++ test/Test_Esander/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles ene.in Esander.dat force.nc
+CleanFiles ene.in Esander.dat force.nc Edpdp.dat
 
 INPUT="-i ene.in"
 
@@ -13,11 +13,22 @@ trajin ../tz2.truncoct.nc
 esander S out Esander.dat saveforces
 trajout force.nc
 EOF
-  RunCpptraj "Libsander test."
+  RunCpptraj "SANDER energy test, PME."
   DoTest Esander.dat.save Esander.dat
   NcTest force.nc.save force.nc
 }
 
+TestGB() {
+  cat > ene.in <<EOF
+parm ../DPDP.parm7
+trajin ../DPDP.nc
+esander DPDP out Edpdp.dat gbsa 1
+EOF
+  RunCpptraj "SANDER energy test, GB."
+  DoTest Edpdp.dat.save Edpdp.dat
+}
+
+TestGB
 TestPME
 
 EndTest
