79e072b2f207bfa5640c5b06420a33994ae30192
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-02-17 12:49:29 -0700

    DRR - Cpptraj: Update test output for format change

--- test/Test_Analyze_Modes/RunTest.sh
+++ test/Test_Analyze_Modes/RunTest.sh
@@ -14,8 +14,8 @@ TestFluct() {
   cat > modes.in <<EOF
 trajin ../tz2.nc
 matrix mwcovar name tz2 @CA
-analyze matrix tz2 name tz2modes vecs 20
-analyze modes fluct stack tz2modes out fluct.dat
+diagmatrix tz2 name tz2modes vecs 20
+modes fluct name tz2modes out fluct.dat setname Fluct prec 10.3
 EOF
   RunCpptraj "Modes analysis, RMS fluctuations"
   DoTest fluct.dat.save fluct.dat
@@ -28,7 +28,7 @@ TestDispl() {
   # sign flips, read in a previously generated set of modes.
   cat > modes.in <<EOF
 readdata tz2.evecs.dat name tz2modes
-analyze modes displ stack tz2modes out displ.dat
+analyze modes displ name tz2modes out displ.dat setname Displ prec 10.3
 EOF
   RunCpptraj "Modes analysis, displacements"
   DoTest displ.dat.save displ.dat
@@ -42,7 +42,7 @@ trajin ../tz2.nc
 matrix mwcovar name tz2
 analyze matrix tz2 name tz2modes vecs 20
 EOF
-  printf "analyze modes corr stack tz2modes out corr.dat" >> modes.in
+  printf "analyze modes corr name tz2modes out corr.dat" >> modes.in
   for ATOMN in 14 38 52 76 91 105 112 134 158 172 196 218 ; do
     ((ATOMH = ATOMN + 1))
     printf " maskp @$ATOMN @$ATOMH" >> modes.in
@@ -68,8 +68,8 @@ TestRMSIP() {
   cat > modes.in <<EOF
 readdata evecs.dat name evecs
 readdata evecs2.dat name evecs2
-modes name evecs name2 evecs2 rmsip beg 1 end 5 out rmsip.dat
-modes name evecs eigenval out eigenval.dat
+modes name evecs name2 evecs2 rmsip beg 1 end 5 out rmsip.dat setname RMSIP
+modes name evecs eigenval out eigenval.dat setname EV prec 12.6
 EOF
   RunCpptraj "Modes analysis, RMSIP and eigenvalue fraction test"
   DoTest rmsip.dat.save rmsip.dat
