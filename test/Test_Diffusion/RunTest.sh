3f376588dc0b9e63f408f8c7b08cfe973f94639a
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-03-03 16:56:17 -0700

    DRR - Cpptraj: Add non-orthorhombic image diffusion test and prevent all imaged diffusion tests from running with > 1 thread

--- test/Test_Diffusion/RunTest.sh
+++ test/Test_Diffusion/RunTest.sh
@@ -3,14 +3,14 @@
 . ../MasterTest.sh
 
 INPUT="diffusion.in"
-TOP=../tz2.ortho.parm7
 
 CleanFiles $INPUT diff_?.xmgr diff.dat diff.1.dat diff.2.dat diff.3.dat nw.dat \
-           WAT_O.agr DC.dat
+           WAT_O.agr DC.dat Nonortho.agr Nonortho.dat
 CheckNetcdf
 # Basic ptraj diffusion test
 # creates <prefix>_X.xmgr, X = {a,r,x,y,z}
 Test_diffusion_oldSyntax() {
+  TOP=../tz2.ortho.parm7
   cat > $INPUT <<EOF
 trajin ../tz2.ortho.nc
 diffusion :1-13 1.0 diff
@@ -24,18 +24,31 @@ EOF
 }
 
 Test_diffusion_newSyntax() {
-cat > $INPUT <<EOF
+  TOP=../tz2.ortho.parm7
+  cat > $INPUT <<EOF
 trajin ../tz2.ortho.nc
 diffusion :WAT@O out WAT_O.agr WAT_O diffout DC.dat
 EOF
   RunCpptraj "Diffusion test, new syntax."
-  DoTest WAT_O.agr.save WAT_O.agr -r 0.0007
-  DoTest DC.dat.save DC.dat -r 0.004
+  DoTest WAT_O.agr.save WAT_O.agr
+  DoTest DC.dat.save DC.dat
+}
+
+Test_diffusion_nonOrtho() {
+  TOP=../tz2.truncoct.parm7
+  cat > $INPUT <<EOF
+trajin ../tz2.truncoct.nc
+diffusion :WAT@O out Nonortho.agr WAT_O diffout Nonortho.dat
+EOF
+  RunCpptraj "Diffusion test, nonorthogonal box"
+  DoTest Nonortho.dat.save Nonortho.dat 
+  DoTest Nonortho.agr.save Nonortho.agr
 }
 
 # ------------------------------------------------------------------------------
 # STFC diffusion tests
 Test_stfc_diffusion() {
+  TOP=../tz2.ortho.parm7
   CMD="stfcdiffusion"
   # Basic test
   cat > $INPUT <<EOF
@@ -68,8 +81,12 @@ EOF
   DoTest nw.dat.save nw.dat
 }
 
-Test_diffusion_oldSyntax
-Test_diffusion_newSyntax
+MaxThreads 1 "Imaged diffusion tests"
+if [[ $? -eq 0 ]] ; then
+  Test_diffusion_oldSyntax
+  Test_diffusion_newSyntax
+  Test_diffusion_nonOrtho
+fi
 NotParallel "STFC diffusion tests."
 if [[ $? -eq 0 ]] ; then
   Test_stfc_diffusion
