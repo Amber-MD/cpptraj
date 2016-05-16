eace7aa0cdbb34bf44f1b456f25766375d36ff88
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-05-16 08:39:36 -0600

    DRR - Cpptraj: Add wavelet tests

---
+++ test/Test_Wavelet/RunTest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles wavelet.in wavelet.gnu cluster.gnu cluster.dat
+
+INPUT="-i wavelet.in"
+
+cat > wavelet.in <<EOF
+parm ../DPDP.parm7
+trajin ../DPDP.nc
+rms @C,CA,N first
+
+wavelet nb 10 s0 2 ds 0.25 type morlet correction 1.01 chival 0.25 \
+        :1-22 out wavelet.gnu usemap
+EOF
+RunCpptraj "Wavelet analysis test"
+DoTest wavelet.gnu.save wavelet.gnu
+
+cat > wavelet.in <<EOF
+parm ../DPDP.parm7
+trajin ../DPDP.nc
+rms @C,CA,N first
+
+wavelet nb 10 s0 2 ds 0.25 type morlet correction 1.01 chival 0.25 \
+        :1-22 name DPDP \
+        cluster clustermapout cluster.gnu clusterout cluster.dat \
+          minpoints 66 epsilon 10.0
+datafile cluster.gnu usemap palette kbvyw
+EOF
+RunCpptraj "Wavelet WAFEX test"
+DoTest cluster.gnu.save cluster.gnu
+DoTest cluster.dat.save cluster.dat
+
+EndTest
+exit 0
