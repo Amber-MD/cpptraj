786c12b0896dfa14d8f2b949afb967ca41cb2def
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-05-27 13:26:59 -0600

    DRR - Cpptraj: Add 'baseref' test

--- test/Test_NAstruct/RunTest.sh
+++ test/Test_NAstruct/RunTest.sh
@@ -3,7 +3,8 @@
 . ../MasterTest.sh
 
 # Clean
-CleanFiles nastruct.in BP.*.dat BPstep.*.dat bases.pdb baseaxes.pdb basepairaxes.pdb Helix.*.dat Param.pdb
+CleanFiles nastruct.in BP.*.dat BPstep.*.dat bases.pdb baseaxes.pdb basepairaxes.pdb \
+           Helix.*.dat Param.pdb
 
 # Test 2
 MaxThreads 3 "NAstruct tests"
@@ -13,12 +14,16 @@ if [[ $? -eq 0 ]] ; then
 parm ../adh026.3.pdb
 trajin ../adh026.3.pdb 
 nastruct naout adh026.dat
+nastruct naout baseref.dat baseref Atomic_G.pdb.nastruct
 nastruct naout groove.dat groovecalc 3dna
 EOF
   RunCpptraj "NAstruct command test."
   DoTest BP.adh026.dat.save BP.adh026.dat
   DoTest BPstep.adh026.dat.save BPstep.adh026.dat
   DoTest Helix.adh026.dat.save Helix.adh026.dat
+  DoTest BP.adh026.dat.save BP.baseref.dat
+  DoTest BPstep.adh026.dat.save BPstep.baseref.dat
+  DoTest Helix.adh026.dat.save Helix.baseref.dat
   DoTest BPstep.groove.dat.save BPstep.groove.dat
 fi
 EndTest
