8d40d6724c7d282bf65820e4cf898438ac78873b
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-06-01 14:33:18 -0600

    DRR - Cpptraj: Update code docs

--- src/ClusterDist.h
+++ src/ClusterDist.h
@@ -14,6 +14,8 @@ class Centroid {
   public:
     virtual ~Centroid() {}
     virtual Centroid* Copy() = 0;
+  // TODO: Should centroids remember number of frames that went into them?
+  //       This would make it so FrameOpCentroid wouldnt require extra arg.
 };
 /// Cluster centroid for generic DataSet.
 class Centroid_Num : public Centroid {
@@ -63,17 +65,25 @@ class Centroid_Coord : public Centroid {
 class ClusterDist {
   public:
     enum CentOpType { ADDFRAME=0, SUBTRACTFRAME };
+    /// Used to pass in absolute frame numbers for centroid calculations.
     typedef std::vector<int> Cframes;
     typedef Cframes::const_iterator Cframes_it;
     typedef std::vector<DataSet*> DsArray;
     virtual ~ClusterDist() {}
     virtual void PairwiseDist(DataSet_Cmatrix&, ClusterSieve::SievedFrames const&) = 0;
+    /// \return distance between given frames.
     virtual double FrameDist(int, int) = 0;
+    /// \return distance between given centroids.
     virtual double CentroidDist( Centroid*, Centroid* ) = 0;
+    /// \return distance between given frame and centroid.
     virtual double FrameCentroidDist(int, Centroid* ) = 0;
+    /// Calculate centroid from given frames.
     virtual void CalculateCentroid(Centroid*, Cframes const&) = 0;
+    /// \return new centroid from given frames.
     virtual Centroid* NewCentroid(Cframes const&) = 0;
+    /// \return copy of this ClusterDist
     virtual ClusterDist* Copy() = 0;
+    /// Update centroid by performing given operation between given frame and centroid.
     virtual void FrameOpCentroid(int, Centroid*, double, CentOpType) = 0;
   protected:
     typedef double (*DistCalc)(double,double);
