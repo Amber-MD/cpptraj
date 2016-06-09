c30b7088b73462b3a8f01a06c795111f52babcfd
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-06-09 11:36:26 -0600

    DRR - Cpptraj: Update test to ensure old cluster matrix file format is tested.

--- test/Test_Cluster/RunTest.sh
+++ test/Test_Cluster/RunTest.sh
@@ -5,7 +5,7 @@
 # Clean
 # NOTE: CpptrajPairDist name defined in Action_Clustering.cpp
 CleanFiles cluster.in cnumvtime.dat avg.summary.dat summary.dat CpptrajPairDist \
-           cpop.agr summary2.dat Cmatrix.nccmatrix summary3.dat
+           cpop.agr summary2.dat Cmatrix.nccmatrix Cmatrix.cmatrix summary3.dat
 
 CheckNetcdf
 INPUT="-i cluster.in"
@@ -14,8 +14,8 @@ cat > cluster.in <<EOF
 noprogress
 parm ../tz2.parm7
 trajin ../tz2.nc
-cluster C1 :2-10 clusters 3 epsilon 4.0 out cnumvtime.dat summary avg.summary.dat nofit savepairdist cpopvtime cpop.agr 
-cluster crd1 :2-10 clusters 3 epsilon 4.0 summary summary.dat complete nofit loadpairdist
+cluster C1 :2-10 clusters 3 epsilon 4.0 out cnumvtime.dat summary avg.summary.dat nofit savepairdist cpopvtime cpop.agr pairdist Cmatrix.cmatrix 
+cluster crd1 :2-10 clusters 3 epsilon 4.0 summary summary.dat complete nofit loadpairdist pairdist Cmatrix.cmatrix
 EOF
 RunCpptraj "Cluster command test, in-memory pairwise distances."
 DoTest cnumvtime.dat.save cnumvtime.dat
@@ -24,7 +24,7 @@ DoTest summary.dat.save summary.dat
 DoTest cpop.agr.save cpop.agr
 # Test loading PW distances from Cmatrix file
 cat > cluster.in <<EOF
-readdata CpptrajPairDist name PW
+readdata Cmatrix.cmatrix name PW
 parm ../tz2.parm7
 loadtraj ../tz2.nc name MyTraj
 runanalysis cluster crd1 crdset MyTraj :2-10 clusters 3 epsilon 4.0 summary summary2.dat \
