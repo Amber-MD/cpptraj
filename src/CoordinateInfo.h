222f366f79f294cae50534f221a3b3a3a27aea99
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2016-06-20 09:43:49 -0600

    DRR - Cpptraj: Fix up CoordinateInfo constructors.

--- src/CoordinateInfo.h
+++ src/CoordinateInfo.h
@@ -9,19 +9,24 @@
 class CoordinateInfo {
   public:
     /// CONSTRUCTOR
-    CoordinateInfo() : ensembleSize_(0), hasCrd_(true), hasVel_(false), hasTemp_(false), hasTime_(false), hasFrc_(false) {}
+    CoordinateInfo() : ensembleSize_(0), hasCrd_(true), hasVel_(false),
+                       hasTemp_(false), hasTime_(false), hasFrc_(false) {}
     /// CONSTRUCTOR - box, velocity, temperature, time
     CoordinateInfo(Box const& b, bool v, bool t, bool m) :
-      box_(b), ensembleSize_(0), hasVel_(v), hasTemp_(t), hasTime_(m), hasFrc_(false) {}
+      box_(b), ensembleSize_(0), hasCrd_(true), hasVel_(v),
+      hasTemp_(t), hasTime_(m), hasFrc_(false) {}
     /// CONSTRUCTOR - all except ensemble size
     CoordinateInfo(ReplicaDimArray const& r, Box const& b, bool v, bool t, bool m, bool f) :
-      remdDim_(r), box_(b), ensembleSize_(0), hasVel_(v), hasTemp_(t), hasTime_(m), hasFrc_(f) {}
+      remdDim_(r), box_(b), ensembleSize_(0), hasCrd_(true), hasVel_(v),
+      hasTemp_(t), hasTime_(m), hasFrc_(f) {}
     /// CONSTRUCTOR - All
     CoordinateInfo(int e, ReplicaDimArray const& r, Box const& b, bool v, bool t, bool m, bool f) :
-      remdDim_(r), box_(b), ensembleSize_(e), hasVel_(v), hasTemp_(t), hasTime_(m), hasFrc_(f) {}
+      remdDim_(r), box_(b), ensembleSize_(e), hasCrd_(true), hasVel_(v),
+      hasTemp_(t), hasTime_(m), hasFrc_(f) {}
     bool HasBox()              const { return box_.HasBox();            }
     const Box& TrajBox()       const { return box_;                     }
     int EnsembleSize()         const { return ensembleSize_;            }
+    bool HasCrd()              const { return hasCrd_;                  }
     bool HasVel()              const { return hasVel_;                  }
     bool HasTemp()             const { return hasTemp_;                 }
     bool HasTime()             const { return hasTime_;                 }
