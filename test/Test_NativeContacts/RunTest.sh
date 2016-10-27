b6c53f3946cc03ebb88287bbf8a7f94590a82ac3
Daniel R. Roe <daniel.r.roe@gmail.com>
2016-10-27 10:43:49 -0400

    DRR - Cpptraj: Do not run test for new options in parallel

--- test/Test_NativeContacts/RunTest.sh
+++ test/Test_NativeContacts/RunTest.sh
@@ -17,9 +17,6 @@ reference ../DPDP.nc 1
 nativecontacts name NC1 :1-12@CA out nc.hp1.ca.dat mindist maxdist map mapout cmap.gnu contactpdb nc1.pdb
 nativecontacts name NC2 :10-14@CA :17-21@CA out nc.hp2.ca.dat mindist maxdist writecontacts nc2.contacts.dat resout nc2.res.dat series seriesout NC2.series.dat
 nativecontacts name NC3 :1-21&!@H= byresidue out nc.all.res.dat mindist maxdist distance 3.0 reference map mapout resmap.gnu
-nativecontacts name NC4 :1-21@CA out nc4.dat savenonnative series \
-  reference seriesnnout nc4.nn.dat resout nc4.res.dat writecontacts nc4.contacts.dat \
-  nncontactpdb nc4.nn.pdb
 EOF
 RunCpptraj "NativeContacts test."
 DoTest nc.hp1.ca.dat.save nc.hp1.ca.dat
@@ -31,10 +28,24 @@ DoTest NC2.series.dat.save NC2.series.dat
 DoTest nc.all.res.dat.save nc.all.res.dat
 DoTest native.resmap.gnu.save native.resmap.gnu
 DoTest nonnative.resmap.gnu.save nonnative.resmap.gnu
-DoTest nc4.dat.save nc4.dat
-DoTest nc4.nn.dat.save nc4.nn.dat
-DoTest nc4.res.dat.save nc4.res.dat
-DoTest nc4.contacts.dat.save nc4.contacts.dat
-DoTest nc4.nn.pdb.save nc4.nn.pdb
+
+TITLE="NativeContacts test, save non-native contacts"
+NotParallel "$TITLE"
+if [ "$?" -eq 0 ] ; then
+  cat > nc.in <<EOF
+parm ../DPDP.parm7
+trajin ../DPDP.nc
+reference ../DPDP.nc 1
+nativecontacts name NC4 :1-21@CA out nc4.dat savenonnative series \
+  reference seriesnnout nc4.nn.dat resout nc4.res.dat writecontacts nc4.contacts.dat \
+  nncontactpdb nc4.nn.pdb
+EOF
+  RunCpptraj "$TITLE"
+  DoTest nc4.dat.save nc4.dat
+  DoTest nc4.nn.dat.save nc4.nn.dat
+  DoTest nc4.res.dat.save nc4.res.dat
+  DoTest nc4.contacts.dat.save nc4.contacts.dat
+  DoTest nc4.nn.pdb.save nc4.nn.pdb
+fi
 EndTest
 exit 0 
