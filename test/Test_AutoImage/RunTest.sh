6e4facda671defb9976ec0e4c7f09854cc554540
Daniel R. Roe <daniel.r.roe@gmail.com>
2016-12-05 10:48:59 -0500

    DRR - Cpptraj: Add test for specifying anchor mask region

--- test/Test_AutoImage/RunTest.sh
+++ test/Test_AutoImage/RunTest.sh
@@ -2,12 +2,12 @@
 
 . ../MasterTest.sh
 
-CleanFiles image.in reimage.mdcrd
+CleanFiles image.in reimage.mdcrd image.G3_3A.rst7
 CheckNetcdf
 TRAJ=ptraj.image.nc
 INPUT="-i image.in"
 MaxThreads 2 "AutoImage test"
-if [[ $? -eq 0 ]] ; then
+if [ "$?" -eq 0 ] ; then
   cat > image.in <<EOF
 parm ../dna30.parm7
 trajin split.duplex.nc
@@ -17,6 +17,20 @@ EOF
   RunCpptraj "AutoImage test"
   DoTest reimage.mdcrd.save reimage.mdcrd
 fi
+
+TESTNAME="AutoImage test, use anchor mask"
+MaxThreads 1 "$TESTNAME"
+if [ "$?" -eq 0 ] ; then
+  cat > image.in <<EOF
+parm nowat.G3_3A.parm7
+trajin G3_3A.rst7
+autoimage anchor :96 origin
+trajout image.G3_3A.rst7
+EOF
+  RunCpptraj "$TESTNAME"
+  DoTest image.G3_3A.rst7.save image.G3_3A.rst7
+fi
+
 EndTest
 
 exit 0 
