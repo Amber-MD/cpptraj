43b383408397e494b0f21ffa2594752e6b1f545c
Daniel R. Roe <daniel.r.roe@gmail.com>
2016-12-08 11:02:33 -0500

    DRR - Cpptraj: Add atom comparison to 'comparetop'. Unhide comparetop command.

--- src/Exec_CompareTop.h
+++ src/Exec_CompareTop.h
@@ -1,12 +1,14 @@
 #ifndef INC_EXEC_COMPARETOP_H
 #define INC_EXEC_COMPARETOP_H
 #include "Exec.h"
-// EXPERIMENTAL ALPHA CODE
+/// Compare atoms/parameters and report differences between two topologies.
 class Exec_CompareTop : public Exec {
   public:
-    Exec_CompareTop() : Exec(HIDDEN) {}
+    Exec_CompareTop() : Exec(PARM) {}
     void Help() const;
     DispatchObject* Alloc() const { return (DispatchObject*)new Exec_CompareTop(); }
     RetType Execute(CpptrajState&, ArgList&);
+  private:
+    void CompareAtoms(Topology const&, Topology const&, CpptrajFile&) const;
 };
 #endif
