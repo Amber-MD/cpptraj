4abb369dd03ae7c592bb56ca2f2e1c7535e429fd
Daniel R. Roe <daniel.r.roe@gmail.com>
2016-12-09 16:41:58 -0500

    DRR - Cpptraj: Angle info test

--- test/Test_TopInfo/RunTest.sh
+++ test/Test_TopInfo/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles info.in atoms.dat bonds.dat
+CleanFiles info.in atoms.dat bonds.dat angles.dat
 
 INPUT="-i info.in"
 cat > info.in <<EOF
@@ -14,11 +14,17 @@ bonds :1
 bonds :1 out bonds.dat
 bonds @%N3 @%H
 bonds @%N3 @%H out bonds.dat
+
+angles @1
+angles @1 out angles.dat
+angles @%H @%N3 @%CT
+angles @%H @%N3 @%CT out angles.dat
 quit
 EOF
 RunCpptraj "Atom/bond info test."
 DoTest atoms.dat.save atoms.dat
 DoTest bonds.dat.save bonds.dat
+DoTest angles.dat.save angles.dat
 
 EndTest
 exit 0
