5da06c0b8ad0f1a7c639930a6bf8140565772285
Daniel R. Roe <daniel.r.roe@gmail.com>
2016-12-09 16:09:53 -0500

    DRR - Cpptraj: Add bond info print

--- test/Test_TopInfo/RunTest.sh
+++ test/Test_TopInfo/RunTest.sh
@@ -2,17 +2,23 @@
 
 . ../MasterTest.sh
 
-CleanFiles info.in atoms.dat
+CleanFiles info.in atoms.dat bonds.dat
 
 INPUT="-i info.in"
 cat > info.in <<EOF
 parm ../tz2.parm7
 atoms :3
 atoms :3 out atoms.dat
+
+bonds :1
+bonds :1 out bonds.dat
+bonds @%N3 @%H
+bonds @%N3 @%H out bonds.dat
 quit
 EOF
-RunCpptraj "Atom info test."
+RunCpptraj "Atom/bond info test."
 DoTest atoms.dat.save atoms.dat
+DoTest bonds.dat.save bonds.dat
 
 EndTest
 exit 0
