4f5c86641913104fb2b0c5133b8fd8a426f21a75
Daniel R. Roe <daniel.r.roe@gmail.com>
2016-12-12 12:01:20 -0500

    DRR - Cpptraj: Make Truncated() function a little smarter, add len() function.

--- src/NameType.h
+++ src/NameType.h
@@ -19,6 +19,8 @@ class NameType {
     const char* operator*() const { return c_array_; }
     char operator[](int) const;
     std::string Truncated() const;
+    /// \return non-space length of name
+    int len() const;
     void ReplaceAsterisk();
     bool operator<(NameType const& rhs) const {
       for (unsigned int i = 0; i != NameSize_; i++)
