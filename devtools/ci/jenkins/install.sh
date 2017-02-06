222eed794d543caf168956bb3fef06d1f9598097
Jason Swails <jason.swails@gmail.com>
2017-02-05 22:03:10 -0500

    Expand test suite to improve parallel tests

--- devtools/ci/jenkins/install.sh
+++ devtools/ci/jenkins/install.sh
@@ -2,10 +2,24 @@
 # is the top-level cpptraj directory. The environment variable COMPILER_FLAGS is
 # set. This is set up exclusively to test the Intel compilers.
 
-# Load the Intel compilers (this also sets MKL_HOME). It also assumes that if
-# -mpi is set, so is DO_PARALLEL
+# Load the Intel compilers (this also sets MKL_HOME)
 module load intel openmpi-intel
 
 ./configure ${COMPILER_FLAGS} -mkl intel
+
+# If the compiler flag is -mpi, set DO_PARALLEL. Test both 2 and 4 CPUs for
+# parallel builds.
 make -j6 install
-make check
+if [ "${COMPILER_FLAGS}" = "-mpi" ]; then
+  export DO_PARALLEL='mpirun -np 2'
+  make check
+  export DO_PARALLEL='mpirun -np 4'
+  make check
+elif [ "${COMPILER_FLAGS}" = "-openmp" ]; then
+  export OMP_NUM_THREADS=2
+  make check
+  export OMP_NUM_THREADS=4
+  make check
+else
+  make check
+fi
