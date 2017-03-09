8f2da38ca38810fe594a9390d5f2bf86d82b2367
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-03-09 09:08:47 -0500

    DRR - Cpptraj: Protect tests in paralleDRR - Cpptraj: Protect tests in parallell

--- test/Test_Ewald/RunTest.sh
+++ test/Test_Ewald/RunTest.sh
@@ -8,62 +8,74 @@ CleanFiles ene.in ene.dat ene1.dat long.dat strip.dat directsum.0 ewald.dat \
 INPUT="-i ene.in"
 
 Direct() {
-  cat > ene.in <<EOF
+  MaxThreads 1 "Direct sum test"
+  if [ "$?" -eq 0 ] ; then
+    cat > ene.in <<EOF
 noprogress
 parm nacl.box.parm7
 trajin nacl.box.rst7
 energy out directsum.0 etype directsum npoints 10 
 EOF
-  RunCpptraj "Direct sum test"
-  DoTest directsum.0.save directsum.0
+    RunCpptraj "Direct sum test"
+    DoTest directsum.0.save directsum.0
+  fi
 }
 
 NaCl() {
-  cat > ene.in <<EOF
+  MaxThreads 1 "Ewald test (NaCl crystal)"
+  if [ "$?" -eq 0 ] ; then
+    cat > ene.in <<EOF
 noprogress
-
 parm nacl.box.parm7
 trajin nacl.box.rst7
 energy out ewald.dat etype ewald cut 5.6 dsumtol 0.0000001 \
                                  rsumtol 0.0000001 skinnb 0.01
 EOF
-  RunCpptraj "Ewald test (NaCl crystal)"
-  DoTest ewald.dat.save ewald.dat
+    RunCpptraj "Ewald test (NaCl crystal)"
+    DoTest ewald.dat.save ewald.dat
+  fi
 }
 
 Trpzip() {
-  cat > ene.in <<EOF
+  MaxThreads 1 "Ewald test (trunc. oct)"
+  if [ "$?" -eq 0 ] ; then
+    cat > ene.in <<EOF
 noprogress
-
 parm ../tz2.truncoct.parm7
 trajin ../tz2.truncoct.nc 1 1
 energy out ew_tz2.dat etype ewald skinnb 0.01
 EOF
-  RunCpptraj "Ewald test (trunc. oct)"
-  DoTest ew_tz2.dat.save ew_tz2.dat
+    RunCpptraj "Ewald test (trunc. oct)"
+    DoTest ew_tz2.dat.save ew_tz2.dat
+  fi
 }
 
 Tz2_10() {
-  cat > ene.in <<EOF
+  MaxThreads 10 "Ewald test (trunc. oct), 10 frames"
+  if [ "$?" -eq 0 ] ; then
+    cat > ene.in <<EOF
 noprogress
-
 parm ../tz2.truncoct.parm7
 trajin ../tz2.truncoct.nc
 energy out ew_tz2_10.dat etype ewald skinnb 0.01
 EOF
-  RunCpptraj "Ewald test (trunc. oct), 10 frames"
-  DoTest ew_tz2_10.dat.save ew_tz2_10.dat
+    RunCpptraj "Ewald test (trunc. oct), 10 frames"
+    DoTest ew_tz2_10.dat.save ew_tz2_10.dat
+  fi
 }
 
 Ortho() {
-  cat > ene.in <<EOF
+  MaxThreads 10 "Ewald test (Ortho), 10 frames"
+  if [ "$?" -eq 0 ] ; then
+    cat > ene.in <<EOF
 noprogress
 parm ../tz2.ortho.parm7
 trajin ../tz2.ortho.nc
 energy out tz2_ortho.dat etype ewald skinnb 0.01
 EOF
-  RunCpptraj "Ewald test (Ortho)"
-  DoTest tz2_ortho.dat.save tz2_ortho.dat
+    RunCpptraj "Ewald test (Ortho), 10 frames"
+    DoTest tz2_ortho.dat.save tz2_ortho.dat
+  fi
 }
 
 Direct
