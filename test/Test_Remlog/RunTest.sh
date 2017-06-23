bc37de4a1855d69ca29519b0a46904804339452d
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-06-23 09:40:54 -0400

    DRR - Cpptraj: Add pH replica log read test

--- test/Test_Remlog/RunTest.sh
+++ test/Test_Remlog/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles log.in trepidx.agr mremdreptime.dat
+CleanFiles log.in trepidx.agr mremdreptime.dat ph.repidx.agr ph.stats.dat
 
 INPUT="-i log.in"
 cat > log.in <<EOF
@@ -12,11 +12,18 @@ runanalysis remlog trem.log out trepidx.agr repidx name Tcrd
 readdata rem.log.1.save rem.log.2.save dimfile remd.dim as remlog nosearch
 remlog rem.log.1.save stats reptime mremdreptime.dat
 
+readdata ph.rem.log name PH
+runanalysis remlog PH out ph.repidx.agr repidx stats statsout ph.stats.dat \
+                   reptime ph.stats.dat name pHrem
+
 
 EOF
 RunCpptraj "Replica log read/analyze test."
 DoTest trepidx.agr.save trepidx.agr
 DoTest mremdreptime.dat.save mremdreptime.dat
+DoTest ph.repidx.agr.save ph.repidx.agr
+DoTest ph.stats.dat.save ph.stats.dat
+
 
 EndTest
 exit 0
