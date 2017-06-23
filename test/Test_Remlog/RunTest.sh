84aaaa952d0361a26b263064a3124414f9246efb
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-06-23 09:55:13 -0400

    DRR - Cpptraj: Add charmm replica log read test

--- test/Test_Remlog/RunTest.sh
+++ test/Test_Remlog/RunTest.sh
@@ -3,7 +3,7 @@
 . ../MasterTest.sh
 
 CleanFiles log.in trepidx.agr mremdreptime.dat ph.repidx.agr ph.stats.dat \
-           accept.dat
+           accept.dat tcharmm.accept.dat
 
 INPUT="-i log.in"
 cat > log.in <<EOF
@@ -17,6 +17,8 @@ readdata ph.rem.log name PH
 runanalysis remlog PH out ph.repidx.agr repidx stats statsout ph.stats.dat \
                    reptime ph.stats.dat name pHrem printtrips
 
+readdata replica.exch_0 nrep 4 name Tcharmm
+remlog Tcharmm acceptout tcharmm.accept.dat
 
 EOF
 RunCpptraj "Replica log read/analyze test."
@@ -25,7 +27,7 @@ DoTest accept.dat.save accept.dat
 DoTest mremdreptime.dat.save mremdreptime.dat
 DoTest ph.repidx.agr.save ph.repidx.agr
 DoTest ph.stats.dat.save ph.stats.dat
-
+DoTest tcharmm.accept.dat.save tcharmm.accept.dat
 
 EndTest
 exit 0
