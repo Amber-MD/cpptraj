2bbdb293b33e6e2c4c90e46ea6f24065d99a8dd8
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-06-23 09:38:19 -0400

    DRR - Cpptraj: Add MREMD log read test

--- test/Test_Remlog/RunTest.sh
+++ test/Test_Remlog/RunTest.sh
@@ -2,16 +2,21 @@
 
 . ../MasterTest.sh
 
-CleanFiles log.in trepidx.agr
+CleanFiles log.in trepidx.agr mremdreptime.dat
 
 INPUT="-i log.in"
 cat > log.in <<EOF
 readdata trem.log crdidx 1,2,3,4,5,6,7,8
 runanalysis remlog trem.log out trepidx.agr repidx name Tcrd
 
+readdata rem.log.1.save rem.log.2.save dimfile remd.dim as remlog nosearch
+remlog rem.log.1.save stats reptime mremdreptime.dat
+
+
 EOF
 RunCpptraj "Replica log read/analyze test."
 DoTest trepidx.agr.save trepidx.agr
+DoTest mremdreptime.dat.save mremdreptime.dat
 
 EndTest
 exit 0
