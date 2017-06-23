b911f59a4f3d62c95ca05c8b11e507d63df99e48
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-06-23 09:42:43 -0400

    DRR - Cpptraj: Add exchange acceptance test

--- test/Test_Remlog/RunTest.sh
+++ test/Test_Remlog/RunTest.sh
@@ -2,12 +2,13 @@
 
 . ../MasterTest.sh
 
-CleanFiles log.in trepidx.agr mremdreptime.dat ph.repidx.agr ph.stats.dat
+CleanFiles log.in trepidx.agr mremdreptime.dat ph.repidx.agr ph.stats.dat \
+           accept.dat
 
 INPUT="-i log.in"
 cat > log.in <<EOF
 readdata trem.log crdidx 1,2,3,4,5,6,7,8
-runanalysis remlog trem.log out trepidx.agr repidx name Tcrd
+runanalysis remlog trem.log out trepidx.agr repidx name Tcrd acceptout accept.dat
 
 readdata rem.log.1.save rem.log.2.save dimfile remd.dim as remlog nosearch
 remlog rem.log.1.save stats reptime mremdreptime.dat
@@ -20,6 +21,7 @@ runanalysis remlog PH out ph.repidx.agr repidx stats statsout ph.stats.dat \
 EOF
 RunCpptraj "Replica log read/analyze test."
 DoTest trepidx.agr.save trepidx.agr
+DoTest accept.dat.save accept.dat
 DoTest mremdreptime.dat.save mremdreptime.dat
 DoTest ph.repidx.agr.save ph.repidx.agr
 DoTest ph.stats.dat.save ph.stats.dat
