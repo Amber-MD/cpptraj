28a509fd73f402b7fc71e62ab17b67a39c46b03c
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-06-27 11:43:33 -0400

    DRR - Cpptraj: Change focus of 'unimage' - now specifically looks for molecules which have been split up due to imaging and tries to fix it. Seems to work well so far. Needs polish.

--- src/Action_Unimage.cpp
+++ src/Action_Unimage.cpp
@@ -1,6 +1,6 @@
+#include <stack>
 #include "Action_Unimage.h"
 #include "CpptrajStdio.h"
-#include "ImageRoutines.h" // CreatePairList
 
 // Action_Unimage::Help()
 void Action_Unimage::Help() const {
@@ -12,60 +12,11 @@ Action::RetType Action_Unimage::Init(ArgList& actionArgs, ActionInit& init, int 
 {
   // Always image
   image_.InitImaging( true );
-  useCenter_ = actionArgs.hasKey("center");
-  if (actionArgs.hasKey("bymol"))
-    imageMode_ = Image::BYMOL;
-  else if (actionArgs.hasKey("byres"))
-    imageMode_ = Image::BYRES;
-  else if (actionArgs.hasKey("byatom")) {
-    imageMode_ = Image::BYATOM;
-    // Unwrapping to center by atom makes no sense
-    if (useCenter_) useCenter_ = false;
-  } else
-    imageMode_ = Image::BYATOM;
-  // Get reference
-  ReferenceFrame REF = init.DSL().GetReferenceFrame( actionArgs );
-  if (REF.error()) return Action::ERR;
-  // Get mask
-  maskExp_ = actionArgs.GetMaskNext();
-  // Set up previous coords
-  if (!REF.empty()) {
-    AtomMask refMask( maskExp_ );
-    if (REF.Parm().SetupIntegerMask( refMask, REF.Coord() )) return Action::ERR;
-    refMask.MaskInfo();
-    if (refMask.None()) {
-      mprinterr("Error: No atoms selected by '%s' for '%s'\n",
-                refMask.MaskString(), REF.refName());
-      return Action::ERR;
-    }
-    previous_.SetupFrame( refMask.Nselected() );
-    unsigned int idx = 0;
-    for (AtomMask::const_iterator at = refMask.begin(); at != refMask.end(); ++at, idx += 3)
-    {
-      const double* XYZ = REF.Coord().XYZ( *at );
-      previous_[idx  ] = XYZ[0];
-      previous_[idx+1] = XYZ[1];
-      previous_[idx+2] = XYZ[2];
-    }
-  }
 
-  mprintf("    UNIMAGE: By %s", Image::ModeString(imageMode_));
-  if (maskExp_.empty())
-    mprintf(" using all atoms");
-  else
-    mprintf(" using mask '%s'", maskExp_.c_str());
-  if (imageMode_ != Image::BYATOM) {
-    if (useCenter_)
-      mprintf(" based on center of mass.");
-    else
-      mprintf(" based on first atom position.");
-  }
-  mprintf("\n");
-  if ( !REF.empty())
-    mprintf("\tReference is %s", REF.refName());
-  else
-    mprintf("\tReference is first frame.");
-  mprintf("\n");
+  mask_.SetMaskString( actionArgs.GetMaskNext() );
+
+  mprintf("\tChecking all bonds selected by mask '%s'\n", mask_.MaskString());
+
   return Action::OK;
 }
 
@@ -78,130 +29,112 @@ Action::RetType Action_Unimage::Setup(ActionSetup& setup)
             setup.Top().c_str());
     return Action::SKIP;
   }
-  // Set up atom pairs to be unwrapped 
-  atomPairs_ = Image::CreatePairList(setup.Top(), imageMode_, maskExp_);
-  if (atomPairs_.empty()) {
-    mprintf("Warning: No atoms selected by '%s'\n", maskExp_.c_str());
+  // Setup mask
+  if ( setup.Top().SetupCharMask( mask_ ) ) return Action::ERR;
+  mask_.MaskInfo();
+  if ( mask_.None() ) {
+    mprintf("Warning: No atoms selected.\n");
     return Action::SKIP;
   }
-  natoms_ = atomPairs_.size() / 2;
-  mprintf("\tNumber of %ss to be unimaged is %i\n",
-          Image::ModeString(imageMode_), natoms_);
-  // If previous frame is set up ensure same number of atoms
-  if (!previous_.empty()) {
-    if (previous_.Natom() != natoms_) {
-      mprinterr("Error: # reference atoms %i != # selected atoms.\n", natoms_);
-      return Action::ERR;
-    }
-  }
+  // Set up imaging info for this parm
+  image_.SetupImaging( setup.CoordInfo().TrajBox().Type() );
+
+  CurrentParm_ = setup.TopAddress();
+
   return Action::OK;
 }
 
 // Action_Unimage::DoAction()
 Action::RetType Action_Unimage::DoAction(int frameNum, ActionFrame& frm)
 {
-  if (previous_.empty()) {
-    previous_.SetupFrame( natoms_ );
-    // Load this frame as reference
-    unsigned int idx = 0;
-    for (Iarray::const_iterator ap = atomPairs_.begin(); ap != atomPairs_.end(); ap += 2)
+  Box const& box = frm.Frm().BoxCrd();
+  // Calculate box info needed for imaging based on cell type
+  if (image_.ImageType() == NONORTHO)
+    box.ToRecip(ucell_, recip_);
+  else
+    boxCenter_ = box.Center();
+  // Starting with the first atom, check every atom bonded to that atom
+  // pseudo-recursively. Ensure that no bond length is longer than half
+  // the box size. If it is adjust the position of the bonded atom to
+  // correct this. This has to be done 1 atom at a time because if an atom
+  // is moved then the atoms it is bonded to must move as well.
+  std::stack<unsigned int> nextAtomToSearch;
+  Topology const& Top = *CurrentParm_;
+  unsigned int Natoms = (unsigned int)Top.Natom();
+  std::vector<bool> atomVisited( Natoms, false );
+  bool uncheckedAtomsRemain = true;
+  unsigned int currentAtom = 0;
+  unsigned int lowestUnassignedAtom = 0;
+  // BEGIN main loop
+  while (uncheckedAtomsRemain) {
+    atomVisited[currentAtom] = true;
+    Atom const& AT = Top[currentAtom];
+    Vec3 currXYZ( frm.Frm().XYZ( currentAtom ) );
+    mprintf("ANCHOR: %i\n", currentAtom);
+    // All atoms bonded to this one are in molecule.
+    for (Atom::bond_iterator batom = AT.bondbegin(); batom != AT.bondend(); ++batom)
     {
-      for (int at = atomPairs_[*ap]; at != atomPairs_[*(ap+1)]; at++, idx += 3)
-      {
-        const double* XYZ = frm.Frm().XYZ( at );
-        previous_[idx  ] = XYZ[0];
-        previous_[idx+1] = XYZ[1];
-        previous_[idx+2] = XYZ[2];
-      }
-    }
-    return Action::OK;
-  } else {
-    Vec3 currXYZ, prevXYZ;
-    boxCenter_ = frm.Frm().BoxCrd().Center();
-    boxCenter_.Print("boxTrans");
-    // Calculate matrices if necessary
-    frm.Frm().BoxCrd().ToRecip(ucell_, recip_);
-    // Perform unwrapping
-    unsigned int pidx = 0;
-    for (Iarray::const_iterator ap = atomPairs_.begin(); ap != atomPairs_.end(); ap += 2)
-    {
-      int eltsize = *(ap+1) - *ap;
-      if (useCenter_) {
-        // Use c.o.m. of elements
-        currXYZ = frm.Frm().VGeometricCenter(*ap, *(ap+1));
-        prevXYZ = previous_.VGeometricCenter(pidx, pidx + eltsize);
-      } else {
-        // Use first atom position only
-        currXYZ = frm.Frm().XYZ( *ap );
-        prevXYZ = previous_.XYZ( pidx );
-      }
-      // Calculate distance to previous frames elements coordinates
-      double delx = currXYZ[0] - prevXYZ[0];
-      double dely = currXYZ[1] - prevXYZ[1];
-      double delz = currXYZ[2] - prevXYZ[2];
-      if ( image_.ImageType() == ORTHO ) {
-        // ----- Orthorhombic imaging ------------
-        // If the element moved more than half the box, assume it was imaged
-        // and adjust the current position.
-        if      (delx >  boxCenter_[0]) boxTrans_[0] = -frm.Frm().BoxCrd().BoxX();
-        else if (delx < -boxCenter_[0]) boxTrans_[0] =  frm.Frm().BoxCrd().BoxX();
-        else                            boxTrans_[0] = 0.0;
-        if      (dely >  boxCenter_[1]) boxTrans_[1] = -frm.Frm().BoxCrd().BoxY();
-        else if (dely < -boxCenter_[1]) boxTrans_[1] =  frm.Frm().BoxCrd().BoxY();
-        else                            boxTrans_[1] = 0.0;
-        if      (delz >  boxCenter_[2]) boxTrans_[2] = -frm.Frm().BoxCrd().BoxZ();
-        else if (delz < -boxCenter_[2]) boxTrans_[2] =  frm.Frm().BoxCrd().BoxZ();
-        else                            boxTrans_[2] = 0.0;
-      } else {
-        // ----- Non-orthorhombic imaging --------
-        // If the element moved more than half the box, assume it was imaged.
-        if (delx > boxCenter_[0] || delx < -boxCenter_[0] ||
-            dely > boxCenter_[1] || dely < -boxCenter_[1] ||
-            delz > boxCenter_[2] || delz < -boxCenter_[2])
-        {
-          // Previous position in Cartesian space is in prevXYZ
-          // Current position in fractional coords
-          Vec3 cFrac = recip_ * currXYZ;
-          // Look for imaged distance closer to previous than current position
-          double minDist2 = frm.Frm().BoxCrd().BoxX() *
-                            frm.Frm().BoxCrd().BoxY() *
-                            frm.Frm().BoxCrd().BoxZ();
-          Vec3 minCurr(0.0);
-          for (int ix = -1; ix < 2; ix++) {
-            for (int iy = -1; iy < 2; iy++) {
-              for (int iz = -1; iz < 2; iz++) {
-                if (ix != 0 || iy != 0 || iz != 0) { // Ignore current position
-                  Vec3 ixyz(ix, iy, iz);
-                  // Current position shifted and back in Cartesian space
-                  Vec3 IMG = ucell_.TransposeMult(cFrac + ixyz);
-                  // Distance from previous position to imaged current position
-                  Vec3 dxyz = IMG - prevXYZ;
-                  double dist2 = dxyz.Magnitude2();
-                  if (dist2 < minDist2) {
-                    minDist2 = dist2;
-                    minCurr = IMG;
-                  }
-                }
-              }
-            }
-          }
-          boxTrans_[0] = minCurr[0] - currXYZ[0];
-          boxTrans_[1] = minCurr[1] - currXYZ[1];
-          boxTrans_[2] = minCurr[2] - currXYZ[2];
+      if (!atomVisited[*batom]) {
+        if ( Top[*batom].Nbonds() > 1 )
+          // Bonded atom has more than 1 bond; needs to be searched.
+          nextAtomToSearch.push( *batom );
+        else
+          // Bonded atom only bonded to current atom. No more search needed.
+          atomVisited[*batom] = true;
+        // Check if bonded atom is too far away
+        Vec3 bondXYZ( frm.Frm().XYZ( *batom ) );
+        Vec3 delta = bondXYZ - currXYZ;
+        Vec3 boxTrans(0.0);
+        if ( image_.ImageType() == ORTHO ) {
+          // ----- Orthorhombic imaging ------------
+          // If the distance between current and bonded atom is more than half the box,
+          // adjust the position of the bonded atom.
+          while (delta[0] >  boxCenter_[0]) { delta[0] -= box.BoxX(); boxTrans[0] -= box.BoxX(); }
+          while (delta[0] < -boxCenter_[0]) { delta[0] += box.BoxX(); boxTrans[0] += box.BoxX(); }
+          while (delta[1] >  boxCenter_[1]) { delta[1] -= box.BoxY(); boxTrans[1] -= box.BoxY(); }
+          while (delta[1] < -boxCenter_[1]) { delta[1] += box.BoxY(); boxTrans[1] += box.BoxY(); }
+          while (delta[2] >  boxCenter_[2]) { delta[2] -= box.BoxZ(); boxTrans[2] -= box.BoxZ(); }
+          while (delta[2] < -boxCenter_[2]) { delta[2] += box.BoxZ(); boxTrans[2] += box.BoxZ(); }
+        } else {
+          // ----- Non-orthorhombic imaging --------
+          Vec3 fdelta = recip_ * delta;
+          // DEBUG
+//          Vec3 dbgdelta = (recip_ * bondXYZ) - (recip_ * currXYZ);
+//          fdelta.Print("fdelta");
+//          dbgdelta.Print("dbgdelta");
+          // If the distance between current and bonded atom is more than half the cell,
+          // adjust position of the bonded atom.
+          while (fdelta[0] >  0.5) { fdelta[0] -= 1.0; boxTrans[0] -= 1.0; }
+          while (fdelta[0] < -0.5) { fdelta[0] += 1.0; boxTrans[0] += 1.0; }
+          while (fdelta[1] >  0.5) { fdelta[1] -= 1.0; boxTrans[1] -= 1.0; }
+          while (fdelta[1] < -0.5) { fdelta[1] += 1.0; boxTrans[1] += 1.0; }
+          while (fdelta[2] >  0.5) { fdelta[2] -= 1.0; boxTrans[2] -= 1.0; }
+          while (fdelta[2] < -0.5) { fdelta[2] += 1.0; boxTrans[2] += 1.0; }
+          boxTrans = ucell_.TransposeMult( boxTrans );
+          delta = ucell_.TransposeMult( fdelta ); // DEBUG
         }
+        // Translate the atom
+        if (boxTrans[0] != 0.0 || boxTrans[1] != 0.0 || boxTrans[2] != 0.0)
+          mprintf("\tMove atom %6i d={%8.3g %8.3g %8.3g} t={%8.3g %8.3g %8.3g}\n",
+                  *batom, delta[0], delta[1], delta[2], boxTrans[0], boxTrans[1], boxTrans[2]);
+        frm.ModifyFrm().Translate(boxTrans, *batom);
+      } // END if not visited
+    } // END loop over bonded atoms
+    if (nextAtomToSearch.empty()) {
+      // No more atoms to search. Find next unmarked atom.
+      unsigned int idx = lowestUnassignedAtom;
+      for (; idx != Natoms; idx++)
+        if (!atomVisited[idx]) break;
+      if (idx == Natoms)
+        uncheckedAtomsRemain = false;
+      else {
+        currentAtom = idx;
+        lowestUnassignedAtom = idx + 1;
       }
-      mprintf("DBG: %8i - %8i delx=%8.3f dely=%8.3f delz=%8.3f trans={%8.3f %8.3f %8.3f}\n",
-              *ap + 1, *(ap+1), delx, dely, delz, boxTrans_[0], boxTrans_[1], boxTrans_[2]);
-      // Move each atom of the element
-      frm.ModifyFrm().Translate(boxTrans_, *ap, *(ap+1));
-      // Save the new positions
-      int i3 = *ap * 3;
-      std::copy( frm.Frm().xAddress()+i3, frm.Frm().xAddress()+(*(ap+1)*3),
-                 previous_.xAddress()+pidx*3 );
-
-      pidx += eltsize;
-    } // END loop over atom pairs
-  }
- 
+    } else {
+      currentAtom = nextAtomToSearch.top();
+      nextAtomToSearch.pop();
+    }
+  } // END main loop
   return Action::MODIFY_COORDS;
 }
--- src/Action_Unimage.h
+++ src/Action_Unimage.h
@@ -2,11 +2,11 @@
 #define INC_ACTION_UNIMAGE_H
 #include "Action.h"
 #include "ImagedAction.h"
-#include "ImageTypes.h"
 /// <Enter description of Action_Unimage here>
 class Action_Unimage : public Action {
   public:
-    Action_Unimage() : natoms_(0), useCenter_(false) {}
+    //Action_Unimage() : natoms_(0), useCenter_(false) {}
+    Action_Unimage() {}
     DispatchObject* Alloc() const { return (DispatchObject*)new Action_Unimage(); }
     void Help() const;
   private:
@@ -15,19 +15,12 @@ class Action_Unimage : public Action {
     Action::RetType DoAction(int, ActionFrame&);
     void Print() {}
 
-    typedef Image::PairType Iarray;
-
     ImagedAction image_ ; ///< Imaging routines
-    Frame previous_; ///< Hold previous frames selected coords
-    Iarray atomPairs_; ///< Hold start and end atom indices for each element in current to unwrap
+    CharMask mask_;
+    BondArray bonds_;
     Vec3 boxCenter_; ///< Box center for current frame
-    Vec3 boxTrans_; ///< Translation vector for current element
     Matrix_3x3 ucell_; ///< Unit cell matrix for current frame
     Matrix_3x3 recip_; ///< Recip (frac) matrix for current frame
-    std::string maskExp_; ///< Mask expression
-    int natoms_; ///< Total # atoms being unwrapped
-    Image::Mode imageMode_; ///< Unwrap mode
-    bool useCenter_; ///< If true use c.o.m. of elements, otherwise first atom
-
+    Topology* CurrentParm_;
 };
 #endif
