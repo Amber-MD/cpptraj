f0f3351001f20077873c461757eb8668d1af0259
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-06-30 13:07:27 -0400

    DRR - Cpptraj: Align numbers, make a bit easier to read.

--- src/Action_Surf.cpp
+++ src/Action_Surf.cpp
@@ -317,10 +317,10 @@ void Action_Surf::SetAtomLCPO(Topology const& currentParm, int atidx, SurfInfo* 
     case Atom::CARBON:
       if (atom.Nbonds() == 4) {
         switch ( numBonds ) {
-          case 1: AssignLCPO(SIptr, 1.70, 0.77887, -0.28063, -0.0012968, 0.00039328); break;
-          case 2: AssignLCPO(SIptr, 1.70, 0.56482, -0.19608, -0.0010219, 0.0002658);  break;
+          case 1: AssignLCPO(SIptr, 1.70, 0.77887, -0.28063,  -0.0012968,  0.00039328); break;
+          case 2: AssignLCPO(SIptr, 1.70, 0.56482, -0.19608,  -0.0010219,  0.0002658);  break;
           case 3: AssignLCPO(SIptr, 1.70, 0.23348, -0.072627, -0.00020079, 0.00007967); break;
-          case 4: AssignLCPO(SIptr, 1.70, 0.00000, 0.00000, 0.00000, 0.00000); break;
+          case 4: AssignLCPO(SIptr, 1.70, 0.00000,  0.00000,   0.00000,    0.00000); break;
           default: WarnLCPO(atom.Type(),atidx + 1,numBonds);
                    AssignLCPO(SIptr, 1.70, 0.77887, -0.28063, -0.0012968, 0.00039328);
         }
@@ -335,12 +335,12 @@ void Action_Surf::SetAtomLCPO(Topology const& currentParm, int atidx, SurfInfo* 
       break;
     case Atom::OXYGEN:
       if (atype0=='O' && atype1==' ') 
-        AssignLCPO(SIptr, 1.60, 0.68563, -0.1868, -0.00135573, 0.00023743);
+        AssignLCPO(SIptr, 1.60, 0.68563, -0.1868,  -0.00135573, 0.00023743);
       else if (atype0=='O' && atype1=='2')
-        AssignLCPO(SIptr, 1.60, 0.88857, -0.33421, -0.0018683, 0.00049372);
+        AssignLCPO(SIptr, 1.60, 0.88857, -0.33421, -0.0018683,  0.00049372);
       else {
         switch ( numBonds ) {
-          case 1: AssignLCPO(SIptr, 1.60, 0.77914, -0.25262, -0.0016056, 0.00035071); break;
+          case 1: AssignLCPO(SIptr, 1.60, 0.77914, -0.25262, -0.0016056,  0.00035071); break;
           case 2: AssignLCPO(SIptr, 1.60, 0.49392, -0.16038, -0.00015512, 0.00016453); break;
           default: WarnLCPO(atom.Type(),atidx + 1,numBonds);
                    AssignLCPO(SIptr, 1.60, 0.77914, -0.25262, -0.0016056, 0.00035071);
@@ -350,17 +350,17 @@ void Action_Surf::SetAtomLCPO(Topology const& currentParm, int atidx, SurfInfo* 
     case Atom::NITROGEN:
       if (atype0=='N' && atype1=='3') {
         switch ( numBonds ) {
-          case 1: AssignLCPO(SIptr, 1.65, 0.078602, -0.29198, -0.0006537, 0.00036247); break;
-          case 2: AssignLCPO(SIptr, 1.65, 0.22599, -0.036648, -0.0012297, 0.000080038); break;
+          case 1: AssignLCPO(SIptr, 1.65, 0.078602, -0.29198,  -0.0006537,  0.00036247); break;
+          case 2: AssignLCPO(SIptr, 1.65, 0.22599,  -0.036648, -0.0012297,  0.000080038); break;
           case 3: AssignLCPO(SIptr, 1.65, 0.051481, -0.012603, -0.00032006, 0.000024774); break;
           default: WarnLCPO(atom.Type(),atidx + 1,numBonds);
                    AssignLCPO(SIptr, 1.65, 0.078602, -0.29198, -0.0006537, 0.00036247);
         }
       } else {
         switch ( numBonds ) {
-          case 1: AssignLCPO(SIptr, 1.65, 0.73511, -0.22116, -0.00089148, 0.0002523); break;
-          case 2: AssignLCPO(SIptr, 1.65, 0.41102, -0.12254, -0.000075448, 0.00011804); break;
-          case 3: AssignLCPO(SIptr, 1.65, 0.062577, -0.017874, -0.00008312, 0.000019849); break;
+          case 1: AssignLCPO(SIptr, 1.65, 0.73511,  -0.22116,  -0.00089148,  0.0002523); break;
+          case 2: AssignLCPO(SIptr, 1.65, 0.41102,  -0.12254,  -0.000075448, 0.00011804); break;
+          case 3: AssignLCPO(SIptr, 1.65, 0.062577, -0.017874, -0.00008312,  0.000019849); break;
           default: WarnLCPO(atom.Type(),atidx + 1,numBonds);
                    AssignLCPO(SIptr, 1.65, 0.078602, -0.29198, -0.0006537, 0.00036247);
         }
@@ -368,13 +368,13 @@ void Action_Surf::SetAtomLCPO(Topology const& currentParm, int atidx, SurfInfo* 
       break;
     case Atom::SULFUR:
       if (atype0=='S' && atype1=='H') 
-        AssignLCPO(SIptr, 1.90, 0.7722, -0.26393, 0.0010629, 0.0002179);
+        AssignLCPO(SIptr, 1.90, 0.7722,  -0.26393,  0.0010629, 0.0002179);
       else 
         AssignLCPO(SIptr, 1.90, 0.54581, -0.19477, -0.0012873, 0.00029247);
       break;
     case Atom::PHOSPHORUS:
       switch ( numBonds ) {
-        case 3: AssignLCPO(SIptr, 1.90, 0.3865, -0.18249, -0.0036598, 0.0004264); break;
+        case 3: AssignLCPO(SIptr, 1.90, 0.3865,  -0.18249,  -0.0036598,    0.0004264); break;
         case 4: AssignLCPO(SIptr, 1.90, 0.03873, -0.0089339, 0.0000083582, 0.0000030381); break;
         default: WarnLCPO(atom.Type(),atidx + 1,numBonds);
                  AssignLCPO(SIptr, 1.90, 0.3865, -0.18249, -0.0036598, 0.0004264);
