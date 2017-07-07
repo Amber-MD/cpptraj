24c412e7db8b23beefa9dab85ca6c882d2d4e2da
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-07-07 13:37:54 -0400

    DRR - Cpptraj: Test 'zeromomentum' and 'modify'

--- test/Test_SetVelocity/RunTest.sh
+++ test/Test_SetVelocity/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles setvel.in tz2.vel.rst7
+CleanFiles setvel.in tz2.vel.rst7 V1.dat
 
 INPUT='-i setvel.in'
 
@@ -14,12 +14,21 @@ parm ../tz2.parm7
 trajin ../tz2.rst7
 setvelocity tempi 298.0 ig 10
 temperature MyTemp
+vector V0 momentum
 trajout tz2.vel.rst7
 go
 printdata MyTemp
+printdata V0
+
+clear trajin
+trajin tz2.vel.rst7
+setvelocity modify zeromomentum
+vector V1 momentum out V1.dat
+go
 EOF
   RunCpptraj "$TESTNAME"
   DoTest tz2.vel.rst7.save tz2.vel.rst7
+  DoTest V1.dat.save V1.dat
 fi
 EndTest
 exit 0
