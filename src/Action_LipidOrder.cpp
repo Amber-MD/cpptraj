443c16355710a1b50dd63f0855d2c72d0899fac8
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-07-17 10:14:09 -0400

    DRR - Cpptraj: Group all carbons with the same name, store carbon and any bonded hydrogen indices

--- src/Action_LipidOrder.cpp
+++ src/Action_LipidOrder.cpp
@@ -15,6 +15,32 @@ Action::RetType Action_LipidOrder::Init(ArgList& actionArgs, ActionInit& init, i
   return Action::OK;
 }
 
+//  Action_LipidOrder::FollowChain()
+void Action_LipidOrder::FollowChain(int idx, Topology const& top, std::vector<bool> Visited,
+                                    int offset)
+{
+  Visited[idx - offset] = true;
+  Atom const& atom = top[idx];
+  // Get site
+  Cmap::iterator it = Carbons_.lower_bound( atom.Name() );
+  if (it == Carbons_.end() || it->first != atom.Name())
+    // New site 
+    it = Carbons_.insert( it, Cpair(atom.Name(), CarbonList(top.Res(atom.ResNum()).Name())) );
+
+  // Add this site
+  mprintf("\t\t%s\n", top.TruncResAtomName(idx).c_str());
+  CarbonSite& site = it->second.AddSite( idx );
+  // Loop over bonded atoms 
+  for (Atom::bond_iterator bnd = top[idx].bondbegin();
+                           bnd != top[idx].bondend(); ++bnd)
+  {
+    if (top[*bnd].Element() == Atom::HYDROGEN)
+      site.AddHindex( *bnd );
+    else if (!Visited[*bnd - offset] && top[*bnd].Element() == Atom::CARBON)
+      FollowChain(*bnd, top, Visited, offset);
+  }
+}
+
 // Action_LipidOrder::Setup()
 Action::RetType Action_LipidOrder::Setup(ActionSetup& setup)
 {
@@ -31,12 +57,17 @@ Action::RetType Action_LipidOrder::Setup(ActionSetup& setup)
   {
     if (!mol->IsSolvent() && mask_.AtomsInCharMask(mol->BeginAtom(), mol->EndAtom()))
     {
+      mprintf("  Mol %zu\n", mol - setup.Top().MolStart() + 1);
       std::vector<bool> Visited(mol->NumAtoms(), false);
-      int idx = 0;
-      for (int at = mol->BeginAtom(); at != mol->EndAtom(); ++at, ++idx)
+      int offset = mol->BeginAtom();
+      // First mark any atoms not in the mask as visited.
+      for (int at = mol->BeginAtom(); at != mol->EndAtom(); ++at)
+        if (!mask_.AtomInCharMask( at ))
+          Visited[at - offset] = true;
+      // Search for the start of lipid chains
+      for (int at = mol->BeginAtom(); at != mol->EndAtom(); ++at)
       {
-        Visited[idx] = true;
-        if (mask_.AtomInCharMask( at )) {
+        if (!Visited[at - offset]) {
           Atom const& atom = setup.Top()[at];
           if (atom.Element() == Atom::CARBON) {
             // Should be bonded to two oxygens and 1 carbon
@@ -58,15 +89,31 @@ Action::RetType Action_LipidOrder::Setup(ActionSetup& setup)
               }
             }
             if (n_O == 2 && n_C == 1) {
-              // Starting at the carbon follow the chain down
+              Visited[at - offset] = true;
+              // Starting at the bonded carbon follow the chain down
               mprintf("DEBUG: Lipid chain starting at %s\n",
                       setup.Top().TruncResAtomName(C_idx).c_str());
+              FollowChain(C_idx, setup.Top(), Visited, offset);
             }
           } // END if Carbon
         } // END if selected
       } // END loop over molecule atoms
     } // END mol not solvent and some of mol selected
   } // END loop over molecules
+
+  mprintf("\t%zu carbon types:\n", Carbons_.size());
+  for (Cmap::const_iterator it = Carbons_.begin(); it != Carbons_.end(); ++it)
+  {
+    mprintf("\t%s %s (%zu)\n", it->second.resName(), *(it->first), it->second.Nsites());
+    for (Carray::const_iterator site = it->second.begin(); site != it->second.end(); ++site)
+    {
+      mprintf("\t%15s", setup.Top().TruncResAtomName(site->Cidx()).c_str());
+      if (site->H1idx() != -1) mprintf(" %15s", setup.Top().TruncResAtomName(site->H1idx()).c_str());
+      if (site->H2idx() != -1) mprintf(" %15s", setup.Top().TruncResAtomName(site->H2idx()).c_str());
+      if (site->H3idx() != -1) mprintf(" %15s", setup.Top().TruncResAtomName(site->H3idx()).c_str());
+      mprintf("\n");
+    }
+  }
   return Action::ERR;
 }
 
--- src/Action_LipidOrder.h
+++ src/Action_LipidOrder.h
@@ -1,5 +1,6 @@
 #ifndef INC_ACTION_LIPIDORDER_H
 #define INC_ACTION_LIPIDORDER_H
+#include <map>
 #include "Action.h"
 /// <Enter description of Action_LipidOrder here>
 class Action_LipidOrder : public Action {
@@ -13,6 +14,62 @@ class Action_LipidOrder : public Action {
     Action::RetType DoAction(int, ActionFrame&);
     void Print() {}
 
+    /// Hold information for a single lipid carbon site
+    class CarbonSite;
+    typedef std::vector<CarbonSite> Carray;
+
+    /// Hold information for all carbons with the same name.
+    class CarbonList;
+
+    /// Pair a carbon name with all carbon sites with that name.
+    typedef std::pair<NameType, CarbonList> Cpair;
+
+    /// Map carbon name to all carbon sites with that name.
+    typedef std::map<NameType, CarbonList> Cmap;
+
+    /// Create carbon site with hydrogens, then follow remaining carbon chain
+    void FollowChain(int, Topology const&, std::vector<bool>, int);
+
     CharMask mask_;
+    Cmap Carbons_;
+};
+
+/// Hold information for a lipid carbon site.
+class Action_LipidOrder::CarbonSite {
+  public:
+    CarbonSite() : c_idx_(-1), h1_idx_(-1), h2_idx_(-1), h3_idx_(-1) {}
+    CarbonSite(int c) : c_idx_(c), h1_idx_(-1), h2_idx_(-1), h3_idx_(-1) {}
+    int Cidx()  const { return c_idx_;  }
+    int H1idx() const { return h1_idx_; }
+    int H2idx() const { return h2_idx_; }
+    int H3idx() const { return h3_idx_; }
+    void AddHindex(int h) {
+      if      (h1_idx_ == -1) h1_idx_ = h;
+      else if (h2_idx_ == -1) h2_idx_ = h;
+      else                    h3_idx_ = h; // TODO check for 4+ h?
+    }
+  private:
+    int c_idx_;  ///< Carbon atom index
+    int h1_idx_; ///< First hydrogen index
+    int h2_idx_; ///< Second hydrogen index if present.
+    int h3_idx_; ///< Third hydrogen index if present.
+};
+
+/// Hold information for all carbons with the same name.
+class Action_LipidOrder::CarbonList {
+  public:
+    CarbonList() {}
+    CarbonList(NameType const& n) : resname_(n) {}
+    const char* resName() const { return *resname_; }
+    unsigned int Nsites() const { return sites_.size(); }
+    CarbonSite& AddSite(int cidx) {
+      sites_.push_back( CarbonSite(cidx) );
+      return sites_.back();
+    }
+    Carray::const_iterator begin() const { return sites_.begin(); }
+    Carray::const_iterator end()   const { return sites_.end();   }
+  private:
+    NameType resname_;
+    Carray sites_;
 };
 #endif
