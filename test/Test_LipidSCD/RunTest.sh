abc67c0d59996eb9eb5454787e0d72d3c7a0fd32
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-07-27 14:54:26 -0400

    DRR - Cpptraj: More tests

--- test/Test_LipidSCD/RunTest.sh
+++ test/Test_LipidSCD/RunTest.sh
@@ -2,11 +2,11 @@
 
 . ../MasterTest.sh
 
-CleanFiles order.in ol?.dat ol3.agr
+CleanFiles order.in ol?.dat ol3.agr dopc.agr
 
 INPUT="-i order.in"
 
-NotParallel "New Lipid Order Parameter Test 1."
+MaxThreads 1 "New Lipid Order Parameter Test."
 if [ $? -eq 0 ] ; then
   cat > order.in <<EOF
 # crd/top courtesy of Callum Dickson, Imperial College London
@@ -24,11 +24,18 @@ datafile ol1.dat xprec 3.0
 datafile ol2.dat xprec 3.0
 datafile ol3.agr xprec 3.0
 EOF
-  RunCpptraj "New Lipid Order Parameter Test 1."
+  RunCpptraj "New Lipid Order Parameter Test."
   DoTest ol1.dat.save ol1.dat
   DoTest ol2.dat.save ol2.dat
   DoTest ol3.agr.save ol3.agr
 fi
+
+cat > order.in <<EOF
+parm dopc.charmm.mol2
+trajin dopc.charmm.nc
+lipidscd DOPC out dopc.agr xydy prec 10.7 xprec 3.0
+EOF
+RunCpptraj "New Lipid Order Parameter Test, DOPC."
 
 EndTest
 
