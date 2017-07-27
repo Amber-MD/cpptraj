c2670ef21bf685bb3ad76230d11b5825548f0060
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-07-27 09:41:41 -0400

    DRR - Cpptraj: Update placeholder test

--- test/Test_LipidOrder/Test.sh
+++ test/Test_LipidOrder/Test.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles order.in 
+CleanFiles order.in ol?.dat
 
 NotParallel "Lipid Order Parameter Test."
 if [[ $? -ne 0 ]] ; then
@@ -17,11 +17,18 @@ cat > order.in <<EOF
 parm ../DOPC.parm7
 trajin ../DOPC.rst7
 
-lipidorder2 :OL,PC
-lipidorder2 :OL2,PC
+lipidorder2 :OL,PC  out ol1.dat
+debug actions 3
+lipidorder2 :OL2,PC out ol2.dat
+precision ol1.dat 10 7
+precision ol2.dat 10 7
+datafile ol1.dat xprec 3.0
+datafile ol2.dat xprec 3.0
 EOF
 
 RunCpptraj "Lipid Order Parameter Test 2."
+DoTest ol1.dat.save ol1.dat
+DoTest ol2.dat.save ol2.dat
 
 EndTest
 
