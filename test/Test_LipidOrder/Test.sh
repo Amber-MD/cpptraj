9aa9936edbb0c2e5b8c2a75431bfa48209448ea6
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-07-27 10:16:41 -0400

    DRR - Cpptraj: Update order parameter output to -SCD

--- test/Test_LipidOrder/Test.sh
+++ test/Test_LipidOrder/Test.sh
@@ -18,12 +18,15 @@ parm ../DOPC.parm7
 trajin ../DOPC.rst7
 
 lipidorder2 :OL,PC  out ol1.dat
-debug actions 3
+#debug actions 3
 lipidorder2 :OL2,PC out ol2.dat
+lipidorder2 out ol3.dat
 precision ol1.dat 10 7
 precision ol2.dat 10 7
+precision ol3.dat 10 7
 datafile ol1.dat xprec 3.0
 datafile ol2.dat xprec 3.0
+datafile ol3.dat xprec 3.0
 EOF
 
 RunCpptraj "Lipid Order Parameter Test 2."
