3f1d8f8b3903c43a47ab4dccdf93c7284fc5c488
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-08-07 13:31:56 -0400

    DRR - Cpptraj: OpenMP-parallelize separate elec and vdw calcs

--- src/Energy.cpp
+++ src/Energy.cpp
@@ -269,21 +269,29 @@ double Energy_Amber::E_VDW(Frame const& fIn, Topology const& tIn, AtomMask const
 {
   time_NB_.Start();
   double Evdw = 0.0;
-  for (AtomMask::const_iterator atom1 = mask.begin(); atom1 != mask.end(); ++atom1)
+  int idx1;
+# ifdef _OPENMP
+# pragma omp parallel private(idx1) reduction(+ : Evdw)
   {
+# pragma omp for
+# endif
+  for (idx1 = 0; idx1 < mask.Nselected(); idx1++)
+  {
+    int atom1 = mask[idx1];
     // Set up coord for this atom
-    const double* crd1 = fIn.XYZ( *atom1 );
+    const double* crd1 = fIn.XYZ( atom1 );
     // Set up exclusion list for this atom
-    Atom::excluded_iterator excluded_atom = tIn[*atom1].excludedbegin();
-    for (AtomMask::const_iterator atom2 = atom1 + 1; atom2 != mask.end(); ++atom2)
+    Atom::excluded_iterator excluded_atom = tIn[atom1].excludedbegin();
+    for (int idx2 = idx1 + 1; idx2 < mask.Nselected(); idx2++)
     {
+      int atom2 = mask[idx2];
       // If atom is excluded, just increment to next excluded atom.
-      if (excluded_atom != tIn[*atom1].excludedend() && *atom2 == *excluded_atom)
+      if (excluded_atom != tIn[atom1].excludedend() && atom2 == *excluded_atom)
         ++excluded_atom;
       else {
-        double rij2 = DIST2_NoImage( crd1, fIn.XYZ( *atom2 ) );
+        double rij2 = DIST2_NoImage( crd1, fIn.XYZ( atom2 ) );
         // VDW
-        NonbondType const& LJ = tIn.GetLJparam(*atom1, *atom2);
+        NonbondType const& LJ = tIn.GetLJparam(atom1, atom2);
         double r2    = 1.0 / rij2;
         double r6    = r2 * r2 * r2;
         double r12   = r6 * r6;
@@ -293,11 +301,14 @@ double Energy_Amber::E_VDW(Frame const& fIn, Topology const& tIn, AtomMask const
         Evdw += e_vdw;
 #       ifdef DEBUG_ENERGY
         mprintf("\tEVDW  %4i -- %4i: A=  %12.5e  B=  %12.5e  r2= %12.5f  E= %12.5e\n",
-                *atom1+1, *atom2+1, LJ.A(), LJ.B(), rij2, e_vdw);
+                atom1+1, atom2+1, LJ.A(), LJ.B(), rij2, e_vdw);
 #       endif
       }
     }
   }
+# ifdef _OPENMP
+  } // END omp parallel
+# endif
   time_NB_.Stop();
   return Evdw;
 }
@@ -307,32 +318,43 @@ double Energy_Amber::E_Elec(Frame const& fIn, Topology const& tIn, AtomMask cons
 {
   time_NB_.Start();
   double Eelec = 0.0;
-  for (AtomMask::const_iterator atom1 = mask.begin(); atom1 != mask.end(); ++atom1)
+  int idx1;
+# ifdef _OPENMP
+# pragma omp parallel private(idx1) reduction(+ : Eelec)
   {
+# pragma omp for
+# endif
+  for (idx1 = 0; idx1 < mask.Nselected(); idx1++)
+  {
+    int atom1 = mask[idx1];
     // Set up coord for this atom
-    const double* crd1 = fIn.XYZ( *atom1 );
+    const double* crd1 = fIn.XYZ( atom1 );
     // Set up exclusion list for this atom
-    Atom::excluded_iterator excluded_atom = tIn[*atom1].excludedbegin();
-    for (AtomMask::const_iterator atom2 = atom1 + 1; atom2 != mask.end(); ++atom2)
+    Atom::excluded_iterator excluded_atom = tIn[atom1].excludedbegin();
+    for (int idx2 = idx1 + 1; idx2 < mask.Nselected(); idx2++)
     {
+      int atom2 = mask[idx2];
       // If atom is excluded, just increment to next excluded atom.
-      if (excluded_atom != tIn[*atom1].excludedend() && *atom2 == *excluded_atom)
+      if (excluded_atom != tIn[atom1].excludedend() && atom2 == *excluded_atom)
         ++excluded_atom;
       else {
-        double rij2 = DIST2_NoImage( crd1, fIn.XYZ( *atom2 ) );
+        double rij2 = DIST2_NoImage( crd1, fIn.XYZ( atom2 ) );
         double rij = sqrt( rij2 );
         // Coulomb
-        double qiqj = QFAC * tIn[*atom1].Charge() * tIn[*atom2].Charge();
+        double qiqj = QFAC * tIn[atom1].Charge() * tIn[atom2].Charge();
         double e_elec = qiqj / rij;
         Eelec += e_elec;
 #       ifdef DEBUG_ENERGY
         mprintf("\tEELEC %4i -- %4i: q1= %12.5e  q2= %12.5e  r=  %12.5f  E= %12.5e\n",
-                *atom1, *atom2, tIn[*atom1].Charge(), tIn[*atom2].Charge(),
+                atom1+1, atom2+1, tIn[atom1].Charge(), tIn[atom2].Charge(),
                 rij, e_elec);
 #       endif
       }
     }
   }
+# ifdef _OPENMP
+  } // END omp parallel
+# endif
   time_NB_.Stop();
   return Eelec;
 }
