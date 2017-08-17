3e67c02772f07d08d59fdb8b0572de2a510e9239
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-08-17 16:13:14 -0400

    DRR - Cpptraj: Check test output order no longer varies based on number of threads

--- test/Test_CheckStructure/RunTest.sh
+++ test/Test_CheckStructure/RunTest.sh
@@ -5,7 +5,7 @@
 CleanFiles check.in report.dat nprob.dat
 
 MaxThreads 1 "Structure check test"
-if [[ $? -ne 0 ]] ; then
+if [ $? -ne 0 ] ; then
   EndTest
   exit 0
 fi
@@ -17,20 +17,7 @@ trajin tz2.stretched.pdb
 check reportfile report.dat offset 0.7 out nprob.dat Tz2Check
 EOF
 RunCpptraj "Structure Check"
-if [[ ! -z $OPENMP ]] ; then
-  # OpenMP check does not bother sorting for efficiency
-  SORT=`which sort`
-  if [[ -z $SORT ]] ; then
-    echo "Warning: 'sort' required for Structure Check OpenMP test. Skipping."
-    exit 0
-  fi
-  sort report.dat.save > sortedReport.save
-  sort report.dat      > sortedReport
-  DoTest sortedReport.save sortedReport
-  CleanFiles sortedReport.save sortedReport
-else
-  DoTest report.dat.save report.dat
-fi
+DoTest report.dat.save report.dat
 DoTest nprob.dat.save nprob.dat
 EndTest
 
