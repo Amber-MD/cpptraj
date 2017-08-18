c85e7ecd922724df2b6946bc482c83a602a896b8
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-08-18 09:20:21 -0400

    DRR - Cpptraj: Add check test with PBC. Can have one with multiple frames because its way faster now.

--- test/Test_CheckStructure/RunTest.sh
+++ test/Test_CheckStructure/RunTest.sh
@@ -2,23 +2,34 @@
 
 . ../MasterTest.sh
 
-CleanFiles check.in report.dat nprob.dat
-
-MaxThreads 1 "Structure check test"
-if [ $? -ne 0 ] ; then
-  EndTest
-  exit 0
-fi
+CleanFiles check.in report.dat nprob.dat tz2.dat
 
 INPUT="-i check.in"
-cat > check.in <<EOF
+
+MaxThreads 1 "Structure Check"
+if [ $? -eq 0 ] ; then
+  cat > check.in <<EOF
 parm ../tz2.parm7
 trajin tz2.stretched.pdb
 check reportfile report.dat offset 0.7 out nprob.dat Tz2Check
 EOF
-RunCpptraj "Structure Check"
-DoTest report.dat.save report.dat
-DoTest nprob.dat.save nprob.dat
+  RunCpptraj "Structure Check"
+  DoTest report.dat.save report.dat
+  DoTest nprob.dat.save nprob.dat
+fi
+MaxThreads 10 "Structure Check with PBC"
+if [ $? -eq 0 ] ; then
+  cat > check.in <<EOF
+parm ../tz2.truncoct.parm7
+trajin ../tz2.truncoct.nc
+rms first :1-13
+scale :1 x 2.0 y 1.2 z 1.2
+check reportfile tz2.dat offset 1.0
+EOF
+  RunCpptraj "Structure Check with PBC"
+  DoTest tz2.dat.save tz2.dat
+fi
+
 EndTest
 
 exit 0
