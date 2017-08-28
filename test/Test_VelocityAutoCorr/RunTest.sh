9f02488853d6c7bb9d2b71f07a724fe597c800eb
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-08-28 09:16:55 -0400

    DRR - Cpptraj: Update test for options change

--- test/Test_VelocityAutoCorr/RunTest.sh
+++ test/Test_VelocityAutoCorr/RunTest.sh
@@ -9,9 +9,9 @@ INPUT='-i vac.in'
 cat > vac.in <<EOF
 parm ../Test_systemVF/systemVF.parm7
 trajin ../Test_systemVF/systemVF.nc
-velocityautocorr out VAC.agr Vel usevelocity norm :WAT@O diffout diff.dat
-velocityautocorr out VAC.agr Crd norm :WAT@O
-velocityautocorr out VAC2.dat Direct usevelocity direct norm :WAT@O
+velocityautocorr out VAC.agr  Vel           norm :WAT@O diffout diff.dat
+velocityautocorr out VAC.agr  Crd           norm :WAT@O usecoords
+velocityautocorr out VAC2.dat Direct direct norm :WAT@O
 EOF
 RunCpptraj "Velocity autocorrelation test"
 DoTest VAC.agr.save VAC.agr
