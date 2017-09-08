db7732df8de5bdb0b45590cfde3e38f4ad1901c0
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-09-08 10:43:55 -0400

    DRR - Cpptraj: Updates upon updates...

--- test/Test_Mask/RunTest.sh
+++ test/Test_Mask/RunTest.sh
@@ -5,12 +5,14 @@
 # Clean
 CleanFiles mask.in mask.out mask.pdb.1 mask.mol2.1 M.dat
 
-CheckNetcdf
+TESTNAME='Mask command tests'
+Requires netcdf
 
 INPUT="-i mask.in"
 # Test 1
-MaxThreads 1 "Mask single frame tests"
-if [[ $? -eq 0 ]] ; then
+UNITNAME='Mask single frame test'
+CheckFor maxthreads 1
+if [ $? -eq 0 ] ; then
   cat > mask.in <<EOF
 noprogress
 parm ../tz2.ortho.parm7
@@ -18,10 +20,10 @@ trajin ../tz2.ortho.nc 1 1
 mask "(:5 <:3.0) & :WAT" maskout mask.out maskpdb mask.pdb
 mask "(:5 <:3.0) & :WAT" maskmol2 mask.mol2
 EOF
-RunCpptraj "Mask command test."
-DoTest mask.out.save mask.out
-DoTest mask.pdb.1.save mask.pdb.1
-DoTest mask.mol2.1.save mask.mol2.1
+  RunCpptraj "$UNITNAME"
+  DoTest mask.out.save mask.out
+  DoTest mask.pdb.1.save mask.pdb.1
+  DoTest mask.mol2.1.save mask.mol2.1
 fi
 
 cat > mask.in <<EOF
