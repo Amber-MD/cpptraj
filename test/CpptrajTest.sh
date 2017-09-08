ac12d59dd5b85c28e4ec00707b004488d03cf7a9
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-09-08 14:43:49 -0400

    DRR - Cpptraj: Direct error message to stderr

--- test/CpptrajTest.sh
+++ test/CpptrajTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f 'MasterTest.sh' ] ; then
-  echo "Fatal Error: MasterTest.sh not present"
+  echo "Fatal Error: MasterTest.sh not present" > /dev/stderr
   exit 1
 fi
 CPPTRAJ_TEST_MODE='master'
