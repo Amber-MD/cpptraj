b73b1ced2aec5dc2e156de9e2bb854f04cb79ece
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-09-14 11:22:16 -0400

    DRR - Cpptraj: Add short version of esander tests

---
+++ test/Test_Esander_Short/RunTest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles esander.in gb.dat pme.dat
+
+TESTNAME='Energy via sander API short tests (GB/PME)'
+Requires sanderlib
+
+INPUT='-i esander.in'
+cat > esander.in <<EOF
+parm ../Test_Hbond/strip.4lztSc_nowat.parm7
+trajin ../Test_Hbond/strip.4lztSc.rst7
+esander 4lztSc out gb.dat \
+  saltcon 1.0 cut 9999.0 gbsa 1 igb 1 ntb 0
+esander PME out pme.dat
+EOF
+RunCpptraj "$TESTNAME"
+DoTest gb.dat.save gb.dat
+DoTest pme.dat.save pme.dat
+
+EndTest
+exit 0
