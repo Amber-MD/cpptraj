c06a4582053ef5b9d3ad0b7d03c3c2014d1bfd82
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-15 13:24:44 -0400

    DRR - Cpptraj: Combine into one run

--- test/Test_DistBasedMask/RunTest.sh
+++ test/Test_DistBasedMask/RunTest.sh
@@ -14,23 +14,16 @@ INPUT='-i mask.in'
 cat > mask.in <<EOF
 parm ../tz2.parm7
 reference ../tz2.nc 1 [FIRST]
+reference ../tz2.nc lastframe [LAST]
 trajin ../tz2.nc 1 1
 
+activeref ref [FIRST]
 # Atoms outside 5 Ang from residue 2
 mask :2>@5.0 maskout First.dat maskpdb First.pdb
 # Atoms within 5 Ang of residue 2
 mask :2<@5.0 maskout Second.dat maskpdb Second.pdb
-run
-EOF
-RunCpptraj "$TESTNAME, by atom."
-DoTest First.pdb.1.save First.pdb.1
-DoTest Second.pdb.1.save Second.pdb.1
 
-cat > mask.in <<EOF
-parm ../tz2.parm7
-reference ../tz2.nc lastframe [LAST]
-trajin ../tz2.nc 1 1
-
+activeref ref [LAST]
 # Residues within 5 Ang of residue 2
 strip !(:2<:5.0)
 outtraj Third.pdb.1 pdb noter
@@ -38,9 +31,12 @@ unstrip
 # Residues outside 5 Ang from residue 2
 strip !(:2>:5.0)
 outtraj Fourth.pdb.1 pdb noter
+
 run
 EOF
-RunCpptraj "$TESTNAME, by residue."
+RunCpptraj "$TESTNAME"
+DoTest First.pdb.1.save First.pdb.1
+DoTest Second.pdb.1.save Second.pdb.1
 DoTest Third.pdb.1.save Third.pdb.1
 DoTest Fourth.pdb.1.save Fourth.pdb.1
 
