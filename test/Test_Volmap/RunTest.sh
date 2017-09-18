738e97623dbd157f0f471a056a97e7da858f6e90
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-18 13:03:26 -0400

    DRR - Cpptraj: Faster version of volmap test

--- test/Test_Volmap/RunTest.sh
+++ test/Test_Volmap/RunTest.sh
@@ -2,13 +2,28 @@
 
 . ../MasterTest.sh
 
-CleanFiles ptraj.in volmap.dx peaks.xyz volmap2.dx volmap3.dx
+CleanFiles ptraj.in volmap.dx peaks.xyz volmap2.dx volmap3.dx peaks1.xyz
 
 TESTNAME='VMD VolMap Algorithm test'
 Requires netcdf maxthreads 10
 TOP="../tz2.ortho.parm7"
 INPUT="ptraj.in"
+
 cat > ptraj.in <<EOF
+trajin ../tz2.ortho.nc
+rms first :1-13
+center :1-13 mass origin 
+volmap volmap3.dx 1.0 1.0 1.0 :WAT@O  centermask !:1-13 \
+       radscale 1.36 size 20,20,20 peakcut 0.10 peakfile peaks1.xyz
+EOF
+RunCpptraj "$TESTNAME"
+DoTest volmap3.dx.save volmap3.dx -r 0.00001
+DoTest peaks1.xyz.save peaks1.xyz -r 0.00001
+
+UNITNAME='VMD VolMap Algorithm longer tests'
+CheckFor long
+if [ $? -eq 0 ] ; then
+  cat > ptraj.in <<EOF
 trajin ../tz2.ortho.nc
 rms first :1-13
 center :1-13 mass origin 
@@ -16,14 +31,12 @@ volmap volmap.dx 0.5 0.5 0.5 :WAT@O buffer 2.0 centermask !:1-13 \
        radscale 1.36
 volmap volmap2.dx 0.5 0.5 0.5 :WAT@O buffer 2.0 centermask !:1-13 \
        radscale 1.36 peakcut 0.10 peakfile peaks.xyz
-volmap volmap3.dx 1.0 1.0 1.0 :WAT@O  centermask !:1-13 \
-       radscale 1.36 size 20,20,20
 EOF
-RunCpptraj "$TESTNAME"
-DoTest volmap.dx.save volmap.dx -r 0.00001
-DoTest volmap.dx.save volmap2.dx -r 0.00001
-DoTest peaks.xyz.save peaks.xyz -r 0.00001
-DoTest volmap3.dx.save volmap3.dx -r 0.00001
+  RunCpptraj "$UNITNAME"
+  DoTest volmap.dx.save volmap.dx -r 0.00001
+  DoTest volmap.dx.save volmap2.dx -r 0.00001
+  DoTest peaks.xyz.save peaks.xyz -r 0.00001
+fi
 
 EndTest
 
