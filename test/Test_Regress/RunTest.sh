64b0c870a2682390236dbf74b585226823ba0578
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-19 10:03:07 -0400

    DRR - Cpptraj: Test using slope and intercept from regress in 'createset'

--- test/Test_Regress/RunTest.sh
+++ test/Test_Regress/RunTest.sh
@@ -9,13 +9,14 @@ INPUT="-i regress.in"
 cat > regress.in <<EOF
 readdata ../Test_LowestCurve/esurf_vs_rmsd.dat.txt index 1 name XY
 regress XY out regress.dat name FitXY nx 100 statsout statsout.dat
-debug 10
-createset "Y = 0.319639 * X + 10.0554" xstep .2 nx 100
+runanalysis
+createset "Y = FitXY[slope] * X + FitXY[intercept]" xstep .2 nx 100
 writedata Y.dat Y
 EOF
 RunCpptraj "Linear regression test."
 DoTest statsout.dat.save statsout.dat
 DoTest regress.dat.save regress.dat
+DoTest Y.dat.save Y.dat
 
 EndTest
 exit 0
