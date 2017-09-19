4faf4363147b5c5cfd209c178c60bab3c37962ea
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-19 13:27:41 -0400

    DRR - Cpptraj: Add pairwise reference test

--- test/Test_Pairwise/RunTest.sh
+++ test/Test_Pairwise/RunTest.sh
@@ -2,7 +2,8 @@
 
 . ../MasterTest.sh
 
-CleanFiles pw.in pair.dat energy.dat avg.dat map.elec.gnu map.vdw.gnu
+CleanFiles pw.in pair.dat energy.dat avg.dat map.elec.gnu map.vdw.gnu \
+           ref.ene.pdb ref.cut.mol2.*.mol2.?
 
 TESTNAME='Pairwise test'
 Requires netcdf
@@ -15,6 +16,12 @@ trajin ../Test_SymmRmsd/TYR.nc
 pairwise TYR out pair.dat eout energy.dat avgout avg.dat \
          vmapout map.vdw.gnu emapout map.elec.gnu \
          printmode or cuteelec 2.0 cutevdw 2.0
+run
+
+clear trajin
+reference ../Test_SymmRmsd/TYR.nc 1
+trajin ../Test_SymmRmsd/TYR.nc 2 2
+pairwise REF reference cutout ref.cut.mol2 pdbout ref.ene.pdb
 EOF
 RunCpptraj "$TESTNAME"
 DoTest pair.dat.save pair.dat
@@ -22,6 +29,9 @@ DoTest energy.dat.save energy.dat
 DoTest avg.dat.save avg.dat
 DoTest map.vdw.gnu.save map.vdw.gnu
 DoTest map.elec.gnu.save map.elec.gnu
+DoTest ref.cut.mol2.eelec.mol2.1.save ref.cut.mol2.eelec.mol2.1
+DoTest ref.cut.mol2.evdw.mol2.1.save ref.cut.mol2.evdw.mol2.1
+DoTest ref.ene.pdb.save ref.ene.pdb
 
 EndTest
 exit 0
