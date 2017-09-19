0ad460f27248f4c59c221e2236ee144efe85537b
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-19 09:47:18 -0400

    DRR - Cpptraj: Text 'nx' instead; smaller output set.

--- test/Test_Regress/RunTest.sh
+++ test/Test_Regress/RunTest.sh
@@ -2,17 +2,20 @@
 
 . ../MasterTest.sh
 
-CleanFiles regress.in fit.dat statsout.dat
+CleanFiles regress.in fit.dat statsout.dat Y.dat regress.dat
 
 INPUT="-i regress.in"
 
 cat > regress.in <<EOF
 readdata ../Test_LowestCurve/esurf_vs_rmsd.dat.txt index 1 name XY
-regress XY out fit.dat name FitXY statsout statsout.dat
+regress XY out regress.dat name FitXY nx 100 statsout statsout.dat
+debug 10
+createset "Y = 0.319639 * X + 10.0554" xstep .2 nx 100
+writedata Y.dat Y
 EOF
 RunCpptraj "Linear regression test."
-DoTest fit.dat.save fit.dat
 DoTest statsout.dat.save statsout.dat
+DoTest regress.dat.save regress.dat
 
 EndTest
 exit 0
