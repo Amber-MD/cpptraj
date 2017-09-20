3f3574df495484893355100f8d7e38abde26edbb
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-20 10:45:26 -0400

    DRR - Cpptraj: Add scaledihedralk test

---
+++ test/Test_ScaleDihedralK/RunTest.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles scale.in scale.res1.0.5.dat scale.N.C.CA.0.5.dat
+
+INPUT='-i scale.in'
+
+cat > scale.in <<EOF
+parm ../tz2.parm7
+dihedrals :1 out res1.0.dat #DEBUG
+scaledihedralk 0.5
+dihedrals :1 out scale.res1.0.5.dat
+EOF
+RunCpptraj "Scale dihedral force constant test"
+DoTest scale.res1.0.5.dat.save scale.res1.0.5.dat
+
+cat > scale.in <<EOF
+parm ../tz2.parm7
+scaledihedralk 0.5 @N,C,CA useall
+dihedrals :1 out scale.N.C.CA.0.5.dat
+EOF
+RunCpptraj "Scale dihedral force constant with 'useall'"
+DoTest scale.N.C.CA.0.5.dat.save scale.N.C.CA.0.5.dat
+
+EndTest
+exit 0
