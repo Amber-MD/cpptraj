e9817beeed3de19fe42c603173bb48b2813e1309
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-27 16:15:17 -0400

    DRR - Cpptraj: Var name change

--- src/Control.cpp
+++ src/Control.cpp
@@ -6,9 +6,9 @@ void Control_For::Help() const {
 }
 
 int Control_For::SetupControl(ArgList& argIn) {
-  // for <var> in <mask> do
-  std::string in_arg = argIn.GetStringKey("in");
-  if (in_arg.empty()) {
+  // for <var> inmask <mask> do
+  std::string inmask_arg = argIn.GetStringKey("inmask");
+  if (inmask_arg.empty()) {
     mprinterr("Error: 'for': expected 'in <expression>'\n");
     return 1;
   }
@@ -21,6 +21,6 @@ int Control_For::SetupControl(ArgList& argIn) {
     mprinterr("Error: 'for': missing variable name.\n");
     return 1;
   }
-  mprintf("DEBUG: for %s in %s do\n", varname_.c_str(), in_arg.c_str());
+  mprintf("DEBUG: for %s in %s do\n", varname_.c_str(), inmask_arg.c_str());
   return 0;
 }
