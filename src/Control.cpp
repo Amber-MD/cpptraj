184a77528a1b124d2856ca18090b83e1187addd0
Daniel R. Roe <daniel.roe@nih.gov>
2017-09-28 14:57:32 -0400

    DRR - Cpptraj: Add some error checking and improve output messages.

--- src/Control.cpp
+++ src/Control.cpp
@@ -1,3 +1,4 @@
+#include <algorithm> // std::min
 #include "Control.h"
 #include "CpptrajStdio.h"
 #include "StringRoutines.h"
@@ -6,12 +7,15 @@ void Control_For_Mask::Help() const {
   mprintf("\t{atoms|residues|molecules} <var> in <mask> %s\n", DataSetList::TopIdxArgs);
 }
 
+/** Set up each mask. */
 int Control_For_Mask::SetupControl(CpptrajState& State, ArgList& argIn) {
   Masks_.clear();
-  description_.clear();
   Topology* currentTop = 0;
+  static const char* TypeStr[] = { "ATOMS ", "RESIDUES ", "MOLECULES " };
+  description_.assign("for ");
   // formask {atoms|residues|molecules} <var> inmask <mask> [TOP KEYWORDS] ...
   std::string inmask_arg = argIn.GetStringKey("in");
+  int Nselected = -1;
   while (!inmask_arg.empty()) {
     Masks_.push_back( MaskHolder() );
     MaskHolder& MH = Masks_.back();
@@ -35,11 +39,17 @@ int Control_For_Mask::SetupControl(CpptrajState& State, ArgList& argIn) {
     if (currentTop->SetupIntegerMask( MH.mask_ )) return 1;
     MH.mask_.MaskInfo();
     if (MH.mask_.None()) return 1;
-    static const char* TypeStr[] = { "ATOMS ", "RESIDUES ", "MOLECULES " };
-    description_.append("for " + std::string(TypeStr[MH.varType_]) +
-                        MH.varname_ + " inmask " + MH.mask_.MaskExpression() + " do ");
+    if (Nselected == -1)
+      Nselected = MH.mask_.Nselected();
+    else if (MH.mask_.Nselected() != Nselected)
+      mprintf("Warning: # selected %i != previous # selected %i\n",
+              MH.mask_.Nselected(), Nselected);
+    if (description_ != "for ") description_.append(", ");
+    description_.append(std::string(TypeStr[MH.varType_]) +
+                        MH.varname_ + " in " + MH.mask_.MaskExpression());
     inmask_arg = argIn.GetStringKey("in");
   }
+  description_.append(" do");
 
   return 0;
 }
