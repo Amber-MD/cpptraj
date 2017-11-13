a33a0afaa26a99a540f335775c433aa4aca38dcb
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-11-13 16:32:34 -0500

    DRR - Cpptraj: Modify DataSet_pH_REMD to use CphResidue.

--- src/DataSet_pH_REMD.cpp
+++ src/DataSet_pH_REMD.cpp
@@ -8,24 +8,19 @@ DataSet_pH_REMD::DataSet_pH_REMD() :
 
 int DataSet_pH_REMD::Allocate(SizeArray const& sizeIn) {
   if (!sizeIn.empty()) {
-    for (Rarray::iterator res = residues_.begin(); res != residues_.end(); ++res)
-      res->Allocate( sizeIn[0] );
+    resStates_.reserve( sizeIn[0] * residues_.size() );
     solvent_pH_.reserve( sizeIn[0] );
   }
   return 0;
 }
 
 void DataSet_pH_REMD::Resize(size_t n) {
-  for (Rarray::iterator res = residues_.begin(); res != residues_.end(); ++res)
-    res->Resize( n );
+  resStates_.resize(n * residues_.size(), 0);
   solvent_pH_.resize(n, 0.0);
 }
 
 void DataSet_pH_REMD::Info() const {
-  mprintf(" (%zu residues", residues_.size());
-  if (!residues_.empty())
-    mprintf(", %zu frames", residues_[0].Nframes());
-  mprintf(")");
+  mprintf(" (%zu residues, %zu frames)", residues_.size(), solvent_pH_.size());
 }
 # ifdef MPI
 void DataSet_pH_REMD::Consolidate(Parallel::Comm const& commIn, int rank)
@@ -41,12 +36,8 @@ void DataSet_pH_REMD::Consolidate(Parallel::Comm const& commIn, int rank)
     commIn.Reduce(rank, &solvent_pH_[0], &ftmp[0], solvent_pH_.size(), MPI_FLOAT, MPI_SUM);
     ftmp.clear();
     if (!residues_.empty()) {
-      std::vector<int> itmp;
-      itmp.reserve( Nframes() );
-      for (Rarray::iterator res = residues_.begin(); res != residues_.end(); ++res) {
-        itmp = res->States();
-        commIn.Reduce(rank, res->StatesPtr(), &itmp[0], res->States().size(), MPI_INT, MPI_SUM);
-      }
+      std::vector<int> itmp = resStates_;
+      commIn.Reduce(rank, &resStates_[0], &itmp[0], resStates_.size(), MPI_INT, MPI_SUM);
     }
   } else {
     //commIn.Barrier(); // DEBUG
@@ -56,45 +47,8 @@ void DataSet_pH_REMD::Consolidate(Parallel::Comm const& commIn, int rank)
     //  rprintf("%4u %1i\n", it-residues_.front().begin()+1, *it);
     commIn.Reduce(rank, 0, &solvent_pH_[0], solvent_pH_.size(), MPI_FLOAT, MPI_SUM);
     if (!residues_.empty()) {
-      for (Rarray::iterator res = residues_.begin(); res != residues_.end(); ++res)
-        commIn.Reduce(rank, 0, res->StatesPtr(), res->States().size(), MPI_INT, MPI_SUM);
+      commIn.Reduce(rank, 0, &resStates_[0], resStates_.size(), MPI_INT, MPI_SUM);
     }
   }
 }
 #endif
-
-// =============================================================================
-DataSet_pH_REMD::Residue::Residue(NameType const& rname, int resid,
-                             Iarray const& protcnts, int max_prots) :
-  resname_(rname), resid_(resid), protcnts_(protcnts)
-{
-  protonated_.reserve(protcnts_.size());
-  for (Iarray::const_iterator it = protcnts_.begin(); it != protcnts_.end(); ++it)
-    protonated_.push_back(*it == max_prots);
-}
-
-DataSet_pH_REMD::Residue::Residue(Residue const& rhs) :
-  resname_(rhs.resname_),
-  resid_(rhs.resid_),
-  protcnts_(rhs.protcnts_),
-  protonated_(rhs.protonated_),
-  states_(rhs.states_)
-{}
-
-DataSet_pH_REMD::Residue& DataSet_pH_REMD::Residue::operator=(Residue const& rhs) {
-  if (this != &rhs) {
-    resname_ = rhs.resname_;
-    resid_ = rhs.resid_;
-    protcnts_ = rhs.protcnts_;
-    protonated_ = rhs.protonated_;
-    states_ = rhs.states_;
-  }
-  return *this;
-}
-
-void DataSet_pH_REMD::Residue::Print() const {
-  mprintf("\t%s %8i (%zu frames)", *resname_, resid_, states_.size());
-  for (unsigned int i = 0; i != protcnts_.size(); i++)
-    mprintf(" %2i (%1i),", protcnts_[i], (int)protonated_[i]);
-  mprintf("\n");
-}
--- src/DataSet_pH_REMD.h
+++ src/DataSet_pH_REMD.h
@@ -1,7 +1,7 @@
 #ifndef INC_DATASET_PH_REMD_H
 #define INC_DATASET_PH_REMD_H
 #include "DataSet.h"
-#include "NameType.h"
+#include "CphResidue.h"
 /// Hold unsorted data from constant pH REMD simulations; protonation states of each residue.
 class DataSet_pH_REMD : public DataSet {
     typedef std::vector<int> Iarray;
@@ -10,83 +10,18 @@ class DataSet_pH_REMD : public DataSet {
   public:
     DataSet_pH_REMD();
     static DataSet* Alloc() { return (DataSet*)new DataSet_pH_REMD(); }
-    // -------------------------------------------
-    class Residue {
-      public:
-        Residue() : resid_(-1) {}
-        /// Res name, num, protcnts, max prot
-        Residue(NameType const&, int, Iarray const&, int);
-        Residue(int nframes) : states_(nframes, 0) {}
-        /// COPY
-        Residue(Residue const&);
-        /// ASSIGN
-        Residue& operator=(Residue const&);
-        int& operator[](int n)          { return states_[n];        }
-        int operator[](int n) const     { return states_[n];        }
-        void push_back(int state)       { states_.push_back(state); }
-        unsigned int Nframes()    const { return states_.size();    }
-        typedef Iarray::const_iterator const_iterator;
-        const_iterator begin()    const { return states_.begin(); }
-        const_iterator end()      const { return states_.end();   }
-        void Print() const;
-        Iarray const& States()    const { return states_; }
-        int State(unsigned int n) const { return states_[n];       }
-        void Allocate(unsigned int n)   { states_.reserve( n );    }
-        void Resize(unsigned int n)     { states_.assign( n, 0 );  }
-#       ifdef MPI
-        int* StatesPtr()                { return &states_[0]; }
-#       endif
-        bool IsProtonated(int s)  const { return protonated_[s]; }
-        int Nprotons(int s)       const { return protcnts_[s];   }
-        NameType const& Name()    const { return resname_;       }
-        int Num()                 const { return resid_;         }
-      private:
-        NameType resname_;  ///< Residue name.
-        int resid_;         ///< Residue number.
-        Iarray protcnts_;   ///< Hold protonation count for each state.
-        Barray protonated_; ///< True if state is protonated.
-        Iarray states_;     ///< Hold protonation state for each frame.
-    };
-    typedef std::vector<Residue> Rarray;
+
+    typedef std::vector<CphResidue> Rarray;
     typedef Rarray::const_iterator const_iterator;
-    // -------------------------------------------
     // Residue functions
     void SetResidueInfo(Rarray const& r) { residues_ = r; }
     Rarray const& Residues()       const { return residues_; }
-    const_iterator begin()         const { return residues_.begin(); }
-    const_iterator end()           const { return residues_.end();   }
-    Residue const& Res(int idx)    const { return residues_[idx]; }
+    CphResidue const& Res(int idx) const { return residues_[idx]; }
 
     typedef Farray::const_iterator ph_iterator;
     Farray const& pH_Values() const { return solvent_pH_; }
 
-    // NOTE: Bounds check should be done outside of here.
-    void AddState(Iarray const& resStates, float pH) {
-      for (unsigned int res = 0; res < resStates.size(); res++)
-        residues_[res].push_back( resStates[res] );
-      solvent_pH_.push_back( pH );
-    }
-    /// Set pH and state for specified frame/residue
-    void SetState(unsigned int res, int frame, int state, float pH) {
-      residues_[res][frame] = state;
-      solvent_pH_[frame] = pH; // TODO should pass in an array for res?
-    }
-    /// Resize pH array and state array for each residue.
     void Resize(size_t);
-    /// \return number of frames
-    unsigned int Nframes() const {
-      if (residues_.empty())
-        return 0;
-      else
-        return residues_.front().Nframes();
-    }
-    /// \return Solvent pH (FIXME not good for unsorted data)
-    float Solvent_pH() const {
-      if (solvent_pH_.empty())
-        return -1.0;
-      else
-        return solvent_pH_.front();
-    }
     // ----- DataSet functions -------------------
     size_t Size() const { return residues_.size(); }
     void Info()   const;
@@ -100,7 +35,8 @@ class DataSet_pH_REMD : public DataSet {
     void Consolidate(Parallel::Comm const&, int);
 #   endif
   private:
-    Farray solvent_pH_;    ///< Solvent pH values each frame
     Rarray residues_;      ///< Array of residues.
+    Farray solvent_pH_;    ///< Solvent pH values each frame
+    Iarray resStates_;     ///< State of each residue each frame: R00, R10, R20, R01, ...
 };
 #endif
