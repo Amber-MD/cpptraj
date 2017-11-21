1d2f90ada015f9d5d6d0df7420d5346c8679c8e3
Daniel R. Roe <daniel.roe@nih.gov>
2017-11-21 15:21:56 -0500

    DRR - Cpptraj: Get rid of old code

--- src/DataSet_pH.cpp
+++ src/DataSet_pH.cpp
@@ -8,7 +8,6 @@ DataSet_pH::DataSet_pH() :
 int DataSet_pH::Allocate(SizeArray const& sizeIn) {
   if (!sizeIn.empty()) {
     states_.reserve( sizeIn[0] );
-    //solvent_pH_.reserve( sizeIn[0] );
   }
   return 0;
 }
@@ -22,7 +21,6 @@ void DataSet_pH::WriteBuffer(CpptrajFile &cbuffer, SizeArray const& pIn) const {
 
 void DataSet_pH::Resize(size_t n) {
   states_.resize( n );
-  //solvent_pH_.resize(n, 0.0);
 }
 
 void DataSet_pH::Info() const {
@@ -38,30 +36,15 @@ void DataSet_pH::Consolidate(Parallel::Comm const& commIn, int rank)
     //  rprintf("%4u %1i\n", it-residues_.front().begin()+1, *it);
     //mflush();
     //commIn.Barrier(); // DEBUG
-    std::vector<float> ftmp = pH_Values_;
-    commIn.Reduce(rank, &pH_Values_[0], &ftmp[0], pH_Values_.size(), MPI_FLOAT, MPI_SUM);
-    ftmp.clear();
     std::vector<int> itmp = states_;
-    commIn.Reduce(rank, &states_[0],    &itmp[0], states_.size(),    MPI_INT,   MPI_SUM);
+    commIn.Reduce(rank, &states_[0], &itmp[0], states_.size(), MPI_INT, MPI_SUM);
   } else {
     //commIn.Barrier(); // DEBUG
     //commIn.Barrier();
     //rprintf("CHILD_:\n");
     //for (std::vector<int>::const_iterator it = residues_.front().begin(); it != residues_.front().end(); ++it)
     //  rprintf("%4u %1i\n", it-residues_.front().begin()+1, *it);
-    commIn.Reduce(rank, 0, &pH_Values_[0], pH_Values_.size(), MPI_FLOAT, MPI_SUM);
-    commIn.Reduce(rank, 0, &states_[0],    states_.size(),    MPI_INT,   MPI_SUM);
+    commIn.Reduce(rank, 0, &states_[0], states_.size(), MPI_INT, MPI_SUM);
   }
 }
 #endif
-
-/* DataSet_pH::Residue& DataSet_pH::Residue::operator=(Residue const& rhs) {
-  if (this != &rhs) {
-    resname_ = rhs.resname_;
-    resid_ = rhs.resid_;
-    protcnts_ = rhs.protcnts_;
-    protonated_ = rhs.protonated_;
-    states_ = rhs.states_;
-  }
-  return *this;
-}*/
