b1d43e36710fcee98da6f8af992502d159dace61
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2017-12-16 23:34:18 -0800

    Give up on splitting the line.  It's a good thing I have two monitors...

--- devtools/ci/appveyor/setup-environment.bat
+++ devtools/ci/appveyor/setup-environment.bat
@@ -8,17 +8,7 @@ set CC=%MINGWPREFIX%-gcc.exe
 set CXX=%MINGWPREFIX%-g++.exe
 set FC=%MINGWPREFIX%-gfortran.exe
 sh -lc "pacman -S --noconfirm --needed mingw-w64-x86_64-openblas mingw-w64-x86_64-arpack mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-ncurses mingw-w64-x86_64-readline diffutils"
-sh -lc "^
- if [ ! -f /usr/local/lib/libnetcdf.a ]; then^
-   curl -fsS -o netcdf-4.3.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.zip &&^
-   7z x netcdf-4.3.3.zip -o$APPVEYOR_BUILD_FOLDER > /dev/null &&^
-   cd $APPVEYOR_BUILD_FOLDER/netcdf-4.3.3 &&^
-   exec 0</dev/null &&^
-   ./configure --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap &&^
-   make -r install;^
- else^
-   echo 'Have Cached NetCDF';^
- fi"
+sh -lc "if [ ! -f /usr/local/lib/libnetcdf.a ]; then curl -fsS -o netcdf-4.3.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.zip && 7z x netcdf-4.3.3.zip -o$APPVEYOR_BUILD_FOLDER > /dev/null && cd $APPVEYOR_BUILD_FOLDER/netcdf-4.3.3 && exec 0</dev/null && ./configure --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap && make -r install; else echo 'Have Cached NetCDF'; fi"
 
 if %USE_CMAKE equ 1 (
 		
