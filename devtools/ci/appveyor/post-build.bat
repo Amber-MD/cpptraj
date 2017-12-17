60a24b7e7895cb0eb746659c98438ffc17bdb2fd
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2017-12-17 01:06:00 -0800

    Use cmake's install instead of copying files

--- devtools/ci/appveyor/post-build.bat
+++ devtools/ci/appveyor/post-build.bat
@@ -6,10 +6,6 @@ if %USE_CMAKE% equ 1 (
 	rem move and rename already created package file
 	echo F | xcopy /Y /F build\cpptraj.zip .\cpptraj-%APPVEYOR_BUILD_ID%.zip
 	
-	rem move executables to bin folder
-	mkdir bin
-	xcopy /Y /F /I build\src\*.exe bin\
-	
 ) else (
 	7z a cpptraj-%APPVEYOR_BUILD_ID%.zip bin/ambpdb.exe bin/cpptraj.exe lib/libcpptraj.dll.a src/*.h
 )
