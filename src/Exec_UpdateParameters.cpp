99144ddb62f6dc0c7ff1e92db04805e4c0a3b0ee
Daniel R. Roe <daniel.roe@nih.gov>
2018-01-18 14:39:39 -0500

    DRR - Cpptraj: Update dihedral, improper, and UB parameters

--- src/Exec_UpdateParameters.cpp
+++ src/Exec_UpdateParameters.cpp
@@ -29,6 +29,18 @@ static inline void AngleTypes(ParmHolder<int>& ParmIndices, Topology const& top,
   }
 }
 
+static inline void DihedralTypes(ParmHolder<int>& ParmIndices, Topology const& top, DihedralArray const& dih) {
+  for (DihedralArray::const_iterator b = dih.begin(); b != dih.end(); ++b)
+  {
+    AtomTypeHolder types(3);
+    types.AddName( top[b->A1()].Type() );
+    types.AddName( top[b->A2()].Type() );
+    types.AddName( top[b->A3()].Type() );
+    types.AddName( top[b->A4()].Type() );
+    ParmIndices.AddParm( types, b->Idx(), false );
+  }
+}
+
 // Exec_UpdateParameters::Execute()
 Exec::RetType Exec_UpdateParameters::Execute(CpptrajState& State, ArgList& argIn)
 {
@@ -54,12 +66,13 @@ Exec::RetType Exec_UpdateParameters::Execute(CpptrajState& State, ArgList& argIn
   }
   Topology& top = static_cast<Topology&>( *dstop );
 
-  // Get list of existing parameters 
+  // Get list of existing parameters.
+  // We assume a parameter in topology is never repeated, so as soon
+  // as it is found we can move to the next one. 
   ParmHolder<int> ParmIndices;
   // Bond parameters
   BondTypes(ParmIndices, top, top.Bonds());
   BondTypes(ParmIndices, top, top.BondsH());
-  // Update bond parameters. We assume a parameter in top is never repeated.
   for (ParmHolder<BondParmType>::const_iterator it1 = prm.BP().begin();
                                                 it1 != prm.BP().end(); ++it1)
   {
@@ -81,7 +94,6 @@ Exec::RetType Exec_UpdateParameters::Execute(CpptrajState& State, ArgList& argIn
   // Angle parameters
   AngleTypes(ParmIndices, top, top.Angles());
   AngleTypes(ParmIndices, top, top.AnglesH());
-  // Update bond parameters. We assume a parameter in top is never repeated.
   for (ParmHolder<AngleParmType>::const_iterator it1 = prm.AP().begin();
                                                  it1 != prm.AP().end(); ++it1)
   {
@@ -98,6 +110,73 @@ Exec::RetType Exec_UpdateParameters::Execute(CpptrajState& State, ArgList& argIn
         break;
       }
     }
+  }
+  ParmIndices.clear();
+  // Dihedral parameters
+  DihedralTypes(ParmIndices, top, top.Dihedrals());
+  DihedralTypes(ParmIndices, top, top.DihedralsH());
+  for (ParmHolder<DihedralParmType>::const_iterator it1 = prm.DP().begin();
+                                                    it1 != prm.DP().end(); ++it1)
+  {
+    for (ParmHolder<int>::const_iterator it0 = ParmIndices.begin();
+                                         it0 != ParmIndices.end(); ++it0)
+    {
+      if (it1->first == it0->first)
+      {
+        DihedralParmType& bp = top.SetDihedralParm(it0->second);
+        mprintf("\tUpdating dihedral parameter %s - %s - %s  %s from %f %f %f to %f %f %f\n",
+                *(it0->first[0]), *(it0->first[1]), *(it0->first[2]), *(it0->first[3]),
+                bp.Pk(), bp.Pn(), bp.Phase(),
+                it1->second.Pk(), it1->second.Pn(), it1->second.Phase());
+        bp = it1->second;
+        break;
+      }
+    }
+  }
+  ParmIndices.clear();
+  if (top.Chamber().HasChamber()) {
+    ChamberParmType& chm = top.SetChamber();
+    // UB parameters
+    BondTypes(ParmIndices, top, chm.UB());
+    for (ParmHolder<BondParmType>::const_iterator it1 = prm.UB().begin();
+                                                  it1 != prm.UB().end(); ++it1)
+    {
+      for (ParmHolder<int>::const_iterator it0 = ParmIndices.begin();
+                                           it0 != ParmIndices.end(); ++it0)
+      {
+        if (it1->first == it0->first)
+        {
+          BondParmType& bp = chm.SetUBparm(it0->second);
+          mprintf("\tUpdating UB parameter %s - %s from %f %f to %f %f\n",
+                  *(it0->first[0]), *(it0->first[1]), bp.Rk(), bp.Req(),
+                  it1->second.Rk(), it1->second.Req());
+          bp = it1->second;
+          break;
+        }
+      }
+    }
+    ParmIndices.clear();
+    // Improper parameters
+    DihedralTypes(ParmIndices, top, chm.Impropers());
+    for (ParmHolder<DihedralParmType>::const_iterator it1 = prm.IP().begin();
+                                                      it1 != prm.IP().end(); ++it1)
+    {
+      for (ParmHolder<int>::const_iterator it0 = ParmIndices.begin();
+                                           it0 != ParmIndices.end(); ++it0)
+      {
+        if (it1->first == it0->first)
+        {
+          DihedralParmType& bp = chm.SetImproperParm(it0->second);
+          mprintf("\tUpdating improper parameter %s - %s - %s  %s from %f %f %f to %f %f %f\n",
+                  *(it0->first[0]), *(it0->first[1]), *(it0->first[2]), *(it0->first[3]),
+                  bp.Pk(), bp.Pn(), bp.Phase(),
+                  it1->second.Pk(), it1->second.Pn(), it1->second.Phase());
+          bp = it1->second;
+          break;
+        }
+      }
+    }
+    ParmIndices.clear();
   }
 
 
