575fdc36f2f83676be9042d6e673c991fc054e00
Daniel R. Roe <daniel.r.roe@gmail.com>
2018-02-08 15:49:47 -0500

    DRR - Cpptraj: Trap bad numbers of threads

--- test/Test_Cphstats/RunTest.sh
+++ test/Test_Cphstats/RunTest.sh
@@ -8,7 +8,22 @@ INPUT='-i cphstats.in'
 TESTNAME='Constant pH stats / data set sort test'
 
 UNITNAME='Ensemble data read / sort'
-cat > cphstats.in <<EOF
+SKIP='no'
+if [ $N_THREADS -lt 7 ] ; then
+  if [ $N_THREADS -eq 4 -o $N_THREADS -eq 5 ] ; then
+     echo "  $UNITNAME cannot be run with 4 or 5 threads."
+     ((CHECKERR++))
+     SkipCheck "$UNITNAME"
+     SKIP='yes'
+  fi
+else
+  CheckFor nthreads 6
+  if [ $? -eq 1 ] ; then
+    SKIP='yes'
+  fi
+fi
+if [ "$SKIP" = 'no' ] ; then
+  cat > cphstats.in <<EOF
 readensembledata cpout.001 cpin cpin name PH
 #readensembledata cpout.001 filenames cpout.002,cpout.003,cpout.004,cpout.005,cpout.006 name PH
 list dataset
@@ -18,13 +33,14 @@ for i=0;i<6;i++ j=1;j++
   writedata sorted.pH_\$j.00 as cpout PH[*]%\$i nwriteheader 50 noensextension
 done
 EOF
-RunCpptraj "$UNITNAME"
-DoTest sorted.pH_1.00.save sorted.pH_1.00
-DoTest sorted.pH_2.00.save sorted.pH_2.00
-DoTest sorted.pH_3.00.save sorted.pH_3.00
-DoTest sorted.pH_4.00.save sorted.pH_4.00
-DoTest sorted.pH_5.00.save sorted.pH_5.00
-DoTest sorted.pH_6.00.save sorted.pH_6.00
+  RunCpptraj "$UNITNAME"
+  DoTest sorted.pH_1.00.save sorted.pH_1.00
+  DoTest sorted.pH_2.00.save sorted.pH_2.00
+  DoTest sorted.pH_3.00.save sorted.pH_3.00
+  DoTest sorted.pH_4.00.save sorted.pH_4.00
+  DoTest sorted.pH_5.00.save sorted.pH_5.00
+  DoTest sorted.pH_6.00.save sorted.pH_6.00
+fi
 
 UNITNAME='Constant pH stats test'
 cat > cphstats.in <<EOF
