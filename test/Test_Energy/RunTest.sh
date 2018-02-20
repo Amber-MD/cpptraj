e10a5ec490dfeab8e7c7aed7b42a6d24dc3fdb3b
Daniel R. Roe <daniel.roe@nih.gov>
2018-02-20 16:01:39 -0500

    DRR - Cpptraj: Add kinetic energy test

--- test/Test_Energy/RunTest.sh
+++ test/Test_Energy/RunTest.sh
@@ -2,11 +2,11 @@
 
 . ../MasterTest.sh
 
-CleanFiles ene.in ene.agr
+CleanFiles ene.in ene.agr short.dat
 
 INPUT="-i ene.in"
 
-TESTNAME='Simple energy test'
+TESTNAME='Simple energy tests'
 Requires netcdf
 
 cat > ene.in <<EOF
@@ -17,5 +17,16 @@ EOF
 RunCpptraj "$TESTNAME"
 DoTest ene.agr.save ene.agr
 
+UNITNAME='Test kinetic energy calculation'
+CheckFor maxthreads 2
+if [ $? -eq 0 ] ; then
+  cat > ene.in <<EOF
+parm ../tz2.nhe.parm7
+trajin ../Test_VelFrc/short.crd mdvel ../Test_VelFrc/short.vel mdfrc ../Test_VelFrc/short.frc
+energy kinetic Short out short.dat dt 0.002
+EOF
+  RunCpptraj "$UNITNAME"
+  DoTest short.dat.save short.dat
+fi
 EndTest
 exit 0
