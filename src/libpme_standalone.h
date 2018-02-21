378bed93f8d8f6a4331405bab8e11b0c17f48ed5
Daniel R. Roe <daniel.roe@nih.gov>
2018-02-21 15:38:13 -0500

    DRR - Cpptraj: Disable the non-double precision templates for diagonalizer wrappers; cpptraj doesnt use them and the LAPACK that comes with Amber only has double precision.

--- src/libpme_standalone.h
+++ src/libpme_standalone.h
@@ -751,29 +751,29 @@ struct gammaComputer {
 #endif
 
 extern "C" {
-extern void F_SGEEV(char *, char *, int *, float *, int *, float *, float *, float *, int *, float *, int *, float *,
-                    int *, int *);
+/*extern void F_SGEEV(char *, char *, int *, float *, int *, float *, float *, float *, int *, float *, int *, float *,
+                    int *, int *);*/
 extern void F_DGEEV(char *, char *, int *, double *, int *, double *, double *, double *, int *, double *, int *,
                     double *, int *, int *);
-extern void F_CGEEV(char *, char *, int *, std::complex<float> *, int *, std::complex<float> *, std::complex<float> *,
+/*extern void F_CGEEV(char *, char *, int *, std::complex<float> *, int *, std::complex<float> *, std::complex<float> *,
                     std::complex<float> *, int *, std::complex<float> *, int *, std::complex<float> *, int *, int *);
 extern void F_ZGEEV(char *, char *, int *, std::complex<double> *, int *, std::complex<double> *,
                     std::complex<double> *, std::complex<double> *, int *, std::complex<double> *, int *,
-                    std::complex<double> *, int *, int *);
+                    std::complex<double> *, int *, int *);*/
 }
 
 namespace libpme {
-
+/*
 static void C_SGEEV(char jobvl, char jobvr, int n, float *a, int lda, float *wr, float *wi, float *vl, int ldvl,
                     float *vr, int ldvr, float *work, int lwork, int *info) {
     ::F_SGEEV(&jobvl, &jobvr, &n, a, &lda, wr, wi, vl, &ldvl, vr, &ldvr, work, &lwork, info);
 }
-
+*/
 static void C_DGEEV(char jobvl, char jobvr, int n, double *a, int lda, double *wr, double *wi, double *vl, int ldvl,
                     double *vr, int ldvr, double *work, int lwork, int *info) {
     ::F_DGEEV(&jobvl, &jobvr, &n, a, &lda, wr, wi, vl, &ldvl, vr, &ldvr, work, &lwork, info);
 }
-
+/*
 static void C_CGEEV(char jobvl, char jobvr, int n, std::complex<float> *a, int lda, std::complex<float> *wr,
                     std::complex<float> *wi, std::complex<float> *vl, int ldvl, std::complex<float> *vr, int ldvr,
                     std::complex<float> *work, int lwork, int *info) {
@@ -785,7 +785,7 @@ static void C_ZGEEV(char jobvl, char jobvr, int n, std::complex<double> *a, int 
                     std::complex<double> *work, int lwork, int *info) {
     ::F_ZGEEV(&jobvl, &jobvr, &n, a, &lda, wr, wi, vl, &ldvl, vr, &ldvr, work, &lwork, info);
 }
-
+*/
 template <typename Real>
 using diagonalizerType =
     std::function<void(char, char, int, Real *, int, Real *, Real *, Real *, int, Real *, int, Real *, int, int *)>;
@@ -798,23 +798,23 @@ class LapackWrapper {
         return diagonalizerType<Real>();
     }
 };
-
+/*
 template <>
 inline diagonalizerType<float> LapackWrapper<float>::diagonalizer() {
     return &C_SGEEV;
-}
+}*/
 template <>
 inline diagonalizerType<double> LapackWrapper<double>::diagonalizer() {
     return &C_DGEEV;
 }
-template <>
+/*template <>
 inline diagonalizerType<std::complex<float>> LapackWrapper<std::complex<float>>::diagonalizer() {
     return &C_CGEEV;
 }
 template <>
 inline diagonalizerType<std::complex<double>> LapackWrapper<std::complex<double>>::diagonalizer() {
     return &C_ZGEEV;
-}
+}*/
 
 }  // Namespace libpme
 #endif  // Header guard
