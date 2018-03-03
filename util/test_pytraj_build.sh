3b4496c10a08f8e56afbd96fec8b47087c5c2fcd
Hai Nguyen <hainm@users.noreply.github.com>
2018-03-03 08:43:25 -0500

    FIX pytraj cloning error ("alert protocol version while accessing") (#588)
    * test
    
    * http
    
    * clone pytraj outside of the docker image

--- util/test_pytraj_build.sh
+++ util/test_pytraj_build.sh
@@ -3,12 +3,17 @@
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/../"; pwd;)
 DOCKER_IMAGE=hainm/pytraj-build-box:17.0
 
+# not clone pytraj inside the docker image
+# https://githubengineering.com/crypto-removal-notice/
+git clone http://github.com/amber-md/pytraj
+
 docker info
 cat << EOF | docker run -i \
                         -v ${FEEDSTOCK_ROOT}:/cpptraj \
                         -a stdin -a stdout -a stderr \
                         ${DOCKER_IMAGE}\
                         bash || exit $?
+
 
 set -x
 cd /cpptraj/
@@ -17,7 +22,6 @@ bash configure -shared -openmp gnu
 make libcpptraj -j4
 export CPPTRAJHOME=/cpptraj
 
-git clone https://github.com/amber-md/pytraj
 cd pytraj
 /opt/python/cp35-cp35m/bin/python setup.py install
 # make sure we can run very simple test
