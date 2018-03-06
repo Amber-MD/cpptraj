d408468bfb7c662cc960283eebbb0f2b48b971f4
Daniel R. Roe <daniel.r.roe@gmail.com>
2018-03-06 10:05:22 -0500

    DRR - Cpptraj: Only execute Exec commands on TrajComm master.

--- src/Command.cpp
+++ src/Command.cpp
@@ -666,7 +666,16 @@ CpptrajState::RetType Command::ExecuteCommand( CpptrajState& State, ArgList cons
         }
         break;
       case Cmd::EXE:
+#       ifdef MPI
+        if (Parallel::TrajComm().Master())
+          ret_val = ((Exec*)obj)->Execute( State, cmdArg );
+        else
+          ret_val = CpptrajState::OK;
+        if (Parallel::World().CheckError( (int)ret_val ) != 0)
+          ret_val = CpptrajState::ERR;
+#       else
         ret_val = ((Exec*)obj)->Execute( State, cmdArg );
+#       endif
         delete obj;
         break;
       case Cmd::ACT: ret_val = State.AddToActionQueue( (Action*)obj, cmdArg ); break;
