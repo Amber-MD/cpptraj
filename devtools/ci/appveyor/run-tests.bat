edb9851ac26893a9ef1a8d01f73ca3800df7abd8
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2018-05-10 22:31:28 -0700

    Try to use MSYS2_PATH_TYPE to fix path issues during testing

--- devtools/ci/appveyor/run-tests.bat
+++ devtools/ci/appveyor/run-tests.bat
@@ -5,7 +5,13 @@ set CPPTRAJ_TEST_OS=windows
 rem debugging: print library dependencies of cpptraj
 dumpbin /dependents bin/cpptraj.exe
 
-rem currently tests can't be run under Visual Studio due to some strange library error
-if %BUILD_TYPE% neq cmake-vs (
+if %BUILD_TYPE% equ cmake-vs (
+	rem make sure MSYS's sh inherits our full path
+	rem (from https://github.com/cisco/ChezScheme/issues/19 )
+	set MSYS2_PATH_TYPE=inherit
+	
 	sh -lc "cd test; make test.showerrors"
+	
+) else (
+	sh -lc "cd test; make test.showerrors" || exit /b
 )
