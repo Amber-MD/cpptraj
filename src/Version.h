0a8e680a945ce3fabad3aba52d5705b4560918cb
Daniel R. Roe <daniel.roe@nih.gov>
2018-05-17 09:30:01 -0400

    DRR - Cpptraj: Update minor internal version number

--- src/Version.h
+++ src/Version.h
@@ -18,5 +18,5 @@
  * Whenever a number that precedes <revision> is incremented, all subsequent
  * numbers should be reset to 0.
  */
-#define CPPTRAJ_INTERNAL_VERSION "V4.3.7"
+#define CPPTRAJ_INTERNAL_VERSION "V4.3.8"
 #endif
