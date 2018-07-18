6f8b70f86f1a01b40b153e2e362f0e6f50f1efa4
Daniel R. Roe <daniel.roe@nih.gov>
2018-07-18 12:04:37 -0400

    DRR - Cpptraj: Update comments for versioning.

--- src/Version.h
+++ src/Version.h
@@ -12,7 +12,9 @@
  * <major>    - Incremented whenever there is a major API change, e.g.
  *              changing the Action base class, etc.
  * <minor>    - Incremented whenever there are changes to behavior, e.g.
- *              syntax, functionality, or output.
+ *              syntax, functionality, or output. The same input should
+ *              produce exactly the same output if the major and minor
+ *              version numbers are the same.
  * <revision> - All other changes (so at least each PR).
  *
  * Whenever a number that precedes <revision> is incremented, all subsequent
