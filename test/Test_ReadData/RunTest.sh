481c6d3e167f7487b37e279b88922c581e01eb11
Daniel R. Roe <daniel.roe@nih.gov>
2018-07-24 11:31:50 -0400

    DRR - Cpptraj: Add CHARMM output read test.

--- test/Test_ReadData/RunTest.sh
+++ test/Test_ReadData/RunTest.sh
@@ -2,17 +2,28 @@
 
 . ../MasterTest.sh
 
-CleanFiles vector.in v6and7.dat
+CleanFiles vector.in v6and7.dat rex-d.dat
+
+TESTNAME='Read data tests'
 
 INPUT="-i vector.in"
-# Test read/append of vector dataset 
+# Test read/append of vector dataset
+UNITNAME='Read vector data test'
 cat > vector.in <<EOF
 readdata ../Test_Vector/vtest.dat.6.save vector name v6and7
 readdata ../Test_Vector/vtest.dat.7.save vector name v6and7
 writedata v6and7.dat v6and7
 EOF
-RunCpptraj "Read vector data test"
+RunCpptraj "$UNITNAME"
 DoTest v6and7.dat.save v6and7.dat
+
+UNITNAME='Read CHARMM output test'
+cat > vector.in <<EOF
+readdata rex-d.out_0 name ENE
+writedata rex-d.dat ENE[*]
+EOF
+RunCpptraj "$UNITNAME"
+DoTest rex-d.dat.save rex-d.dat
 
 EndTest
   
