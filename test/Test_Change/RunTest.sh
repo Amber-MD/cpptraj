8e6e75fc2bc34269017a5a0f3ec82ed099d6003d
Daniel R. Roe <daniel.roe@nih.gov>
2018-08-08 09:36:57 -0400

    DRR - Cpptraj: Add change chainid test

--- test/Test_Change/RunTest.sh
+++ test/Test_Change/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles change.in ala3.mod.pdb
+CleanFiles change.in ala3.mod.pdb ala3.chain.pdb
 
 TESTNAME='Change command test'
 Requires maxthreads 1
@@ -17,8 +17,17 @@ change parmindex 0 resname from :3 to CALA
 change parmindex 0 atomname from @HN to H
 trajout ala3.mod.pdb
 EOF
-RunCpptraj "$TESTNAME"
+RunCpptraj "Change atom and residue name tests"
 DoTest ala3.mod.pdb.save ala3.mod.pdb
+
+cat > change.in <<EOF
+parm ../Test_Charmm/ala3.psf
+trajin ../Test_Charmm/ala3.dcd 1 1
+change parmindex 0 chainid of * to A
+trajout ala3.chain.pdb
+EOF
+RunCpptraj "Change chain ID test"
+DoTest ala3.chain.pdb.save ala3.chain.pdb
 
 EndTest
 exit 0
