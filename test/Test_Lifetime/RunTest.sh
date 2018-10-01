1db17925897c33c046cac7e8d5df2e286ce03b1d
Daniel R. Roe <daniel.roe@nih.gov>
2018-10-01 13:13:45 -0400

    DRR - Cpptraj: Add lifetime test with data set caching

--- test/Test_Lifetime/RunTest.sh
+++ test/Test_Lifetime/RunTest.sh
@@ -3,9 +3,12 @@
 . ../MasterTest.sh
 
 CleanFiles avg.life.5.gnu max.life.5.gnu life.5.gnu life.in perres.avg.gnu \
-           perres.cumulative.gnu crv.life.5.gnu solutehb.gnu
+           perres.cumulative.gnu crv.life.5.gnu solutehb.gnu \
+           avg.life2.5.gnu max.life2.5.gnu life2.5.gnu crv.life2.5.gnu
 TESTNAME='Lifetime analysis tests'
 Requires netcdf
+
+# Basic lifetime test
 cat > life.in <<EOF
 parm ../DPDP.parm7
 trajin ../DPDP.nc
@@ -28,6 +31,27 @@ DoTest life.5.gnu.save life.5.gnu
 DoTest crv.life.5.gnu.save crv.life.5.gnu
 DoTest solutehb.gnu.save solutehb.gnu
 
+# Using data set caching.
+cat > life.in <<EOF
+parm ../DPDP.parm7
+trajin ../DPDP.nc
+usediskcache on
+hbond H1 series @N,H,C,O #uuseries solutehb.gnu
+go
+usediskcache off
+lifetime H1[solutehb] out life2.5.gnu window 5
+datafile avg.life2.5.gnu title avg.life.5.gnu
+datafile max.life2.5.gnu title max.life.5.gnu
+datafile life2.5.gnu title life.5.gnu
+datafile crv.life2.5.gnu title crv.life.5.gnu
+EOF
+RunCpptraj "Lifetime test with data set caching."
+DoTest avg.life.5.gnu.save avg.life2.5.gnu
+DoTest max.life.5.gnu.save max.life2.5.gnu
+DoTest life.5.gnu.save life2.5.gnu
+DoTest crv.life.5.gnu.save crv.life2.5.gnu
+
+# Average/cumulative average
 cat > life.in <<EOF
 parm ../DPDP.parm7
 trajin ../DPDP.nc
