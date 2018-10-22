4f12072be6f5c2610b75fc3b0d2d742fa1aeeacb
Daniel R. Roe <daniel.roe@nih.gov>
2018-10-22 14:19:10 -0400

    DRR - Cpptraj: Use new update routines in Topology.

--- src/Exec_UpdateParameters.cpp
+++ src/Exec_UpdateParameters.cpp
@@ -8,98 +8,133 @@ void Exec_UpdateParameters::Help() const
   mprintf("\t%s setname <parm set>\n", DataSetList::TopArgs);
 }
 
-static inline void BondTypes(ParmHolder<int>& ParmIndices, Topology const& top, BondArray const& bonds, int& skipCount) {
-  for (BondArray::const_iterator b = bonds.begin(); b != bonds.end(); ++b)
+/** Add/update bond parameters.
+  * \param bp0 Bond parameters to add to/update.
+  * \param bp1 New bond parameters.
+  * \return number of bond parameters added or updated.
+  */
+int Exec_UpdateParameters::UpdateBondParams(ParmHolder<BondParmType>& bp0,
+                                            ParmHolder<BondParmType> const& bp1) const
+{
+  int updateCount = 0;
+  for (ParmHolder<BondParmType>::const_iterator newp = bp1.begin();
+                                                newp != bp1.end(); ++newp)
   {
-    if (b->Idx() == -1)
-      skipCount++;
-    else {
-      AtomTypeHolder types(2);
-      types.AddName( top[b->A1()].Type() );
-      types.AddName( top[b->A2()].Type() );
-      ParmIndices.AddParm( types, b->Idx(), false );
+    ParameterHolders::RetType ret = bp0.AddParm( newp->first, newp->second, true );
+    if (ret != ParameterHolders::ERR) {
+      if (ret == ParameterHolders::ADDED) {
+        mprintf("\tAdded NEW bond parameter:");
+        updateCount++;
+      } else if (ret == ParameterHolders::UPDATED) {
+        mprintf("\tUpdated bond parameter:");
+        updateCount++;
+      } else if (ret == ParameterHolders::SAME)
+        mprintf("\tBond parameter already present:");
+      mprintf(" %s %s %12.4f %12.4f\n", 
+              *(newp->first[0]), *(newp->first[1]), newp->second.Rk(), newp->second.Req());
     }
   }
+  return updateCount;
 }
 
-static inline void AngleTypes(ParmHolder<int>& ParmIndices, Topology const& top, AngleArray const& angles, int& skipCount) {
-  for (AngleArray::const_iterator b = angles.begin(); b != angles.end(); ++b)
+/** Update parameter set of given Topology with parameters from new set.
+  * \param top The Topology to update.
+  * \param set1 Set with new/updated parameters.
+  */
+int Exec_UpdateParameters::UpdateParams(Topology& top, ParameterSet const& set1) const
+{
+  ParameterSet set0 = top.GetParameters();
+  set0.Debug("originalp.dat");
+
+  //StatArray status;
+  //status = UpdateParamX<ParmHolder<BondParmType>>(set0.BP(), set1.BP());
+  unsigned int updateCount;
+  // Bond parameters
+  updateCount = UpdateBondParams(set0.BP(), set1.BP());
+  if (updateCount > 0) {
+    mprintf("\tRegenerating bond parameters.\n");
+    top.AssignBondParams( set0.BP() );
+  }
+  // Angle parameters
+  updateCount = 0;
+  for (ParmHolder<AngleParmType>::const_iterator newp = set1.AP().begin();
+                                                 newp != set1.AP().end(); ++newp)
   {
-    if (b->Idx() == -1)
-      skipCount++;
-    else {
-      AtomTypeHolder types(3);
-      types.AddName( top[b->A1()].Type() );
-      types.AddName( top[b->A2()].Type() );
-      types.AddName( top[b->A3()].Type() );
-      ParmIndices.AddParm( types, b->Idx(), false );
+    ParameterHolders::RetType ret = set0.AP().AddParm( newp->first, newp->second, true );
+    if (ret != ParameterHolders::ERR) {
+      if (ret == ParameterHolders::ADDED) {
+        mprintf("\tAdded NEW angle parameter:");
+        updateCount++;
+      } else if (ret == ParameterHolders::UPDATED) {
+        mprintf("\tUpdated angle parameter:");
+        updateCount++;
+      } else if (ret == ParameterHolders::SAME)
+        mprintf("\tAngle parameter already present:");
+      mprintf(" %s %s %s %12.4f %12.4f\n", 
+              *(newp->first[0]), *(newp->first[1]), *(newp->first[2]), newp->second.Tk(), newp->second.Teq());
     }
   }
-}
-
-static inline void ImproperTypes(ParmHolder<int>& ParmIndices, Topology const& top, DihedralArray const& dih, int& skipCount) {
-  for (DihedralArray::const_iterator b = dih.begin(); b != dih.end(); ++b)
+  if (updateCount > 0) {
+    mprintf("\tRegenerating angle parameters.\n");
+    top.AssignAngleParams( set0.AP() );
+  }
+  // Dihedral parameters
+  updateCount = 0;
+  for (DihedralParmHolder::const_iterator newp = set1.DP().begin();
+                                          newp != set1.DP().end(); ++newp)
   {
-    if (b->Idx() == -1)
-      skipCount++;
-    else {
-      AtomTypeHolder types(4);
-      types.AddName( top[b->A1()].Type() );
-      types.AddName( top[b->A2()].Type() );
-      types.AddName( top[b->A3()].Type() );
-      types.AddName( top[b->A4()].Type() );
-      ParmIndices.AddParm( types, b->Idx(), false );
+    ParameterHolders::RetType ret = set0.DP().AddParm( newp->first, newp->second, true );
+    if (ret != ParameterHolders::ERR) {
+      if (ret == ParameterHolders::ADDED) {
+        mprintf("\tAdded NEW dihedral parameters:\n");
+        updateCount++;
+      } else if (ret == ParameterHolders::UPDATED) {
+        mprintf("\tUpdated dihedral parameters:\n");
+        updateCount++;
+      } else if (ret == ParameterHolders::SAME)
+        mprintf("\tDihedral parameters already present:\n");
+      for (DihedralParmArray::const_iterator it = newp->second.begin();
+                                             it != newp->second.end(); ++it)
+        mprintf("\t\t%s %s %s %s %12.4f %12.4f %12.4f\n", 
+                *(newp->first[0]), *(newp->first[1]), *(newp->first[2]), *(newp->first[3]), it->Pk(), it->Pn(), it->Phase());
     }
   }
-}
-
-/// Hold unique dihedral parameter indices
-typedef std::set<int> PidxArray;
-
-/// Pair unique dihedral atom types to one or more parameter indices.
-typedef std::pair< AtomTypeHolder, PidxArray > DihTypeIdxPair;
-
-/// Map atom types to parameter indices
-typedef std::map< AtomTypeHolder, PidxArray > DihParmMap;
-
-static inline void DihedralTypes(DihParmMap& DPmap, Topology const& top, DihedralArray const& dih, int& skipCount) {
-  // Loop over all dihedrals
-  for (DihedralArray::const_iterator b = dih.begin(); b != dih.end(); ++b)
+  if (updateCount > 0) {
+    mprintf("\tRegenerating dihedral parameters.\n");
+    top.AssignDihedralParams( set0.DP() );
+  }
+  // Urey-Bradley
+  updateCount = UpdateBondParams( set0.UB(), set1.UB() );
+  if (updateCount > 0) {
+    mprintf("\tRegenerating UB parameters.\n");
+    top.AssignUBParams( set0.UB() );
+  }
+  // Improper parameters
+  updateCount = 0;
+  for (ParmHolder<DihedralParmType>::const_iterator newp = set1.IP().begin();
+                                                    newp != set1.IP().end(); ++newp)
   {
-    if (b->Idx() == -1)
-      skipCount++;
-    else {
-      AtomTypeHolder types(4);
-      types.AddName( top[b->A1()].Type() );
-      types.AddName( top[b->A2()].Type() );
-      types.AddName( top[b->A3()].Type() );
-      types.AddName( top[b->A4()].Type() );
-      // Check for existing
-      DihParmMap::iterator it = DPmap.find( types );
-      if (it == DPmap.end()) {
-        // Types have not been seen.
-        PidxArray pidx;
-        pidx.insert( b->Idx() );
-        //std::pair<DihParmMap::iterator,bool> ret = // DEBUG
-          DPmap.insert( DihTypeIdxPair(types, pidx) );
-        //mprintf("DEBUG: New dihedral type: %s %s %s %s %i %i\n",
-        //        *(ret.first->first[0]), *(ret.first->first[1]),
-        //        *(ret.first->first[2]), *(ret.first->first[3]), b->Idx(), (int)ret.second);
-        //mprintf("DEBUG: New dihedral type: %s %s %s %s %i\n", *types[0], *types[1], *types[2], *types[3], b->Idx());
-        it = DPmap.find( types );
-        //if (it == DPmap.end()) {
-        //  mprintf("Error: Not found after insert.\n");
-        //  return;
-        //}
-      } else {
-        // Add index to types
-        //std::pair<PidxArray::iterator,bool> ret = // DEBUG
-          it->second.insert( b->Idx() );
-        //if (ret.second)
-        //  mprintf("DEBUG: New index for existing type: %s %s %s %s %i\n", *types[0], *types[1], *types[2], *types[3], b->Idx());
-      }
+    ParameterHolders::RetType ret = set0.IP().AddParm( newp->first, newp->second, true );
+    if (ret != ParameterHolders::ERR) {
+      if (ret == ParameterHolders::ADDED) {
+        mprintf("\tAdded NEW improper parameter:\n");
+        updateCount++;
+      } else if (ret == ParameterHolders::UPDATED) {
+        mprintf("\tUpdated improper parameter:\n");
+        updateCount++;
+      } else if (ret == ParameterHolders::SAME)
+        mprintf("\tImproper parameter already present:\n");
+        mprintf("\t\t%s %s %s %s %12.4f %12.4f\n", 
+                *(newp->first[0]), *(newp->first[1]), *(newp->first[2]), *(newp->first[3]), newp->second.Pk(), newp->second.Phase());
     }
   }
+  if (updateCount > 0) {
+    mprintf("\tRegenerating dihedral parameters.\n");
+    top.AssignImproperParams( set0.IP() );
+  }
+
+  set0.Debug("newp.dat");
+  return 0;
 }
 
 // Exec_UpdateParameters::Execute()
@@ -127,175 +162,11 @@ Exec::RetType Exec_UpdateParameters::Execute(CpptrajState& State, ArgList& argIn
   }
   Topology& top = static_cast<Topology&>( *dstop );
 
+
   mprintf("\tUpdating parameters in topology '%s' using those in set '%s'\n",
           top.c_str(), prm.legend());
-  // We assume a parameter in topology is never repeated, so as soon
-  // as it is found we can move to the next one.
-  // ParmIndices will map type names for existing bonds, angles, etc. to
-  // existing type indices.
-  ParmHolder<int> ParmIndices;
-  // Bond parameters
-  int skipCount = 0;
-  BondTypes(ParmIndices, top, top.Bonds(), skipCount);
-  BondTypes(ParmIndices, top, top.BondsH(), skipCount);
-  if (skipCount > 0)
-    mprintf("Warning: %i bonds were missing parameters and were skipped.\n", skipCount);
-  for (ParmHolder<BondParmType>::const_iterator it1 = prm.BP().begin();
-                                                it1 != prm.BP().end(); ++it1)
-  {
-    for (ParmHolder<int>::const_iterator it0 = ParmIndices.begin();
-                                         it0 != ParmIndices.end(); ++it0)
-    {
-      if (it1->first == it0->first)
-      {
-        BondParmType& bp = top.SetBondParm(it0->second);
-        mprintf("\tUpdating bond parameter %s - %s from %f %f to %f %f\n",
-                *(it0->first[0]), *(it0->first[1]), bp.Rk(), bp.Req(),
-                it1->second.Rk(), it1->second.Req());
-        bp = it1->second;
-        break;
-      }
-    }
-  }
-  ParmIndices.clear();
-  // Angle parameters
-  skipCount = 0;
-  AngleTypes(ParmIndices, top, top.Angles(), skipCount);
-  AngleTypes(ParmIndices, top, top.AnglesH(), skipCount);
-  if (skipCount > 0)
-    mprintf("Warning: %i angles were missing parameters and were skipped.\n", skipCount);
-  for (ParmHolder<AngleParmType>::const_iterator it1 = prm.AP().begin();
-                                                 it1 != prm.AP().end(); ++it1)
-  {
-    for (ParmHolder<int>::const_iterator it0 = ParmIndices.begin();
-                                         it0 != ParmIndices.end(); ++it0)
-    {
-      if (it1->first == it0->first)
-      {
-        AngleParmType& bp = top.SetAngleParm(it0->second);
-        mprintf("\tUpdating angle parameter %s - %s - %s from %f %f to %f %f\n",
-                *(it0->first[0]), *(it0->first[1]), *(it0->first[2]), bp.Tk(), bp.Teq(),
-                it1->second.Tk(), it1->second.Teq());
-        bp = it1->second;
-        break;
-      }
-    }
-  }
-  ParmIndices.clear();
-  // Dihedral parameters
-  DihParmMap DPmap;
-  skipCount = 0;
-  DihedralTypes(DPmap, top, top.Dihedrals(), skipCount);
-  DihedralTypes(DPmap, top, top.DihedralsH(), skipCount);
-/*
-  // DEBUG: Print map
-  for (DihParmMap::const_iterator it0 = DPmap.begin(); it0 != DPmap.end(); ++it0)
-  {
-    // it0->first  : AtomTypeHolder
-    // it0->second : Array of parameter indices
-    mprintf("%s %s %s %s :",
-            *(it0->first[0]), *(it0->first[1]),
-            *(it0->first[2]), *(it0->first[3]));
-    for (PidxArray::const_iterator it1 = it0->second.begin();
-                                   it1 != it0->second.end(); ++it1)
-      mprintf(" %i", *it1);
-    mprintf("\n");
-  }
-*/
-  if (skipCount > 0)
-    mprintf("Warning: %i dihedrals were missing parameters and were skipped.\n", skipCount);
-  for (DihedralParmHolder::const_iterator it1 = prm.DP().begin();
-                                          it1 != prm.DP().end(); ++it1)
-  {
-    // it1->first  : AtomTypeHolder
-    // it1->second : Array of dihedral parameters
-    for (DihParmMap::const_iterator it0 = DPmap.begin();
-                                    it0 != DPmap.end(); ++it0)
-    {
-      // it0->first  : AtomTypeHolder
-      // it0->second : Set of parameter indices
-      if (it1->first == it0->first)
-      {
-        // Atom types match. Check number of old parameters vs new.
-        mprintf("DEBUG: Match found: %s %s %s %s. New= %zu, old= %zu\n",
-                *(it0->first[0]), *(it0->first[1]),
-                *(it0->first[2]), *(it0->first[3]),
-                it1->second.size(), it0->second.size());
-        if (it1->second.size() == it0->second.size()) {
-          // Just replace each old parameter with new parameters
-          DihedralParmArray::const_iterator newParm = it1->second.begin();
-          for (PidxArray::const_iterator pidx = it0->second.begin();
-                                         pidx != it0->second.end(); ++pidx)
-          {
-            DihedralParmType& bp = top.SetDihedralParm( *pidx );
-            mprintf("\tUpdating dihedral parameter %s - %s - %s  %s from %f %f %f to %f %f %f\n",
-                      *(it0->first[0]), *(it0->first[1]),
-                      *(it0->first[2]), *(it0->first[3]),
-                      bp.Pk(), bp.Pn(), bp.Phase(),
-                      newParm->Pk(), newParm->Pn(), newParm->Phase());
-            bp = *newParm;
-          }
-        } else {
-          mprintf("Warning: Number of dihedral terms do not match.\n");
-        }
-      } // END if types match
-    } // END loop over exisiting parameters
-  } // END loop over new parameters
-  ParmIndices.clear();
-  // CHARMM-related stuff
-  if (top.Chamber().HasChamber()) {
-    ChamberParmType& chm = top.SetChamber();
-    // UB parameters
-    skipCount = 0;
-    BondTypes(ParmIndices, top, chm.UB(), skipCount);
-    if (skipCount > 0)
-      mprintf("Warning: %i Urey-Bradley terms were missing parameters and were skipped.\n", skipCount);
-    for (ParmHolder<BondParmType>::const_iterator it1 = prm.UB().begin();
-                                                  it1 != prm.UB().end(); ++it1)
-    {
-      for (ParmHolder<int>::const_iterator it0 = ParmIndices.begin();
-                                           it0 != ParmIndices.end(); ++it0)
-      {
-        if (it1->first == it0->first)
-        {
-          BondParmType& bp = chm.SetUBparm(it0->second);
-          mprintf("\tUpdating UB parameter %s - %s from %f %f to %f %f\n",
-                  *(it0->first[0]), *(it0->first[1]), bp.Rk(), bp.Req(),
-                  it1->second.Rk(), it1->second.Req());
-          bp = it1->second;
-          break;
-        }
-      }
-    }
-    ParmIndices.clear();
-    // Improper parameters
-    skipCount = 0;
-    ImproperTypes(ParmIndices, top, chm.Impropers(), skipCount);
-    if (skipCount > 0)
-      mprintf("Warning: %i impropers were missing parameters and were skipped.\n", skipCount);
-    for (ParmHolder<DihedralParmType>::const_iterator it1 = prm.IP().begin();
-                                                      it1 != prm.IP().end(); ++it1)
-    {
-      for (ParmHolder<int>::const_iterator it0 = ParmIndices.begin();
-                                           it0 != ParmIndices.end(); ++it0)
-      {
-        if (it1->first == it0->first)
-        {
-          DihedralParmType& bp = chm.SetImproperParm(it0->second);
-          mprintf("\tUpdating improper parameter %s - %s - %s  %s from %f %f %f to %f %f %f\n",
-                  *(it0->first[0]), *(it0->first[1]), *(it0->first[2]), *(it0->first[3]),
-                  bp.Pk(), bp.Pn(), bp.Phase(),
-                  it1->second.Pk(), it1->second.Pn(), it1->second.Phase());
-          bp = it1->second;
-          break;
-        }
-      }
-    }
-    ParmIndices.clear();
-  }
 
+  UpdateParams(top, prm);
 
-  //for (ParmHolder<int>::const_iterator it = ParmIndices.begin(); it != ParmIndices.end(); ++it)
-  //  mprintf("\t%s %s %i\n", *(it->first[0]), *(it->first[1]), it->second);
   return CpptrajState::OK;
 }
--- src/Exec_UpdateParameters.h
+++ src/Exec_UpdateParameters.h
@@ -1,6 +1,7 @@
 #ifndef INC_EXEC_UPDATEPARAMETERS_H
 #define INC_EXEC_UPDATEPARAMETERS_H
 #include "Exec.h"
+#include "ParameterSet.h"
 /// Update parameters in a topology with those from a data set. 
 class Exec_UpdateParameters : public Exec {
   public:
@@ -8,5 +9,9 @@ class Exec_UpdateParameters : public Exec {
     void Help() const;
     DispatchObject* Alloc() const { return (DispatchObject*)new Exec_UpdateParameters(); }
     RetType Execute(CpptrajState&, ArgList&);
+  private:
+    ParameterSet GetParameters(Topology const&) const;
+    int UpdateBondParams(ParmHolder<BondParmType>&, ParmHolder<BondParmType> const&) const;
+    int UpdateParams(Topology&, ParameterSet const&) const;
 };
 #endif
