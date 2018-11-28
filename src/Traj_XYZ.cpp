88ff48a8c1d5b5e574b0e24d08fee0aecaffe9a7
Daniel R. Roe <daniel.roe@nih.gov>
2018-11-28 10:40:49 -0500

    DRR - Cpptraj: Initial incarnation of the simple XYZ trajectory format reader

---
+++ src/Traj_XYZ.cpp
@@ -0,0 +1,220 @@
+#include "Traj_XYZ.h"
+#include "CpptrajStdio.h"
+#include "StringRoutines.h"
+
+/// CONSTRUCTOR
+Traj_XYZ::Traj_XYZ() :
+  fmt_(0)
+{}
+
+/** \param line1 First line. Will be set to title line if title is present, cleared otherwise.
+  * \param line2 Second line.
+  */
+Traj_XYZ::Type Traj_XYZ::DetermineFormat(std::string& line1,
+                                         std::string const& line2)
+const
+{
+  std::string line = line1;
+  // This line will either be a title line, atom XYZ, or XYZ
+  RemoveAllWhitespace( line );
+  if (!line.empty() && line[0] == '#') {
+    line1 = line;
+    // Ignore the title. Get the next line, which must be atom XYZ or XYZ
+    line = line2;
+  } else
+    line1.clear();
+  static const unsigned int tkSize = 64;
+  char tk0[tkSize];
+  char tk1[tkSize];
+  char tk2[tkSize];
+  char tk3[tkSize];
+  int nscan = sscanf(line.c_str(), "%s %s %s %s", tk0, tk1, tk2, tk3);
+  if (nscan == 4) {
+    if ( validInteger( std::string(tk0) ) &&
+         validDouble( std::string(tk1) ) &&
+         validDouble( std::string(tk2) ) &&
+         validDouble( std::string(tk3) ) )
+      return ATOM_XYZ;
+  } else if (nscan == 3) {
+    if ( validDouble( std::string(tk0) ) &&
+         validDouble( std::string(tk1) ) &&
+         validDouble( std::string(tk2) ) )
+      return XYZ;
+  }
+  return UNKNOWN;
+}
+
+/** Identify trajectory format. File should be setup for READ */
+bool Traj_XYZ::ID_TrajFormat(CpptrajFile& fileIn) {
+  // File must already be set up for read.
+  if (fileIn.OpenFile()) return false;
+  std::string line1 = fileIn.GetLine();
+  std::string line2 = fileIn.GetLine();
+  if ( DetermineFormat(line1, line2) == UNKNOWN ) return false;
+
+  return true;
+}
+
+/** Print trajectory info to stdout. */
+void Traj_XYZ::Info() {
+  mprintf("is an XYZ trajectory");
+}
+
+/** Close file. */
+void Traj_XYZ::closeTraj() {
+  infile_.CloseFile();
+}
+
+// -----------------------------------------------------------------------------
+/** Open trajectory for reading. */
+int Traj_XYZ::openTrajin() {
+  return infile_.OpenFile();
+}
+
+/** Read help */
+void Traj_XYZ::ReadHelp() {
+
+}
+
+/** Process read arguments. */
+int Traj_XYZ::processReadArgs(ArgList& argIn) {
+
+  return 0;
+}
+
+const char* FMT_XYZ_ = "%lf %lf %lf";
+
+const char* FMT_ATOM_XYZ_ = "%*i %lf %lf %lf";
+
+/** Set up trajectory for reading.
+  * \return Number of frames in trajectory.
+  */
+int Traj_XYZ::setupTrajin(FileName const& fname, Topology* trajParm)
+{
+  if (infile_.OpenFileRead( fname )) return TRAJIN_ERR;
+  // Initial format determiniation.
+  std::string line1 = infile_.GetLine();
+  std::string line2 = infile_.GetLine();
+  Type ftype = DetermineFormat(line1, line2);
+  switch (ftype) {
+    case XYZ      : fmt_ = FMT_XYZ_; break;
+    case ATOM_XYZ : fmt_ = FMT_ATOM_XYZ_; break;
+    case UNKNOWN  :
+      mprinterr("Internal Error: '%s' does not appear to be XYZ format anymore.\n",
+                infile_.Filename().full());
+      return TRAJIN_ERR;
+  }
+  if (line1.empty())
+    titleType_ = NO_TITLE;
+  else
+    titleType_ = SINGLE;
+  // Read one frame, see if the header is repeated.
+  closeTraj();
+  int nframes = TRAJIN_UNK;
+  if (openTrajin()) return TRAJIN_ERR;
+  if (titleType_ != NO_TITLE) infile_.Line();
+  for (int at = 0; at != trajParm->Natom(); at++) {
+    const char* atline = infile_.Line();
+    if (atline == 0) {
+      mprinterr("Error: Unexpected EOF when reading first frame of '%s'\n", atline);
+      return TRAJIN_ERR;
+    }
+  }
+  line2 = infile_.GetLine();
+  if (!line2.empty()) {
+    RemoveAllWhitespace( line2 );
+    if (line2[0] == '#')
+      titleType_ = MULTIPLE;
+  } else
+    nframes = 1;
+
+  if (!line1.empty()) SetTitle( line1 );
+  SetCoordInfo( CoordinateInfo(Box(), false, false, false) );
+  
+  return nframes;
+}
+
+/** Read specified trajectory frame. */
+int Traj_XYZ::readFrame(int set, Frame& frameIn) {
+
+  return 0;
+}
+
+/** Read velocities from specified frame. */
+int Traj_XYZ::readVelocity(int set, Frame& frameIn) {
+
+  return 0;
+}
+
+/** Read forces from specified frame. */
+int Traj_XYZ::readForce(int set, Frame& frameIn) {
+
+  return 0;
+}
+
+// -----------------------------------------------------------------------------
+/** Write help. */
+void Traj_XYZ::WriteHelp() {
+
+}
+
+/** Process write arguments. */
+int Traj_XYZ::processWriteArgs(ArgList& argIn) {
+
+  return 0;
+}
+
+/** Set up trajectory for write. */
+int Traj_XYZ::setupTrajout(FileName const& fname, Topology* trajParm,
+                                   CoordinateInfo const& cInfoIn, 
+                                   int NframesToWrite, bool append)
+{
+
+  return 1;
+}
+
+/** Write specified trajectory frame. */
+int Traj_XYZ::writeFrame(int set, Frame const& frameOut) {
+
+  return 0;
+}
+
+// =============================================================================
+#ifdef MPI
+/** Open trajectory for reading in parallel. */
+int Traj_XYZ::parallelOpenTrajin(Parallel::Comm const& commIn) {
+  return 1;
+}
+
+/** Open trajectory for writing in parallel. */
+int Traj_XYZ::parallelOpenTrajout(Parallel::Comm const& commIn) {
+  return 1;
+}
+
+/** Set up trajectory for write in parallel. */
+int Traj_XYZ::parallelSetupTrajout(FileName const& fname, Topology* trajParm,
+                                           CoordinateInfo const& cInfoIn,
+                                           int NframesToWrite, bool append,
+                                           Parallel::Comm const& commIn)
+{
+
+  return 1;
+}
+
+/** Read frame in parallel. */
+int Traj_XYZ::parallelReadFrame(int set, Frame& frameIn) {
+
+  return 1;
+}
+
+/** Write frame in parallel. */
+int Traj_XYZ::parallelWriteFrame(int set, Frame const& frameOut) {
+
+  return 1;
+}
+
+/** Close trajectory in parallel. */
+void Traj_XYZ::parallelCloseTraj() {
+
+}
+#endif
---
+++ src/Traj_XYZ.h
@@ -0,0 +1,51 @@
+#ifndef INC_TRAJ_XYZ_H
+#define INC_TRAJ_XYZ_H
+#include "TrajectoryIO.h"
+#include "BufferedLine.h"
+/// Read simple XYZ trajectories. 
+class Traj_XYZ : public TrajectoryIO {
+  public:
+    Traj_XYZ();
+    static BaseIOtype* Alloc() { return (BaseIOtype*)new Traj_XYZ(); }
+    static void WriteHelp();
+    static void ReadHelp();
+  private:
+    // ----- Inherited functions -----------------
+    bool ID_TrajFormat(CpptrajFile&);
+    int setupTrajin(FileName const&, Topology*);
+    int setupTrajout(FileName const&, Topology*, CoordinateInfo const&,int, bool);
+    int openTrajin();
+    void closeTraj();
+    int readFrame(int,Frame&);
+    int writeFrame(int,Frame const&);
+    void Info();
+    int readVelocity(int, Frame&);
+    int readForce(int, Frame&);
+    int processWriteArgs(ArgList&);
+    int processReadArgs(ArgList&);
+    // -------------------------------------------
+#   ifdef MPI
+    // ----- Parallel functions ------------------
+    int parallelOpenTrajin(Parallel::Comm const&);
+    int parallelOpenTrajout(Parallel::Comm const&);
+    int parallelSetupTrajout(FileName const&, Topology*, CoordinateInfo const&,
+                             int, bool, Parallel::Comm const&);
+    int parallelReadFrame(int, Frame&);
+    int parallelWriteFrame(int, Frame const&);
+    void parallelCloseTraj();
+    // -------------------------------------------
+#   endif
+
+    enum Type { UNKNOWN=0, XYZ, ATOM_XYZ };
+    enum TitleType { NO_TITLE = 0, SINGLE, MULTIPLE };
+
+    Type DetermineFormat(std::string&, std::string const&) const;
+
+    static const char* FMT_XYZ_;
+    static const char* FMT_ATOM_XYZ_;
+
+    BufferedLine infile_;
+    const char* fmt_; ///< Format for reading
+    TitleType titleType_;
+};
+#endif
