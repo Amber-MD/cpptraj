207cc8c247e4a37c0c2a724b85c60f548a012056
Daniel R. Roe <daniel.roe@nih.gov>
2018-11-29 13:18:41 -0500

    DRR - Cpptraj: Test all xyz output options

--- test/Test_XYZformat/RunTest.sh
+++ test/Test_XYZformat/RunTest.sh
@@ -4,7 +4,8 @@
 
 TESTNAME='XYZ format tests'
 
-CleanFiles xyz.in tz2.xyz test1.crd.save test1.crd
+CleanFiles xyz.in tz2.xyz test1.crd.save test?.crd tz2.st.xyz tz2.nt.at.xyz \
+           tz2.nt.xyz tz2.mt.at.xyz tz2.mt.xyz
 
 INPUT='-i xyz.in'
 
@@ -13,18 +14,33 @@ cat > xyz.in <<EOF
 parm ../tz2.parm7
 trajin ../tz2.crd 1 10
 trajout test1.crd.save crd
+# Single title, atom-xyz
 trajout tz2.xyz
+# Single title, xyz
+trajout tz2.st.xyz    titletype single   ftype xyz
+# No title, atom-xyz
+trajout tz2.nt.at.xyz titletype none     ftype atomxyz
+# No title, xyz
+trajout tz2.nt.xyz    titletype none     ftype xyz
+# Multi title, atom-xyz
+trajout tz2.mt.at.xyz titletype perframe ftype atomxyz
+# Multi title, xyz
+trajout tz2.mt.xyz    titletype perframe ftype xyz
 EOF
 RunCpptraj "$UNITNAME"
 
 UNITNAME='Atom XYZ format read'
-cat > xyz.in <<EOF
+N=1
+for FILE in tz2.xyz tz2.st.xyz tz2.nt.at.xyz tz2.nt.xyz tz2.mt.at.xyz tz2.mt.xyz ; do
+  cat > xyz.in <<EOF
 parm ../tz2.parm7
-trajin tz2.xyz
-trajout test1.crd
+trajin $FILE 
+trajout test$N.crd
 EOF
-RunCpptraj "$UNITNAME"
-DoTest test1.crd.save test1.crd
+  RunCpptraj "$UNITNAME, test $N"
+  DoTest test1.crd.save test$N.crd
+  ((N++))
+done
 
 EndTest
 exit 0
