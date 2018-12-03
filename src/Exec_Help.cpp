6ab4d14a21fa240f08a2c55a060896ecb46519fd
Daniel R. Roe <daniel.roe@nih.gov>
2018-12-03 09:08:34 -0500

    DRR - Cpptraj: Add help topic for trajectory formats.

--- src/Exec_Help.cpp
+++ src/Exec_Help.cpp
@@ -14,6 +14,35 @@ void Exec_Help::Help() const {
           "  command. If a category is specified list only commands in that category.\n");
 }
 
+/** Print help for file formats. */
+int Exec_Help::Formats(ArgList& argIn) const {
+  std::string ftype = argIn.GetStringNext();
+  if (ftype == "trajin") {
+    mprintf("    Available input trajectory formats:\n");
+    TrajectoryFile::ReadOptions(argIn.GetStringNext());
+  } else if (ftype == "trajout") {
+    mprintf("    Available output trajectory formats:\n");
+    TrajectoryFile::WriteOptions(argIn.GetStringNext());
+  } else {
+    mprintf("\t[{trajin|trajout} [<format keyword>]]\n"
+            "  trajin           : List available input trajectory formats.\n"
+            "  trajout          : List available output trajectory formats.\n"
+            "  <format keyword> : If specified provide specific help for that format.\n");
+  }
+  return 1;
+}
+
+/** \return 1 if a help topic was found, 0 otherwise. */
+int Exec_Help::Topics(ArgList& argIn) const {
+  // By convention, Topics will start with uppercase letters and
+  // commands will start with lower case.
+  if ( isupper(argIn[0][0]) ) {
+    if (argIn[0].compare(0,6,"Format")==0)
+      return Formats(argIn);
+  }
+  return 0;
+}
+
 Exec::RetType Exec_Help::Execute(CpptrajState& State, ArgList& argIn) {
   ArgList arg = argIn;
   arg.RemoveFirstArg();
@@ -21,6 +50,10 @@ Exec::RetType Exec_Help::Execute(CpptrajState& State, ArgList& argIn) {
     // NONE in this context means list all commands
     Command::ListCommands( NONE );
   else {
+    arg.MarkArg(0);
+    // Check for help topic.
+    if (Topics(arg)) return CpptrajState::OK;
+    // Check for command category.
     for (int i = 1; i != (int)DEPRECATED; i++) {
       if (arg.CommandIs( ObjKeyword((Otype)i) )) {
         Command::ListCommands( (Otype)i );
@@ -34,7 +67,7 @@ Exec::RetType Exec_Help::Execute(CpptrajState& State, ArgList& argIn) {
     else {
       if (cmd.Obj().Type() == DispatchObject::DEPRECATED)
         mprintf("Warning: '%s' is deprecated.\n", arg.Command());
-      arg.MarkArg(0);
+      //arg.MarkArg(0);
       cmd.Help( arg );
     }
   }
