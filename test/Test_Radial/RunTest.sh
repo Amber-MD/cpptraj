5aa16c5f421c0029734942d66cfe3e9bd9bdc15f
Daniel R. Roe <daniel.roe@nih.gov>
2018-12-21 13:48:34 -0500

    DRR - Cpptraj: Simple test of radial byres

--- test/Test_Radial/RunTest.sh
+++ test/Test_Radial/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles radial.in Radial.agr cRadial.agr WatO-Trp4.agr WatO-Trp4.raw.agr
+CleanFiles radial.in Radial.agr cRadial.agr WatO-Trp4.agr WatO-Trp4.raw.agr WatO-Trp4.byres.agr
 
 TESTNAME='Radial test'
 Requires netcdf maxthreads 10
@@ -15,6 +15,7 @@ radial Radial.agr    0.5 10.0 :5@CD  :WAT@O
 radial cRadial.agr   0.5 10.0 :5     :WAT@O center1
 radial WatO-Trp4.agr 0.5 10.0 :WAT@O :4&!@C,O,CA,HA,N,H center2 \
        intrdf WatO-Trp4.raw.agr rawrdf WatO-Trp4.raw.agr
+radial WatO-Trp4.byres.agr 0.5 10.0 :WAT@O :4&!@C,O,CA,HA,N,H byres2
 EOF
 
 INPUT="-i radial.in"
@@ -23,6 +24,8 @@ DoTest Radial.agr.save Radial.agr
 DoTest cRadial.agr.save cRadial.agr
 DoTest WatO-Trp4.agr.save WatO-Trp4.agr
 DoTest WatO-Trp4.raw.agr.save WatO-Trp4.raw.agr
+DoTest WatO-Trp4.agr.save WatO-Trp4.byres.agr
+
 
 EndTest
 
