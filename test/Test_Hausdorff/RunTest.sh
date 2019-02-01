e9a6aa8b604a1336180a55fe6a23a34ac4ff799f
Daniel R. Roe <daniel.roe@nih.gov>
2019-02-01 10:57:13 -0500

    DRR - Cpptraj: Fix output for real triangular matrix

--- test/Test_Hausdorff/RunTest.sh
+++ test/Test_Hausdorff/RunTest.sh
@@ -23,12 +23,12 @@ RunCpptraj "Simple Hausdorff distance test."
 DoTest hd.dat.save hd.dat
 
 # 2D RMS
-#cat > hd.in <<EOF
-#parm ../DPDP.parm7
-#trajin ../DPDP.nc
-#rms2d DPDP out rms2d.gnu
-#EOF
-#  RunCpptraj "2D RMS"
+cat > hd.in <<EOF
+parm ../DPDP.parm7
+trajin ../DPDP.nc
+rms2d DPDP out rms2d.gnu
+EOF
+RunCpptraj "2D RMS"
 
 # Create 10 traectory chunks, do Hausdorff between 2D rms sets
 cat > hd.in <<EOF
@@ -37,11 +37,16 @@ for beg=1;beg<100;beg+=10 end=10;end+=10 i=1;i++
   loadcrd ../DPDP.nc \$beg \$end name Chunk\$i
 done
 # Do the 2drms in chunks
-for i=1;i<11;i++
-  for j=1;j<11;j++
-    2drms crdset Chunk\$i reftraj Chunk\$j M\$i.\$j
+EOF
+for ((i=1; i < 11; i++)) ; do
+  ((start = $i + 1))
+  for ((j=$start; j < 11; j++)) ; do
+#for i=1;i<11;i++
+#  for j=1;j<11;j++
+  echo "2drms crdset Chunk$i reftraj Chunk$j M$i.$j" >> hd.in
   done
 done
+cat >> hd.in <<EOF
 hausdorff M* out hausdorff.matrix.gnu outtype trimatrix nrows 10
 runanalysis
 list dataset
