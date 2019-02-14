fa46d6c09265dae8a44a99dcb8206accfd855bd1
Daniel R. Roe <daniel.roe@nih.gov>
2019-02-14 10:48:28 -0500

    DRR - Cpptraj: Add lj PME kappa sweep test.

--- test/Test_LJ_PME/RunTest.sh
+++ test/Test_LJ_PME/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles lj.in ene.dat
+CleanFiles lj.in ene.dat ene.dat.?
 
 INPUT='-i lj.in'
 TESTNAME='LJ PME tests.'
@@ -13,11 +13,30 @@ parm water_2.parm7
 trajin water_2.crd
 
 box x 20 y 20 z 20 alpha 90 beta 90 gamma 90
-debug 10
+#debug 10
 energy out ene.dat prec 16.8 etype pme cut 8.0 dsumtol 0.0000001 skinnb 0.01 ewcoeff 0.3 ewcoefflj 0.3
 EOF
 RunCpptraj "LJ PME test."
 DoTest ene.dat.save ene.dat
+
+# Kappa sweep
+i=0
+for kappa in '0.25' '0.35' '0.45' '0.5' ; do
+  cat > lj.in <<EOF
+parm water_2.parm7
+trajin water_2.crd
+
+box x 20 y 20 z 20 alpha 90 beta 90 gamma 90
+#debug 10
+energy out ene.dat.$i prec 16.8 etype pme cut 8.0 dsumtol 0.0000001 skinnb 0.01 \
+       ewcoeff 0.3 ewcoefflj $kappa order 8 nfft 64,64,64
+EOF
+  RunCpptraj "LJ PME kappa sweep test, $kappa"
+  if [ $i -gt 0 ] ; then
+    DoTest ene.dat.0 ene.dat.$i
+  fi
+  ((i++))
+done
 
 EndTest
 exit 0
