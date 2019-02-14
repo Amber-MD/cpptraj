f611ee880b956697191bd1731209fbc0afcb7aec
Daniel R. Roe <daniel.roe@nih.gov>
2019-02-14 10:13:32 -0500

    DRR - Cpptraj: Simple LJ pme test

--- test/Test_LJ_PME/RunTest.sh
+++ test/Test_LJ_PME/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles lj.in
+CleanFiles lj.in ene.dat
 
 INPUT='-i lj.in'
 TESTNAME='LJ PME tests.'
@@ -14,9 +14,10 @@ trajin water_2.crd
 
 box x 20 y 20 z 20 alpha 90 beta 90 gamma 90
 debug 10
-energy out ene.dat etype pme cut 8.0 dsumtol 0.0000001 skinnb 0.01 ewcoeff 0.4
+energy out ene.dat prec 16.8 etype pme cut 8.0 dsumtol 0.0000001 skinnb 0.01 ewcoeff 0.3 ewcoefflj 0.3
 EOF
 RunCpptraj "LJ PME test."
+DoTest ene.dat.save ene.dat
 
 EndTest
 exit 0
