1b2bf96e7ee44878f74f25c7485ec618d128e285
Daniel R. Roe <daniel.r.roe@gmail.com>
2019-02-18 13:31:23 -0500

    DRR - Cpptraj: Enable Euclid distance metric

--- src/Cluster/Control.cpp
+++ src/Cluster/Control.cpp
@@ -6,6 +6,7 @@
 // Metric classes
 #include "Metric_RMS.h"
 #include "Metric_DME.h"
+#include "Metric_Data_Euclid.h"
 // Algorithms
 #include "Algorithm_HierAgglo.h"
 #include "Algorithm_DBscan.h"
@@ -214,8 +215,9 @@ Cpptraj::Cluster::Metric* Cpptraj::Cluster::Control::AllocateMetric(Metric::Type
 {
   Metric* met = 0;
   switch (mtype) {
-    case Metric::RMS : met = new Metric_RMS(); break;
-    case Metric::DME : met = new Metric_DME(); break;
+    case Metric::RMS    : met = new Metric_RMS(); break;
+    case Metric::DME    : met = new Metric_DME(); break;
+    case Metric::EUCLID : met = new Metric_Data_Euclid(); break;
     default: mprinterr("Error: Unhandled Metric in AllocateMetric.\n");
   }
   return met;
