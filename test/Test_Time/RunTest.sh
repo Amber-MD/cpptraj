83fb9fffa227cc5610eafee34655b658efbea51c
Daniel R. Roe <daniel.roe@nih.gov>
2019-05-20 09:05:31 -0400

    DRR - Cpptraj: Add time to trajectory test

--- test/Test_Time/RunTest.sh
+++ test/Test_Time/RunTest.sh
@@ -2,23 +2,39 @@
 
 . ../MasterTest.sh
 
-CleanFiles cpptraj.in  withtime.rst7
+CleanFiles cpptraj.in withtime.rst7 withtime.nc
 
 INPUT='-i cpptraj.in'
 
 TESTNAME='Set Time tests'
-Requires maxthreads 1
 
-UNITNAME='Set Time test'
-cat > cpptraj.in <<EOF
+UNITNAME='Add time to restart test'
+CheckFor maxthreads 1
+if [ $? -eq 0 ] ; then
+  cat > cpptraj.in <<EOF
 parm ../tz2.parm7
 trajin ../tz2.rst7
 time time0 10.1
 trajout withtime.rst7
 go
 EOF
-RunCpptraj "$UNITNAME"
-DoTest withtime.rst7.save withtime.rst7
+  RunCpptraj "$UNITNAME"
+  DoTest withtime.rst7.save withtime.rst7
+fi
+
+UNITNAME='Add time to trajectory test'
+CheckFor netcdf maxthreads 10
+if [ $? -eq 0 ] ; then
+  cat > cpptraj.in <<EOF
+parm ../tz2.parm7
+trajin ../tz2.nc 1 10
+time time0 100 dt 0.002
+trajout withtime.nc
+go
+EOF
+  RunCpptraj "$UNITNAME"
+  NcTest withtime.nc.save withtime.nc
+fi
 
 EndTest
 exit 0
