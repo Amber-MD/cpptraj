3f28c32196b61e95f7e50526d25ae0c350518897
Daniel R. Roe <daniel.roe@nih.gov>
2019-06-17 13:17:26 -0400

    DRR - Cpptraj: Remove obsolete test stuff, split into parallel/not parallel tests

--- test/Test_DihedralRMS/RunTest.sh
+++ test/Test_DihedralRMS/RunTest.sh
@@ -8,7 +8,8 @@ TESTNAME='Dihedral RMSD tests'
 Requires maxthreads 10
 
 INPUT="-i dih.in"
-UNITNAME='Dihedral RMSD test'
+
+UNITNAME='Dihedral RMSD tests (first, reference, reference traj.)'
 CheckFor netcdf
 if [ $? -eq 0 ] ; then
   cat > dih.in <<EOF
@@ -17,56 +18,27 @@ trajin ../tz2.nc 1 10
 reference ../tz2.nc 1 [MyRef]
 dihrms ToFirst out dihrms.dat noheader phi psi
 dihrms ToRef ref [MyRef] out toref.dat noheader phi psi
-dihrms ToPrev previous out previous.dat phi psi
 # NOTE: All calculated values for same traj should be 0.0
 dihrms ToTraj reftraj ../tz2.nc out totraj.dat phi psi
 EOF
   RunCpptraj "$UNITNAME"
   DoTest dihrms.dat.save dihrms.dat
   DoTest dihrms.dat.save toref.dat
-  DoTest previous.dat.save previous.dat
   DoTest totraj.dat.save totraj.dat
 fi
 
-Disable() {
-# Test nucleic acid chi
-UNITNAME='Multidihedral Nucleotide CHI test'
-CheckFor maxthreads 3
+UNITNAME='Dihedral RMSD test (to previous)'
+CheckFor notparallel
 if [ $? -eq 0 ] ; then
   cat > dih.in <<EOF
-parm ../adh026.3.pdb 
-trajin ../adh026.3.pdb 
-multidihedral chin out chin.dat
+parm ../tz2.parm7
+trajin ../tz2.nc 1 10
+dihrms ToPrev previous out previous.dat phi psi
 EOF
   RunCpptraj "$UNITNAME"
-  DoTest chin.dat.save chin.dat
+  DoTest previous.dat.save previous.dat
 fi
 
-# Test protein dihedrals 
-UNITNAME='Multidihedral protein dihedrals test'
-CheckFor maxthreads 1
-if [ $? -eq 0 ] ; then
-  cat > dih.in <<EOF
-parm ARG.mol2
-trajin ARG.mol2
-multidihedral out arg.dat
-EOF
-  RunCpptraj "$UNITNAME"
-  DoTest arg.dat.save arg.dat
-fi
-
-UNITNAME='Multidihedral cyclic molecule test'
-CheckFor maxthreads 1
-if [ $? -eq 0 ] ; then
-  cat > dih.in <<EOF
-parm cyclic.mol2
-trajin cyclic.mol2
-multidihedral phi psi omega out cyclic.dat
-EOF
-  RunCpptraj "$UNITNAME"
-  DoTest cyclic.dat.save cyclic.dat
-fi
-}
 EndTest
 
 exit 0
