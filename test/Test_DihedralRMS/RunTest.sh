ee86b58d6429bdcedc40b3f836c23c8e9b0a80a1
Daniel R. Roe <daniel.roe@nih.gov>
2019-06-20 08:42:58 -0400

    DRR - Cpptraj: Add dihedral fluctuation (multidihedral plus avg) test

--- test/Test_DihedralRMS/RunTest.sh
+++ test/Test_DihedralRMS/RunTest.sh
@@ -2,17 +2,15 @@
 
 . ../MasterTest.sh
 
-CleanFiles dih.in dihrms.dat toref.dat previous.dat totraj.dat
+CleanFiles dih.in dihrms.dat toref.dat previous.dat totraj.dat phipsifluct.dat
 
 TESTNAME='Dihedral RMSD tests'
-Requires maxthreads 10
+Requires netcdf maxthreads 10
 
 INPUT="-i dih.in"
 
 UNITNAME='Dihedral RMSD tests (first, reference, reference traj.)'
-CheckFor netcdf
-if [ $? -eq 0 ] ; then
-  cat > dih.in <<EOF
+cat > dih.in <<EOF
 parm ../tz2.parm7
 trajin ../tz2.nc 1 10
 reference ../tz2.nc 1 [MyRef]
@@ -21,11 +19,10 @@ dihrms ToRef ref [MyRef] out toref.dat noheader phi psi
 # NOTE: All calculated values for same traj should be 0.0
 dihrms ToTraj reftraj ../tz2.nc out totraj.dat phi psi
 EOF
-  RunCpptraj "$UNITNAME"
-  DoTest dihrms.dat.save dihrms.dat
-  DoTest dihrms.dat.save toref.dat
-  DoTest totraj.dat.save totraj.dat
-fi
+RunCpptraj "$UNITNAME"
+DoTest dihrms.dat.save dihrms.dat
+DoTest dihrms.dat.save toref.dat
+DoTest totraj.dat.save totraj.dat
 
 UNITNAME='Dihedral RMSD test (to previous)'
 CheckFor notparallel
@@ -38,6 +35,17 @@ EOF
   RunCpptraj "$UNITNAME"
   DoTest previous.dat.save previous.dat
 fi
+
+UNITNAME='Dihedral RMS fluctuation test.'
+cat > dih.in <<EOF
+parm ../tz2.parm7
+trajin ../tz2.nc 1 10
+multidihedral PhiPsi phi psi
+run
+runanalysis avg PhiPsi[*] out phipsifluct.dat name Fluct
+EOF
+RunCpptraj "$UNITNAME"
+DoTest phipsifluct.dat.save phipsifluct.dat
 
 EndTest
 
