6ec9bdbd37746fc248b632e3e5c04988d01dca20
Daniel R. Roe <daniel.roe@nih.gov>
2019-06-20 12:44:28 -0400

    DRR - Cpptraj: Take max of format specifier and actual data width when writing strings using Write

--- src/DataSet_string.cpp
+++ src/DataSet_string.cpp
@@ -38,7 +38,8 @@ void DataSet_string::WriteBuffer(CpptrajFile &cbuffer, SizeArray const& pIn) con
     cbuffer.Printf(format_.fmt(), "NoData");
   else {
     // Protect against CpptrajFile buffer overflow.
-    if (format_.Width() >= (int)CpptrajFile::BUF_SIZE) {
+    size_t maxWidth = std::max( (size_t)format_.Width(), Data_[pIn[0]].size() );
+    if (maxWidth >= CpptrajFile::BUF_SIZE) {
       // FIXME: Data sets should not have to worry about spaces in format strings.
       if (format_.fmt()[0] == ' ') cbuffer.Printf(" ");
       cbuffer.Write(Data_[pIn[0]].c_str(), Data_[pIn[0]].size());
