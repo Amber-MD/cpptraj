c61c4e46c1d6aa3958c83cabcdb80a4d37cdac7a
Daniel R. Roe <daniel.roe@nih.gov>
2019-08-15 09:48:02 -0400

    DRR - Cpptraj: Make a blank chain ID null, and make the default chain ID when null 'Z'.

--- src/Residue.h
+++ src/Residue.h
@@ -4,11 +4,13 @@
 // Class: Residue
 /// Hold information for a residue.
 class Residue {
+    static const char BLANK_CHAINID_ = '\0';
+    static const char DEFAULT_CHAINID_ = 'Z';
   public:
     /// CONSTRUCTOR
     Residue() :
       resname_(""), firstAtom_(0), lastAtom_(0), originalResNum_(0), segID_(-1),
-      icode_(' '), chainID_(' '), isTerminal_(false)
+      icode_(' '), chainID_(BLANK_CHAINID_), isTerminal_(false)
     {}
     /// CONSTRUCTOR - Copy given Residue, set first and last atom indices.
     Residue(Residue const& r, int first, int last) :
@@ -30,7 +32,7 @@ class Residue {
     /// CONSTRUCTOR - Res name, original resnum, res icode, segment ID
     Residue(NameType const& n, int r, char i, int s) :
       resname_(n), firstAtom_(-1), lastAtom_(-1), originalResNum_(r), segID_(s),
-       icode_(i), chainID_(' '), isTerminal_(false)
+       icode_(i), chainID_(BLANK_CHAINID_), isTerminal_(false)
     {}
     inline void SetFirstAtom(int i)        { firstAtom_ = i;      }
     inline void SetLastAtom(int i)         { lastAtom_ = i;       }
@@ -47,7 +49,7 @@ class Residue {
     inline int OriginalResNum()   const { return originalResNum_; }
     inline int SegID()            const { return segID_;          }
     inline char Icode()           const { return icode_;          }
-    inline char ChainID()         const { return chainID_;        }
+    inline char ChainID()         const;
     inline const char *c_str()    const { return *resname_;       }
     inline NameType const& Name() const { return resname_;        }
     inline bool IsTerminal()      const { return isTerminal_;     }
@@ -72,4 +74,11 @@ class Residue {
     char chainID_;       ///< Residue chain ID
     bool isTerminal_;    ///< True if residue was originally a terminal residue
 };
+// ----- INLINE ROUTINES -------------------------
+char Residue::ChainID() const {
+  if (chainID_ == BLANK_CHAINID_)
+    return DEFAULT_CHAINID_;
+  else
+    return chainID_;
+}
 #endif
