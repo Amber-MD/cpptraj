1bcd71732e6be3473dabbea23572ce3c4e0f9302
Daniel R. Roe <daniel.r.roe@gmail.com>
2019-08-21 15:56:38 -0400

    DRR - Cpptraj: Start new DSSP action that more closely follows the definitions of Kabsch and Sander

---
+++ src/Action_DSSP2.cpp
@@ -0,0 +1,171 @@
+#include <algorithm> // std::fill
+#include "Action_DSSP2.h"
+#include "CpptrajStdio.h"
+
+// ----- SSres -----------------------------------------------------------------
+Action_DSSP2::SSres::SSres() :
+  resDataSet_(0),
+  chirality_(0),
+  pattern_(NOHBOND),
+  sstype_(NONE),
+  idx_(-1),
+  C_(-1),
+  O_(-1),
+  N_(-1),
+  H_(-1),
+  CA_(-1),
+  isSelected_(false)
+{
+  std::fill(SScount_, SScount_ + NSSTYPE_, 0); 
+}
+
+void Action_DSSP2::SSres::Deselect() {
+  isSelected_ = false;
+  C_ = -1;
+  H_ = -1;
+  N_ = -1;
+  O_ = -1;
+  CA_ = -1;
+}
+
+// ----- Action_DSSP2 ----------------------------------------------------------
+
+/** From the original Kabsch & Sander 1983 paper. Obtained via
+  *   q1 = 0.42e
+  *   q2 = 0.20e
+  *   f = 332 (approximate conversion factor to get energies in kcal/mol)
+  *   fac = q1*q2*f
+  */
+const double Action_DSSP2::DSSP_fac_ = 27.888;
+
+const double Action_DSSP2::DSSP_cut_ = -0.5;
+
+const char  Action_DSSP2::DSSP_char_[] = { ' ', 'E', 'B', 'G', 'H', 'I', 'T', 'S' };
+
+const char* Action_DSSP2::SSchar_[]    = { "0", "b", "B", "G", "H", "I", "T", "S" };
+
+const char* Action_DSSP2::SSname_[]={"None", "Extended", "Bridge", "3-10", "Alpha", "Pi", "Turn", "Bend"};
+
+const std::string Action_DSSP2::SSzlabels_ = "zlabels None,Para,Anti,3-10,Alpha,Pi,Turn,Bend";
+
+Action_DSSP2::Action_DSSP2() :
+  debug_(0),
+  outfile_(0),
+  dsspFile_(0),
+  assignout_(0)
+{}
+
+// Action_DSSP2::Help()
+void Action_DSSP2::Help() const {
+  mprintf("\t[<name>] [out <filename>] [<mask>] [sumout <filename>]\n"
+          "\t[assignout <filename>] [totalout <filename>] [ptrajformat]\n"
+          "\t[namen <N name>] [nameh <H name>] [nameca <CA name>]\n"
+          "\t[namec <C name>] [nameo <O name>]\n"
+          "  Calculate secondary structure content for residues in <mask>.\n"
+          "  If sumout not specified, the filename specified by out is used with .sum suffix.\n");
+}
+
+// Action_DSSP2::Init()
+Action::RetType Action_DSSP2::Init(ArgList& actionArgs, ActionInit& init, int debugIn)
+{
+  debug_ = debugIn;
+  //Nframe_ = 0;
+  // Get keywords
+  outfile_ = init.DFL().AddDataFile( actionArgs.GetStringKey("out"), actionArgs );
+  std::string temp = actionArgs.GetStringKey("sumout");
+  if (temp.empty() && outfile_ != 0) 
+    temp = outfile_->DataFilename().Full() + ".sum";
+  dsspFile_ = init.DFL().AddDataFile( temp );
+  DataFile* totalout = init.DFL().AddDataFile( actionArgs.GetStringKey("totalout"), actionArgs );
+  assignout_ = init.DFL().AddCpptrajFile(actionArgs.GetStringKey("assignout"), "SS assignment");
+  printString_ = actionArgs.hasKey("ptrajformat");
+  temp = actionArgs.GetStringKey("namen");
+  if (!temp.empty()) BB_N_ = temp;
+  temp = actionArgs.GetStringKey("nameh");
+  if (!temp.empty()) BB_H_ = temp;
+  temp = actionArgs.GetStringKey("namec");
+  if (!temp.empty()) BB_C_ = temp;
+  temp = actionArgs.GetStringKey("nameo");
+  if (!temp.empty()) BB_O_ = temp;
+  temp = actionArgs.GetStringKey("nameca");
+  if (!temp.empty()) BB_CA_ = temp;
+  // Get masks
+  if (Mask_.SetMaskString( actionArgs.GetMaskNext() )) return Action::ERR;
+
+  // Set up the DSSP data set
+  dsetname_ = actionArgs.GetStringNext();
+  // Set default name if none specified
+  if (dsetname_.empty())
+    dsetname_ = init.DSL().GenerateDefaultName("DSSP");
+  // Set up Z labels
+  if (outfile_ != 0)
+    outfile_->ProcessArgs(SSzlabels_);
+  // Create data sets for total fraction SS vs time.
+  for (int i = 0; i < NSSTYPE_; i++) {
+    totalDS_[i] = init.DSL().AddSet(DataSet::FLOAT, MetaData(dsetname_, SSname_[i]));
+    if (totalDS_[i] == 0) {
+      mprinterr("Error: Could not create DSSP total frac v time data set.\n");
+      return Action::ERR;
+    }
+    // For now dont add 'None' so colors match up.
+    if (totalout != 0 && i > 0) totalout->AddDataSet( totalDS_[i] );
+  }
+
+  mprintf( "    SECSTRUCT: Calculating secondary structure using mask [%s]\n",Mask_.MaskString());
+  if (outfile_ != 0) 
+    mprintf("\tDumping results to %s\n", outfile_->DataFilename().full());
+  if (dsspFile_ != 0)
+    mprintf("\tSum results to %s\n", dsspFile_->DataFilename().full());
+  if (printString_) { 
+    mprintf("\tSS data for each residue will be stored as a string.\n");
+    for (int i = 0; i < NSSTYPE_; i++)
+      mprintf("\t\t%s = %s\n", SSchar_[i], SSname_[i]);
+  } else {
+    mprintf("\tSS data for each residue will be stored as integers.\n");
+    for (int i = 0; i < NSSTYPE_; i++)
+      mprintf("\t\t%i = %s\n", i, SSname_[i]);
+  }
+  if (assignout_ != 0)
+    mprintf("\tOverall assigned SS will be written to %s\n", assignout_->Filename().full());
+  mprintf("\tBackbone Atom Names: N=[%s]  H=[%s]  C=[%s]  O=[%s]  CA=[%s]\n",
+          *BB_N_, *BB_H_, *BB_C_, *BB_O_, *BB_CA_ );
+  mprintf("# Citation: Kabsch, W.; Sander, C.; \"Dictionary of Protein Secondary Structure:\n"
+          "#           Pattern Recognition of Hydrogen-Bonded and Geometrical Features.\"\n"
+          "#           Biopolymers (1983), V.22, pp.2577-2637.\n" );
+  init.DSL().SetDataSetsPending(true);
+  Init_ = init;
+  return Action::OK;
+}
+
+// Action_DSSP2::Setup()
+/** Set up secondary structure calculation for all residues selected by the
+  * mask. A residue is selected if at least one atom in the residue is
+  * selected by the mask. The coordinate indices (i.e. atom # * 3) for
+  * the C, O, N, H, and CA atoms are set up if those atoms are present.
+  * The residue is only initialized if it has not been previously selected
+  * and set up by a prior call to setup.
+  */
+Action::RetType Action_DSSP2::Setup(ActionSetup& setup)
+{
+  // Set up mask for this parm
+  if ( setup.Top().SetupIntegerMask( Mask_ ) ) return Action::ERR;
+  if ( Mask_.None() ) {
+    mprintf("Warning: DSSP: Mask has no atoms.\n");
+    return Action::SKIP;
+  }
+  // Deselect any existing residues
+  for (SSarrayType::iterator it = Residues_.begin(); it != Residues_.end(); ++it)
+    it->Deselect();
+  // Set up for each solute residue 
+  Range soluteRes = setup.Top().SoluteResidues();
+  mprintf("\tSetting up for %i solute residues.\n", soluteRes.Size());
+  if ((unsigned int)soluteRes.Size() > Residues_.size())
+    Residues_.resize( soluteRes.Size() );
+  
+}
+
+// Action_DSSP2::DoAction()
+Action::RetType Action_DSSP2::DoAction(int frameNum, ActionFrame& frm)
+{
+
+}
---
+++ src/Action_DSSP2.h
@@ -0,0 +1,102 @@
+#ifndef INC_ACTION_DSSP2_H
+#define INC_ACTION_DSSP2_H
+#include <vector>
+#include <string>
+#include "Action.h"
+#include "NameType.h"
+#include "AtomMask.h"
+/// <Enter description of Action_DSSP2 here>
+/** Based on protein secondary structure definitions given in:
+  *   Kabsch, W.; Sander, C.; \"Dictionary of Protein Secondary Structure:
+  *   Pattern Recognition of Hydrogen-Bonded and Geometrical Features.
+  *   Biopolymers (1983), V.22, pp.2577-2637.
+  * The various secondary structure types that can be assigned (in order
+  * of decreasing priority) are:
+  *   H - Alpha helix (4)
+  *   B - Single bridge (ladder of length 1)
+  *   E - Beta strand (ladder length > 1)
+  *   G - 3-10 helix (3)
+  *   I - Pi helix (5)
+  *   T - Turn (3, 4, 5)
+  *   S - Bend (Angle i-2, i, i+2 > 70 deg.)
+  */
+class Action_DSSP2 : public Action {
+  public:
+    Action_DSSP2();
+    DispatchObject* Alloc() const { return (DispatchObject*)new Action_DSSP2(); }
+    void Help() const;
+  private:
+    Action::RetType Init(ArgList&, ActionInit&, int);
+    Action::RetType Setup(ActionSetup&);
+    Action::RetType DoAction(int, ActionFrame&);
+    void Print() {}
+
+    class ElemHbond;
+    class SSres;
+
+    /// Secondary structure types
+    enum SStype { NONE=0, EXTENDED, BRIDGE, H3_10, ALPHA, HPI, TURN, BEND };
+    static const int NSSTYPE_ = 8;  ///< # of secondary structure types.
+    static const char DSSP_char_[]; ///< DSSP 1 char names corresponding to SStype
+    static const char* SSname_[];   ///< Full secondary structure names corresponding to SStype
+    static const char* SSchar_[];   ///< PTRAJ 1 character names corresponding to SStype
+    static const std::string SSzlabels_; ///< Output graph Z labels corresponding to SStype
+
+    /// Elementary hydrogen bond pattern
+    enum PatternType {
+      NOHBOND = 0, ///< No hydrogen bond
+      TURNBEG,     ///< Start of n-Turn, >
+      TURNEND,     ///< End of n-Turn, <
+      TURNX,       ///< Coincidence of TURNBEG and TURNEND, X
+      TURN3,       ///< Inside i to i + 3 turn (T)
+      TURN4,       ///< Inside i to i + 4 turn (T)
+      TURN5,       ///< Inside i to i + 5 turn (T)
+      BRIDGEPARA,  ///< (i-1,j) and (j,i+1) or (j-1,i) and (i,j+1), lower case
+      BRIDGEANTI   ///< (i,j) and (j,i) or (i-1,j+1) and (j-1,i+1), upper case
+    };
+
+    static const double DSSP_fac_;  ///< Original DSSP factor for calc. H-bond "energy"
+    static const double DSSP_cut_;  ///< Original DSSP H-bond energy cutoff in kcal/mol
+
+    typedef std::vector<SSres> SSarrayType;
+
+    int debug_;            ///< Action debug level
+    SSarrayType Residues_; ///< Hold SS data for all residues.
+    NameType BB_N_;        ///< Protein N atom name ('N')
+    NameType BB_H_;        ///< Protein N-H atom name ('H')
+    NameType BB_C_;        ///< Protein C atom name ('C')
+    NameType BB_O_;        ///< Protein C-O atom name ('O')
+    NameType BB_CA_;       ///< Protein alpha C name ('CA')
+    AtomMask Mask_;        ///< Mask used to determine selected residues.
+    DataFile* outfile_;          ///< Output Data file
+    DataFile* dsspFile_;         ///< Sum output file
+    CpptrajFile* assignout_;     ///< Assignment output file.
+    std::string dsetname_;       ///< DSSP data set name
+    DataSet* totalDS_[NSSTYPE_]; ///< Hold total SS each frame for each SS type
+    ActionInit Init_;            ///< Hold pointers to master DSL/DFL
+    bool printString_;           ///< If true print 1 char per residue indicating ss type
+};
+
+
+class Action_DSSP2::SSres {
+  public:
+    SSres();
+    /// Deselect this residue and reset coordinate indices.
+    void Deselect();
+  private:
+    typedef std::vector<int> HbArrayType;
+    HbArrayType CO_HN_Hbonds_; ///< This res C-O hbonded to these res H-N.
+    DataSet* resDataSet_;      ///< DataSet for SS assignment each frame for this res.
+    double chirality_;         ///< dihedral CA[i-1, i, i+1, i+2]
+    int SScount_[NSSTYPE_];    ///< Hold count for each SS type
+    PatternType pattern_;      ///< Assigned hbond pattern for this frame
+    SStype sstype_;            ///< SS assignment for this frame
+    int idx_;                  ///< Residue index in topology
+    int C_;                    ///< Coord idx of BB carbon
+    int O_;                    ///< Coord idx of BB oxygen
+    int N_;                    ///< Coord idx of BB nitrogen
+    int H_;                    ///< Coord idx of BB hydrogen
+    int CA_;                   ///< Coord idx of BB alpha carbon
+    bool isSelected_;          ///< True if calculating SS for this residue.
+};
+#endif
