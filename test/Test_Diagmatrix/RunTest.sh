d320d7043f430f6d1e714086e47159b62759fba7
Daniel R. Roe <daniel.r.roe@gmail.com>
2019-09-28 08:54:30 -0400

    DRR - Cpptraj: Do not compare eigenvectors since the sign can flip depending on what math library is being used (same reason it was disabled in the Test_Matrix directory)

--- test/Test_Diagmatrix/RunTest.sh
+++ test/Test_Diagmatrix/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles matrix.in MyEvecs.dat MyThermo.dat
+CleanFiles matrix.in MyThermo.dat
 
 TESTNAME='MW covariance matrix thermo analysis test'
 Requires netcdf 
@@ -12,10 +12,9 @@ cat > matrix.in <<EOF
 parm ../DPDP.parm7
 trajin ../DPDP.nc
 matrix MyMatrix mwcovar @1-33&!@H=
-diagmatrix MyMatrix out MyEvecs.dat thermo outthermo MyThermo.dat
+diagmatrix MyMatrix thermo outthermo MyThermo.dat
 EOF
 RunCpptraj "$TESTNAME"
-DoTest MyEvecs.dat.save MyEvecs.dat
 DoTest MyThermo.dat.save MyThermo.dat
 
 EndTest
