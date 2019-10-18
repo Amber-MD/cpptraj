ced4edc347d0a41a3dab118c12eab4a34ecbd6c1
Daniel R. Roe <daniel.roe@nih.gov>
2019-10-18 08:51:00 -0400

    DRR - Add tng test with offset

--- test/Test_GromacsTNG/RunTest.sh
+++ test/Test_GromacsTNG/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles cpptraj.in temperature.dat rmsd.dat ene.dat
+CleanFiles cpptraj.in temperature.dat rmsd.dat ene.dat offset.*.dat
 
 TESTNAME='TNG read test'
 
@@ -10,27 +10,60 @@ Requires tng notparallel
 
 INPUT='-i cpptraj.in'
 
-cat > cpptraj.in <<EOF
+TestTng() {
+  UNITNAME=$TESTNAME
+  trajin_args=$1
+  refcmd=''
+  rmsref='first'
+  xmin=0
+  xstep=10
+  tout='temperature.dat'
+  rout='rmsd.dat'
+  eout='ene.dat'
+  if [ ! -z "$trajin_args" ] ; then
+    UNITNAME="$TESTNAME (with offset)"
+    refcmd='reference md_1_1.tng 1'
+    rmsref='reference'
+    xmin=10
+    xstep=20
+    tout='offset.temperature.dat'
+    rout='offset.rmsd.dat'
+    eout='offset.ene.dat'
+  fi
+  cat > cpptraj.in <<EOF
 parm topol.parm7
-trajin md_1_1.tng
+noprogress
+$refcmd
+trajin md_1_1.tng $trajin_args
 # Temperature calc tests velocity read
-temperature MyTemp ntc 2 out temperature.dat xmin 0 xstep 10 prec 12.7
+temperature MyTemp ntc 2 out $tout xmin $xmin xstep $xstep prec 12.7
 # Energy (specifically kinetic VV) tests velocity/force read.
 # It is probably not correct since this really requires the half
 # step velocities, but is good enough for a regression test.
-energy MyEne ^1 out ene.dat xmin 0 xstep 10 prec 12.7 \
+energy MyEne ^1 out $eout xmin $xmin xstep $xstep prec 12.7 \
   kinetic ketype vv dt 0.002 
 # RMSD tests coordinates read
-rms MyRms first ^1&@C,CA,N
+rms MyRms $rmsref ^1&@C,CA,N
 run
 # Convert RMSD to nm
 MyRmsNm = MyRms / 10.0
-writedata rmsd.dat xmin 0 xstep 10 MyRmsNm prec 12.7
+writedata $rout xmin $xmin xstep $xstep MyRmsNm prec 12.7
 EOF
-RunCpptraj "$TESTNAME"
-DoTest temperature.dat.save temperature.dat
-DoTest rmsd.dat.save rmsd.dat
-DoTest ene.dat.save ene.dat
+  RunCpptraj "$TESTNAME"
+  if [ -z "$trajin_args" ] ; then
+    DoTest temperature.dat.save temperature.dat
+    DoTest rmsd.dat.save rmsd.dat
+    DoTest ene.dat.save ene.dat
+  else
+    DoTest offset.temperature.dat.save offset.temperature.dat
+    DoTest offset.rmsd.dat.save offset.rmsd.dat
+    DoTest offset.ene.dat.save offset.ene.dat
+  fi
+}
+
+TestTng
+
+TestTng "2 8 2"
 
 EndTest
 
