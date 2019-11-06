e0a739de4286d86e58e7fb1dc84331da2eec7d90
Daniel R. Roe <daniel.r.roe@gmail.com>
2019-11-06 10:21:05 -0500

    DRR - Add charmm param write test.

--- test/Test_CharmmParams/RunTest.sh
+++ test/Test_CharmmParams/RunTest.sh
@@ -4,7 +4,7 @@
 
 TESTNAME='Charmm parameters tests'
 
-CleanFiles cpptraj.in test3.parm7
+CleanFiles cpptraj.in test3.parm7 testdata.prm
 
 INPUT='-i cpptraj.in'
 
@@ -15,9 +15,11 @@ readdata kcx.str as charmmrtfprm name MyParm
 list dataset
 updateparameters parmindex 0 setname MyParm
 parmwrite out test3.parm7
+writedata testdata.prm lys.parm7 
 EOF
 RunCpptraj "$UNITNAME"
 DoTest test3.parm7.save test3.parm7 -I %VERSION
+DoTest testdata.prm.save testdata.prm
 
 EndTest
 exit 0
