fe3cfdaa98a582516b9fcb4336b8f97ea6bb8c1a
Daniel R. Roe <daniel.roe@nih.gov>
2019-12-13 09:15:21 -0500

    DRR - Add test for temperature update

--- test/Test_Temperature/RunTest.sh
+++ test/Test_Temperature/RunTest.sh
@@ -27,9 +27,12 @@ DoTest T2.dat.save T2.dat
 cat > temp.in <<EOF
 parm Ala10.99SB.mbondi2.parm7
 trajin run.ntc3.nc
-temperature out T3.dat ntc 3 T_ShakeAll
+# Write data with no header so they can be compared to each other.
+temperature out T3.dat noheader ntc 3 T_ShakeAll update
+temperature frame out T4.dat noheader
 EOF
 RunCpptraj "Temperature with SHAKE on all atoms."
 DoTest T3.dat.save T3.dat
+DoTest T3.dat.save T4.dat
 
 EndTest
