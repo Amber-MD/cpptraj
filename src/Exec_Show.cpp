bf7140a88ce67a284ebd9f51a15efa4721e3c761
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-01-07 10:33:50 -0500

    DRR - Allow multiple variables to be passed to show

--- src/Exec_Show.cpp
+++ src/Exec_Show.cpp
@@ -3,7 +3,10 @@
 #include "DataSet_StringVar.h"
 
 void Exec_Show::Help() const {
-  mprintf("  Show all current script variables and their values.\n");
+  mprintf("\t[<var1> ...]\n"
+          "  If no variable names specified, show all current script variables and\n"
+          "  their values. Otherwise, show the values of the specified script\n"
+          "  variables.\n");
 }
 
 Exec::RetType Exec_Show::Execute(CpptrajState& State, ArgList& argIn)
@@ -12,15 +15,18 @@ Exec::RetType Exec_Show::Execute(CpptrajState& State, ArgList& argIn)
   if (varname.empty()) {
     State.DSL().ListStringVar();
   } else {
-    DataSetList Svars = State.DSL().GetMultipleSets( varname );
-    if (Svars.empty()) {
-      return CpptrajState::ERR;
-    }
-    for (DataSetList::const_iterator ds = Svars.begin(); ds != Svars.end(); ++ds) {
-      if ( (*ds)->Type() == DataSet::STRINGVAR ) {
-        DataSet_StringVar const& var = static_cast<DataSet_StringVar const&>( *(*ds) );
-        mprintf("\t%s = '%s'\n", var.legend(), var.Value().c_str());
+    while (!varname.empty()) {
+      DataSetList Svars = State.DSL().GetMultipleSets( varname );
+      if (Svars.empty()) {
+        return CpptrajState::ERR;
       }
+      for (DataSetList::const_iterator ds = Svars.begin(); ds != Svars.end(); ++ds) {
+        if ( (*ds)->Type() == DataSet::STRINGVAR ) {
+          DataSet_StringVar const& var = static_cast<DataSet_StringVar const&>( *(*ds) );
+          mprintf("\t%s = '%s'\n", var.legend(), var.Value().c_str());
+        }
+      }
+      varname = argIn.GetStringNext();
     }
   }
   return CpptrajState::OK;
