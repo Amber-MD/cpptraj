7cba78d0aa265d1803916aac91878c5aee259b14
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-01-07 08:32:42 -0500

    DRR - Allow 'show' to only show specified string vars

--- src/Exec_Show.cpp
+++ src/Exec_Show.cpp
@@ -1,5 +1,6 @@
 #include "Exec_Show.h"
 #include "CpptrajStdio.h"
+#include "DataSet_StringVar.h"
 
 void Exec_Show::Help() const {
   mprintf("  Show all current script variables and their values.\n");
@@ -7,6 +8,20 @@ void Exec_Show::Help() const {
 
 Exec::RetType Exec_Show::Execute(CpptrajState& State, ArgList& argIn)
 {
-  State.DSL().ListStringVar();
+  std::string varname = argIn.GetStringNext();
+  if (varname.empty()) {
+    State.DSL().ListStringVar();
+  } else {
+    DataSetList Svars = State.DSL().GetMultipleSets( varname );
+    if (Svars.empty()) {
+      return CpptrajState::ERR;
+    }
+    for (DataSetList::const_iterator ds = Svars.begin(); ds != Svars.end(); ++ds) {
+      if ( (*ds)->Type() == DataSet::STRINGVAR ) {
+        DataSet_StringVar const& var = static_cast<DataSet_StringVar const&>( *(*ds) );
+        mprintf("\t%s = '%s'\n", var.legend(), var.Value().c_str());
+      }
+    }
+  }
   return CpptrajState::OK;
 }
