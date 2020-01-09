31260711d6dbce946c9b975c6582b422fdc216e1
Daniel R. Roe <daniel.roe@nih.gov>
2020-01-09 10:48:57 -0500

    DRR - Add TIP4P gist test

--- test/Test_GIST_Order/RunTest.sh
+++ test/Test_GIST_Order/RunTest.sh
@@ -2,22 +2,32 @@
 
 . ../MasterTest.sh
 
-CleanFiles gist.in gist-*.dx gistout.dat
+CleanFiles gist.in gist-*.dx gistout.dat tip4p.dat
 
-TESTNAME='GIST tetrahedral water cluster test'
+TESTNAME='GIST tetrahedral water cluster tests'
 Requires notparallel
 
 INPUT="-i gist.in"
 
+UNITNAME='TIP3P water cluster'
 cat > gist.in <<EOF
 parm test_gist.prmtop
 trajin test_gist-center.crd
 gist doorder gridcntr 0.5 0.5 0.5 griddim 1 1 1 gridspacn 1.0 out gistout.dat refdens 0.0333
 EOF
-RunCpptraj "$TESTNAME"
-
+RunCpptraj "$UNITNAME"
 DoTest gist-order-norm.dx.save gist-order-norm.dx
 DoTest gistout.dat.save gistout.dat
+
+UNITNAME='TIP4P water cluster'
+cat > gist.in <<EOF
+parm tip4pew.cluster.parm7
+trajin tip4pew.cluster.rst7
+gist doorder gridcntr 0.5 0.5 0.5 griddim 1 1 1 gridspacn 1.0 out tip4p.dat refdens 0.0333
+EOF
+RunCpptraj "$UNITNAME"
+#DoTest gist-order-norm.dx.save gist-order-norm.dx
+DoTest tip4p.dat.save tip4p.dat
 
 EndTest
 exit 0
