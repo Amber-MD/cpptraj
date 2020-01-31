34ec87fac5bd2fa714b643609e354d5a4d5600f1
Daniel R. Roe <daniel.roe@nih.gov>
2020-01-31 13:29:14 -0500

    DRR - Fix netcdf URL

--- devtools/ci/appveyor/setup-environment.bat
+++ devtools/ci/appveyor/setup-environment.bat
@@ -35,7 +35,7 @@ if %BUILD_TYPE% equ cmake-vs (
 	sh -lc "pacman -S --noconfirm --needed mingw-w64-x86_64-openblas mingw-w64-x86_64-arpack mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-ncurses mingw-w64-x86_64-readline diffutils"
 
 	rem build NetCDF (we need our own version since the one in Pacman has an unwanted dependency on HDF5 and Termcap)
-	sh -lc "if [ ! -f /usr/local/lib/libnetcdf.a ]; then curl -fsS -o netcdf-4.3.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.zip && 7z x netcdf-4.3.3.zip -o$APPVEYOR_BUILD_FOLDER > /dev/null && cd $APPVEYOR_BUILD_FOLDER/netcdf-4.3.3 && exec 0</dev/null && ./configure --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap && make -r install; else echo 'Have Cached NetCDF'; fi"
+	sh -lc "if [ ! -f /usr/local/lib/libnetcdf.a ]; then curl -fsS -o netcdf-c-4.7.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-4.7.3.zip  && 7z x netcdf-c-4.7.3.zip -o$APPVEYOR_BUILD_FOLDER > /dev/null && cd $APPVEYOR_BUILD_FOLDER/netcdf-c-4.7.3 && exec 0</dev/null && ./configure --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap && make -r install; else echo 'Have Cached NetCDF'; fi"
 	
 	rem path from here: https://www.appveyor.com/docs/build-environment/#mingw-msys-cygwin
 	set MINGWDIR=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1
