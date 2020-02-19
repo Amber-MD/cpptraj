7a2dbd86d4b26a2db89315239567b144d90a2fd7
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-02-19 16:07:12 -0500

    DRR - Use UNITNAME

--- test/Test_Temperature/RunTest.sh
+++ test/Test_Temperature/RunTest.sh
@@ -8,22 +8,26 @@ TESTNAME='Temperature tests'
 Requires netcdf maxthreads 10
 
 INPUT="-i temp.in"
+
+UNITNAME="Temperature test"
 cat > temp.in <<EOF
 parm Ala10.99SB.mbondi2.parm7
 trajin run0.nc
 temperature out T.dat ntc 1 T_NoShake
 EOF
-RunCpptraj "Temperature test."
+RunCpptraj "$UNITNAME."
 DoTest T.dat.save T.dat
 
+UNITNAME="Temperature with SHAKE on hydrogens"
 cat > temp.in <<EOF
 parm Ala10.99SB.mbondi2.parm7
 trajin run.ntc2.nc
 temperature out T2.dat ntc 2 T_ShakeH
 EOF
-RunCpptraj "Temperature with SHAKE on hydrogens."
+RunCpptraj "$UNITNAME."
 DoTest T2.dat.save T2.dat
 
+UNITNAME="Temperature with SHAKE on all atoms"
 cat > temp.in <<EOF
 parm Ala10.99SB.mbondi2.parm7
 trajin run.ntc3.nc
@@ -31,7 +35,7 @@ trajin run.ntc3.nc
 temperature out T3.dat noheader ntc 3 T_ShakeAll update
 temperature frame out T4.dat noheader
 EOF
-RunCpptraj "Temperature with SHAKE on all atoms."
+RunCpptraj "$UNITNAME."
 DoTest T3.dat.save T3.dat
 DoTest T3.dat.save T4.dat
 
@@ -43,7 +47,7 @@ temperature Total ntc 2 out T.XP.agr
 temperature Water ntc 2 out T.XP.agr :WAT
 temperature NoXP  ntc 2 out T.XP.agr :WAT@O,H1,H2
 EOF
-RunCpptraj "$UNITNAME"
+RunCpptraj "$UNITNAME."
 DoTest T.XP.agr.save T.XP.agr
 
 EndTest
