6f7b37d602ec2f6a0cc7dae9e6563446091ce9c5
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-02-19 16:08:56 -0500

    DRR - Fix test protection

--- test/Test_Temperature/RunTest.sh
+++ test/Test_Temperature/RunTest.sh
@@ -5,39 +5,48 @@
 CleanFiles temp.in T.dat T2.dat T3.dat T4.dat T.XP.agr
 
 TESTNAME='Temperature tests'
-Requires netcdf maxthreads 10
+Requires maxthreads 10
 
 INPUT="-i temp.in"
 
 UNITNAME="Temperature test"
-cat > temp.in <<EOF
+CheckFor netcdf
+if [ $? -eq 0 ] ; then
+  cat > temp.in <<EOF
 parm Ala10.99SB.mbondi2.parm7
 trajin run0.nc
 temperature out T.dat ntc 1 T_NoShake
 EOF
-RunCpptraj "$UNITNAME."
-DoTest T.dat.save T.dat
+  RunCpptraj "$UNITNAME."
+  DoTest T.dat.save T.dat
+fi
 
 UNITNAME="Temperature with SHAKE on hydrogens"
-cat > temp.in <<EOF
+CheckFor netcdf
+if [ $? -eq 0 ] ; then
+  cat > temp.in <<EOF
 parm Ala10.99SB.mbondi2.parm7
 trajin run.ntc2.nc
 temperature out T2.dat ntc 2 T_ShakeH
 EOF
-RunCpptraj "$UNITNAME."
-DoTest T2.dat.save T2.dat
+  RunCpptraj "$UNITNAME."
+  DoTest T2.dat.save T2.dat
+fi
 
 UNITNAME="Temperature with SHAKE on all atoms"
-cat > temp.in <<EOF
+CheckFor netcdf
+if [ $? -eq 0 ] ; then
+  cat > temp.in <<EOF
 parm Ala10.99SB.mbondi2.parm7
 trajin run.ntc3.nc
 # Write data with no header so they can be compared to each other.
 temperature out T3.dat noheader ntc 3 T_ShakeAll update
 temperature frame out T4.dat noheader
 EOF
-RunCpptraj "$UNITNAME."
-DoTest T3.dat.save T3.dat
-DoTest T3.dat.save T4.dat
+  RunCpptraj "$UNITNAME."
+  DoTest T3.dat.save T3.dat
+  DoTest T3.dat.save T4.dat
+fi
 
 UNITNAME='Temperature calc with extra points'
 cat > temp.in <<EOF
