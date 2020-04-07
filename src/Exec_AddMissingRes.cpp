1f5dfda6062e135abc4391cd401fae7d3fc3d9d1
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-04-07 14:06:20 -0400

    Put class definition for Gap in header; use DFL to allocate output file

--- src/Exec_AddMissingRes.cpp
+++ src/Exec_AddMissingRes.cpp
@@ -10,60 +10,12 @@ void Exec_AddMissingRes::Help() const
 
 }
 
-typedef std::vector<std::string> Sarray;
-
-/// Record a gap in PDB
-class Gap {
-  public:
-    Gap() {}
-    /// Start gap with res name, res num, and chain
-    Gap(std::string const& startNameIn, int startResIn, std::string const& startChainIn) :
-      resNames_(1, startNameIn), startRes_(startResIn), stopRes_(-1), chainId_(startChainIn[0])
-      {}
-    /// Start gap with res num and chain
-    Gap(int startResIn, std::string const& startChainIn) :
-      startRes_(startResIn), stopRes_(-1), chainId_(startChainIn[0])
-      {}
-
-    std::string const& FirstName() const { return resNames_.front(); }
-    std::string const& LastName()  const { return resNames_.back(); }
-    int StartRes()                 const { return startRes_; }
-    int StopRes()                  const { return stopRes_; }
-    char Chain()                   const { return chainId_; }
-    unsigned int Nres()            const { return resNames_.size(); }
-    typedef Sarray::const_iterator name_iterator;
-    name_iterator nameBegin()      const { return resNames_.begin(); }
-    name_iterator nameEnd()        const { return resNames_.end(); }
-
-    void SetStopRes(int s) { stopRes_ = s; }
-    void AddGapRes(std::string const& r) { resNames_.push_back( r ); }
-  private:
-    Sarray resNames_; ///< Residue names in the Gap
-    int startRes_;    ///< pdb start residue number for the gap 
-    int stopRes_;     ///< pdb stop residue number for the gap
-    char chainId_;    ///< chain ID of the gap
-};
-
-// Exec_AddMissingRes::Execute()
-Exec::RetType Exec_AddMissingRes::Execute(CpptrajState& State, ArgList& argIn)
+/** Get gap info from PDB */
+int Exec_AddMissingRes::FindGaps(Garray& Gaps, CpptrajFile& outfile, std::string const& pdbname)
+const
 {
-  std::string pdbname = argIn.GetStringKey("pdbname");
-  if (pdbname.empty()) {
-    mprinterr("Error: provide PDB name.\n");
-    return CpptrajState::ERR;
-  }
-  std::string outname = argIn.GetStringKey("out");
-
-  mprintf("\tPDB name: %s\n", pdbname.c_str());
-  CpptrajFile outfile; // TODO use DFL
-  outfile.OpenWrite(outname);
-  mprintf("\tOutput file: %s\n", outfile.Filename().full());
-
-  // Get gap info from PDB
-  typedef std::vector<Gap> Garray;
-  Garray Gaps;
   BufferedLine infile;
-  if (infile.OpenFileRead( pdbname )) return CpptrajState::ERR;
+  if (infile.OpenFileRead( pdbname )) return 1; 
   const char* linePtr = infile.Line();
   int inMissing = 0;
   int nmissing = 0;
@@ -157,8 +109,27 @@ Exec::RetType Exec_AddMissingRes::Execute(CpptrajState& State, ArgList& argIn)
   outfile.Printf("%i missing residues.\n", nmissing);
   if (Gaps.empty()) {
     mprintf("Warning: No gaps found.\n");
-    return CpptrajState::OK;
   }
+  return 0;
+} 
+
+// Exec_AddMissingRes::Execute()
+Exec::RetType Exec_AddMissingRes::Execute(CpptrajState& State, ArgList& argIn)
+{
+  std::string pdbname = argIn.GetStringKey("pdbname");
+  if (pdbname.empty()) {
+    mprinterr("Error: provide PDB name.\n");
+    return CpptrajState::ERR;
+  }
+  mprintf("\tPDB name: %s\n", pdbname.c_str());
+  CpptrajFile* outfile = State.DFL().AddCpptrajFile(argIn.GetStringKey("out"),
+                                                    "AddMissingRes", DataFileList::TEXT, true);
+  if (outfile==0) return CpptrajState::ERR;
+  mprintf("\tOutput file: %s\n", outfile->Filename().full());
+
+  Garray Gaps;
+  if (FindGaps(Gaps, *outfile, pdbname))
+    return CpptrajState::ERR;
 
   return CpptrajState::OK;
 }
--- src/Exec_AddMissingRes.h
+++ src/Exec_AddMissingRes.h
@@ -1,12 +1,53 @@
 #ifndef INC_EXEC_ADDMISSINGRES_H
 #define INC_EXEC_ADDMISSINGRES_H
 #include "Exec.h"
-/// <Enter description of Exec_AddMissingRes here>
+/// Attempt to add missing residues in a PDB 
 class Exec_AddMissingRes : public Exec {
   public:
     Exec_AddMissingRes() : Exec(GENERAL) {}
     void Help() const;
     DispatchObject* Alloc() const { return (DispatchObject*)new Exec_AddMissingRes(); }
     RetType Execute(CpptrajState&, ArgList&);
+  private:
+    typedef std::vector<std::string> Sarray;
+    // Forward declaration of Gap class
+    class Gap;
+    typedef std::vector<Gap> Garray;
+
+    int FindGaps(Garray&, CpptrajFile&, std::string const&) const;
 };
+
+// ----- Gap class -------------------------------------------------------------
+/// Record a gap in PDB
+class Exec_AddMissingRes::Gap {
+  public:
+    Gap() {}
+    /// Start gap with res name, res num, and chain
+    Gap(std::string const& startNameIn, int startResIn, std::string const& startChainIn) :
+      resNames_(1, startNameIn), startRes_(startResIn), stopRes_(-1), chainId_(startChainIn[0])
+      {}
+    /// Start gap with res num and chain
+    Gap(int startResIn, std::string const& startChainIn) :
+      startRes_(startResIn), stopRes_(-1), chainId_(startChainIn[0])
+      {}
+
+    std::string const& FirstName() const { return resNames_.front(); }
+    std::string const& LastName()  const { return resNames_.back(); }
+    int StartRes()                 const { return startRes_; }
+    int StopRes()                  const { return stopRes_; }
+    char Chain()                   const { return chainId_; }
+    unsigned int Nres()            const { return resNames_.size(); }
+    typedef Sarray::const_iterator name_iterator;
+    name_iterator nameBegin()      const { return resNames_.begin(); }
+    name_iterator nameEnd()        const { return resNames_.end(); }
+
+    void SetStopRes(int s) { stopRes_ = s; }
+    void AddGapRes(std::string const& r) { resNames_.push_back( r ); }
+  private:
+    Sarray resNames_; ///< Residue names in the Gap
+    int startRes_;    ///< pdb start residue number for the gap 
+    int stopRes_;     ///< pdb stop residue number for the gap
+    char chainId_;    ///< chain ID of the gap
+};
+
 #endif
