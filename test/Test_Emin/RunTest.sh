b939f726d67ff84de64cd9e0c11a9cbb409b297a
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-04-28 16:34:53 -0400

    No need to write an output traj

--- test/Test_Emin/RunTest.sh
+++ test/Test_Emin/RunTest.sh
@@ -14,7 +14,7 @@ cat > emin.in <<EOF
 parm O2mol.parm7
 loadcrd O2mol.rst7 name O2mol
 
-emin crdset O2mol trajoutname cpptraj.emin.nc nsteps 100 out cpptraj.ene.dat
+emin crdset O2mol nsteps 100 out cpptraj.ene.dat
 EOF
 RunCpptraj "$UNITNAME"
 DoTest cpptraj.ene.dat.save cpptraj.ene.dat
