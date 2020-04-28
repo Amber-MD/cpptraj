4be90c880461c422731982883b0d677ae31d6bfb
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-04-28 18:48:27 -0400

    Add some simple minimization code. (#813)
    * DRR - Start adding potential terms
    
    * Add setup routine
    
    * Add the force calc
    
    * Update comments
    
    * Add EnergyArray class
    
    * Use EnergyArray
    
    * Start setting up potential function
    
    * Add setup and force calc functions.
    
    * Add a steepest descent min class
    
    * Add total energy. Put mask setup and DoF calc in the potential function.
    
    * Default unknown traj format
    
    * Update dependecies
    
    * Add emin command
    
    * Enable emin command. Ensure space allocated for forces in frame
    
    * Allow initial step size to be varied.
    
    * Add dx0 and out keywords
    
    * Fix output, change default rms tol to match sander
    
    * Add min test
    
    * Enable basic minimization test.
    
    * Add code comments.
    
    * Revision bump for adding 'emin' command (still hidden)
    
    * Print a warning that this command is still under development in case someone stumbles onto it
    
    * No need to write an output traj

---
+++ src/PotentialTerm.h
@@ -0,0 +1,21 @@
+#ifndef INC_POTENTIALTERM_H
+#define INC_POTENTIALTERM_H
+#include <string>
+// Forward declares
+class Topology;
+class CharMask;
+class Frame;
+class EnergyArray;
+/// Abstract base class for a term of a potential function.
+class PotentialTerm {
+  public:
+    enum Type { BOND = 0, NTERMS };
+    PotentialTerm(Type t) : type_(t) {}
+
+    virtual int SetupTerm(Topology const&, CharMask const&, EnergyArray&) = 0;
+    virtual void CalcForce(Frame&, CharMask const&) const = 0;
+
+  private:
+    Type type_;
+};
+#endif
