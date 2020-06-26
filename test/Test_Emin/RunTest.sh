3182e42c5425874a944abe19c1464561f6261025
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-06-26 16:28:52 -0400

    Add openmm test. Needs to be protected.

--- test/Test_Emin/RunTest.sh
+++ test/Test_Emin/RunTest.sh
@@ -9,7 +9,6 @@ CleanFiles emin.in cpptraj.ene.dat cpptraj.emin.nc
 INPUT='-i emin.in'
 
 UNITNAME='Basic energy minimization test.'
-
 cat > emin.in <<EOF
 parm O2mol.parm7
 loadcrd O2mol.rst7 name O2mol
@@ -18,5 +17,15 @@ emin crdset O2mol nsteps 100 out cpptraj.ene.dat trajoutname cpptraj.emin.nc
 EOF
 RunCpptraj "$UNITNAME"
 DoTest cpptraj.ene.dat.save cpptraj.ene.dat
+
+UNITNAME='OpenMM basic energy minimization test.'
+cat > emin.in <<EOF
+parm O2mol.parm7
+loadcrd O2mol.rst7 name O2mol
+
+emin crdset O2mol nsteps 100 out omm.ene.dat openmm
+EOF
+RunCpptraj "$UNITNAME"
+DoTest cpptraj.ene.dat.save omm.ene.dat
 
 EndTest
