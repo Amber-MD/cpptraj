5efffcf0181a2f5c49615dc97e9a8342008c269e
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-07-06 14:53:33 -0400

    Add openmm angle min test

--- test/Test_Emin/RunTest.sh
+++ test/Test_Emin/RunTest.sh
@@ -4,7 +4,8 @@
 
 TESTNAME='Emin tests'
 
-CleanFiles emin.in cpptraj.ene.dat cpptraj.emin.nc
+CleanFiles emin.in cpptraj.ene.dat cpptraj.emin.nc \
+           omm.ene.dat ommangle.ene.dat
 
 INPUT='-i emin.in'
 
@@ -18,17 +19,26 @@ EOF
 RunCpptraj "$UNITNAME"
 DoTest cpptraj.ene.dat.save cpptraj.ene.dat
 
-UNITNAME='OpenMM basic energy minimization test.'
+UNITNAME='OpenMM basic energy minimization tests'
 CheckFor openmm
 if [ $? -eq 0 ] ; then
+  # Bonds
   cat > emin.in <<EOF
 parm O2mol.parm7
 loadcrd O2mol.rst7 name O2mol
-
 emin crdset O2mol nsteps 100 out omm.ene.dat openmm
 EOF
-  RunCpptraj "$UNITNAME"
+  RunCpptraj "$UNITNAME (bonds)"
   DoTest cpptraj.ene.dat.save omm.ene.dat
+  # Angles
+  cat > emin.in <<EOF
+parm HOHmol.parm7
+loadcrd HOHmol.rst7 name HOHmol
+emin crdset HOHmol nsteps 100 out ommangle.ene.dat openmm
+EOF
+  RunCpptraj "$UNITNAME (angles)"
+  DoTest ommangle.ene.dat.save ommangle.ene.dat
 fi
+
 
 EndTest
