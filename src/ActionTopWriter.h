b05a2ebd1d6efa304839d756657b0ce3e370a408
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-08-06 11:11:07 -0400

    Finish ActionTopWriter

--- src/ActionTopWriter.h
+++ src/ActionTopWriter.h
@@ -3,6 +3,7 @@
 #include <string>
 // Forward declares
 class ArgList;
+class Topology;
 /// Class to hold common functionality for actions that will write modified topologies.
 class ActionTopWriter {
   public:
@@ -12,12 +13,16 @@ class ActionTopWriter {
     static const char* Options();
 
     /// Parse arguments.
-    int InitTopWriter(ArgList&);
+    int InitTopWriter(ArgList&, const char*, int);
     /// Write options to stdout.
-    void PrintOptions(const char*) const;
+    void PrintOptions() const;
+    /// Write the Topology to file(s)
+    int WriteTops(Topology const&) const;
   private:
     std::string prefix_;      ///< Prefix for writing topology as <prefix>.<originalname>
     std::string parmoutName_; ///< Output topology file name
     std::string parmOpts_;    ///< Topology file write args
+    std::string typeStr_;     ///< Label for kind of topology being written.
+    int debug_;               ///< Debug level to pass to Topology file writer.
 };
 #endif
