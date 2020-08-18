48f2b06b0ae88619d12e02e2f5f4d4ec91d24b00
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-08-18 11:15:57 -0400

    Finish graft test.

--- test/Test_Graft/RunTest.sh
+++ test/Test_Graft/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles cpptraj.in 
+CleanFiles cpptraj.in Final.graft.mol2
 TESTNAME='Graft test'
 Requires notparallel
 
@@ -22,26 +22,17 @@ loadcrd \$PRYFILE parmindex 1 name PRY
 graft \
   tgt TYR \
   src PRY \
-  srcmask
+  name Final \
+  srcfitmask @O1,C5,C6,C4,H6,H5,C7,C3,H7,H4,C2 \
+  tgtfitmask @OH,CZ,CE1,CE2,HE1,HE2,CD1,CD2,HD1,HD2,CG \
+  srcmask !(@1-4) \
+  tgtmask @C,O,CA,HA,N,H,CB,HB2,HB3 \
+  bond :1@CB,:1@C2
+crdout Final Final.graft.mol2
 
-quit
-# Load the fragment, fit it on top of the existing tyrosine atoms
-parm PRY-gauss-fragment.mol2 [pryparm]
-loadcrd PRY-gauss-fragment.mol2 parm [pryparm] PRY
-crdaction PRY strip @1-4
-#crdaction PRY center @C2 origin
-crdaction PRY rms reference @O1,C5,C6,C4,H6,H5,C7,C3,H7,H4,C2 @OH,CZ,CE1,CE2,HE1,HE2,CD1,CD2,HD1,HD2,CG
-
-loadcrd Tyr.mol2 parm Tyr.mol2 TYR
-crdaction TYR strip !@C,O,CA,HA,N,H,CB,HB2,HB3
-#crdaction TYR center @CB origin
-#crdaction TYR translate x 1.0 z -1.0
-
-combinecrd TYR PRY parmname Final.PRY crdname Final
-crdout Final Final.PRY.mol2
 EOF
 RunCpptraj "$TESTNAME"
-
+DoTest Final.graft.mol2.save Final.graft.mol2
 
 EndTest
 exit 0
