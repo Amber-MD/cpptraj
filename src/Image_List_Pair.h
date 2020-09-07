2435d490835897ba2fbce56cf409a0b776120b65
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-09-07 19:36:53 -0400

    Add translation

--- src/Image_List_Pair.h
+++ src/Image_List_Pair.h
@@ -1,6 +1,8 @@
 #ifndef INC_IMAGE_LIST_PAIR_H
 #define INC_IMAGE_LIST_PAIR_H
 #include "Image_List.h"
+#include "Frame.h"
+#include "Vec3.h"
 #include <vector>
 namespace Image {
 /// List with entities defined by a single range
@@ -8,6 +10,9 @@ class List_Pair : public List {
   public:
     List_Pair() {}
     int SetupList(Topology const&, std::string const&);
+    void DoTranslation(Frame& frm, unsigned int idx, Vec3 const& boxTrans) const {
+      frm.Translate(boxTrans, begin_[idx], end_[idx]);
+    }
   private:
     typedef std::vector<int> Iarray;
     Iarray begin_;
