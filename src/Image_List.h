cb6ced30b96322bad2438137e727e20e586ac78e
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-09-07 19:15:37 -0400

    Add abstract base class for image list

---
+++ src/Image_List.h
@@ -0,0 +1,23 @@
+#ifndef INC_IMAGE_LIST_H
+#define INC_IMAGE_LIST_H
+#include <string>
+class Topology;
+class Frame;
+class Vec3;
+namespace Image {
+/// Abstract base class for holding entities to be imaged
+class List {
+  public:
+    List() {}
+    virtual ~List() {}
+    /// \return Number of entities to be imaged
+    virtual unsigned int nEntities() const = 0;
+    /// Set up the list according to Topology and mask expression
+    virtual int SetupList(Topology const&, std::string const&) = 0;
+    /// \return Coordinates of specified entity
+    virtual Vec3 GetCoord(unsigned int) const = 0;
+    /// Do translation of specified entity according to given translation vector.
+    virtual void DoTranslation(Frame&, unsigned int, Vec3 const&) const = 0;
+};
+}
+#endif
