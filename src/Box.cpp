3f6f0d154a923c244bcdd0355bba6bf9cea2e9a8
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-11-20 10:28:32 -0500

    Start rewriting the box class with the aim being unit cell and frac are always calculated along with xyz abg. The xyz abg array is no longer the main way to represent the unit cell.

--- src/Box.cpp
+++ src/Box.cpp
@@ -16,6 +16,7 @@ Box::Box() :
   box_[5] = 0;
 }
 
+/*
 Box::Box(const double* bIn) //: debug_(0)
 {
   SetBox( bIn );
@@ -24,6 +25,7 @@ Box::Box(const double* bIn) //: debug_(0)
 Box::Box(const float* bIn) { SetBox( bIn ); }
 
 Box::Box(Matrix_3x3 const& ucell) { SetBox( ucell ); }
+*/
 
 // COPY CONSTRUCTOR
 Box::Box(const Box& rhs) :
@@ -41,7 +43,7 @@ Box::Box(const Box& rhs) :
 }
 
 // Assignment
-Box &Box::operator=(const Box& rhs) {
+Box& Box::operator=(const Box& rhs) {
   if (&rhs == this) return *this;
   //debug_ = rhs.debug_;
   btype_ = rhs.btype_;
@@ -57,6 +59,47 @@ Box &Box::operator=(const Box& rhs) {
   return *this;
 }
 
+/*
+static inline void dswap(double& d1, double& d2) {
+  double dtemp = d1;
+  d1 = d2;
+  d2 = dtemp;
+}
+
+static inline void bswap(Box::BoxType& b1, Box::BoxType& b2) {
+  Box::BoxType btemp = b1;
+  b1 = b2;
+  b2 = btemp;
+}
+
+void Box::swap(Box& rhs) {
+  bswap( btype_,  rhs.btype_ );
+  dswap( box_[0], rhs.box_[0] );
+  dswap( box_[1], rhs.box_[1] );
+  dswap( box_[2], rhs.box_[2] );
+  dswap( box_[3], rhs.box_[3] );
+  dswap( box_[4], rhs.box_[4] );
+  dswap( box_[5], rhs.box_[5] );
+  for (int i=0; i<9; i++) {
+    dswap(unitCell_[i], rhs.unitCell_[i]);
+    dswap(fracCell_[i], rhs.fracCell_[i]);
+  }
+  dswap(cellVolume_, rhs.cellVolume_);
+}
+*/
+
+#ifdef MPI
+int Box::SyncBox(Parallel::Comm const& commIn) {
+  commIn.MasterBcast( &btype_, 1, MPI_INT );
+  commIn.MasterBcast( box_,    6, MPI_DOUBLE );
+  unitCell_.SyncMatrix( commIn );
+  fracCell_.SyncMatrix( commIn );
+  commIn.MasterBcast( &cellVolume_, 1, MPI_DOUBLE );
+  return 0;
+}
+#endif
+
+// -----------------------------------------------------------------------------
 const double Box::TRUNCOCTBETA_ = 2.0*acos(1.0/sqrt(3.0))*Constants::RADDEG;
 
 /** This value is chosen so that TRUNCOCTBETA_ - TruncOctDelta_ is just
@@ -76,106 +119,7 @@ const char* Box::BoxNames_[] = {
   "None", "Orthogonal", "Trunc. Oct.", "Rhombic Dodec.", "Non-orthogonal"
 };
 
-// Box::SetBetaLengths()
-void Box::SetBetaLengths(double beta, double xin, double yin, double zin) {
-  box_[0] = xin;
-  box_[1] = yin;
-  box_[2] = zin;
-  box_[3] = 0;
-  box_[4] = beta;
-  box_[5] = 0;
-  SetBoxType();
-}
-
-/** Set box from double[6] array */
-void Box::SetBox(const double* xyzabg) {
-  if (xyzabg == 0) {
-    mprinterr("Error: SetBox: Input array is null\n");
-    return;
-  }
-  box_[0] = xyzabg[0];
-  box_[1] = xyzabg[1];
-  box_[2] = xyzabg[2];
-  box_[3] = xyzabg[3];
-  box_[4] = xyzabg[4];
-  box_[5] = xyzabg[5];
-  SetBoxType();
-}
-
-/** Set box from float[6] array */
-void Box::SetBox(const float* xyzabg) {
-  if (xyzabg == 0) {
-    mprinterr("Error: Box input array is null\n");
-    return;
-  }
-  box_[0] = (double)xyzabg[0];
-  box_[1] = (double)xyzabg[1];
-  box_[2] = (double)xyzabg[2];
-  box_[3] = (double)xyzabg[3];
-  box_[4] = (double)xyzabg[4];
-  box_[5] = (double)xyzabg[5];
-  SetBoxType();
-}
-
-/** Set box from unit cell matrix. */
-void Box::SetBox(Matrix_3x3 const& ucell) {
-  Vec3 x_axis = ucell.Row1();
-  Vec3 y_axis = ucell.Row2();
-  Vec3 z_axis = ucell.Row3();
-  box_[0] = x_axis.Normalize(); // A
-  box_[1] = y_axis.Normalize(); // B
-  box_[2] = z_axis.Normalize(); // C
-  box_[3] = y_axis.Angle( z_axis ) * Constants::RADDEG; // alpha
-  box_[4] = x_axis.Angle( z_axis ) * Constants::RADDEG; // beta
-  box_[5] = x_axis.Angle( y_axis ) * Constants::RADDEG; // gamma
-  SetBoxType();
-}
-
-void Box::SetBox(float A, float B, float C, float alpha, float beta, float gamma)
-{
-  box_[0] = A;
-  box_[1] = B;
-  box_[2] = C;
-  box_[3] = alpha;
-  box_[4] = beta;
-  box_[5] = gamma;
-  SetBoxType();
-}
-// Box::SetTruncOct()
-/** Set as truncated octahedron with all lengths set to whatever X is. */
-void Box::SetTruncOct() {
-  box_[1] = box_[0];
-  box_[2] = box_[0];
-  box_[3] = TRUNCOCTBETA_;
-  box_[4] = TRUNCOCTBETA_;
-  box_[5] = TRUNCOCTBETA_;
-  btype_ = TRUNCOCT;
-  mprintf("Info: Setting box to be perfect truncated octahedron (a=b=g=%g)\n", box_[3]);
-}
-
-// Box::SetNoBox()
-void Box::SetNoBox() {
-  box_[0] = 0;
-  box_[1] = 0;
-  box_[2] = 0;
-  box_[3] = 0;
-  box_[4] = 0;
-  box_[5] = 0;
-  btype_ = NOBOX;
-}
-
-// Box::SetMissingInfo()
-/** Set this box info from rhs if <= 0. */
-void Box::SetMissingInfo(const Box& rhs) {
-  if (box_[0] <= 0) box_[0] = rhs.box_[0];
-  if (box_[1] <= 0) box_[1] = rhs.box_[1];
-  if (box_[2] <= 0) box_[2] = rhs.box_[2];
-  if (box_[3] <= 0) box_[3] = rhs.box_[3];
-  if (box_[4] <= 0) box_[4] = rhs.box_[4];
-  if (box_[5] <= 0) box_[5] = rhs.box_[5];
-  SetBoxType();
-}
-
+// -----------------------------------------------------------------------------
 bool Box::IsTruncOct(double angle) {
   return (angle > TruncOctMin_ && angle < TruncOctMax_);
 }
@@ -263,7 +207,13 @@ void Box::SetBoxType() {
   }
 }
 
-double Box::CalcFrac(Matrix_3x3& recip, Matrix_3x3 const& ucell) {
+// -----------------------------------------------------------------------------
+// Static calculation routines.
+
+/** Calculate fractional matrix from unit cell matrix. 
+  * \return Volume of the unit cell.
+  */
+double Box::CalcFracFromUcell(Matrix_3x3& recip, Matrix_3x3 const& ucell) {
   // Get reciprocal vectors
   double u23x = ucell[4]*ucell[8] - ucell[5]*ucell[7];
   double u23y = ucell[5]*ucell[6] - ucell[3]*ucell[8];
@@ -290,7 +240,98 @@ double Box::CalcFrac(Matrix_3x3& recip, Matrix_3x3 const& ucell) {
   return volume;
 }
 
-void Box::SetUcellFromShapeMatrix(const double* shape) {
+/** Calculate unit cell matrix from XYZ ABG array. */
+void Box::CalcUcellFromXyzAbg(Matrix_3x3& ucell, const double* xyzabg) {
+  // If box lengths are zero no imaging possible
+  //if (xyzabg[0]==0.0 || xyzabg[1]==0.0 || xyzabg[2]==0.0) {
+  //  ucell.Zero();
+  //  recip.Zero();
+  //  return -1.0;
+  //}
+  ucell[0] = xyzabg[0]; // u(1,1)
+  ucell[1] = 0.0;     // u(2,1)
+  ucell[2] = 0.0;     // u(3,1)
+  ucell[3] = xyzabg[1]*cos(Constants::DEGRAD*xyzabg[5]); // u(1,2)
+  ucell[4] = xyzabg[1]*sin(Constants::DEGRAD*xyzabg[5]); // u(2,2)
+  ucell[5] = 0.0;                                    // u(3,2)
+  ucell[6] = xyzabg[2]*cos(Constants::DEGRAD*xyzabg[4]);
+  ucell[7] = (xyzabg[1]*xyzabg[2]*cos(Constants::DEGRAD*xyzabg[3]) - ucell[6]*ucell[3]) / ucell[4];
+  ucell[8] = sqrt(xyzabg[2]*xyzabg[2] - ucell[6]*ucell[6] - ucell[7]*ucell[7]);
+}
+
+// Box::CalcUcellFromXyzAbg()
+void Box::CalcUcellFromXyzAbg(Matrix_3x3& ucell, BoxType btype, const double* xyzabg, double scale) {
+  double by, bz;
+  switch (btype) {
+    case NOBOX: ucell.Zero(); break;
+    case ORTHO:
+      ucell[0] = xyzabg[0] * scale;
+      ucell[1] = 0.0;
+      ucell[2] = 0.0;
+      ucell[3] = 0.0;
+      ucell[4] = xyzabg[1] * scale;
+      ucell[5] = 0.0;
+      ucell[6] = 0.0;
+      ucell[7] = 0.0;
+      ucell[8] = xyzabg[2] * scale;
+      break;
+    case TRUNCOCT:
+    case RHOMBIC:
+    case NONORTHO:
+      by = xyzabg[1] * scale;
+      bz = xyzabg[2] * scale;
+      ucell[0] = xyzabg[0] * scale;
+      ucell[1] = 0.0;
+      ucell[2] = 0.0;
+      ucell[3] = by*cos(Constants::DEGRAD*xyzabg[5]);
+      ucell[4] = by*sin(Constants::DEGRAD*xyzabg[5]);
+      ucell[5] = 0.0;
+      ucell[6] = bz*cos(Constants::DEGRAD*xyzabg[4]);
+      ucell[7] = (by*bz*cos(Constants::DEGRAD*xyzabg[3]) - ucell[6]*ucell[3]) / ucell[4];
+      ucell[8] = sqrt(bz*bz - ucell[6]*ucell[6] - ucell[7]*ucell[7]);
+      break;
+  }
+}
+
+/** Calculate XYZ ABG array from unit cell matrix */
+void Box::CalcXyzAbgFromUcell(double* box, Matrix_3x3 const& ucell) {
+  Vec3 x_axis = ucell.Row1();
+  Vec3 y_axis = ucell.Row2();
+  Vec3 z_axis = ucell.Row3();
+  box[0] = x_axis.Normalize(); // A
+  box[1] = y_axis.Normalize(); // B
+  box[2] = z_axis.Normalize(); // C
+  box[3] = y_axis.Angle( z_axis ) * Constants::RADDEG; // alpha
+  box[4] = x_axis.Angle( z_axis ) * Constants::RADDEG; // beta
+  box[5] = x_axis.Angle( y_axis ) * Constants::RADDEG; // gamma
+}
+
+/** Convert symmetric shape matrix data to params x, y, z, a, b, g */
+void Box::CalcXyzAbgFromShape(double* box, const double* boxtmp)
+{
+  double boxX = sqrt( boxtmp[0]*boxtmp[0] + boxtmp[1]*boxtmp[1] + boxtmp[3]*boxtmp[3] );
+  double boxY = sqrt( boxtmp[1]*boxtmp[1] + boxtmp[2]*boxtmp[2] + boxtmp[4]*boxtmp[4] );
+  double boxZ = sqrt( boxtmp[3]*boxtmp[3] + boxtmp[4]*boxtmp[4] + boxtmp[5]*boxtmp[5] );
+  double boxXY = boxtmp[1]*(boxtmp[0] + boxtmp[2]) + boxtmp[3]*boxtmp[4];
+  double boxYZ = boxtmp[4]*(boxtmp[2] + boxtmp[5]) + boxtmp[1]*boxtmp[3];
+  double boxXZ = boxtmp[3]*(boxtmp[0] + boxtmp[5]) + boxtmp[1]*boxtmp[4];
+  double alpha = acos( boxYZ / (boxY*boxZ) ) * Constants::RADDEG;
+  double beta  = acos( boxXZ / (boxX*boxZ) ) * Constants::RADDEG;
+  double gamma = acos( boxXY / (boxX*boxY) ) * Constants::RADDEG;
+  //mprintf("DEBUG: Box XYZ= %g %g %g  ABG= %g %g %g\n", boxX, boxY, boxZ, alpha, beta, gamma);
+  box[0] = boxX;
+  box[1] = boxY;
+  box[2] = boxZ;
+  box[3] = alpha;
+  box[4] = beta;
+  box[5] = gamma;
+}
+
+
+// -----------------------------------------------------------------------------
+// Setup routines
+/** Set unit cell, fractional cell, and ABG cell from shape matrix. */
+void Box::SetupUcellFromShapeMatrix(const double* shape) {
   unitCell_[0] = shape[0];
   unitCell_[1] = shape[1];
   unitCell_[2] = shape[3];
@@ -303,22 +344,120 @@ void Box::SetUcellFromShapeMatrix(const double* shape) {
   unitCell_[7] = shape[4];
   unitCell_[8] = shape[5];
 
-  ShapeToUcell(box_, shape);
+  CalcXyzAbgFromShape(box_, shape);
 
-  cellVolume_ = CalcFrac(fracCell_, unitCell_);
+  cellVolume_ = CalcFracFromUcell(fracCell_, unitCell_);
 
   SetBoxType();
 }
+
+
+/*
+// Box::SetBetaLengths()
+void Box::SetBetaLengths(double beta, double xin, double yin, double zin) {
+  box_[0] = xin;
+  box_[1] = yin;
+  box_[2] = zin;
+  box_[3] = 0;
+  box_[4] = beta;
+  box_[5] = 0;
+  SetBoxType();
+}*/
+
+/** Set box from double[6] array */
+/*
+void Box::SetBox(const double* xyzabg) {
+  if (xyzabg == 0) {
+    mprinterr("Error: SetBox: Input array is null\n");
+    return;
+  }
+  box_[0] = xyzabg[0];
+  box_[1] = xyzabg[1];
+  box_[2] = xyzabg[2];
+  box_[3] = xyzabg[3];
+  box_[4] = xyzabg[4];
+  box_[5] = xyzabg[5];
+  SetBoxType();
+}*/
+
+/** Set box from float[6] array */
+/*
+void Box::SetBox(const float* xyzabg) {
+  if (xyzabg == 0) {
+    mprinterr("Error: Box input array is null\n");
+    return;
+  }
+  box_[0] = (double)xyzabg[0];
+  box_[1] = (double)xyzabg[1];
+  box_[2] = (double)xyzabg[2];
+  box_[3] = (double)xyzabg[3];
+  box_[4] = (double)xyzabg[4];
+  box_[5] = (double)xyzabg[5];
+  SetBoxType();
+}
+*/
+
+
+/*
+void Box::SetBox(float A, float B, float C, float alpha, float beta, float gamma)
+{
+  box_[0] = A;
+  box_[1] = B;
+  box_[2] = C;
+  box_[3] = alpha;
+  box_[4] = beta;
+  box_[5] = gamma;
+  SetBoxType();
+}*/
+
+// Box::SetTruncOct()
+/** Set as truncated octahedron with all lengths set to whatever X is. */
+/*
+void Box::SetTruncOct() {
+  box_[1] = box_[0];
+  box_[2] = box_[0];
+  box_[3] = TRUNCOCTBETA_;
+  box_[4] = TRUNCOCTBETA_;
+  box_[5] = TRUNCOCTBETA_;
+  btype_ = TRUNCOCT;
+  mprintf("Info: Setting box to be perfect truncated octahedron (a=b=g=%g)\n", box_[3]);
+}*/
+
+// Box::SetNoBox()
+/*
+void Box::SetNoBox() {
+  box_[0] = 0;
+  box_[1] = 0;
+  box_[2] = 0;
+  box_[3] = 0;
+  box_[4] = 0;
+  box_[5] = 0;
+  btype_ = NOBOX;
+}*/
+
+// Box::SetMissingInfo()
+/** Set this box info from rhs if <= 0. */
+/*
+void Box::SetMissingInfo(const Box& rhs) {
+  if (box_[0] <= 0) box_[0] = rhs.box_[0];
+  if (box_[1] <= 0) box_[1] = rhs.box_[1];
+  if (box_[2] <= 0) box_[2] = rhs.box_[2];
+  if (box_[3] <= 0) box_[3] = rhs.box_[3];
+  if (box_[4] <= 0) box_[4] = rhs.box_[4];
+  if (box_[5] <= 0) box_[5] = rhs.box_[5];
+  SetBoxType();
+}*/
 
 // Box::ToRecip()
 /** Use box coordinates to calculate unit cell and fractional matrix for use
   * with imaging routines. Return cell volume.
   */
+/*
 double Box::ToRecip(Matrix_3x3& ucell, Matrix_3x3& recip) const {
   ucell = unitCell_;
   recip = fracCell_;
   return cellVolume_;
-/*
+*
   double u12x,u12y,u12z;
   double u23x,u23y,u23z;
   double u31x,u31y,u31z;
@@ -362,106 +501,21 @@ double Box::ToRecip(Matrix_3x3& ucell, Matrix_3x3& recip) const {
   recip[7] = u12y*onevolume;
   recip[8] = u12z*onevolume;
 
-  return volume;*/
-}
-
-// Box::UnitCell()
-Matrix_3x3 Box::UnitCell(double scale) const {
-  Matrix_3x3 ucell;
-  double by, bz;
-  switch (btype_) {
-    case NOBOX: ucell.Zero(); break;
-    case ORTHO:
-      ucell[0] = box_[0] * scale;
-      ucell[1] = 0.0;
-      ucell[2] = 0.0;
-      ucell[3] = 0.0;
-      ucell[4] = box_[1] * scale;
-      ucell[5] = 0.0;
-      ucell[6] = 0.0;
-      ucell[7] = 0.0;
-      ucell[8] = box_[2] * scale;
-      break;
-    case TRUNCOCT:
-    case RHOMBIC:
-    case NONORTHO:
-      by = box_[1] * scale;
-      bz = box_[2] * scale;
-      ucell[0] = box_[0] * scale;
-      ucell[1] = 0.0;
-      ucell[2] = 0.0;
-      ucell[3] = by*cos(Constants::DEGRAD*box_[5]);
-      ucell[4] = by*sin(Constants::DEGRAD*box_[5]);
-      ucell[5] = 0.0;
-      ucell[6] = bz*cos(Constants::DEGRAD*box_[4]);
-      ucell[7] = (by*bz*cos(Constants::DEGRAD*box_[3]) - ucell[6]*ucell[3]) / ucell[4];
-      ucell[8] = sqrt(bz*bz - ucell[6]*ucell[6] - ucell[7]*ucell[7]);
-      break;
-  }
-  return ucell;
-}
+  return volume;*
+}*/
 
 //  Box::RecipLengths()
+/*
 Vec3 Box::RecipLengths(Matrix_3x3 const& recip) {
   return Vec3( 1.0/sqrt(recip[0]*recip[0] + recip[1]*recip[1] + recip[2]*recip[2]),
                1.0/sqrt(recip[3]*recip[3] + recip[4]*recip[4] + recip[5]*recip[5]),
                1.0/sqrt(recip[6]*recip[6] + recip[7]*recip[7] + recip[8]*recip[8]) );
-}
+}*/
 
-/** Convert symmetric shape matrix data to unit cell params (x, y, z, a, b, g) */
-void Box::ShapeToUcell(double* box, const double* boxtmp)
-{
-  double boxX = sqrt( boxtmp[0]*boxtmp[0] + boxtmp[1]*boxtmp[1] + boxtmp[3]*boxtmp[3] );
-  double boxY = sqrt( boxtmp[1]*boxtmp[1] + boxtmp[2]*boxtmp[2] + boxtmp[4]*boxtmp[4] );
-  double boxZ = sqrt( boxtmp[3]*boxtmp[3] + boxtmp[4]*boxtmp[4] + boxtmp[5]*boxtmp[5] );
-  double boxXY = boxtmp[1]*(boxtmp[0] + boxtmp[2]) + boxtmp[3]*boxtmp[4];
-  double boxYZ = boxtmp[4]*(boxtmp[2] + boxtmp[5]) + boxtmp[1]*boxtmp[3];
-  double boxXZ = boxtmp[3]*(boxtmp[0] + boxtmp[5]) + boxtmp[1]*boxtmp[4];
-  double alpha = acos( boxYZ / (boxY*boxZ) ) * Constants::RADDEG;
-  double beta  = acos( boxXZ / (boxX*boxZ) ) * Constants::RADDEG;
-  double gamma = acos( boxXY / (boxX*boxY) ) * Constants::RADDEG;
-  //mprintf("DEBUG: Box XYZ= %g %g %g  ABG= %g %g %g\n", boxX, boxY, boxZ, alpha, beta, gamma);
-  box[0] = boxX;
-  box[1] = boxY;
-  box[2] = boxZ;
-  box[3] = alpha;
-  box[4] = beta;
-  box[5] = gamma;
-}
+
 
 void Box::PrintInfo() const {
   mprintf("\tBox: '%s' XYZ= { %8.3f %8.3f %8.3f } ABG= { %6.2f %6.2f %6.2f }\n",
           BoxNames_[btype_], box_[0], box_[1], box_[2], box_[3], box_[4], box_[5]);
 }
 
-static inline void dswap(double& d1, double& d2) {
-  double dtemp = d1;
-  d1 = d2;
-  d2 = dtemp;
-}
-
-static inline void bswap(Box::BoxType& b1, Box::BoxType& b2) {
-  Box::BoxType btemp = b1;
-  b1 = b2;
-  b2 = btemp;
-}
-
-void Box::swap(Box& rhs) {
-  bswap( btype_,  rhs.btype_ );
-  dswap( box_[0], rhs.box_[0] );
-  dswap( box_[1], rhs.box_[1] );
-  dswap( box_[2], rhs.box_[2] );
-  dswap( box_[3], rhs.box_[3] );
-  dswap( box_[4], rhs.box_[4] );
-  dswap( box_[5], rhs.box_[5] );
-}
-#ifdef MPI
-int Box::SyncBox(Parallel::Comm const& commIn) {
-  commIn.MasterBcast( &btype_, 1, MPI_INT );
-  commIn.MasterBcast( box_,    6, MPI_DOUBLE );
-  unitCell_.SyncMatrix( commIn );
-  fracCell_.SyncMatrix( commIn );
-  commIn.MasterBcast( &cellVolume_, 1, MPI_DOUBLE );
-  return 0;
-}
-#endif
--- src/Box.h
+++ src/Box.h
@@ -11,38 +11,40 @@ class Box {
     enum BoxType { NOBOX=0, ORTHO, TRUNCOCT, RHOMBIC, NONORTHO }; 
 
     Box();
-    Box(const double*);
-    Box(const float*);
-    Box(Matrix_3x3 const&);
+    //Box(const double*);
+    //Box(const float*);
+    //Box(Matrix_3x3 const&);
     Box(const Box&);
     Box& operator=(const Box&);
+#   ifdef MPI
+    int SyncBox(Parallel::Comm const&);
+#   endif
 
-    const char* TypeName() const { return BoxNames_[btype_]; }
+    void SetupUcellFromShapeMatrix(const double*);
 
-    void SetUcellFromShapeMatrix(const double*);
-
-    void SetBetaLengths(double,double,double,double);
-    void SetBox(const double*);
-    void SetBox(const float*);
-    void SetBox(Matrix_3x3 const&);
-    void SetBox(float,float,float,float,float,float);
-    void SetTruncOct();
-    void SetNoBox();
-    void SetMissingInfo(const Box&);
+    //void SetBetaLengths(double,double,double,double);
+    //void SetBox(const double*);
+    //void SetBox(const float*);
+    //void SetBox(Matrix_3x3 const&);
+    //void SetBox(float,float,float,float,float,float);
+    //void SetTruncOct();
+    //void SetNoBox();
+    //void SetMissingInfo(const Box&);
     /// Calculate Frac->Cart and Cart->Frac matrices.
-    double ToRecip(Matrix_3x3&, Matrix_3x3&) const;
+    //double ToRecip(Matrix_3x3&, Matrix_3x3&) const;
     /// Calculate unit cell matrix, optionally scaling lengths.
-    Matrix_3x3 UnitCell(double) const;
+    //Matrix_3x3 UnitCell(double) const;
     /// Print Box info to STDOUT
     void PrintInfo() const;
 
-    void SetX(double xin)     { box_[0] = xin; }
-    void SetY(double yin)     { box_[1] = yin; }
-    void SetZ(double zin)     { box_[2] = zin; }
-    void SetAlpha(double ain) { box_[3] = ain; }
-    void SetBeta(double bin)  { box_[4] = bin; }
-    void SetGamma(double gin) { box_[5] = gin; }
+    //void SetX(double xin)     { box_[0] = xin; }
+    //void SetY(double yin)     { box_[1] = yin; }
+    //void SetZ(double zin)     { box_[2] = zin; }
+    //void SetAlpha(double ain) { box_[3] = ain; }
+    //void SetBeta(double bin)  { box_[4] = bin; }
+    //void SetGamma(double gin) { box_[5] = gin; }
 
+    const char* TypeName() const { return BoxNames_[btype_]; }
     BoxType Type() const { return btype_;  }
     double BoxX()  const { return box_[0]; }
     double BoxY()  const { return box_[1]; }
@@ -53,36 +55,48 @@ class Box {
     bool HasBox()  const { return (btype_ != NOBOX); }
     Vec3 Center()  const { return Vec3(box_[0]/2.0, box_[1]/2.0, box_[2]/2.0); }
     Vec3 Lengths() const { return Vec3(box_[0], box_[1], box_[2]);             }
-    static Vec3 RecipLengths(Matrix_3x3 const&);
-    static void ShapeToUcell(double*, const double*);
-    void swap(Box&);
+
+    //void swap(Box&);
     // For interfacing with file IO
-    double* boxPtr()             { return box_; }
-    const double* boxPtr() const { return box_; }
+    //double* boxPtr()             { return box_; }
+    //const double* boxPtr() const { return box_; }
 
-    double const& operator[](int idx) const { return box_[idx]; }
-    double&       operator[](int idx)       { return box_[idx]; }
-#   ifdef MPI
-    int SyncBox(Parallel::Comm const&);
-#   endif
+    //double const& operator[](int idx) const { return box_[idx]; }
+    //double&       operator[](int idx)       { return box_[idx]; }
   private:
-    static inline bool IsTruncOct(double);
-    static inline bool BadTruncOctAngle(double);
-    static inline bool IsAngle(double,double);
-    static inline double CalcFrac(Matrix_3x3&, Matrix_3x3 const&);
-    void SetBoxType();
-
     static const double TRUNCOCTBETA_;
     static const double TruncOctDelta_;
     static const double TruncOctMin_;
     static const double TruncOctMax_;
     static const double TruncOctEps_;
     static const char* BoxNames_[];
+
+    static inline bool IsTruncOct(double);
+    static inline bool BadTruncOctAngle(double);
+    static inline bool IsAngle(double,double);
+
+    void SetBoxType();
+
+    /// Calculate fractional matrix from unit cell matrix.
+    static inline double CalcFracFromUcell(Matrix_3x3&, Matrix_3x3 const&);
+    /// Calculate unit cell matrix from XYZ ABG array.
+    static inline void CalcUcellFromXyzAbg(Matrix_3x3&, const double*);
+    /// Calculate unit cell matrix from XYZ ABG array based on box type, optionally scaling lengths.
+    static void CalcUcellFromXyzAbg(Matrix_3x3&, BoxType, const double*, double);
+    /// Calculate XYZ ABG array from unit cell matrix
+    static void CalcXyzAbgFromUcell(double*, Matrix_3x3 const&);
+    /// Calculate XYZ ABG array from symmetric shape matrix
+    static void CalcXyzAbgFromShape(double*, const double*);
+
+    //static Vec3 RecipLengths(Matrix_3x3 const&);
+    //static void XyzAbgToUcell(Matrix_3x3&, const double*);
+
+
     //int debug_; // TODO: Replace with ifdefs or just comment out?
-    BoxType btype_; ///< Box type
-    double box_[6]; ///< Box X Y Z alpha beta gamma
+    BoxType btype_;       ///< Box type
+    double box_[6];       ///< Box X Y Z alpha beta gamma
     Matrix_3x3 unitCell_; ///< Unit cell (Cartesian) matrix
     Matrix_3x3 fracCell_; ///< Fractional coord. cell matrix
-    double cellVolume_;
+    double cellVolume_;   ///< Unit cell volume
 };
 #endif
