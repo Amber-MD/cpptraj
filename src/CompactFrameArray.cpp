eca7650cd773b7b096a7ab2a4835137f8f9d3c0e
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-11-22 13:30:28 -0500

    Make CompactFrameArray float only, add a setup routine.

---
+++ src/CompactFrameArray.cpp
@@ -0,0 +1,35 @@
+#include "CompactFrameArray.h"
+#include <algorithm> // std::fill
+
+/** CONSTRUCTOR */
+CompactFrameArray::CompactFrameArray() :
+  frameSize_(0)
+{
+  std::fill(hasComponent_, hasComponent_+CoordinateInfo::NCOMPONENTS, false);
+}
+
+void CompactFrameArray::addComponent(long int& currentOffset, CoordinateInfo::Component compIn,
+                                     long int sizeIn)
+{
+  hasComponent_[compIn] = true;
+  components_.push_back( compIn );
+  offsets_.push_back( currentOffset );
+  currentOffset += sizeIn;
+}
+
+/** Set up frame array. */
+int CompactFrameArray::SetupFrameArray(CoordinateInfo const& cinfoIn, unsigned int natoms, int nframes)
+{
+  std::fill(hasComponent_, hasComponent_+CoordinateInfo::NCOMPONENTS, false);
+  components_.clear();
+  offsets_.clear();
+  long int currentOffset = 0;
+
+  if (cinfoIn.HasCrd())
+    addComponent(currentOffset, CoordinateInfo::POSITION, (long int)natoms * 3);
+
+  // Final "offset" is the total frame size
+  offsets_.push_back( currentOffset );
+
+  return 0;
+}
--- src/CompactFrameArray.h
+++ src/CompactFrameArray.h
@@ -1,17 +1,21 @@
 #ifndef INC_COMPACTFRAMEARRAY_H
 #define INC_COMPACTFRAMEARRAY_H
 #include "CoordinateInfo.h"
-#include <algorithm>
 /// Hold a compact version of the time-depedent information contained in an array of Frames
-template <class T> class CompactFrameArray {
+class CompactFrameArray {
   public:
-    CompactFrameArray() { std::fill(hasComponent_, hasComponent_+CoordinateInfo::NCOMPONENTS, false); }
+    CompactFrameArray();
+
+    /// Set up frame array to hold coordinate info with specified # atoms, optionally # frames.
+    int SetupFrameArray(CoordinateInfo const&, unsigned int, int);
   private:
-    typedef std::vector<T> ArrayType;
-    std::vector<ArrayType> compactFrames_;              ///< Array storing all info.
+    void addComponent(long int&, CoordinateInfo::Component, long int);
+
+    typedef std::vector<float> Farray;
+    Farray compactFrames_;                              ///< Array storing all info.
     bool hasComponent_[CoordinateInfo::NCOMPONENTS];    ///< True if array contains indicated component
     std::vector<CoordinateInfo::Component> components_; ///< List info contained in this array.
-    unsigned int frameSize_;                            ///< Size of each individual frame.
     std::vector<long int> offsets_;                     ///< Offsets for each component present.
+    unsigned int frameSize_;                            ///< Size of each individual frame.
 };
 #endif
