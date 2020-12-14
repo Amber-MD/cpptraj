6c6644489b9ec4b9cda7db6eeef5be338b33670d
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-12-14 10:23:26 -0500

    Add library from https://github.com/simonpf/fastexp for testing

---
+++ src/FastExp_product.h
@@ -0,0 +1,33 @@
+#ifndef FASTEXP_PRODUCT_H
+#define FASTEXP_PRODUCT_H
+
+namespace fastexp {
+
+template<typename Real, size_t degree, size_t i>
+    struct Recursion {
+        static Real evaluate(Real x) {
+            constexpr Real c = 1.0 / static_cast<Real>(1u << degree);
+            x = Recursion<Real, degree, i + 1>::evaluate(x);
+            return x * x;
+        }
+    };
+
+template<typename Real, size_t degree>
+    struct Recursion<Real, degree, degree> {
+    static Real evaluate(Real x) {
+        constexpr Real c = 1.0 / static_cast<Real>(1u << degree);
+        x = 1.0 + c * x;
+        return x;
+    }
+};
+
+template<typename Real, size_t degree>
+struct Product {
+    static Real evaluate(Real x) {
+        return Recursion<Real, degree, 0>::evaluate(x);
+    }
+};
+
+
+}      // fastexp
+#endif // FASTEXP_PRODUCT_H
