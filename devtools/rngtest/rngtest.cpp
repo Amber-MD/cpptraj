c2173c4159d8977b76019e61afaa8733dfa1e243
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-02-22 09:38:55 -0500

    Add test for shuffle

--- devtools/rngtest/rngtest.cpp
+++ devtools/rngtest/rngtest.cpp
@@ -1,42 +1,18 @@
 #include <limits>
 #include <string>
+#include <vector>
 #include <cstdio>
 #include <cstdlib>
 #include "../../src/Random.h"
 #include "../../src/CpptrajStdio.h"
 
-// Test the random number generators in cpptraj
-int main(int argc, char** argv) {
-  SetWorldSilent(true);
-  Random_Number RNG;
-
-  int iseed = 0;
-  int icount = 0;
-  int itype = 0;
-  // Parse options
-  for (int iarg = 1; iarg < argc; iarg++) {
-    std::string arg( argv[iarg] );
-    if (arg == "-S") {
-      iseed = atoi( argv[++iarg] );
-    } else if (arg == "-t") {
-      icount = atoi( argv[++iarg] );
-    } else if (arg == "-r") {
-      itype = atoi( argv[++iarg] );
-    }
-  }
+static int DieHard_Tests(Random_Number const& RNG, int itype, int iseed, int icount) {
   printf("#==================================================================\n");
   printf("# Cpptraj generator %i  seed = %i                                  \n", itype, iseed);
   printf("#==================================================================\n");
   printf("type: d\n");
   printf("count: %i\n", icount);
   printf("numbit: 32\n");
-  //printf("\tSeed  : %i\n", iseed);
-  //printf("\tCount : %i\n", icount);
-  //printf("\tType  : %i\n", itype);
-
-  Random_Number::RngType rt = (Random_Number::RngType)itype;
-  Random_Number::SetDefaultRng(rt);
-  RNG.rn_set( iseed );
 
 /*
   double d_imax = (double)std::numeric_limits<int>::max();
@@ -50,6 +26,71 @@ int main(int argc, char** argv) {
   }
 
   //printf("\n");
-
   return 0;
+}
+
+static int Shuffle_Tests(Random_Number const& RNG, int itype, int iseed, int icount) {
+  printf("Shuffle tests\n");
+  printf("\tSeed  : %i\n", iseed);
+  printf("\tCount : %i\n", icount);
+  printf("\tType  : %i\n", itype);
+
+  std::vector<int> nums;
+  nums.reserve(icount);
+  for (int i = 0; i < icount; i++)
+    nums.push_back( i );
+
+  RNG.ShufflePoints( nums );
+  printf("Shuffled points:");
+  for (std::vector<int>::const_iterator it = nums.begin(); it != nums.end(); ++it)
+    printf(" %i", *it);
+  printf("\n");
+  return 0;
+}
+
+// Test the random number generators in cpptraj
+int main(int argc, char** argv) {
+  SetWorldSilent(true);
+  Random_Number RNG;
+
+  enum ModeType {DIEHARD = 0, SHUFFLE };
+  ModeType mode = DIEHARD;
+  int iseed = 0;
+  int icount = 0;
+  int itype = 0;
+  // Parse options
+  for (int iarg = 1; iarg < argc; iarg++) {
+    std::string arg( argv[iarg] );
+    if (arg == "-S") {
+      iseed = atoi( argv[++iarg] );
+    } else if (arg == "-t") {
+      icount = atoi( argv[++iarg] );
+    } else if (arg == "-r") {
+      itype = atoi( argv[++iarg] );
+    } else if (arg == "--mode") {
+      std::string marg(argv[++iarg]);
+      if (marg == "diehard")
+        mode = DIEHARD;
+      else if (marg == "shuffle")
+        mode = SHUFFLE;
+      else {
+        fprintf(stderr,"Error: Unrecognized mode: %s\n", marg.c_str());
+        return 1;
+      }
+    }
+  }
+
+  // Setup RNG
+  Random_Number::RngType rt = (Random_Number::RngType)itype;
+  Random_Number::SetDefaultRng(rt);
+  RNG.rn_set( iseed );
+
+  int err = 0;
+  if (mode == DIEHARD) {
+    err = DieHard_Tests(RNG, itype, iseed, icount);
+  } else if (mode == SHUFFLE) {
+    err = Shuffle_Tests(RNG, itype, iseed, icount);
+  }
+
+  return err;
 }
