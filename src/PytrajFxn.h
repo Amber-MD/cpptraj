83d47bd4cb338f050339bf238121b8c408a0b2bc
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-02-25 08:53:06 -0500

    Add file which will contain functions that only pytraj needs.

---
+++ src/PytrajFxn.h
@@ -0,0 +1,8 @@
+#ifndef INC_PYTRAJFXN_H
+#define INC_PYTRAJFXN_H
+/*! \file PytrajFxn.h
+    \brief This file contains some functions that pytraj needs.
+ */
+/// Calls Random_Number::SetDefaultRng()
+void PT_SetDefaultRng(int);
+#endif
