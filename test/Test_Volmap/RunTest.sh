28a8f14d9396712553cc7107daca68797dafdf32
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-02-26 12:49:24 -0500

    Do not test volume yet, not ready for prime time.

--- test/Test_Volmap/RunTest.sh
+++ test/Test_Volmap/RunTest.sh
@@ -16,14 +16,14 @@ center :1-13 mass origin
 volmap volmap3.dx 1.0 1.0 1.0 :WAT@O  \
        radscale 1.36 size 20,20,20 peakcut 0.10 peakfile peaks1.xyz
 #bounds :1-13 name Grid dx 1.0 offset 2
-volmap :1-13 name Tz2 1.0 1.0 1.0 size 18,24,20
+#volmap :1-13 name Tz2 1.0 1.0 1.0 size 18,24,20
 run
-writedata Tz2.volume Tz2[totalvol]
+#writedata Tz2.volume Tz2[totalvol]
 EOF
 RunCpptraj "$TESTNAME"
 DoTest volmap3.dx.save volmap3.dx -r 0.00001
 DoTest peaks1.xyz.save peaks1.xyz -r 0.00001
-DoTest Tz2.volume.save Tz2.volume
+#DoTest Tz2.volume.save Tz2.volume
 
 UNITNAME='VMD VolMap Algorithm longer tests'
 CheckFor long
