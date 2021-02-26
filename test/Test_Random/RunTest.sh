8dd0f68690161c43e6f3b8744f379938c7ffe506
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-02-26 13:42:56 -0500

    Split off pcg32 test; does not compile for windows

--- test/Test_Random/RunTest.sh
+++ test/Test_Random/RunTest.sh
@@ -4,19 +4,30 @@
 
 TESTNAME='Random number generator tests'
 
-CleanFiles rng.in random.dat mt.dat stdlib.dat
+CleanFiles rng.in random.dat mt.dat stdlib.dat pcg32.dat
 
 INPUT='-i rng.in'
 
 UNITNAME='Test Marsaglia, PCG32, and Xoshiro128++ RNGs'
 cat > rng.in <<EOF
 rng setdefault marsaglia createset Marsaglia    settype int count 10 seed 10 out random.dat
-rng setdefault pcg32     createset PCG32        settype int count 10 seed 10 out random.dat
 rng setdefault xo128     createset Xoshiro128++ settype int count 10 seed 10 out random.dat
 list
 EOF
 RunCpptraj "$UNITNAME"
 DoTest random.dat.save random.dat
+
+UNITNAME='PCG32 RNG test.'
+# PCG32 does not yet compile for windows.
+CheckFor notos windows
+if [ $? -eq 0 ] ; then
+  cat > rng.in <<EOF
+rng setdefault pcg32     createset PCG32        settype int count 10 seed 10 out pcg32.dat
+list
+EOF
+  RunCpptraj "$UNITNAME"
+  DoTest pcg32.dat.save pcg32.dat
+fi
 
 UNITNAME='Test Stdlib RNG'
 # Stdlib rand implementation is different on windows
