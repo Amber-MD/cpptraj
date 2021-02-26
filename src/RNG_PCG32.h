ff4a3d393f63b273efd0b067eae414ed9ccac32f
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-02-26 13:43:19 -0500

    PCG32 does not currently compile under visual studio

--- src/RNG_PCG32.h
+++ src/RNG_PCG32.h
@@ -1,7 +1,9 @@
 #ifndef INC_RNG_PCG32_H
 #define INC_RNG_PCG32_H
 #include "RNG.h"
+#ifndef _MSC_VER
 #include "pcg_random.hpp"
+#endif
 namespace Cpptraj {
 
 class RNG_PCG32 : public RNG {
@@ -12,8 +14,9 @@ class RNG_PCG32 : public RNG {
     unsigned int Number();
   private:
     int SetupRng();
-
+#   ifndef _MSC_VER
     pcg32 rng_;
+#   endif
 };
 }
 #endif
