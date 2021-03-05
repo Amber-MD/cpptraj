d431feac157841a080bf14133a55a3b3c052f0be
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-03-05 15:25:46 -0500

    Add more tests.

--- unitTests/ArgList/main.cpp
+++ unitTests/ArgList/main.cpp
@@ -14,6 +14,7 @@ bool f_not_equals(double d1, double d2) {
 }
 
 int main() {
+  //          7       6 6    4               1    1 2  3    3       5
   ArgList a1("This is a test 3.14159 arglist with 4 45 darg 123.323 :12@H=");
 
   int iarg = a1.getKeyInt("with", -1);
@@ -27,6 +28,23 @@ int main() {
 
   darg = a1.getNextDouble(0);
   if (f_not_equals(darg, 3.14159)) return Err("getNextDouble() failed.");
+
+  std::string sarg = a1.GetMaskNext();
+  if (sarg != ":12@H=") return Err("GetMaskNext() failed.");
+
+  sarg = a1.GetStringKey("a");
+  if (sarg != "test") return Err("GetStringKey() failed.");
+
+  sarg = a1.GetStringNext();
+  if (sarg != "This") return Err("GetStringKey() failed.");
+
+  ArgList r1 = a1.RemainingArgs();
+  if (a1.NremainingArgs() > 0) return Err("RemainingArgs() failed (# remaining args).");
+  if (r1.Nargs() != 2) return Err("RemainingArgs() failed (# new args).");
+
+  r1.MarkArg(0);
+  sarg = r1.GetStringNext();
+  if (sarg != "arglist") return Err("MarkArg() failed.");
 
   return 0;
 }
