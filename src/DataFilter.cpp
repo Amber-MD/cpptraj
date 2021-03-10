872eca88cb9bb9584977f2f85421d84e820c1774
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-03-10 08:59:47 -0500

    Start adding separate DataFilter class

---
+++ src/DataFilter.cpp
@@ -0,0 +1,11 @@
+#include "DataFilter.h"
+
+/** CONSTRUCTOR */
+DataFilter::DataFilter() :
+  filterSet_(0),
+  Npassed_(0),
+  Nfiltered_(0),
+  multi_(false)
+{}
+
+
---
+++ src/DataFilter.h
@@ -0,0 +1,31 @@
+#ifndef INC_DATAFILTER_H
+#define INC_DATAFILTER_H
+#include <vector>
+class ArgList;
+class DataSetList;
+class DataSet;
+class DataSet_integer;
+/// Can be used to count/filter out data set elements based on user-defined criteria.
+class DataFilter {
+  public:
+    DataFilter();
+    /// \return Keywords recognized by InitFilter();
+    static const char* Keywords();
+    /// Process arguments, get sets to filter
+    int InitFilter(ArgList&, DataSetList&, int);
+    /// \return 1 if specified index was filtered, 0 otherwise
+    int FilterIndex(int);
+  private:
+    typedef std::vector<double> Darray;
+    typedef std::vector<DataSet*> SetArray;
+
+    DataSet_integer* filterSet_; ///< DataSet containing for each index 1 for OK, 0 for filtered out (not multi).
+    Darray Max_;                 ///< Only allow values less than these
+    Darray Min_;                 ///< Only allow values greater than these
+    SetArray inpSets_;           ///< Sets to base criteria on
+    SetArray outSets_;           ///< Sets containing for each index 1 for OK, 0 for filtered out (multi).
+    unsigned int Npassed_;       ///< Number of indices not filtered out.
+    unsigned int Nfiltered_;     ///< Number of indices filtered out.
+    bool multi_;                 ///< If false, filter based on each set. If true, filter each set.
+};
+#endif
