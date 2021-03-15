ce74ee54554ad6ada7fb4382542fc4292d7da1af
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-03-15 09:55:33 -0400

    Remove old code

--- src/Action_FilterByData.cpp
+++ src/Action_FilterByData.cpp
@@ -23,73 +23,7 @@ Action::RetType Action_FilterByData::Init(ArgList& actionArgs, ActionInit& init,
 {
   if (dataFilter_.InitFilter(actionArgs, init.DSL(), init.DFL(), debugIn))
     return Action::ERR;
-/*
-  Npassed_ = 0;
-  Nfiltered_ = 0;
-  multi_ = actionArgs.hasKey("multi");
-  std::string dsname = actionArgs.GetStringKey("name");
-  if (dsname.empty())
-    dsname = init.DSL().GenerateDefaultName("Filter");
-  DataFile* maxminfile = init.DFL().AddDataFile( actionArgs.GetStringKey("out"), actionArgs );
-  // Get min and max args.
-  while (actionArgs.Contains("min"))
-    Min_.push_back( actionArgs.getKeyDouble("min", 0.0) );
-  while (actionArgs.Contains("max"))
-    Max_.push_back( actionArgs.getKeyDouble("max", 0.0) );
-  if (Min_.empty()) {
-    mprinterr("Error: At least one 'min' arg must be specified.\n");
-    return Action::ERR;
-  }
-  if (Max_.empty()) {
-    mprinterr("Error: At least one 'max' arg must be specified.\n");
-    return Action::ERR;
-  }
-  if (Min_.size() != Max_.size()) {
-    mprinterr("Error: # of 'min' args (%zu) != # of 'max' args (%zu)\n",
-              Min_.size(), Max_.size());
-    return Action::ERR;
-  }
-  // Get DataSets from remaining arguments
-  Dsets_.AddSetsFromArgs( actionArgs.RemainingArgs(), init.DSL() );
-  if (Dsets_.empty()) {
-    mprinterr("Error: No data sets specified.\n");
-    return Action::ERR;
-  }
-  // Check number of DataSets and min/max args.
-  if ( Dsets_.size() < Min_.size() ) {
-    mprinterr("Error: More 'min'/'max' args (%zu) than data sets (%zu).\n",
-              Min_.size(), Dsets_.size());
-    return Action::ERR;
-  }
-  if ( Dsets_.size() > Min_.size() ) {
-    unsigned int Nremaining = Dsets_.size() - Min_.size();
-    double useMin = Min_.back();
-    double useMax = Max_.back();
-    mprintf("Warning: More data sets than 'min'/'max' args.\n"
-            "Warning:  Using min=%f and max=%f for last %u data sets.\n",
-            useMin, useMax, Nremaining);
-    for (unsigned int ds = 0; ds < Nremaining; ++ds) {
-      Min_.push_back( useMin );
-      Max_.push_back( useMax );
-    }
-  }
-  // Set up output data set(s)
-  if (!multi_) {
-    maxmin_ = init.DSL().AddSet( DataSet::INTEGER, dsname );
-    if (maxmin_ == 0) return Action::ERR;
-    if (maxminfile != 0)
-      maxminfile->AddDataSet( maxmin_ );
-  } else {
-    for (unsigned int idx = 0; idx < Dsets_.size(); idx++) {
-      DataSet* ds = init.DSL().AddSet(DataSet::INTEGER, MetaData(dsname, idx));
-      if (ds == 0) return Action::ERR;
-      ds->SetLegend("Filter("+ds->Meta().PrintName()+")");
-      outsets_.push_back( ds );
-      if (maxminfile != 0)
-        maxminfile->AddDataSet( ds );
-    }
-  }
-*/
+
   mprintf("    FILTER:");
   if (!dataFilter_.IsMulti())
     mprintf(" Filtering out frames using %u data sets.\n", dataFilter_.NinputSets());
@@ -115,65 +49,11 @@ Action::RetType Action_FilterByData::DoAction(int frameNum, ActionFrame& frm)
 
   if (result == DataFilter::FILTERED)
     return Action::SUPPRESS_COORD_OUTPUT;
-/*
-  static int ONE = 1;
-  static int ZERO = 0;
-  if (!multi_) {
-    // Check if frame is within max/min of every data set.
-    for (unsigned int ds = 0; ds < Dsets_.size(); ++ds)
-    {
-      double dVal = Dsets_[ds]->Dval(frm.TrajoutNum());
-      //mprintf("DBG: maxmin[%u]: dVal = %f, min = %f, max = %f\n",ds,dVal,Min_[ds],Max_[ds]);
-      // If value from dataset not within min/max, exit now.
-      if (dVal < Min_[ds] || dVal > Max_[ds]) {
-        maxmin_->Add( frameNum, &ZERO );
-        Nfiltered_++;
-        return Action::SUPPRESS_COORD_OUTPUT;
-      }
-    }
-    maxmin_->Add( frameNum, &ONE );
-    Npassed_++;
-  } else {
-    // For each data set 1 if within min/max, 0 otherwise.
-    for (unsigned int ds = 0; ds < Dsets_.size(); ++ds)
-    {
-      double dVal = Dsets_[ds]->Dval(frm.TrajoutNum());
-      if (dVal < Min_[ds] || dVal > Max_[ds]) 
-        outsets_[ds]->Add( frameNum, &ZERO );
-      else
-        outsets_[ds]->Add( frameNum, &ONE  );
-    }
-  }*/
+
   return Action::OK;
 }
-
-/** \return Minimum number of frames among all input data sets. */
-/*size_t Action_FilterByData::DetermineFrames() const {
-  if (Dsets_.empty()) return 0;
-  size_t nframes = Dsets_[0]->Size();
-  for (Array1D::const_iterator it = Dsets_.begin(); it != Dsets_.end(); ++it)
-  {
-    if ((*it)->Size() < nframes)
-      nframes = (*it)->Size();
-  }
-  // Warn if any datasets are larger.
-  for (Array1D::const_iterator it = Dsets_.begin(); it != Dsets_.end(); ++it)
-  {
-    if ((*it)->Size() > nframes)
-      mprintf("Warning: '%s' size %zu is larger than other sets; only processing %zu\n",
-              (*it)->legend(), (*it)->Size(), nframes);
-  }
-  return nframes;
-}*/
 
 // Action_FilterByData::Print()
 void Action_FilterByData::Print() {
   dataFilter_.Finalize();
-/*
-  if (!multi_) {
-    mprintf("    FILTER: %i frames passed through, %i frames were filtered out.\n",
-            Npassed_, Nfiltered_);
-    for (unsigned int ds = 0; ds < Dsets_.size(); ds++)
-      mprintf("\t%.4f < '%s' < %.4f\n", Min_[ds], Dsets_[ds]->legend(), Max_[ds]);
-  }*/
 }
