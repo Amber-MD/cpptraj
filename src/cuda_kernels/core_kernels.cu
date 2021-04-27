9b9a584c45c87f06d2380926df0eae78c846de5e
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-04-27 10:35:11 -0400

    Remove the forward declare

--- src/cuda_kernels/core_kernels.cu
+++ src/cuda_kernels/core_kernels.cu
@@ -4,8 +4,256 @@
 #define BLOCKDIM 512
 #define RSIZE 512
 
-// Forward declaration for non-orthorhombic distance calc.
-__device__ double NonOrtho_dist(double,double,double,double,double,double,const double*);
+// -----------------------------------------------------------------------------
+/** \return Shortest imaged distance between given coordinates in fractional space.
+  * NOTE: This function is complicated hence we will put into a __device__ only function.
+  */
+__device__ double NonOrtho_dist(double a0, double a1, double a2,
+                                double b0, double b1, double b2,
+                                const double *ucell)
+{
+  int ixyz[3];
+  double minIn  = -1.0;
+
+   //double closest2
+  // The floor() calls serve to bring each point back in the main unit cell.
+  double fx = a0 - floor(a0);
+  double fy = a1 - floor(a1);
+  double fz = a2 - floor(a2); 
+  double f2x = b0 - floor(b0);
+  double f2y = b1 - floor(b1);
+  double f2z = b2 - floor(b2);
+  // f2 back in Cartesian space
+  double X_factor = (f2x*ucell[0] + f2y*ucell[3] + f2z*ucell[6]);
+  double Y_factor = (f2x*ucell[1] + f2y*ucell[4] + f2z*ucell[7]);
+  double Z_factor = (f2x*ucell[2] + f2y*ucell[5] + f2z*ucell[8]);
+  // Precompute some factors
+  double fxm1 = fx - 1.0;
+  double fxp1 = fx + 1.0;
+  double fym1 = fy - 1.0;
+  double fyp1 = fy + 1.0;
+  double fzm1 = fz - 1.0;
+  double fzp1 = fz + 1.0;
+
+  double fxm1u0 = fxm1 * ucell[0];
+  double fxu0   = fx   * ucell[0];
+  double fxp1u0 = fxp1 * ucell[0];
+  double fxm1u1 = fxm1 * ucell[1];
+  double fxu1   = fx   * ucell[1];
+  double fxp1u1 = fxp1 * ucell[1];
+  double fxm1u2 = fxm1 * ucell[2];
+  double fxu2   = fx   * ucell[2];
+  double fxp1u2 = fxp1 * ucell[2];
+
+  double fym1u3 = fym1 * ucell[3];
+  double fyu3   = fy   * ucell[3];
+  double fyp1u3 = fyp1 * ucell[3];
+  double fym1u4 = fym1 * ucell[4];
+  double fyu4   = fy   * ucell[4];
+  double fyp1u4 = fyp1 * ucell[4];
+  double fym1u5 = fym1 * ucell[5];
+  double fyu5   = fy   * ucell[5];
+  double fyp1u5 = fyp1 * ucell[5];
+
+  double fzm1u6 = fzm1 * ucell[6];
+  double fzu6   = fz   * ucell[6];
+  double fzp1u6 = fzp1 * ucell[6];
+  double fzm1u7 = fzm1 * ucell[7];
+  double fzu7   = fz   * ucell[7];
+  double fzp1u7 = fzp1 * ucell[7];
+  double fzm1u8 = fzm1 * ucell[8];
+  double fzu8   = fz   * ucell[8];
+  double fzp1u8 = fzp1 * ucell[8];
+
+  // Calc ix iy iz = 0 case
+  double x = (fxu0 + fyu3 + fzu6) - X_factor;
+  double y = (fxu1 + fyu4 + fzu7) - Y_factor;
+  double z = (fxu2 + fyu5 + fzu8) - Z_factor;
+  // DEBUG
+  //mprintf("DEBUG: a2: %g %g %g\n",(fxu0 + fyu3 + fzu6), (fxu1 + fyu4 + fzu7), (fxu2 + fyu5 + fzu8));
+  //mprintf("DEBUG: a1: %g %g %g\n", X_factor, Y_factor, Z_factor);
+  double min = (x*x) + (y*y) + (z*z);
+
+  if (minIn > 0.0 && minIn < min) min = minIn;
+
+  ixyz[0] = 0;
+  ixyz[1] = 0;
+  ixyz[2] = 0;
+
+  // -1 -1 -1
+  x = (fxm1u0 + fym1u3 + fzm1u6) - X_factor;
+  y = (fxm1u1 + fym1u4 + fzm1u7) - Y_factor;
+  z = (fxm1u2 + fym1u5 + fzm1u8) - Z_factor;
+  double D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] = -1; }
+  // -1 -1  0
+  x = (fxm1u0 + fym1u3 + fzu6  ) - X_factor;
+  y = (fxm1u1 + fym1u4 + fzu7  ) - Y_factor;
+  z = (fxm1u2 + fym1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] =  0; }
+  // -1 -1 +1
+  x = (fxm1u0 + fym1u3 + fzp1u6) - X_factor;
+  y = (fxm1u1 + fym1u4 + fzp1u7) - Y_factor;
+  z = (fxm1u2 + fym1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] =  1; }
+  // -1  0 -1
+  x = (fxm1u0 + fyu3   + fzm1u6) - X_factor;
+  y = (fxm1u1 + fyu4   + fzm1u7) - Y_factor;
+  z = (fxm1u2 + fyu5   + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] = -1; }
+  // -1  0  0
+  x = (fxm1u0 + fyu3   + fzu6  ) - X_factor;
+  y = (fxm1u1 + fyu4   + fzu7  ) - Y_factor;
+  z = (fxm1u2 + fyu5   + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] =  0; }
+  // -1  0 +1
+  x = (fxm1u0 + fyu3   + fzp1u6) - X_factor;
+  y = (fxm1u1 + fyu4   + fzp1u7) - Y_factor;
+  z = (fxm1u2 + fyu5   + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] =  1; }
+  // -1 +1 -1
+  x = (fxm1u0 + fyp1u3 + fzm1u6) - X_factor;
+  y = (fxm1u1 + fyp1u4 + fzm1u7) - Y_factor;
+  z = (fxm1u2 + fyp1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] = -1; }
+  // -1 +1  0
+  x = (fxm1u0 + fyp1u3 + fzu6  ) - X_factor;
+  y = (fxm1u1 + fyp1u4 + fzu7  ) - Y_factor;
+  z = (fxm1u2 + fyp1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] =  0; }
+  // -1 +1 +1
+  x = (fxm1u0 + fyp1u3 + fzp1u6) - X_factor;
+  y = (fxm1u1 + fyp1u4 + fzp1u7) - Y_factor;
+  z = (fxm1u2 + fyp1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] =  1; }
+
+  //  0 -1 -1
+  x = (fxu0   + fym1u3 + fzm1u6) - X_factor;
+  y = (fxu1   + fym1u4 + fzm1u7) - Y_factor;
+  z = (fxu2   + fym1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] = -1; }
+  //  0 -1  0
+  x = (fxu0   + fym1u3 + fzu6  ) - X_factor;
+  y = (fxu1   + fym1u4 + fzu7  ) - Y_factor;
+  z = (fxu2   + fym1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] =  0; }
+  //  0 -1 +1
+  x = (fxu0   + fym1u3 + fzp1u6) - X_factor;
+  y = (fxu1   + fym1u4 + fzp1u7) - Y_factor;
+  z = (fxu2   + fym1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] =  1; }
+  //  0  0 -1
+  x = (fxu0   + fyu3   + fzm1u6) - X_factor;
+  y = (fxu1   + fyu4   + fzm1u7) - Y_factor;
+  z = (fxu2   + fyu5   + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  0; ixyz[2] = -1; }
+  //  0  0  0
+  //  0  0 +1
+  x = (fxu0   + fyu3   + fzp1u6) - X_factor;
+  y = (fxu1   + fyu4   + fzp1u7) - Y_factor;
+  z = (fxu2   + fyu5   + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  0; ixyz[2] =  1; }
+  //  0 +1 -1
+  x = (fxu0   + fyp1u3 + fzm1u6) - X_factor;
+  y = (fxu1   + fyp1u4 + fzm1u7) - Y_factor;
+  z = (fxu2   + fyp1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] = -1; }
+  //  0 +1  0
+  x = (fxu0   + fyp1u3 + fzu6  ) - X_factor;
+  y = (fxu1   + fyp1u4 + fzu7  ) - Y_factor;
+  z = (fxu2   + fyp1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] =  0; }
+  //  0 +1 +1
+  x = (fxu0   + fyp1u3 + fzp1u6) - X_factor;
+  y = (fxu1   + fyp1u4 + fzp1u7) - Y_factor;
+  z = (fxu2   + fyp1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] =  1; }
+
+  // +1 -1 -1
+  x = (fxp1u0 + fym1u3 + fzm1u6) - X_factor;
+  y = (fxp1u1 + fym1u4 + fzm1u7) - Y_factor;
+  z = (fxp1u2 + fym1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] = -1; }
+  // +1 -1  0
+  x = (fxp1u0 + fym1u3 + fzu6  ) - X_factor;
+  y = (fxp1u1 + fym1u4 + fzu7  ) - Y_factor;
+  z = (fxp1u2 + fym1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] =  0; }
+  // +1 -1 +1
+  x = (fxp1u0 + fym1u3 + fzp1u6) - X_factor;
+  y = (fxp1u1 + fym1u4 + fzp1u7) - Y_factor;
+  z = (fxp1u2 + fym1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] =  1; }
+  // +1  0 -1
+  x = (fxp1u0 + fyu3   + fzm1u6) - X_factor;
+  y = (fxp1u1 + fyu4   + fzm1u7) - Y_factor;
+  z = (fxp1u2 + fyu5   + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] = -1; }
+  // +1  0  0
+  x = (fxp1u0 + fyu3   + fzu6  ) - X_factor;
+  y = (fxp1u1 + fyu4   + fzu7  ) - Y_factor;
+  z = (fxp1u2 + fyu5   + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] =  0; }
+  // +1  0 +1
+  x = (fxp1u0 + fyu3   + fzp1u6) - X_factor;
+  y = (fxp1u1 + fyu4   + fzp1u7) - Y_factor;
+  z = (fxp1u2 + fyu5   + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] =  1; }
+  // +1 +1 -1
+  x = (fxp1u0 + fyp1u3 + fzm1u6) - X_factor;
+  y = (fxp1u1 + fyp1u4 + fzm1u7) - Y_factor;
+  z = (fxp1u2 + fyp1u5 + fzm1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] = -1; }
+  // +1 +1  0
+  x = (fxp1u0 + fyp1u3 + fzu6  ) - X_factor;
+  y = (fxp1u1 + fyp1u4 + fzu7  ) - Y_factor;
+  z = (fxp1u2 + fyp1u5 + fzu8  ) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] =  0; }
+  // +1 +1 +1
+  x = (fxp1u0 + fyp1u3 + fzp1u6) - X_factor;
+  y = (fxp1u1 + fyp1u4 + fzp1u7) - Y_factor;
+  z = (fxp1u2 + fyp1u5 + fzp1u8) - Z_factor;
+  D = (x*x) + (y*y) + (z*z);
+  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] =  1; }
+
+  //if (closest2 != 0.0 && min < closest2) return (min);
+//  this->ClosestImage(a1, a2, ixyz);
+//  fprintf(stdout,"DEBUG: Predict  = %2i %2i %2i\n",ixyz[0],ixyz[1],ixyz[2]);
+
+//  ix = ixyz[0];
+//  iy = ixyz[1];
+//  iz = ixyz[2];
+
+//D = sqrt(min);
+//  fprintf(stdout,"DEBUG: MinDist  = %2i %2i %2i = %8.3f\n", ixmin, iymin, izmin, D);
+//  printf("---------------------------------------------------------------\n");
+  return(min);
+
+}
 
 // -----------------------------------------------------------------------------
 //try thread coarsening 
@@ -499,253 +747,4 @@ __global__ void kClosestDistsToAtoms_Nonortho(double*D_,
   }
 }
 
-// -----------------------------------------------------------------------------
-/** \return Shortest imaged distance between given coordinates in fractional space.
-  * NOTE: This function is complicated hence we will put into a __device__ only function.
-  */
-__device__ double NonOrtho_dist(double a0, double a1, double a2,
-                                double b0, double b1, double b2,
-                                const double *ucell)
-{
-  int ixyz[3];
-  double minIn  = -1.0;
 
-   //double closest2
-  // The floor() calls serve to bring each point back in the main unit cell.
-  double fx = a0 - floor(a0);
-  double fy = a1 - floor(a1);
-  double fz = a2 - floor(a2); 
-  double f2x = b0 - floor(b0);
-  double f2y = b1 - floor(b1);
-  double f2z = b2 - floor(b2);
-  // f2 back in Cartesian space
-  double X_factor = (f2x*ucell[0] + f2y*ucell[3] + f2z*ucell[6]);
-  double Y_factor = (f2x*ucell[1] + f2y*ucell[4] + f2z*ucell[7]);
-  double Z_factor = (f2x*ucell[2] + f2y*ucell[5] + f2z*ucell[8]);
-  // Precompute some factors
-  double fxm1 = fx - 1.0;
-  double fxp1 = fx + 1.0;
-  double fym1 = fy - 1.0;
-  double fyp1 = fy + 1.0;
-  double fzm1 = fz - 1.0;
-  double fzp1 = fz + 1.0;
-
-  double fxm1u0 = fxm1 * ucell[0];
-  double fxu0   = fx   * ucell[0];
-  double fxp1u0 = fxp1 * ucell[0];
-  double fxm1u1 = fxm1 * ucell[1];
-  double fxu1   = fx   * ucell[1];
-  double fxp1u1 = fxp1 * ucell[1];
-  double fxm1u2 = fxm1 * ucell[2];
-  double fxu2   = fx   * ucell[2];
-  double fxp1u2 = fxp1 * ucell[2];
-
-  double fym1u3 = fym1 * ucell[3];
-  double fyu3   = fy   * ucell[3];
-  double fyp1u3 = fyp1 * ucell[3];
-  double fym1u4 = fym1 * ucell[4];
-  double fyu4   = fy   * ucell[4];
-  double fyp1u4 = fyp1 * ucell[4];
-  double fym1u5 = fym1 * ucell[5];
-  double fyu5   = fy   * ucell[5];
-  double fyp1u5 = fyp1 * ucell[5];
-
-  double fzm1u6 = fzm1 * ucell[6];
-  double fzu6   = fz   * ucell[6];
-  double fzp1u6 = fzp1 * ucell[6];
-  double fzm1u7 = fzm1 * ucell[7];
-  double fzu7   = fz   * ucell[7];
-  double fzp1u7 = fzp1 * ucell[7];
-  double fzm1u8 = fzm1 * ucell[8];
-  double fzu8   = fz   * ucell[8];
-  double fzp1u8 = fzp1 * ucell[8];
-
-  // Calc ix iy iz = 0 case
-  double x = (fxu0 + fyu3 + fzu6) - X_factor;
-  double y = (fxu1 + fyu4 + fzu7) - Y_factor;
-  double z = (fxu2 + fyu5 + fzu8) - Z_factor;
-  // DEBUG
-  //mprintf("DEBUG: a2: %g %g %g\n",(fxu0 + fyu3 + fzu6), (fxu1 + fyu4 + fzu7), (fxu2 + fyu5 + fzu8));
-  //mprintf("DEBUG: a1: %g %g %g\n", X_factor, Y_factor, Z_factor);
-  double min = (x*x) + (y*y) + (z*z);
-
-  if (minIn > 0.0 && minIn < min) min = minIn;
-
-  ixyz[0] = 0;
-  ixyz[1] = 0;
-  ixyz[2] = 0;
-
-  // -1 -1 -1
-  x = (fxm1u0 + fym1u3 + fzm1u6) - X_factor;
-  y = (fxm1u1 + fym1u4 + fzm1u7) - Y_factor;
-  z = (fxm1u2 + fym1u5 + fzm1u8) - Z_factor;
-  double D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] = -1; }
-  // -1 -1  0
-  x = (fxm1u0 + fym1u3 + fzu6  ) - X_factor;
-  y = (fxm1u1 + fym1u4 + fzu7  ) - Y_factor;
-  z = (fxm1u2 + fym1u5 + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] =  0; }
-  // -1 -1 +1
-  x = (fxm1u0 + fym1u3 + fzp1u6) - X_factor;
-  y = (fxm1u1 + fym1u4 + fzp1u7) - Y_factor;
-  z = (fxm1u2 + fym1u5 + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] = -1; ixyz[2] =  1; }
-  // -1  0 -1
-  x = (fxm1u0 + fyu3   + fzm1u6) - X_factor;
-  y = (fxm1u1 + fyu4   + fzm1u7) - Y_factor;
-  z = (fxm1u2 + fyu5   + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] = -1; }
-  // -1  0  0
-  x = (fxm1u0 + fyu3   + fzu6  ) - X_factor;
-  y = (fxm1u1 + fyu4   + fzu7  ) - Y_factor;
-  z = (fxm1u2 + fyu5   + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] =  0; }
-  // -1  0 +1
-  x = (fxm1u0 + fyu3   + fzp1u6) - X_factor;
-  y = (fxm1u1 + fyu4   + fzp1u7) - Y_factor;
-  z = (fxm1u2 + fyu5   + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  0; ixyz[2] =  1; }
-  // -1 +1 -1
-  x = (fxm1u0 + fyp1u3 + fzm1u6) - X_factor;
-  y = (fxm1u1 + fyp1u4 + fzm1u7) - Y_factor;
-  z = (fxm1u2 + fyp1u5 + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] = -1; }
-  // -1 +1  0
-  x = (fxm1u0 + fyp1u3 + fzu6  ) - X_factor;
-  y = (fxm1u1 + fyp1u4 + fzu7  ) - Y_factor;
-  z = (fxm1u2 + fyp1u5 + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] =  0; }
-  // -1 +1 +1
-  x = (fxm1u0 + fyp1u3 + fzp1u6) - X_factor;
-  y = (fxm1u1 + fyp1u4 + fzp1u7) - Y_factor;
-  z = (fxm1u2 + fyp1u5 + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] = -1; ixyz[1] =  1; ixyz[2] =  1; }
-
-  //  0 -1 -1
-  x = (fxu0   + fym1u3 + fzm1u6) - X_factor;
-  y = (fxu1   + fym1u4 + fzm1u7) - Y_factor;
-  z = (fxu2   + fym1u5 + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] = -1; }
-  //  0 -1  0
-  x = (fxu0   + fym1u3 + fzu6  ) - X_factor;
-  y = (fxu1   + fym1u4 + fzu7  ) - Y_factor;
-  z = (fxu2   + fym1u5 + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] =  0; }
-  //  0 -1 +1
-  x = (fxu0   + fym1u3 + fzp1u6) - X_factor;
-  y = (fxu1   + fym1u4 + fzp1u7) - Y_factor;
-  z = (fxu2   + fym1u5 + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] = -1; ixyz[2] =  1; }
-  //  0  0 -1
-  x = (fxu0   + fyu3   + fzm1u6) - X_factor;
-  y = (fxu1   + fyu4   + fzm1u7) - Y_factor;
-  z = (fxu2   + fyu5   + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  0; ixyz[2] = -1; }
-  //  0  0  0
-  //  0  0 +1
-  x = (fxu0   + fyu3   + fzp1u6) - X_factor;
-  y = (fxu1   + fyu4   + fzp1u7) - Y_factor;
-  z = (fxu2   + fyu5   + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  0; ixyz[2] =  1; }
-  //  0 +1 -1
-  x = (fxu0   + fyp1u3 + fzm1u6) - X_factor;
-  y = (fxu1   + fyp1u4 + fzm1u7) - Y_factor;
-  z = (fxu2   + fyp1u5 + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] = -1; }
-  //  0 +1  0
-  x = (fxu0   + fyp1u3 + fzu6  ) - X_factor;
-  y = (fxu1   + fyp1u4 + fzu7  ) - Y_factor;
-  z = (fxu2   + fyp1u5 + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] =  0; }
-  //  0 +1 +1
-  x = (fxu0   + fyp1u3 + fzp1u6) - X_factor;
-  y = (fxu1   + fyp1u4 + fzp1u7) - Y_factor;
-  z = (fxu2   + fyp1u5 + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  0; ixyz[1] =  1; ixyz[2] =  1; }
-
-  // +1 -1 -1
-  x = (fxp1u0 + fym1u3 + fzm1u6) - X_factor;
-  y = (fxp1u1 + fym1u4 + fzm1u7) - Y_factor;
-  z = (fxp1u2 + fym1u5 + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] = -1; }
-  // +1 -1  0
-  x = (fxp1u0 + fym1u3 + fzu6  ) - X_factor;
-  y = (fxp1u1 + fym1u4 + fzu7  ) - Y_factor;
-  z = (fxp1u2 + fym1u5 + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] =  0; }
-  // +1 -1 +1
-  x = (fxp1u0 + fym1u3 + fzp1u6) - X_factor;
-  y = (fxp1u1 + fym1u4 + fzp1u7) - Y_factor;
-  z = (fxp1u2 + fym1u5 + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] = -1; ixyz[2] =  1; }
-  // +1  0 -1
-  x = (fxp1u0 + fyu3   + fzm1u6) - X_factor;
-  y = (fxp1u1 + fyu4   + fzm1u7) - Y_factor;
-  z = (fxp1u2 + fyu5   + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] = -1; }
-  // +1  0  0
-  x = (fxp1u0 + fyu3   + fzu6  ) - X_factor;
-  y = (fxp1u1 + fyu4   + fzu7  ) - Y_factor;
-  z = (fxp1u2 + fyu5   + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] =  0; }
-  // +1  0 +1
-  x = (fxp1u0 + fyu3   + fzp1u6) - X_factor;
-  y = (fxp1u1 + fyu4   + fzp1u7) - Y_factor;
-  z = (fxp1u2 + fyu5   + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  0; ixyz[2] =  1; }
-  // +1 +1 -1
-  x = (fxp1u0 + fyp1u3 + fzm1u6) - X_factor;
-  y = (fxp1u1 + fyp1u4 + fzm1u7) - Y_factor;
-  z = (fxp1u2 + fyp1u5 + fzm1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] = -1; }
-  // +1 +1  0
-  x = (fxp1u0 + fyp1u3 + fzu6  ) - X_factor;
-  y = (fxp1u1 + fyp1u4 + fzu7  ) - Y_factor;
-  z = (fxp1u2 + fyp1u5 + fzu8  ) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] =  0; }
-  // +1 +1 +1
-  x = (fxp1u0 + fyp1u3 + fzp1u6) - X_factor;
-  y = (fxp1u1 + fyp1u4 + fzp1u7) - Y_factor;
-  z = (fxp1u2 + fyp1u5 + fzp1u8) - Z_factor;
-  D = (x*x) + (y*y) + (z*z);
-  if (D < min) { min = D; ixyz[0] =  1; ixyz[1] =  1; ixyz[2] =  1; }
-
-  //if (closest2 != 0.0 && min < closest2) return (min);
-//  this->ClosestImage(a1, a2, ixyz);
-//  fprintf(stdout,"DEBUG: Predict  = %2i %2i %2i\n",ixyz[0],ixyz[1],ixyz[2]);
-
-//  ix = ixyz[0];
-//  iy = ixyz[1];
-//  iz = ixyz[2];
-
-//D = sqrt(min);
-//  fprintf(stdout,"DEBUG: MinDist  = %2i %2i %2i = %8.3f\n", ixmin, iymin, izmin, D);
-//  printf("---------------------------------------------------------------\n");
-  return(min);
-
-}
