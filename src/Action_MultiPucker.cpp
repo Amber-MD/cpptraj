93e26a04fd50c00d355131fa21d75fe89b0f5d56
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-05-10 13:24:02 -0400

    Start the multipucker action

---
+++ src/Action_MultiPucker.cpp
@@ -0,0 +1,25 @@
+#include "Action_MultiPucker.h"
+#include "CpptrajStdio.h"
+
+// Action_MultiPucker::Help()
+void Action_MultiPucker::Help() const {
+
+}
+
+// Action_MultiPucker::Init()
+Action::RetType Action_MultiPucker::Init(ArgList& actionArgs, ActionInit& init, int debugIn)
+{
+
+}
+
+// Action_MultiPucker::Setup()
+Action::RetType Action_MultiPucker::Setup(ActionSetup& setup)
+{
+
+}
+
+// Action_MultiPucker::DoAction()
+Action::RetType Action_MultiPucker::DoAction(int frameNum, ActionFrame& frm)
+{
+
+}
---
+++ src/Action_MultiPucker.h
@@ -0,0 +1,19 @@
+#ifndef INC_ACTION_MULTIPUCKER_H
+#define INC_ACTION_MULTIPUCKER_H
+#include "Action.h"
+#include "Pucker_PuckerSearch.h"
+/// Automatically detect and calculate puckers within a residue range. 
+class Action_MultiPucker : public Action {
+  public:
+    Action_MultiPucker() {}
+    DispatchObject* Alloc() const { return (DispatchObject*)new Action_MultiPucker(); }
+    void Help() const;
+  private:
+    Action::RetType Init(ArgList&, ActionInit&, int);
+    Action::RetType Setup(ActionSetup&);
+    Action::RetType DoAction(int, ActionFrame&);
+    void Print() {}
+
+    Cpptraj::Pucker::PuckerSearch puckerSearch_;
+};
+#endif
