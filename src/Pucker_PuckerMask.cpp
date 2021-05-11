1f31760cfcf9a460da8ee241a23955b021ad36f9
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-05-11 10:09:41 -0400

    Add residue number and name to PuckerMask

--- src/Pucker_PuckerMask.cpp
+++ src/Pucker_PuckerMask.cpp
@@ -5,10 +5,14 @@ using namespace Cpptraj;
 
 Pucker::PuckerMask::PuckerMask() {}
 
-Pucker::PuckerMask::PuckerMask(std::vector<int> const& atomsIn) :
-  atoms_(atomsIn)
+/** CONSTRUCTOR - res #, name, index */
+Pucker::PuckerMask::PuckerMask(int resnumIn, std::string const& nameIn, std::vector<int> const& atomsIn) :
+  atoms_(atomsIn),
+  aspect_(nameIn),
+  resnum_(resnumIn)
 {}
 
+/** \return string based on atoms in mask. */
 std::string Pucker::PuckerMask::PuckerMaskString(Topology const& topIn) const {
   std::string out;
   for (std::vector<int>::const_iterator it = atoms_.begin(); it != atoms_.end(); ++it)
--- src/Pucker_PuckerMask.h
+++ src/Pucker_PuckerMask.h
@@ -9,13 +9,20 @@ namespace Pucker {
 class PuckerMask {
   public:
     PuckerMask();
-
-    PuckerMask(std::vector<int> const&);
-
+    /// CONSTRUCTOR - residue number, name (DataSet aspect), res # (DataSet index)
+    PuckerMask(int, std::string const&, std::vector<int> const&);
+    /// \return true if mask is empty
     bool None() const { return atoms_.empty(); }
+    /// \return string based on atoms in the mask
     std::string PuckerMaskString(Topology const&) const;
+    /// \return Residue number pucker belongs to
+    int ResNum() const { return resnum_; }
+    /// \return Name of pucker
+    std::string const& Name() const { return aspect_; }
   private:
     std::vector<int> atoms_; ///< Hold atom indices defining pucker
+    std::string aspect_;     ///< DataSet aspect
+    int resnum_;             ///< Residue number (DataSet index)
 };
 
 }
