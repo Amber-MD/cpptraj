43119416b7df7fdbfda70c117a6f44066070021d
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-05-11 09:06:47 -0400

    Add function to create string from pucker atoms

--- src/Pucker_PuckerMask.cpp
+++ src/Pucker_PuckerMask.cpp
@@ -1,4 +1,5 @@
 #include "Pucker_PuckerMask.h"
+#include "Topology.h"
 
 using namespace Cpptraj;
 
@@ -7,3 +8,14 @@ Pucker::PuckerMask::PuckerMask() {}
 Pucker::PuckerMask::PuckerMask(std::vector<int> const& atomsIn) :
   atoms_(atomsIn)
 {}
+
+std::string Pucker::PuckerMask::PuckerMaskString(Topology const& topIn) const {
+  std::string out;
+  for (std::vector<int>::const_iterator it = atoms_.begin(); it != atoms_.end(); ++it)
+  {
+    if (it != atoms_.begin())
+      out.append(" ");
+    out.append( topIn.TruncResNameAtomName( *it ) );
+  }
+  return out;
+}
--- src/Pucker_PuckerMask.h
+++ src/Pucker_PuckerMask.h
@@ -1,6 +1,8 @@
 #ifndef INC_PUCKER_PUCKERMASK_H
 #define INC_PUCKER_PUCKERMASK_H
 #include <vector>
+#include <string>
+class Topology;
 namespace Cpptraj {
 namespace Pucker {
 /// Used to define found puckers in Topology
@@ -11,6 +13,7 @@ class PuckerMask {
     PuckerMask(std::vector<int> const&);
 
     bool None() const { return atoms_.empty(); }
+    std::string PuckerMaskString(Topology const&) const;
   private:
     std::vector<int> atoms_; ///< Hold atom indices defining pucker
 };
