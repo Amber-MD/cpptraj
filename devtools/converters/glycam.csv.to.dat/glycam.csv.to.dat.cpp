eaee6a542309db733d4cf67da3711145108782d7
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-07-28 10:28:50 -0400

    Start reading the file and using arglist

--- devtools/converters/glycam.csv.to.dat/glycam.csv.to.dat.cpp
+++ devtools/converters/glycam.csv.to.dat/glycam.csv.to.dat.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include "../../../src/ArgList.h"
 
 /** Convert CSV containing Abbreviation,Name,PDB,Glycam to
   * DAT file containing #Name GLYCAM PDB
@@ -16,11 +17,21 @@ int main(int argc, char** argv) {
   static const int num = 1023;
 
   char* line = 0;
+  int lineNum = 1;
+  int err = 0;
   while ( (line = fgets( buffer, num, infile )) != 0 )
   {
-    printf("%s", line);
+    //printf("%s", line);
+    ArgList argline(line, ",");
+    if (argline.Nargs() != 4) {
+      fprintf(stderr,"Error: Line %i has %i columns separated by commas, expected 4.\n", argline.Nargs());
+      fprintf(stderr,"%s", line);
+      err = 1;
+      break;
+    }
+    lineNum++;
   }
   fclose( infile );
 
-  return 0;
+  return err;
 }
