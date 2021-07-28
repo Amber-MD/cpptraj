daf59b0a411f84833a2dd575cd7684b381cda4c7
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-07-28 10:43:40 -0400

    Finish conversion program

--- devtools/converters/glycam.csv.to.dat/glycam.csv.to.dat.cpp
+++ devtools/converters/glycam.csv.to.dat/glycam.csv.to.dat.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
+#include <string>
 #include "../../../src/ArgList.h"
+#include "../../../src/StringRoutines.h"
 
 /** Convert CSV containing Abbreviation,Name,PDB,Glycam to
   * DAT file containing #Name GLYCAM PDB
@@ -11,7 +13,18 @@ int main(int argc, char** argv) {
   }
 
   FILE* infile = fopen( argv[1], "rb" );
-  if (infile == 0) return 1;
+  if (infile == 0) {
+    fprintf(stderr,"Could not open input file.\n");
+    return 1;
+  }
+  FILE* outfile = fopen( "../../../dat/Carbohydrate_PDB_Glycam_Names.txt", "wb");
+  if (outfile == 0) {
+    fprintf(stderr,"Could not open output file.\n");
+    return 1;
+  }
+  fprintf(outfile,"# This file contains the mapping from common PDB names to Glycam residue codes.\n");
+  fprintf(outfile,"# Information largely obtained from http://glycam.org/docs/othertoolsservice/2016/06/09/3d-snfg-list-of-residue-names/#PDB\n");
+  fprintf(outfile,"# Last updated %s\n", TimeString().c_str());
 
   char buffer[1024];
   static const int num = 1023;
@@ -22,12 +35,31 @@ int main(int argc, char** argv) {
   while ( (line = fgets( buffer, num, infile )) != 0 )
   {
     //printf("%s", line);
-    ArgList argline(line, ",");
+    ArgList argline(line, ",\r\n");
     if (argline.Nargs() != 4) {
       fprintf(stderr,"Error: Line %i has %i columns separated by commas, expected 4.\n", argline.Nargs());
       fprintf(stderr,"%s", line);
       err = 1;
       break;
+    }
+    if (argline[0] == "Abbreviation") continue;
+    //         0            1    2   3
+    // infile: Abbreviation Name PDB Glycam
+    if (argline[2] == ".") {
+      printf("\"%s\" has no PDB name(s), skipping.\n", argline[1].c_str());
+    } else if (argline[3] == ".") {
+      printf("\"%s\" has no Gycam name, skipping.\n", argline[1].c_str());
+    } else {
+      // outfile: Name Glycam PDB(commas)
+      std::string name_with_commas = "\"" + argline[1] + "\"";
+      fprintf(outfile, "%-30s %3s ", name_with_commas.c_str(), argline[3].c_str());
+      ArgList pdbnames(argline[2], " ");
+      for (int n = 0; n < pdbnames.Nargs(); n++) {
+        if (n > 0)
+          fprintf(outfile,",");
+        fprintf(outfile, "%s", pdbnames[n].c_str());
+      }
+      fprintf(outfile,"\n");
     }
     lineNum++;
   }
