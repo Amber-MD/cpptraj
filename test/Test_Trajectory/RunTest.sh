3839d44ca58cf4adf5101f3e7fca56feeabe7d2b
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-08-16 15:54:58 -0400

    Add new restart read test

---
+++ test/Test_Trajectory/RunTest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+TESTNAME='Trajectory format tests'
+
+CleanFiles traj.in
+
+INPUT='-i traj.in'
+
+cat > traj.in <<EOF
+parm prmtop/ti.prmtop
+debug 10
+trajin trajin/inpcrd2
+trajout test1.rst7
+EOF
+RunCpptraj "Read/write Amber restart format"
+
+EndTest
