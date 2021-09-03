eca5b0088a827799838fb1a7030942f087453b8a
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-03 15:26:20 -0400

    Start adding DrawGraph

---
+++ src/Cluster/DrawGraph.h
@@ -0,0 +1,11 @@
+#ifndef INC_CLUSTER_DRAWGRAPH_H
+#define INC_CLUSTER_DRAWGRAPH_H
+class DataSet;
+namespace Cpptraj {
+namespace Cluster {
+
+void DrawGraph(bool, DataSet*, double, int, int);
+
+}
+}
+#endif
