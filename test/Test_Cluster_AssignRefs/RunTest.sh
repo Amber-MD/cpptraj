65a305c5004d038aa24d9496f6b16e5fb6ff901e
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-05 11:52:09 -0400

    Assign refs

--- test/Test_Cluster_AssignRefs/RunTest.sh
+++ test/Test_Cluster_AssignRefs/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles cluster.in summary.dat ksummary.dat
+CleanFiles cluster.in summary.dat ksummary.dat PW
 
 INPUT='-i cluster.in'
 
@@ -12,8 +12,13 @@ cat > cluster.in <<EOF
 parm ../tz2.parm7
 trajin ../tz2.nc
 
-cluster Tz2 rms :1-12@N,CA,C,O hieragglo clusters 5 summary summary.dat pairdist PW
-cluster Kmeans rms :1-12@N,CA,C,O kmeans clusters 5 summary ksummary.dat pairdist PW
+#cluster Tz2 rms :1-12@N,CA,C,O hieragglo clusters 5 summary summary.dat repout rep repfmt mol2
+distance D1 :1 :12
+
+for i=0;i<5;i++
+  reference rep.c\$i.mol2 [C\$i]
+done
+cluster Kmeans data D1 kmeans clusters 5 summary ksummary.dat assignrefs refcut 3.0 refmask :1-12&!@H=
 EOF
 RunCpptraj "Test1"
 
