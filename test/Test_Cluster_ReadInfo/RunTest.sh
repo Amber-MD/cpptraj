29f49a03964d2afc3f704c57910bc87342061764
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-08 15:02:45 -0400

    Add cluster restart from info file test

---
+++ test/Test_Cluster_ReadInfo/RunTest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+# Clean
+CleanFiles cluster.in C?.info.dat PW
+
+TESTNAME='Cluster read info tests'
+Requires netcdf
+INPUT="-i cluster.in"
+
+cat > cluster.in <<EOF
+noprogress
+parm ../tz2.parm7
+loadcrd ../tz2.nc name MyCrd
+
+# Cluster to 3 clusters
+cluster crdset MyCrd C1 :2-10 hieragglo clusters 3 info C1.info.dat savepairdist pairdist PW
+# Cluster to 8 clusters, use existing PW
+cluster crdset MyCrd C2 :2-10 hieragglo clusters 8 info C2.info.dat loadpairdist pairdist PW
+EOF
+RunCpptraj "Cluster restart test, part 1"
+
+cat > cluster.in <<EOF
+noprogress
+parm ../tz2.parm7
+loadcrd ../tz2.nc name MyCrd
+
+# Cluster to 3 clusters, start from existing info with 8 clusters
+cluster crdset MyCrd C3 :2-10 hieragglo clusters 3 \
+  readinfo infofile C2.info.dat \
+  info C3.info.dat \
+  loadpairdist pairdist PW
+EOF
+RunCpptraj "Cluster restart test, part 2"
+DoTest C1.info.dat C3.info.dat
+
+EndTest
+
+exit 0
