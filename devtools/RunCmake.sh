e0c6d3e6507db65a07605702ca5393303b5253df
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-11 09:57:21 -0400

    Add file that can be used to test cmake

---
+++ devtools/RunCmake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Can be used to test the cmake install.
+# Assumes 'git submodule update --init --recursive' has been run in top dir.
+
+if [ -z "$CPPTRAJHOME" ] ; then
+  echo "CPPTRAJHOME must be set."
+  exit 1
+fi
+
+HOME=$CPPTRAJHOME
+#export BUILD_FLAGS="-DOPENMP=TRUE"
+#export BUILD_FLAGS="-DMPI=TRUE ${BUILD_FLAGS}"
+#-DNetCDF_LIBRARIES_C=$HOME/lib/libnetcdf.so -DNetCDF_INCLUDES=$HOME/include
+installdir=$HOME/install
+COMPILER=gnu
+cmake .. $BUILD_FLAGS -DCOMPILER=${COMPILER^^} -DINSTALL_HEADERS=FALSE \
+         -DCMAKE_INSTALL_PREFIX=$installdir -DCMAKE_LIBRARY_PATH=$HOME/lib \
+         -DPRINT_PACKAGING_REPORT=TRUE 
+
