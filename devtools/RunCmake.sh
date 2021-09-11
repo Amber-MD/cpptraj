d6109daf9f768f6de499bfc45df6c221ec2dd115
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-11 09:58:49 -0400

    Ensure installdir is created

--- devtools/RunCmake.sh
+++ devtools/RunCmake.sh
@@ -7,12 +7,16 @@ if [ -z "$CPPTRAJHOME" ] ; then
   echo "CPPTRAJHOME must be set."
   exit 1
 fi
-
 HOME=$CPPTRAJHOME
+
+installdir=$HOME/install
+if [ ! -d "$installdir" ] ; then
+  mkdir $installdir
+fi
+
 #export BUILD_FLAGS="-DOPENMP=TRUE"
 #export BUILD_FLAGS="-DMPI=TRUE ${BUILD_FLAGS}"
 #-DNetCDF_LIBRARIES_C=$HOME/lib/libnetcdf.so -DNetCDF_INCLUDES=$HOME/include
-installdir=$HOME/install
 COMPILER=gnu
 cmake .. $BUILD_FLAGS -DCOMPILER=${COMPILER^^} -DINSTALL_HEADERS=FALSE \
          -DCMAKE_INSTALL_PREFIX=$installdir -DCMAKE_LIBRARY_PATH=$HOME/lib \
