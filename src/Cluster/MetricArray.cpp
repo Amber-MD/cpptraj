2be8f6588f0aa0d8e3704b14708b422f0fe84a02
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-12 10:48:55 -0400

    Add copy/assignment

--- src/Cluster/MetricArray.cpp
+++ src/Cluster/MetricArray.cpp
@@ -4,4 +4,22 @@
 /** CONSTRUCTOR */
 Cpptraj::Cluster::MetricArray::MetricArray() {}
 
+/** COPY CONSTRUCTOR */
+Cpptraj::Cluster::MetricArray::MetricArray(MetricArray const& rhs) {
+  metrics_.reserve( rhs.metrics_.size() );
+  for (std::vector<Metric*>::const_iterator it = rhs.metrics_.begin(); it != rhs.metrics_.end(); ++it)
+    metrics_.push_back( (*it)->Copy() );
+}
+
+/** ASSIGNMENT */
+Cpptraj::Cluster::MetricArray&
+  Cpptraj::Cluster::MetricArray::operator=(MetricArray const& rhs)
+{
+  if (this == &rhs) return *this;
+  metrics_.clear();
+  metrics_.reserve( rhs.metrics_.size() );
+  for (std::vector<Metric*>::const_iterator it = rhs.metrics_.begin(); it != rhs.metrics_.end(); ++it)
+    metrics_.push_back( (*it)->Copy() );
+  return *this;
+}
 
--- src/Cluster/MetricArray.h
+++ src/Cluster/MetricArray.h
@@ -7,9 +7,17 @@ class Metric;
 /// Hold Metrics of various types
 class MetricArray {
   public:
+    /// CONSTRUCTOR
     MetricArray();
+    /// COPY
+    MetricArray(MetricArray const&);
+    /// ASSIGN
+    MetricArray& operator=(MetricArray const&);
+    /// Types of distance calculation
+    enum DistanceType { MANHATTAN = 0, EUCLID };
   private:
-    std::vector<Metric*> metrics_;
+    std::vector<Metric*> metrics_; ///< Hold each Metric
+    DistanceType type_;            ///< Type of distance calc to perform
 };
 
 }
