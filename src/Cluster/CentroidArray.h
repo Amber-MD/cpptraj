521d46d7952df74c240e4fd6309efb81122eea7b
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-13 14:24:45 -0400

    Add destruct, assign and copy to CentroidArray

--- src/Cluster/CentroidArray.h
+++ src/Cluster/CentroidArray.h
@@ -7,7 +7,16 @@ class Centroid;
 /// Hold Centroids of various types
 class CentroidArray {
   public:
+    /// CONSTRUCTOR
     CentroidArray();
+    /// DESTRUCTOR
+    ~CentroidArray();
+    /// COPY
+    CentroidArray(CentroidArray const&);
+    /// ASSIGN
+    CentroidArray& operator=(CentroidArray const&);
+    /// Clear the centroid array
+    void Clear();
   private:
     std::vector<Centroid*> centroids_;
 };
