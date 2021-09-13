2e2ae6f2bd6f96c519632546885767db24936afa
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-13 11:56:29 -0400

    Start adding init to MetricArray

--- src/Cluster/MetricArray.cpp
+++ src/Cluster/MetricArray.cpp
@@ -1,8 +1,22 @@
 #include "MetricArray.h"
 #include "Metric.h"
+#include "../ArgList.h"
+#include "../CpptrajStdio.h"
+#include "../DataSetList.h"
+// Metric classes
+#include "Metric_RMS.h"
+#include "Metric_DME.h"
+#include "Metric_Scalar.h"
+#include "Metric_SRMSD.h"
+#include "Metric_Torsion.h"
 
 /** CONSTRUCTOR */
 Cpptraj::Cluster::MetricArray::MetricArray() {}
+
+/** DESTRUCTOR */
+Cpptraj::Cluster::MetricArray::~MetricArray() {
+  Clear();
+}
 
 /** COPY CONSTRUCTOR */
 Cpptraj::Cluster::MetricArray::MetricArray(MetricArray const& rhs) {
@@ -23,3 +37,77 @@ Cpptraj::Cluster::MetricArray&
   return *this;
 }
 
+/** Clear the metric array. */
+void Cpptraj::Cluster::MetricArray::Clear() {
+  for (std::vector<Metric*>::iterator it = metrics_.begin(); it != metrics_.end(); ++it)
+    delete *it;
+  sets_.clear();
+}
+
+/** /return Pointer to Metric of given type. */
+Cpptraj::Cluster::Metric* Cpptraj::Cluster::MetricArray::AllocateMetric(Metric::Type mtype)
+{
+  Metric* met = 0;
+  switch (mtype) {
+    case Metric::RMS       : met = new Metric_RMS(); break;
+    case Metric::DME       : met = new Metric_DME(); break;
+    case Metric::SRMSD     : met = new Metric_SRMSD(); break;
+    case Metric::SCALAR    : met = new Metric_Scalar(); break;
+    case Metric::TORSION   : met = new Metric_Torsion(); break;
+    default: mprinterr("Error: Unhandled Metric in AllocateMetric.\n");
+  }
+  return met;
+}
+
+/** Initialize with given sets and arguments. */
+int Cpptraj::Cluster::MetricArray::InitMetricArray(DataSetList const& dslIn, ArgList& analyzeArgs)
+{
+  // Get rid of any previous metrics
+  Clear();
+  // Get arguments for any COORDS metrics
+  int usedme = (int)analyzeArgs.hasKey("dme");
+  int userms = (int)analyzeArgs.hasKey("rms");
+  int usesrms = (int)analyzeArgs.hasKey("srmsd");
+  bool useMass = analyzeArgs.hasKey("mass");
+  bool nofit   = analyzeArgs.hasKey("nofit");
+  std::string maskExpr = analyzeArgs.GetMaskNext();
+  // Check args
+  if (usedme + userms + usesrms > 1) {
+    mprinterr("Error: Specify either 'dme', 'rms', or 'srmsd'.\n");
+    return 1;
+  }
+  Metric::Type coordsMetricType;
+  if      (usedme)  coordsMetricType = Metric::DME;
+  else if (userms)  coordsMetricType = Metric::RMS;
+  else if (usesrms) coordsMetricType = Metric::SRMSD;
+  else coordsMetricType = Metric::RMS; // default
+
+  // For each input set, set up the appropriate metric
+  for (DataSetList::const_iterator ds = dslIn.begin(); ds != dslIn.end(); ++ds)
+  {
+    Metric::Type mtype = Metric::UNKNOWN_METRIC;
+    if ( (*ds)->Group() == DataSet::COORDINATES )
+    {
+      mtype = coordsMetricType;
+    } else if ((*ds)->Group() == DataSet::SCALAR_1D) {
+      if ( (*ds)->Meta().IsTorsionArray() )
+        mtype = Metric::TORSION;
+      else
+        mtype = Metric::SCALAR;
+    }
+
+    if (mtype == Metric::UNKNOWN_METRIC) {
+      mprinterr("Error: Set '%s' is a type not yet supported by Cluster.\n", (*ds)->legend());
+      return 1;
+    }
+
+    Metric* met = AllocateMetric( mtype );
+    if (met == 0) {
+      mprinterr("Internal Error: Could not allocate metric for set '%s'\n", (*ds)->legend());
+      return 1;
+    }
+
+  }
+
+  return 0;
+} 
--- src/Cluster/MetricArray.h
+++ src/Cluster/MetricArray.h
@@ -1,22 +1,36 @@
 #ifndef CPPTRAJ_CLUSTER_METRICARRAY_H
 #define CPPTRAJ_CLUSTER_METRICARRAY_H
 #include <vector>
+#include "Metric.h" // Metric::Type
+class ArgList;
+class DataSet;
+class DataSetList;
 namespace Cpptraj {
 namespace Cluster {
-class Metric;
+
 /// Hold Metrics of various types
 class MetricArray {
   public:
     /// CONSTRUCTOR
     MetricArray();
+    /// DESTRUCTOR
+    ~MetricArray();
     /// COPY
     MetricArray(MetricArray const&);
     /// ASSIGN
     MetricArray& operator=(MetricArray const&);
     /// Types of distance calculation
     enum DistanceType { MANHATTAN = 0, EUCLID };
+    /// Initialize with data sets and user arguments
+    int InitMetricArray(DataSetList const&, ArgList&);
   private:
+    /// Clear array
+    void Clear();
+    /// Allocate metric of given type
+    Metric* AllocateMetric(Metric::Type);
+
     std::vector<Metric*> metrics_; ///< Hold each Metric
+    std::vector<DataSet*> sets_;   ///< Sets corresponding to each Metric
     DistanceType type_;            ///< Type of distance calc to perform
 };
 
