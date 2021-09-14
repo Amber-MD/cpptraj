8bca61ee4bb6c146a148921f34bfd9c587a2b5d3
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-14 12:02:30 -0400

    Switch from PairwiseMatrix to MetricArray

--- src/Cluster/Algorithm.cpp
+++ src/Cluster/Algorithm.cpp
@@ -1,18 +1,17 @@
 #include "Algorithm.h"
-#include "Metric.h"
+#include "MetricArray.h"
 #include "Node.h"
-#include "PairwiseMatrix.h"
 #include "../CpptrajStdio.h"
 
 /** \return Distance between given cluster centroids. */
 double Cpptraj::Cluster::Algorithm::ClusterDistance(Node const& C1, Node const& C2,
-                                                    PairwiseMatrix const& pmatrix,
+                                                    MetricArray& pmatrix,
                                                     bool includeSieved,
                                                     Cframes const& sievedOut) const
 {
-  if (C1.Cent() == 0 || C2.Cent() == 0) {
+  if (C1.Cent().empty() || C2.Cent().empty()) {
     mprinterr("Internal Error: One or both centroids are null in ClusterDistance().\n");
     return -1.0;
   }
-  return pmatrix.MetricPtr()->CentroidDist( C1.Cent(), C2.Cent() );
+  return pmatrix.CentroidDist( C1.Cent(), C2.Cent() );
 }
--- src/Cluster/Algorithm.h
+++ src/Cluster/Algorithm.h
@@ -7,7 +7,7 @@ namespace Cluster {
 class Cframes;
 class List;
 class Node;
-class PairwiseMatrix;
+class MetricArray;
 /// Abstract base class for implementing clustering algorithms.
 class Algorithm {
   public:
@@ -22,11 +22,11 @@ class Algorithm {
     /// Report brief details on algorithm setup to a file.
     virtual void Results(CpptrajFile&) const = 0;
     /// Perform clustering on specified frames using given distance matrix.
-    virtual int DoClustering(List&, Cframes const&, PairwiseMatrix const&) = 0;
+    virtual int DoClustering(List&, Cframes const&, MetricArray&) = 0;
     /// Report any timing data
     virtual void Timing(double) const = 0;
     /// /return Algorithm-specific between-cluster distance. Default to centroid distance.
-    virtual double ClusterDistance(Node const&, Node const&, PairwiseMatrix const&,
+    virtual double ClusterDistance(Node const&, Node const&, MetricArray&,
                                    bool, Cframes const&) const;
     /// /return Epsilon for density-based algorithms; intended for use with sieve restore.
     virtual double Epsilon() const { return 0.0; }
