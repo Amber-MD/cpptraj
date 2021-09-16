74660ebe86896baa2686ab7691161adf2fbb6b35
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-09-16 10:24:17 -0400

    Add metricstats test save

--- test/Test_Cluster_CoordsAndData/RunTest.sh
+++ test/Test_Cluster_CoordsAndData/RunTest.sh
@@ -4,7 +4,7 @@
 
 TESTNAME='Test clustering on coordinates and data'
 
-CleanFiles cluster.in c1.info.dat c1.summary.dat c1.cnvt.dat data.dat
+CleanFiles cluster.in c1.info.dat c1.summary.dat c1.cnvt.dat data.dat metricstats.dat
 
 INPUT='-i cluster.in'
 
@@ -20,11 +20,12 @@ runanalysis cluster c1 \
   rms :1-12&!@H= \
   out c1.cnvt.dat \
   info c1.info.dat \
-  summary c1.summary.dat metricstats
+  summary c1.summary.dat metricstats metricstats.dat
 EOF
 RunCpptraj "$TESTNAME"
 DoTest c1.info.dat.save c1.info.dat
 DoTest c1.summary.dat.save c1.summary.dat
 DoTest c1.cnvt.dat.save c1.cnvt.dat
+DoTest metricstats.dat.save metricstats.dat
 
 EndTest
