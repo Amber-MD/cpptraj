0b1f4fc7d88f9e3ee1280ee5ab346192649b692d
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-11-04 20:32:00 -0400

    Add function to return last type read

--- src/ParmFile.h
+++ src/ParmFile.h
@@ -43,6 +43,8 @@ class ParmFile {
     FileName const& ParmFilename() { return parmName_; }
     /// \return ParmFormatType of given file or UNKNOWN_PARM.
     static ParmFormatType DetectFormat(FileName const&);
+    /// \return Format of last read file
+    ParmFormatType CurrentFormat() const { return pfType_; }
   private :
     /// \return Allocated ParmIO if given file matches known type, 0 otherwise.
     static ParmIO* DetectFormat(FileName const&, ParmFormatType&);
