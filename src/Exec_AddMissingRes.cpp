4ab567dc8735f60b00dc33379e0b9a9dfd9e48cc
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-12-08 08:55:19 -0500

    Remove old code

--- src/Exec_AddMissingRes.cpp
+++ src/Exec_AddMissingRes.cpp
@@ -211,7 +211,7 @@ const
   return 0;
 }
 
-/** Get missing residues from PDB, organize them into "gaps", i.e.
+/** Organize missing residues in given topology into "gaps", i.e.
   * contiguous sequences.
   */
 int Exec_AddMissingRes::FindGaps(Garray& Gaps, CpptrajFile& outfile, Topology const& topIn)
@@ -240,79 +240,6 @@ const
     }
   }
 
-/*  BufferedLine infile;
-  if (infile.OpenFileRead( pdbname )) {
-    mprinterr("Error: Could not open '%s' for reading.\n", pdbname.c_str());
-    return 1;
-  }
-  const char* linePtr = infile.Line();
-  int inMissing = 0;
-  int nmissing = 0;
-  while (linePtr != 0) {
-    if (strncmp(linePtr, "REMARK", 6) == 0)
-    {
-      ArgList line(linePtr);
-      if (line.Nargs() > 2) {
-        if (inMissing == 0) {
-          // MISSING section not yet encountered.
-          if (line[0] == "REMARK" && line[1] == "465" && line[2] == "MISSING" && line[3] == "RESIDUES") {
-            inMissing = 1;
-          }
-        } else if (inMissing == 1) {
-          // In MISSING, looking for start of missing residues
-          if (line[0] == "REMARK" && line[2] == "M") {
-            inMissing = 2;
-            nmissing = 0;
-          }
-        } else if (inMissing == 2) {
-          // Reading MISSING residues
-          if (line[1] != "465") {
-            //mprinterr("END REACHED.\n"); // DEBUG
-            break; 
-          } else {
-            // This is a missing residue
-            nmissing++;
-            //           11111111112222222
-            // 012345678901234567890123456
-            // REMARK 465   M RES C SSSEQI
-            std::string const& currentname = line[2];
-            char currentchain = linePtr[19];
-            // Need to be able to parse out insertion code
-            char currenticode = linePtr[26];
-            int currentres;
-            if (currenticode == ' ') {
-              currentres = atoi(line[4].c_str());
-            } else {
-              char numbuf[6];
-              std::copy(linePtr+21, linePtr+26, numbuf);
-              numbuf[5] = '\0';
-              currentres = atoi(numbuf);
-            }
-            mprintf("DEBUG: Missing residue %s %i icode= %c chain= %c\n",
-                    currentname.c_str(), currentres, currenticode, currentchain);
-            Pres thisRes(currentname, currentres, currenticode, currentchain);
-            // Is this the first "gap"?
-            if (Gaps.empty())
-              Gaps.push_back( ResArray(1, thisRes) );
-            else {
-              ResArray& currentGap = Gaps.back();
-              if ( currentres - currentGap.back().Onum() > 1 ||
-                   currentchain != currentGap.back().Chain() )
-              {
-                // Starting a new "gap"
-                Gaps.push_back( ResArray(1, thisRes) );
-              } else {
-                // Add to existing "gap"
-                currentGap.push_back( thisRes );
-              }
-            }
-          } // END missing residue
-        } // END inMissing == 2
-      } // END nargs > 2
-    } // END REMARK
-    linePtr = infile.Line();
-  } // END while linePtr != 0
-*/
   // Printout
   for (Garray::const_iterator it = Gaps.begin(); it != Gaps.end(); ++it) {
     outfile.Printf("  Gap %c %4s %6i to %4s %6i %6zu\n",
