6aad51075b443a951f0cb512ff1706b0fd475e28
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-01-28 10:59:40 -0500

    Start exploring a different way of speeding up finding cluster to cluster min distance by keeping track of closest cluster to each cluster and updating as needed.

--- src/Cluster/DynamicMatrix.cpp
+++ src/Cluster/DynamicMatrix.cpp
@@ -61,6 +61,34 @@ double Cpptraj::Cluster::DynamicMatrix::FindMin(int& iOut, int& jOut) const {
       }
     }
   }
+  mprintf("DEBUG: Min found at row %i col %i (%f)\n", iOut, jOut, min);
+
+
+  float currentMin = std::numeric_limits<float>::max();
+  int minRow = -1;
+  int minCol = -1;
+  for (unsigned int col = 0; col != closestIdx_.size(); col++)
+  {
+    int row = closestIdx_[col];
+    if (!ignore_[col] && !ignore_[row]) {
+      float mval = Mat_.element(col, row);
+      if (mval < currentMin) {
+        currentMin = mval;
+        minRow = (int)row;
+        minCol = (int)col;
+      }
+    }
+  }
+  if (minCol < minRow) { //FIXME i think only needed for testing
+    int itmp = minCol;
+    minCol = minRow;
+    minRow = itmp;
+  }
+  mprintf("DEBUG: Second try: Min found at row %i col %i (%f)\n", minRow, minCol, currentMin);
+
+  if (iOut != minRow || jOut != minCol)
+    mprintf("Error: Column/row mismatch.\n");
+
   return (double)min;
 }
 #endif
@@ -80,10 +108,68 @@ void Cpptraj::Cluster::DynamicMatrix::PrintElements() const {
   }
 }
 
+/** Print elements like a square. */
+void Cpptraj::Cluster::DynamicMatrix::PrintElementsSquare() const {
+/*  mprintf("%4s", "");
+  for (unsigned int col = 0; col < Mat_.Ncols(); col++)
+    mprintf(" %12u", col);
+  mprintf("\n");
+  for (unsigned int row = 0; row < Mat_.Nrows(); row++) {
+    mprintf("%4u", row);
+    for (unsigned int col = 0; col < Mat_.Ncols(); col++) {
+      if (row <= col)
+        mprintf(" %12s", "X");
+      else if (ignore_[row] || ignore_[col])
+        mprintf(" %12s", "i");
+      else
+        mprintf(" %12.4f", Mat_.element(col, row));
+    }
+    mprintf("\n");
+  }*/
+  for (unsigned int row = 0; row < Mat_.Nrows(); row++) {
+    if (!ignore_[row]) {
+      mprintf("%4u :", row);
+      for (unsigned int col = 0; col < Mat_.Ncols(); col++) {
+        if (col != row && !ignore_[col])
+          mprintf(" %4u=%8.3f", col, Mat_.element(col, row));
+      }
+      mprintf("\n");
+    }
+  }
+}
+
+/** Print closest indices to STDOUT. */
+void Cpptraj::Cluster::DynamicMatrix::PrintClosest() const {
+  for (unsigned int idx = 0; idx != closestIdx_.size(); idx++) {
+    if (!ignore_[idx]) {
+      mprintf("DEBUG: Closest cluster to %u is %i (%f)\n", idx, closestIdx_[idx], Mat_.element(idx, closestIdx_[idx]));
+      // Check that this is actually the case
+      int cidx = -1;
+      float cdist = 0;
+      for (unsigned int jdx = 0; jdx != closestIdx_.size(); jdx++) {
+        if (idx != jdx && !ignore_[jdx]) {
+          float fdist = Mat_.element(idx, jdx);
+          if (cidx == -1) {
+            cidx = jdx;
+            cdist = fdist;
+          } else if (fdist < cdist) {
+            cidx = jdx;
+            cdist = fdist;
+          }
+        }
+      }
+      if (cidx != closestIdx_[idx]) {
+        mprintf("Error: ACTUAL closest idx to %u is %i (%f)\n", idx, cidx, cdist);
+      }
+    }
+  }
+}
+
 // DynamicMatrix::SetupMatrix()
 int Cpptraj::Cluster::DynamicMatrix::SetupMatrix(size_t sizeIn) {
   if (Mat_.resize( 0L, sizeIn )) return 1;
   ignore_.assign( sizeIn, false );
+  closestIdx_.assign( sizeIn, -1 );
 # ifdef _OPENMP
   int n_threads = 0;
 # pragma omp parallel
--- src/Cluster/DynamicMatrix.h
+++ src/Cluster/DynamicMatrix.h
@@ -2,6 +2,7 @@
 #define INC_CLUSTER_DYNAMICMATRIX_H
 #include <vector>
 #include "../Matrix.h"
+#include "../CpptrajStdio.h" // DEBUG
 namespace Cpptraj {
 namespace Cluster {
 
@@ -10,7 +11,8 @@ class DynamicMatrix {
   public:
     DynamicMatrix() {}
     /// Indicate given row/col should be ignored.
-    void Ignore(int row)            { ignore_[row] = true;   }
+    //void Ignore(int row)            { ignore_[row] = true;   }
+    inline void Ignore(int);
     /// \return true if given row/col has been ignored.
     bool IgnoringRow(int row) const { return ignore_[row];   }
     /// \return Original number of rows in matrix
@@ -25,21 +27,132 @@ class DynamicMatrix {
     inline double GetCdist(int c, int r) const { return Mat_.element(c,r); }
     /// Print all matrix elements to STDOUT
     void PrintElements() const;
+    /// Print matrix elements to STDOUT as a square
+    void PrintElementsSquare() const;
+    /// Print closest index to each col to STDOUT
+    void PrintClosest() const;
     /// Add given element to matrix.
-    int AddCdist(double d)        { return Mat_.addElement((float)d); }
+    //int AddCdist(double d)        { return Mat_.addElement((float)d); }
     /// Set element at column/row to given value
-    void SetCdist(int col, int row, double val) { Mat_.setElement(col, row, val); }
+    //void SetCdist(int col, int row, double val) { Mat_.setElement(col, row, val); }
+    inline void SetCdist(int, int, float);
     /// Set up matrix for given number of rows
     int SetupMatrix(size_t);
   private:
+    inline void updateClosestIdx(unsigned int);
+
     Matrix<float> Mat_;        ///< Upper-triangle matrix holding cluster distances.
     std::vector<bool> ignore_; ///< If true, ignore the row/col when printing/searching etc.
+    std::vector<int> closestIdx_; ///< For each cluster, index of the cluster closest to it
 #   ifdef _OPENMP
     std::vector<int> minRow_;
     std::vector<int> minCol_;
     std::vector<float> minVal_;
 #   endif
 };
+
+/** Update closest to idx */
+void DynamicMatrix::updateClosestIdx(unsigned int idx) {
+      closestIdx_[idx] = -1;
+      float current_closest = 0;
+      for (unsigned int jdx = 0; jdx != closestIdx_.size(); jdx++) {
+        if (!ignore_[jdx] && idx != jdx) {
+          if (closestIdx_[idx] == -1) {
+            closestIdx_[idx] = jdx;
+            current_closest = Mat_.element(idx, jdx);
+          } else {
+            float fdist = Mat_.element(idx, jdx);
+            if (fdist < current_closest) {
+              closestIdx_[idx] = jdx;
+              current_closest = fdist;
+            }
+          }
+        }
+      } // END loop jdx
+      mprintf("DEBUG: New closest to %u is %i\n", idx, closestIdx_[idx]);
+}
+
+/** Set cluster distance. Record closest distance for each col. */
+void DynamicMatrix::SetCdist(int col, int row, float val) {
+  mprintf("DEBUG: Setting cluster %i to cluster %i distance= %f\n", col, row, val);
+
+  int update_col = -1;
+  int update_row = -1;
+
+  // Check col->row distance
+  if (closestIdx_[col] < 0) {
+    mprintf("DEBUG: This is the initial distance for col.\n");
+    closestIdx_[col] = row;
+  } else {
+    float closest_dist_to_col = Mat_.element(col, closestIdx_[col]);
+    mprintf("DEBUG: Current closest to %i is %i (%f)\n", col, closestIdx_[col], closest_dist_to_col); 
+    if (val < closest_dist_to_col) {
+      mprintf("DEBUG: Updated col.\n");
+      closestIdx_[col] = row;
+    } else if (row == closestIdx_[col] && val > closest_dist_to_col) {
+      // We are increasing what was previously the closest distance. May need to find new closest distance to col.
+      update_col = col;
+    }
+  }
+
+  // Check row->col distance
+  if (closestIdx_[row] < 0) {
+    mprintf("DEBUG: This is the initial distance for row.\n");
+    closestIdx_[row] = col;
+  } else {
+    float closest_dist_to_row = Mat_.element(row, closestIdx_[row]);
+    mprintf("DEBUG: Current closest to %i is %i (%f)\n", row, closestIdx_[row], closest_dist_to_row);
+    if (val < closest_dist_to_row) {
+      mprintf("DEBUG: Updated row.\n");
+      closestIdx_[row] = col;
+    } else if (col == closestIdx_[row] && val > closest_dist_to_row) {
+      // We are increasing what was previously the closest distance. May need to find new closest distance to row.
+      update_row = row;
+    }
+  }
+
+  // Actually update the element
+  Mat_.setElement(col, row, val);
+
+  // Update closest if necessary
+  if (update_col != -1) {
+    mprintf("DEBUG: Closest to column %i increased. Need to update.\n", update_col);
+    updateClosestIdx(update_col);
+  }
+  if (update_row != -1) {
+    mprintf("DEBUG: Closest to row %i increased. Need to update.\n", update_row);
+    updateClosestIdx(update_row);
+  }
+}
+
+/** Specify given row should be ignored. Update closest distances if necessary. */
+void DynamicMatrix::Ignore(int row) {
+  ignore_[row] = true;
+  // If any remaining indices have the ignored row that needs to be updated
+  for (unsigned int idx = 0; idx != closestIdx_.size(); idx++) {
+    if (!ignore_[idx] && closestIdx_[idx] == row) {
+      mprintf("DEBUG: closest to %u was just ignored; must be updated.\n", idx);
+      updateClosestIdx(idx);
+/*      closestIdx_[idx] = -1;
+      float current_closest = 0;
+      for (unsigned int jdx = 0; jdx != closestIdx_.size(); jdx++) {
+        if (!ignore_[jdx] && idx != jdx) {
+          if (closestIdx_[idx] == -1) {
+            closestIdx_[idx] = jdx;
+            current_closest = Mat_.element(idx, jdx);
+          } else {
+            float fdist = Mat_.element(idx, jdx);
+            if (fdist < current_closest) {
+              closestIdx_[idx] = jdx;
+              current_closest = fdist;
+            }
+          }
+        }
+      } // END loop jdx
+      mprintf("DEBUG: New closest to %u is %i\n", idx, closestIdx_[idx]);*/
+    }
+  } // END loop idx
+}
 
 }
 }
