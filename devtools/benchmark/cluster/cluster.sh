44073b6070a3d5cade91fac87ef8d31070b8546f
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-01-28 12:33:36 -0500

    Bench hieragglo

--- devtools/benchmark/cluster/cluster.sh
+++ devtools/benchmark/cluster/cluster.sh
@@ -54,10 +54,11 @@ EOF
   ((SPLIT3 = $NFRAMES * 3))
   echo "Total $TOTAL, Each section $NFRAMES splits $SPLIT1 $SPLIT2 $SPLIT3"
 
+#  kmeans clusters 5 randompoint kseed 42 \
   cat >> cpptraj.in <<EOF
 
 cluster \
-  kmeans clusters 5 randompoint kseed 42 \
+  hieragglo clusters 5 \
   rms :10-260@CA sieve 20 random sieveseed 23 savepairdist \
   summaryhalf \$PREFIX.split.dat splitframe $SPLIT1,$SPLIT2,$SPLIT3 \
   info \$PREFIX.info.dat \
