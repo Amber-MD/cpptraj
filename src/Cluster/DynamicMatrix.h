ed8c66c3d1109e8b12e707bd325a2e808beff9de
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-01-28 12:45:08 -0500

    Remove old code and openmp code. Finding min is no longer the bottleneck

--- src/Cluster/DynamicMatrix.h
+++ src/Cluster/DynamicMatrix.h
@@ -11,18 +11,13 @@ class DynamicMatrix {
   public:
     DynamicMatrix() {}
     /// Indicate given row/col should be ignored.
-    //void Ignore(int row)            { ignore_[row] = true;   }
     inline void Ignore(int);
     /// \return true if given row/col has been ignored.
     bool IgnoringRow(int row) const { return ignore_[row];   }
     /// \return Original number of rows in matrix
     size_t Nrows()            const { return Mat_.Nrows();   }
     /// Set the row and column of the smallest element not being ignored.
-#   ifdef _OPENMP
-    double FindMin(int&, int&);
-#   else
     double FindMin(int&, int&) const;
-#   endif
     /// \return an element.
     inline double GetCdist(int c, int r) const { return Mat_.element(c,r); }
     /// Print all matrix elements to STDOUT
@@ -31,45 +26,38 @@ class DynamicMatrix {
     void PrintElementsSquare() const;
     /// Print closest index to each col to STDOUT
     void PrintClosest() const;
-    /// Add given element to matrix.
-    //int AddCdist(double d)        { return Mat_.addElement((float)d); }
     /// Set element at column/row to given value
-    //void SetCdist(int col, int row, double val) { Mat_.setElement(col, row, val); }
     inline void SetCdist(int, int, float);
     /// Set up matrix for given number of rows
     int SetupMatrix(size_t);
   private:
+    /// Find the closest cluster to the given cluster
     inline void updateClosestIdx(unsigned int);
 
-    Matrix<float> Mat_;        ///< Upper-triangle matrix holding cluster distances.
-    std::vector<bool> ignore_; ///< If true, ignore the row/col when printing/searching etc.
+    Matrix<float> Mat_;           ///< Upper-triangle matrix holding cluster distances.
+    std::vector<bool> ignore_;    ///< If true, ignore the row/col when printing/searching etc.
     std::vector<int> closestIdx_; ///< For each cluster, index of the cluster closest to it
-#   ifdef _OPENMP
-    std::vector<int> minRow_;
-    std::vector<int> minCol_;
-    std::vector<float> minVal_;
-#   endif
 };
 
-/** Update closest to idx */
+/** Update closest to given idx */
 void DynamicMatrix::updateClosestIdx(unsigned int idx) {
-      closestIdx_[idx] = -1;
-      float current_closest = 0;
-      for (unsigned int jdx = 0; jdx != closestIdx_.size(); jdx++) {
-        if (!ignore_[jdx] && idx != jdx) {
-          if (closestIdx_[idx] == -1) {
-            closestIdx_[idx] = jdx;
-            current_closest = Mat_.element(idx, jdx);
-          } else {
-            float fdist = Mat_.element(idx, jdx);
-            if (fdist < current_closest) {
-              closestIdx_[idx] = jdx;
-              current_closest = fdist;
-            }
-          }
+  closestIdx_[idx] = -1;
+  float current_closest = 0;
+  for (unsigned int jdx = 0; jdx != closestIdx_.size(); jdx++) {
+    if (!ignore_[jdx] && idx != jdx) {
+      if (closestIdx_[idx] == -1) {
+        closestIdx_[idx] = jdx;
+        current_closest = Mat_.element(idx, jdx);
+      } else {
+        float fdist = Mat_.element(idx, jdx);
+        if (fdist < current_closest) {
+          closestIdx_[idx] = jdx;
+          current_closest = fdist;
         }
-      } // END loop jdx
-//      mprintf("DEBUG: New closest to %u is %i\n", idx, closestIdx_[idx]);
+      }
+    }
+  } // END loop jdx
+//mprintf("DEBUG: New closest to %u is %i\n", idx, closestIdx_[idx]);
 }
 
 /** Set cluster distance. Record closest distance for each col. */
@@ -133,23 +121,6 @@ void DynamicMatrix::Ignore(int row) {
     if (!ignore_[idx] && closestIdx_[idx] == row) {
 //      mprintf("DEBUG: closest to %u was just ignored; must be updated.\n", idx);
       updateClosestIdx(idx);
-/*      closestIdx_[idx] = -1;
-      float current_closest = 0;
-      for (unsigned int jdx = 0; jdx != closestIdx_.size(); jdx++) {
-        if (!ignore_[jdx] && idx != jdx) {
-          if (closestIdx_[idx] == -1) {
-            closestIdx_[idx] = jdx;
-            current_closest = Mat_.element(idx, jdx);
-          } else {
-            float fdist = Mat_.element(idx, jdx);
-            if (fdist < current_closest) {
-              closestIdx_[idx] = jdx;
-              current_closest = fdist;
-            }
-          }
-        }
-      } // END loop jdx
-      mprintf("DEBUG: New closest to %u is %i\n", idx, closestIdx_[idx]);*/
     }
   } // END loop idx
 }
