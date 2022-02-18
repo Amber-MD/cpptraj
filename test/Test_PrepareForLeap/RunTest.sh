fe64c970cee3500ef7de9dbe07edbb7c512135f4
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-02-18 09:37:31 -0500

    Add test with keepaltloc

--- test/Test_PrepareForLeap/RunTest.sh
+++ test/Test_PrepareForLeap/RunTest.sh
@@ -2,7 +2,8 @@
 
 . ../MasterTest.sh
 
-CleanFiles cpptraj.in 1qos.cpptraj.pdb leap.1qos.in
+CleanFiles cpptraj.in 1qos.cpptraj.pdb leap.1qos.in \
+           leap.4zzw.in 4zzw.cpptraj.pdb
 
 INPUT='-i cpptraj.in'
 
@@ -21,5 +22,21 @@ EOF
 RunCpptraj "Prepare PDB 1qos for LEaP"
 DoTest leap.1qos.in.save leap.1qos.in
 DoTest 1qos.cpptraj.pdb.save 1qos.cpptraj.pdb
+
+cat > cpptraj.in <<EOF
+parm 4zzw.pdb
+loadcrd 4zzw.pdb name MyCrd
+
+prepareforleap \
+  crdset MyCrd \
+  name Final \
+  out leap.4zzw.in \
+  leapunitname m \
+  pdbout 4zzw.cpptraj.pdb \
+  nowat noh keepaltloc highestocc
+EOF
+RunCpptraj "Prepare PDB 4zzw for LEaP"
+DoTest leap.4zzw.in.save leap.4zzw.in
+DoTest 4zzw.cpptraj.pdb.save 4zzw.cpptraj.pdb
 
 EndTest
