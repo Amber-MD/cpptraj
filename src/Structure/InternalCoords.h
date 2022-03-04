d887aae5575a01e082f21e767f787eb4b73c69c6
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-03-04 14:05:33 -0500

    Start InternalCoords class

---
+++ src/Structure/InternalCoords.h
@@ -0,0 +1,27 @@
+#ifndef INC_STRUCTURE_INTERNALCOORDS_H
+#define INC_STRUCTURE_INTERNALCOORDS_H
+namespace Cpptraj {
+namespace Structure {
+/// Hold internal coordinates for an atom
+/** Given that this is atom i connected to atoms j, k, and l:
+  *   k - j
+  *  /     \
+  * l       i
+  * hold the following:
+  *   distance j - i
+  *   angle    k - j - i
+  *   torsion  l - k - j - i
+  */
+class InternalCoords {
+  public:
+    /// CONSTRUCTOR
+    InternalCoords();
+
+    enum IdxType { DISTANCE = 0, ANGLE, TORSION };
+  private:
+    int idx_[3];    ///< Atom index for distance, angle, torsion
+    double val_[3]; ///< Value for distance, angle, torsion
+};
+}
+}
+#endif
