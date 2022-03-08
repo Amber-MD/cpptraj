ec32a4c8e6d3d7732f25cab9a12c2dbd07db5147
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-03-08 08:37:54 -0500

    Start adding separate cmake build system

---
+++ cmake-cpptraj/BuildSystemInit.cmake
@@ -0,0 +1,11 @@
+# print header
+# --------------------------------------------------------------------
+message(STATUS "**************************************************************************")
+message(STATUS "Starting configuration of ${PROJECT_NAME} version ${${PROJECT_NAME}_VERSION}...")
+
+# configure cmake module path
+# --------------------------------------------------------------------
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+# Basic Utilities
+include (Shorthand)
