40703b0d85fbbbe3700ec20eac2c5360f092fc3e
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-03-08 09:18:05 -0500

    Start adding 3rd party tools

--- cmake-cpptraj/BuildSystemInit.cmake
+++ cmake-cpptraj/BuildSystemInit.cmake
@@ -5,9 +5,15 @@ message(STATUS "Starting configuration of ${PROJECT_NAME} version ${${PROJECT_NA
 
 # configure cmake module path
 # --------------------------------------------------------------------
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+list(
+  APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}
+                          "${CMAKE_CURRENT_LIST_DIR}/ThirdPartyTools"
+    )
 
 # Basic Utilities
+# These files CANNOT use any sort of compile checks or system introspection
+#  because no languages are enabled yet.
+include(Policies NO_POLICY_SCOPE)
 include(Utils)
 include(Shorthand)
 include(ColorMessage)
--- cmake-cpptraj/BuildSystemSetup.cmake
+++ cmake-cpptraj/BuildSystemSetup.cmake
@@ -1,4 +1,8 @@
 # Parts of the build system that happen after enable_language()
 
+include(CheckLinkerFlag)
+include(TryLinkLibrary)
+
 include(BuildReport)
 include(CopyTarget)
+include(LibraryUtils)
