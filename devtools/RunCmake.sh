6c99a73b242cd61fb36e8966ace29a0dae8160e5
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-03-10 09:08:23 -0500

    Add nofftw3 and nocolor options

--- devtools/RunCmake.sh
+++ devtools/RunCmake.sh
@@ -3,6 +3,13 @@
 # Can be used to test the cmake install.
 # Assumes 'git submodule update --init --recursive' has been run in top dir.
 
+WORKDIR=`pwd`
+# For safety
+if [ "`basename $WORKDIR`" != 'build' ] ; then
+  echo "Please run from a 'build' directory"
+  exit 1
+fi
+
 if [ -z "$CPPTRAJHOME" ] ; then
   echo "CPPTRAJHOME must be set."
   exit 1
@@ -17,12 +24,16 @@ fi
 CUDA=0
 OPENMP=0
 MPI=0
+FFT=1
+COLOR=1
 while [ ! -z "$1" ] ; do
   case "$1" in
     'cuda' ) CUDA=1 ;;
     'openmp' ) OPENMP=1 ;;
     'mpi' ) MPI=1 ;;
     'all' ) CUDA=1 ; OPENMP=1 ; MPI=1 ;;
+    'nofftw3') FFT=0 ;;
+    'nocolor') COLOR=0 ;;
   esac
   shift
 done
@@ -39,8 +50,14 @@ if [ $MPI -eq 1 ] ; then
   export BUILD_FLAGS="-DPnetCDF_LIBRARY=$PNETCDF_HOME/lib/libpnetcdf.a ${BUILD_FLAGS}"
   export BUILD_FLAGS="-DPnetCDF_INCLUDE_DIR=$PNETCDF_HOME/include ${BUILD_FLAGS}"
 fi
+if [ $FFT -eq 0 ] ; then
+  export BUILD_FLAGS="-DUSE_FFT=FALSE ${BUILD_FLAGS}"
+fi
+if [ $COLOR -eq 0 ] ; then
+  export BUILD_FLAGS="-DCOLOR_CMAKE_MESSAGES=FALSE ${BUILD_FLAGS}"
+fi
 #-DNetCDF_LIBRARIES_C=$HOME/lib/libnetcdf.so -DNetCDF_INCLUDES=$HOME/include
-cmake .. -DCOLOR_CMAKE_MESSAGES=FALSE \
+cmake .. \
          $BUILD_FLAGS -DCOMPILER=${COMPILER^^} -DINSTALL_HEADERS=FALSE \
          -DCMAKE_INSTALL_PREFIX=$installdir -DCMAKE_LIBRARY_PATH=$HOME/lib \
          -DPRINT_PACKAGING_REPORT=TRUE 
