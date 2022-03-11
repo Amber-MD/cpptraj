e3b54119fa127952371df362d4019992e70d0692
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-03-11 12:18:28 -0500

    Revamp the Cmake build system (#955)
    * Get rid of libcpptraj_mpi target, not needed.
    
    * Start adding separate cmake build system
    
    * More updates. Mostly taking from the Amber build system
    
    * Finish initial pass at adapting Amber build system for cpptraj
    standalone
    
    * Start adding 3rd party tools
    
    * Add more files
    
    * Finish initial adaptation of the build
    
    * Try to add some debug. Dont look for netcdf fortran
    
    * Ensure everything is enabled for testing with cmake
    
    * Turn off color messages to redirect output to file.
    
    * Fix readline and install
    
    * Try to add arpack support
    
    * Start tool for keeping cpptraj cmake system in sync with amber
    
    * Bring Utils.cmake fully in line with Amber version. Add more files to
    cmake update script.
    
    * Finish initial modules
    
    * Rename since it has been significantly modified from its Amber
    equivalent.
    
    * Finish cmake update tool
    
    * Add some checks from the Amber build system
    
    * Add AmberCompilerVars module
    
    * Try to enable arpack config. Ensure fortran is included if pubfft or
    arpack needs to be built.
    
    * Add missing newline
    
    * Fix building/linking arpack
    
    * Remove unnecessary code
    
    * Always build bundled xdr library as static
    
    * Fix bundled TNG support
    
    * Relocate compiler to the top
    
    * Link tng library to cuda binary if enabled
    
    * Make cpptraj internal cuda routines build into a static library. Add
    logic to ensure the PIC flag is properly set for NVCC.
    
    * Turn off debug info
    
    * Add options to script
    
    * Check differences in CMakeLists.txt files.
    
    * This now works with the built-in cmake build system
    
    * Fix error conditions
    
    * Add nofftw3 and nocolor options
    
    * Add an actual check for C++11 support
    
    * Add CMAKE_CONFIGURATION_TYPES
    
    * Make comment same as in amber build system
    
    * Get actual cpptraj version from Version.h
    
    * No need for list length
    
    * Add intel option
    
    * Get rid of references to old cmake system
    
    * If MKL is enabled and no FFTW, use FFTW from MKL
    
    * Add missing define
    
    * Only add subdirs when not inside amber
    
    * Also check AMBERHOME for prepareforleap files.
    
    * Look in AMBERHOME as well
    
    * Add the BUILDTYPE flag
    
    * Ensure dat dir is installed
    
    * 6.4.2. Revision bump for adding AMBERHOME search to prepareforleap
    
    * Ensure LD_LIBRARY_PATH is set for netcdf
    
    * Add the RPATHConfig module
    
    * Add option to disable arpack support
    
    * Fortran not available with MSVC so disable arpack
    
    * Check that fortran is available before trying to enabled it
    
    * Add missing zlib external setup
    
    * Add autotest option
    
    * Try AUTO to try to get around appveyor flang issue
    
    * Add some missing check_ functions
    
    * Add CheckConstantExists module from Amber
    
    * Add USE_TNG option
    
    * Apparently libz on windows (MSVC) is missing some functionality and TNG cannot
    be built.
    
    * Cmake cant seem to build TNG on windows and honestly I dont want to
    invest more time in figuring out why for this corner case, so disabling
    TNG+CMAKE on windows tests.

--- devtools/RunCmake.sh
+++ devtools/RunCmake.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Can be used to test the cmake install.
-# Assumes 'git submodule update --init --recursive' has been run in top dir.
+
+WORKDIR=`pwd`
+# For safety
+if [ "`basename $WORKDIR`" != 'build' ] ; then
+  echo "Please run from a 'build' directory"
+  exit 1
+fi
 
 if [ -z "$CPPTRAJHOME" ] ; then
   echo "CPPTRAJHOME must be set."
@@ -14,11 +20,62 @@ if [ ! -d "$installdir" ] ; then
   mkdir $installdir
 fi
 
-#export BUILD_FLAGS="-DOPENMP=TRUE"
-#export BUILD_FLAGS="-DMPI=TRUE ${BUILD_FLAGS}"
-#-DNetCDF_LIBRARIES_C=$HOME/lib/libnetcdf.so -DNetCDF_INCLUDES=$HOME/include
+CUDA=0
+OPENMP=0
+MPI=0
+FFT=1
+ARPACK=1
+TNG=1
+COLOR=1
 COMPILER=gnu
-cmake .. $BUILD_FLAGS -DCOMPILER=${COMPILER^^} -DINSTALL_HEADERS=FALSE \
+while [ ! -z "$1" ] ; do
+  case "$1" in
+    'cuda' ) CUDA=1 ;;
+    'openmp' ) OPENMP=1 ;;
+    'mpi' ) MPI=1 ;;
+    'all' ) CUDA=1 ; OPENMP=1 ; MPI=1 ;;
+    'nofftw3') FFT=0 ;;
+    'noarpack') ARPACK=0 ;;
+    'notng' ) TNG=0 ;;
+    'nocolor') COLOR=0 ;;
+    'intel' ) COMPILER=intel ;;
+    'autotest' )
+      FFT=0
+      ARPACK=0
+      COMPILER='AUTO'
+      #export BUILD_FLAGS="-DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc ${BUILD_FLAGS}"
+      #export BUILD_FLAGS="-DCMAKE_Fortran_COMPILER=ifort ${BUILD_FLAGS}"
+    ;;
+  esac
+  shift
+done
+
+if [ $CUDA -eq 1 ] ; then
+  export BUILD_FLAGS="-DCUDA=TRUE ${BUILD_FLAGS}"
+fi
+if [ $OPENMP -eq 1 ] ; then
+  export BUILD_FLAGS="-DOPENMP=TRUE ${BUILD_FLAGS}"
+fi
+if [ $MPI -eq 1 ] ; then
+  export BUILD_FLAGS="-DMPI=TRUE ${BUILD_FLAGS}"
+  export BUILD_FLAGS="-DPnetCDF_LIBRARY=$PNETCDF_HOME/lib/libpnetcdf.a ${BUILD_FLAGS}"
+  export BUILD_FLAGS="-DPnetCDF_INCLUDE_DIR=$PNETCDF_HOME/include ${BUILD_FLAGS}"
+fi
+if [ $FFT -eq 0 ] ; then
+  export BUILD_FLAGS="-DUSE_FFT=FALSE ${BUILD_FLAGS}"
+fi
+if [ $ARPACK -eq 0 ] ; then
+  export BUILD_FLAGS="-DUSE_ARPACK=FALSE ${BUILD_FLAGS}"
+fi
+if [ $TNG -eq 0 ] ; then
+  export BUILD_FLAGS="-DUSE_TNG=FALSE ${BUILD_FLAGS}"
+fi
+if [ $COLOR -eq 0 ] ; then
+  export BUILD_FLAGS="-DCOLOR_CMAKE_MESSAGES=FALSE ${BUILD_FLAGS}"
+fi
+#-DNetCDF_LIBRARIES_C=$HOME/lib/libnetcdf.so -DNetCDF_INCLUDES=$HOME/include
+cmake .. \
+         $BUILD_FLAGS -DCOMPILER=${COMPILER^^} -DINSTALL_HEADERS=FALSE \
          -DCMAKE_INSTALL_PREFIX=$installdir -DCMAKE_LIBRARY_PATH=$HOME/lib \
          -DPRINT_PACKAGING_REPORT=TRUE 
 
