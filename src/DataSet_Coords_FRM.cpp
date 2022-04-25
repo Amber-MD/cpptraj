405b748b9f4c2f8b137222ddac5569b59d58f716
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-04-25 09:30:11 -0400

    Finish and enable the set

--- src/DataSet_Coords_FRM.cpp
+++ src/DataSet_Coords_FRM.cpp
@@ -2,11 +2,38 @@
 #include "CpptrajStdio.h"
 
 /** CONSTRUCTOR */
-DataSet_Coords_FRM::DataSet_Coords_FRM() {}
+DataSet_Coords_FRM::DataSet_Coords_FRM() :
+  DataSet_Coords(FRAMES)
+{}
 
 // -----------------------------------------------
 #ifdef MPI
-// FIXME Sync
+/** Sync all frames to the master process. */
+int DataSet_Coords_FRM::Sync(size_t total, std::vector<int> const& rank_frames,
+                             Parallel::Comm const& commIn)
+{
+  if (commIn.Size()==1) return 0;
+  if (commIn.Master()) {
+    // Resize for total number of frames.
+    frames_.resize( total, AllocateFrame() );
+    int cidx = rank_frames[0]; // Frame Index on master
+    // Receive data from each rank
+    for (int rank = 1; rank < commIn.Size(); rank++) {
+      for (int idx = 0; idx < rank_frames[rank]; idx++) {
+        //rprintf("DEBUG: MASTER RECEIVE from rank %i cidx=%i rank_frames=%i FrameSize=%u\n",
+        //        rank, cidx, rank_frames[rank], frames_.FrameSize());
+        frames_[cidx++].RecvFrame( rank, commIn );
+      }
+    }
+  } else {// Send data to master
+    for (unsigned int idx = 0; idx < Size(); idx++) {
+      //rprintf("DEBUG: RANK %i SEND TO MASTER Size=%zu FrameSize=%u\n",
+      //        commIn.Rank(), Size(), frames_.FrameSize());
+      frames_[idx].SendFrame(0, commIn);
+    }
+  }
+  return 0;
+}
 #endif
 
 /** Add a single element. */
@@ -69,5 +96,45 @@ void DataSet_Coords_FRM::CopyBlock(size_t startIdx, DataSet const* dptrIn, size_
 }
 
 // -----------------------------------------------
+/** Set up COORDS with given Topology and coordinate info. */
+int DataSet_Coords_FRM::CoordsSetup(Topology const& topIn, CoordinateInfo const& cInfoIn) {
+  top_ = topIn;
+  cInfo_ = cInfoIn;
 
+  //if (frames_.SetupFrameArray(cInfo_, topIn.Natom(), framesToReserve_)) {
+  //  mprinterr("Internal Error: Could not set up CompactFrameArray for '%s'\n", legend());
+  //  return 1;
+  //}
 
+  return 0;
+}
+
+/** Add frame to array. */
+void DataSet_Coords_FRM::AddFrame(Frame const& fIn) {
+  if (fIn.Natom() != top_.Natom()) {
+    mprintf("Warning: DataSet_Coords_FRM::AddFrame: Incoming frame has %i atoms but topology '%s' has %i\n",
+            fIn.Natom(), top_.c_str(), top_.Natom());
+  }
+  frames_.push_back( fIn );
+}
+
+/** Copy frame to specified position in array. */
+void DataSet_Coords_FRM::SetCRD(int idx, Frame const& fIn) {
+  if (fIn.Natom() != top_.Natom()) {
+    mprintf("Warning: DataSet_Coords_FRM::SetCrd: Incoming frame has %i atoms but topology '%s' has %i\n",
+            fIn.Natom(), top_.c_str(), top_.Natom());
+  }
+  if (idx > (int)frames_.size()) // TODO fix this sign issue
+    frames_.resize( idx );
+  frames_[idx] = fIn; 
+}
+
+/** Get a frame from specified position in array. */
+void DataSet_Coords_FRM::GetFrame(int idx, Frame& fOut) {
+  fOut.SetFrame( frames_[idx] );
+}
+
+/** Get selected atoms from a frame from specified position in array. */
+void DataSet_Coords_FRM::GetFrame(int idx, Frame& fOut, AtomMask const& mask) {
+  fOut.SetFrame( frames_[idx], mask );
+}
