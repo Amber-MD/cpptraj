f732450fa605dc3482451e0d83643c45c2a36b4a
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-05-13 10:51:06 -0400

    Test bycrdidx keyword

--- test/Test_Ensemble_HREMD/RunTest.sh
+++ test/Test_Ensemble_HREMD/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles cpptraj.in sorted.remlog.crd.?
+CleanFiles cpptraj.in sorted.remlog.crd.? sorted.crdidx.crd.?
 
 TESTNAME='H-REMD sorting tests'
 Requires netcdf
@@ -20,5 +20,17 @@ DoTest sorted.remlog.crd.0.save sorted.remlog.crd.0
 DoTest sorted.remlog.crd.1.save sorted.remlog.crd.1
 DoTest sorted.remlog.crd.2.save sorted.remlog.crd.2
 DoTest sorted.remlog.crd.3.save sorted.remlog.crd.3
+
+UNITNAME='Sort by crdidx from trajectory'
+cat > cpptraj.in <<EOF
+parm ../tz2.nhe.parm7
+ensemble rem.crd.001 bycrdidx 
+trajout sorted.crdidx.crd
+EOF
+RunCpptraj "$UNITNAME"
+DoTest sorted.remlog.crd.0.save sorted.crdidx.crd.0
+DoTest sorted.remlog.crd.1.save sorted.crdidx.crd.1
+DoTest sorted.remlog.crd.2.save sorted.crdidx.crd.2
+DoTest sorted.remlog.crd.3.save sorted.crdidx.crd.3
 
 EndTest
