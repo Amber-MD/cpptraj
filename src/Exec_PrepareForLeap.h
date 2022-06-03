54f1cc0fa20fe190d3226806f04bab7af0d19973
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-06-03 11:14:55 -0400

    Remove Sugar class from prepareforleap

--- src/Exec_PrepareForLeap.h
+++ src/Exec_PrepareForLeap.h
@@ -16,8 +16,6 @@ class Exec_PrepareForLeap : public Exec {
     DispatchObject* Alloc() const { return (DispatchObject*)new Exec_PrepareForLeap(); }
     RetType Execute(CpptrajState&, ArgList&);
   private:
-    /// Hold indices for sugar
-    class Sugar;
     /// Hold indices for sugar link atoms
     class Link;
     /// Hold information for the various functional group types (FunctionalGroupType)
@@ -38,11 +36,7 @@ class Exec_PrepareForLeap : public Exec {
                        SUGAR_SETUP_FAILED };
     typedef std::vector<ResStatType> ResStatArray;
 
-    /// Base ring type
-    enum RingTypeEnum { PYRANOSE = 0,  ///< Ring is 5 carbons, 1 oxygen
-                        FURANOSE,      ///< Ring is 4 carbons, 1 oxygen
-                        UNKNOWN_RING   ///< Some unknown ring type
-                  };
+    
     /// Sugar form
     enum FormTypeEnum { ALPHA = 0, BETA, UNKNOWN_FORM };
     /// Sugar chirality
@@ -50,9 +44,7 @@ class Exec_PrepareForLeap : public Exec {
 
     /// Keep synced with FunctionalGroupType
     static const char* FunctionalGroupStr_[];
-    /// Keep synced with RingTypeEnum
-    static const char* ringstr_[];
-    /// Keep synced with FormTypeEnum
+        /// Keep synced with FormTypeEnum
     static const char* formstr_[];
     /// Keep synced with ChirTypeEnum
     static const char* chirstr_[];
@@ -176,63 +168,6 @@ class Exec_PrepareForLeap : public Exec {
     std::string solventResName_; ///< Solvent residue name
     AtomMap myMap_;
     std::vector<FunctionalGroup> functionalGroups_; ///< Recognized functional groups (FunctionalGroupType).
-};
-// ----- Sugar class ----------------------------------------------------------
-class Exec_PrepareForLeap::Sugar {
-  public:
-
-    /// Sugar status. Keep synced with StatTypeStr_
-    enum StatType { SETUP_OK = 0,    ///< Regular sugar, setup complete
-                    MISSING_O,       ///< Could not find ring oxygen
-                    MULTIPLE_O,      ///< Multiple potential ring oxygen atoms
-                    MISSING_CHAIN,   ///< Could not find all chain carbons
-                    MISSING_ANO_REF, ///< Missing anomeric reference atom
-                    MISSING_CONFIG,  ///< Missing configurational carbon
-                    MISSING_C1X      ///< Missing C1 X substituent
-                  };
-    /// CONSTRUCTOR - Status and residue first atom, incomplete setup
-    Sugar(StatType, int);
-    /// CONSTRUCTOR - Status, ring O, anomeric, ring atoms, chain atoms, incomplete setup
-    Sugar(StatType, int, int, Iarray const&,Iarray const&);
-    /// CONSTRUCTOR - ring O, Anomeric, Anomeric Ref, Highest Sterocenter, ring atoms, chain atoms, isMissingAtoms
-    Sugar(int,int,int,int,Iarray const&,Iarray const&, bool);
-
-    inline int ResNum(Topology const&) const;
-    StatType Status()          const { return stat_; }
-    int RingOxygenAtom()       const { return ring_oxygen_atom_; }
-    int AnomericAtom()         const { return anomeric_atom_; }
-    int AnomericRefAtom()      const { return ano_ref_atom_; }
-    int HighestStereocenter()  const { return highest_stereocenter_; }
-    RingTypeEnum RingType()    const { return ringType_; }
-    bool IsMissingAtoms()      const { return isMissingAtoms_; }
-    Iarray const& RingAtoms()  const { return ring_atoms_; }
-    int RingEndAtom()          const { return ring_atoms_.back(); }
-    Iarray const& ChainAtoms() const { return chain_atoms_; }
-//    typedef std::vector<int>::const_iterator const_iterator;
-//    const_iterator ringbegin() const { return ring_atoms_.begin(); }
-//    const_iterator ringend()   const { return ring_atoms_.end(); }
-
-    bool NotSet() const { return (stat_ != SETUP_OK); }
-    /// \return Number of ring atoms
-    unsigned int NumRingAtoms() const;
-    void PrintInfo(Topology const&) const;
-    /// Remap internal indices according to given atom map.
-    void RemapIndices(Iarray const&, int, int);
-
-    void SetStatus(StatType s) { stat_ = s; }
-  private:
-    /// Strings corresponding to StatType
-    static const char* StatTypeStr_[];
-
-    StatType stat_;            ///< Setup status
-    int ring_oxygen_atom_;     ///< Index of the ring oxygen atom
-    int anomeric_atom_;        ///< Index of the anomeric C atom (ring start)
-    int ano_ref_atom_;         ///< Index of the anomeric reference C atom
-    int highest_stereocenter_; ///< Index of the highest stereocenter in the carbon chain
-    RingTypeEnum ringType_;    ///< Will be set to ring type
-    bool isMissingAtoms_;      ///< True if original PDB indicates sugar has missing atoms.
-    Iarray ring_atoms_;        ///< Index of all non-oxygen ring atoms
-    Iarray chain_atoms_;       ///< Index of all chain carbon atoms (from anomeric carbon).
 };
 // ----- Link class ------------------------------------------------------------
 class Exec_PrepareForLeap::Link {
