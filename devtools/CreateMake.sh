9dbcfeae57c24fd0cf800e39c4067a36a67fd9ef
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-06-03 10:48:12 -0400

    Add Makefile creation

--- devtools/CreateMake.sh
+++ devtools/CreateMake.sh
@@ -13,4 +13,45 @@ if [ "$BASENAME" != 'src' ] ; then
   exit 1
 fi
 
+DIR=`basename $WORKDIR`
+name=`echo "$DIR" | awk '{print tolower($0)}'`
+upper=`echo "$DIR" | awk '{print toupper($0)}'`
+echo "Dir= $DIR, name= $name"
 
+SOURCEFILE="$name"files
+DEPENDFILE="$name"depend
+VARNAME="$upper"_SOURCES
+
+echo "File containing Makefile sources: $SOURCEFILE"
+echo "File containing Makefile depends: $DEPENDFILE"
+
+#if [ ! -f 'Makefile' ] ; then
+  # Create the Makefile
+  cat > Makefile <<EOF
+# $DIR Makefile
+include ../../config.h
+
+include $SOURCEFILE
+
+DEL_FILE = /bin/rm -f
+
+# Objects
+OBJECTS=\$($VARNAME:.cpp=.o)
+
+# Default target: objects
+all: \$(OBJECTS)
+
+clean:
+	\$(DEL_FILE) *.o
+
+uninstall: clean
+
+# Dependency targets
+../findDepend:
+	cd ../ && \$(MAKE) findDepend
+
+depend: ../findDepend
+	../findDepend \$($VARNAME) > $DEPENDFILE
+
+include $DEPENDFILE
+EOF
