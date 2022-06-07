9827ee0c9248f5c1ebaf9e2e99d19cd08aac9694
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-06-07 09:23:31 -0400

    Add iterator

--- src/Structure/ResStatArray.h
+++ src/Structure/ResStatArray.h
@@ -6,21 +6,27 @@ namespace Structure {
 class ResStatArray {
   public:
     enum Type { UNKNOWN = 0,
-                       VALIDATED,
-                       SUGAR_UNRECOGNIZED_LINK_RES,
-                       SUGAR_UNRECOGNIZED_LINKAGE,
-                       SUGAR_NO_LINKAGE,
-                       SUGAR_NO_CHAIN_FOR_LINK,
-                       SUGAR_NAME_MISMATCH,
-                       //SUGAR_MISSING_C1X,
-                       SUGAR_SETUP_FAILED };
+                VALIDATED,
+                SUGAR_UNRECOGNIZED_LINK_RES,
+                SUGAR_UNRECOGNIZED_LINKAGE,
+                SUGAR_NO_LINKAGE,
+                SUGAR_NO_CHAIN_FOR_LINK,
+                SUGAR_NAME_MISMATCH,
+                //SUGAR_MISSING_C1X,
+                SUGAR_SETUP_FAILED };
 
-    /// CONSTRUCTOR
-    ResStatArray() {}
+    /// CONSTRUCTOR - take number of residues
+    ResStatArray(int nres) : resStat_(nres, UNKNOWN) {}
 
     Type const& operator[](int idx) const { return resStat_[idx]; }
 
     Type& operator[](int idx) { return resStat_[idx]; }
+
+    typedef std::vector<Type>::iterator iterator;
+    /// \return iterator to beginning
+    iterator begin() { return resStat_.begin(); }
+    /// \return iterator to end
+    iterator end()   { return resStat_.end(); }
   private:
     typedef std::vector<Type> Rarray;
     Rarray resStat_; ///< Status of each residue in a topology
