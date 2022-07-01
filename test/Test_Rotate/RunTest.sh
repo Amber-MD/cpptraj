3827196dcd807d2a3f1e7f700764024688bf17f3
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-07-01 11:20:11 -0400

    For basic rotate test, first rotate each axis separately then combined to make sure the combined rotation is working properly. Center on origin first.

--- test/Test_Rotate/RunTest.sh
+++ test/Test_Rotate/RunTest.sh
@@ -3,7 +3,7 @@
 . ../MasterTest.sh
 
 CleanFiles rotate.in fromMatrices.crd TCS.rotated.mol2 inverse.crd \
-           tz2.rotate.rst7
+           tz2.rotate.rst7 tz2.*.rotate.rst7 tz2.?.rst7
 
 TESTNAME='Coordinates rotation tests'
 Requires maxthreads 10
@@ -44,12 +44,48 @@ CheckFor maxthreads 1
 if [ $? -eq 0 ] ; then
   cat > rotate.in <<EOF
 parm ../tz2.parm7
+
 trajin ../tz2.rst7
+center origin
+outtraj tz2.0.rst7
+rotate * x 30
+outtraj tz2.1.rst7
+rotate * y 45
+outtraj tz2.2.rst7
+rotate * z 60
+trajout tz2.separate.rotate.rst7
+run
+
+clear trajin
+
+trajin ../tz2.rst7
+center origin
+debug actions 1
 rotate * x 30 y 45 z 60
-trajout tz2.rotate.rst7
+trajout tz2.combined.rotate.rst7
+
+#rotate * x 30 y 45 z 60
+#rotate * x -1.75581 y 51.4013 z -63.7329
+#trajout tz2.rotate.rst7
 EOF
   RunCpptraj "$UNITNAME"
-  DoTest tz2.rotate.rst7.save tz2.rotate.rst7
+  DoTest tz2.separate.rotate.rst7.save tz2.separate.rotate.rst7
+  DoTest tz2.separate.rotate.rst7 tz2.combined.rotate.rst7
+  #DoTest tz2.rotate.rst7.save tz2.rotate.rst7
+fi
+
+UNITNAME='Calculate rotations from rotation matrices test'
+CheckFor maxthreads 1
+if [ $? -eq 0 ] ; then
+  cat > rotate.in <<EOF
+parm ../tz2.parm7
+reference ../tz2.rst7 name REF
+crdaction REF center origin
+trajin tz2.separate.rotate.rst7.save
+rms R0 reference savematrices
+rotate calcfrom R0[RM] name Rot
+EOF
+  RunCpptraj "$UNITNAME"
 fi
 
 EndTest
