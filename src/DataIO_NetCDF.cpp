993be9fd6b8691d916ae9d0b640fc425240d8e0f
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-09-01 15:40:09 -0400

    Start generic netcdf data io class

---
+++ src/DataIO_NetCDF.cpp
@@ -0,0 +1,63 @@
+#include "DataIO_NetCDF.h"
+#include "CpptrajStdio.h"
+
+/// CONSTRUCTOR
+DataIO_NetCDF::DataIO_NetCDF()
+{
+
+}
+
+// DataIO_NetCDF::ID_DataFormat()
+bool DataIO_NetCDF::ID_DataFormat(CpptrajFile& infile)
+{
+  if (infile.OpenFile()) return false;
+  // Read first 3 bytes
+  unsigned char magic[3];
+  magic[0] = 0;
+  magic[1] = 0;
+  magic[2] = 0;
+  infile.Read(magic, 3);
+  infile.CloseFile();
+  if (magic[0] == 'C' && magic[1] == 'D' && magic[2] == 'F') return true;
+  return false;
+}
+
+// DataIO_NetCDF::ReadHelp()
+void DataIO_NetCDF::ReadHelp()
+{
+
+}
+
+// DataIO_NetCDF::processReadArgs()
+int DataIO_NetCDF::processReadArgs(ArgList& argIn)
+{
+
+  return 0;
+}
+
+// DataIO_NetCDF::ReadData()
+int DataIO_NetCDF::ReadData(FileName const& fname, DataSetList& dsl, std::string const& dsname)
+{
+
+  return 1;
+}
+
+// DataIO_NetCDF::WriteHelp()
+void DataIO_NetCDF::WriteHelp()
+{
+
+}
+
+// DataIO_NetCDF::processWriteArgs()
+int DataIO_NetCDF::processWriteArgs(ArgList& argIn)
+{
+
+  return 0;
+}
+
+// DataIO_NetCDF::WriteData()
+int DataIO_NetCDF::WriteData(FileName const& fname, DataSetList const& dsl)
+{
+
+  return 1;
+}
---
+++ src/DataIO_NetCDF.h
@@ -0,0 +1,17 @@
+#ifndef INC_DATAIO_NETCDF_H
+#define INC_DATAIO_NETCDF_H
+#include "DataIO.h"
+/// Generic NetCDF DataSet 
+class DataIO_NetCDF : public DataIO {
+  public:
+    DataIO_NetCDF();
+    static void ReadHelp();
+    static void WriteHelp();
+    static BaseIOtype* Alloc() { return (BaseIOtype*)new DataIO_NetCDF(); }
+    int processReadArgs(ArgList&);
+    int ReadData(FileName const&, DataSetList&, std::string const&);
+    int processWriteArgs(ArgList&);
+    int WriteData(FileName const&, DataSetList const&);
+    bool ID_DataFormat(CpptrajFile&);
+};
+#endif
