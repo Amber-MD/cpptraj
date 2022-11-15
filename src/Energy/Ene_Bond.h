123fd66617114ba3f09edafe0482830c664bb157
Daniel R. Roe <daniel.roe@nih.gov>
2022-11-15 14:23:40 -0500

    Add bond energy template

---
+++ src/Energy/Ene_Bond.h
@@ -0,0 +1,20 @@
+#ifndef INC_ENERGY_ENE_BOND_H
+#define INC_ENERGY_ENE_BOND_H
+namespace Cpptraj {
+namespace Energy {
+/// \return Energy of the bond between xyz1 and xyz2
+template <typename T>
+T Ene_Bond(T const* xyz1, T const* xyz2, T const& req, T const& rk)
+{
+  T x = xyz1[0] - xyz2[0];
+  T y = xyz1[1] - xyz2[1];
+  T z = xyz1[2] - xyz2[2];
+  T r2 = (x*x + y*y + z*z);
+  T r = sqrt(r2);
+  T rdiff = r - req;
+  T ene = rk * (rdiff * rdiff);
+  return ene;
+}
+}
+}
+#endif
