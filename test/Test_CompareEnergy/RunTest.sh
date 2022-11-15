0640919d502241a332742453577bf3cc565ba070
Daniel R. Roe <daniel.roe@nih.gov>
2022-11-15 14:42:16 -0500

    Test angles and dihedrals

--- test/Test_CompareEnergy/RunTest.sh
+++ test/Test_CompareEnergy/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles compare.in compare.bonds.dat
+CleanFiles compare.in compare.bonds.dat compare.angles.dat compare.dih.dat
 
 TESTNAME='Compare energy tests'
 Requires maxthreads 1
@@ -17,7 +17,12 @@ loadcrd ../tz2.nc 1 1 name CRD1
 loadcrd ../tz2.nc 2 2 name CRD2
 
 compareenergy crd0 CRD1 crd1 CRD2 bondout compare.bonds.dat mask1 :2 mask2 :2
+compareenergy crd0 CRD1 crd1 CRD2 angleout compare.angles.dat mask1 :2 mask2 :2 mask3 :2
+compareenergy crd0 CRD1 crd1 CRD2 dihedralout compare.dih.dat mask1 :2 mask2 :2 mask3 :2 mask4 :2
 EOF
 RunCpptraj "$TESTNAME"
+DoTest compare.bonds.dat.save compare.bonds.dat
+DoTest compare.angles.dat.save compare.angles.dat
+DoTest compare.dih.dat.save compare.dih.dat
 
 EndTest
