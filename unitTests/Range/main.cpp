b7c1385ea712dcc8f6082c7a78eb55ef4e4ec8f6
Daniel R. Roe <daniel.roe@nih.gov>
2022-12-13 09:33:22 -0500

    Start Range unit test

---
+++ unitTests/Range/main.cpp
@@ -0,0 +1,14 @@
+// Unit test for Range class
+#include <cstdio>
+#include "Range.h"
+
+static const int Err(const char* msg) {
+  fprintf(stderr, "Error: %s\n", msg);
+  return 1;
+}
+
+int main() {
+  Range myRange; 
+
+  return 0;
+}
