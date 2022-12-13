a76e3aa0e7cafa81843c16c1a1bde80d86ac1e39
Daniel R. Roe <daniel.roe@nih.gov>
2022-12-13 09:41:10 -0500

    Implement range test

--- unitTests/Range/main.cpp
+++ unitTests/Range/main.cpp
@@ -8,7 +8,20 @@ static const int Err(const char* msg) {
 }
 
 int main() {
-  Range myRange; 
+  Range myRange;
+  if (myRange.SetRange("3-6,9,12-14")) return Err("SetRange failed.");
+  myRange.PrintRange("myRange + 1", 1);
+  printf("\n");
+  printf("Range arg: '%s'\n", myRange.RangeArg());
+  Range::const_iterator it = myRange.begin();
+  for (int i = 3; i < 7; i++, ++it) {
+    if (*it != i) return Err("Iterator range 3-6 does not match.\n");
+  }
+  if (*it != 9) return Err("Iterator 9 does not match.\n");
+  ++it;
+  for (int i = 12; i < 15; i++, ++it) {
+    if (*it != i) return Err("Iterator range 12-14 does not match.\n");
+  }
 
   return 0;
 }
