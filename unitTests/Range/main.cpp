05a6261ab96ba010286aa38da41da5c7535f2325
Daniel R. Roe <daniel.roe@nih.gov>
2022-12-13 10:00:18 -0500

    Add range test with duplicate numbers in the range expression

--- unitTests/Range/main.cpp
+++ unitTests/Range/main.cpp
@@ -7,21 +7,34 @@ static const int Err(const char* msg) {
   return 1;
 }
 
+int TestRange(Range const& myRange) {
+  Range::const_iterator it = myRange.begin();
+  for (int i = 3; i < 7; i++, ++it) {
+    if (*it != i) return Err("Iterator range 3-6 does not match.");
+  }
+  if (*it != 9) return Err("Iterator 9 does not match.");
+  ++it;
+  for (int i = 12; i < 15; i++, ++it) {
+    if (*it != i) return Err("Iterator range 12-14 does not match.");
+  }
+  return 0;
+}
+
 int main() {
   Range myRange;
   if (myRange.SetRange("3-6,9,12-14")) return Err("SetRange failed.");
+  myRange.PrintToStdout();
+  if (myRange.Size() != 8) return Err("myRange size is not 8.");
   myRange.PrintRange("myRange + 1", 1);
   printf("\n");
   printf("Range arg: '%s'\n", myRange.RangeArg());
-  Range::const_iterator it = myRange.begin();
-  for (int i = 3; i < 7; i++, ++it) {
-    if (*it != i) return Err("Iterator range 3-6 does not match.\n");
-  }
-  if (*it != 9) return Err("Iterator 9 does not match.\n");
-  ++it;
-  for (int i = 12; i < 15; i++, ++it) {
-    if (*it != i) return Err("Iterator range 12-14 does not match.\n");
-  }
+  if (TestRange(myRange)) return 1;
+
+  // Test range with duplicates
+  myRange.Clear();
+  if (myRange.SetRange("5,12,9,3-6,9,12-14")) return Err("SetRange duplicate failed.");
+  myRange.PrintToStdout();
+  if (myRange.Size() != 8) return Err("myRange duplicate size is not 8.");
 
   return 0;
 }
