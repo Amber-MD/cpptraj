ac044e7a59397f1cb05cda31283fc16d9a1ee4f7
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-11 12:22:51 -0400

    Add average trunc oct test

--- test/Test_AvgBox/RunTest.sh
+++ test/Test_AvgBox/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles box.in box.ortho.dat
+CleanFiles box.in box.ortho.dat box.truncoct.dat
 
 INPUT='-i box.in'
 
@@ -14,8 +14,16 @@ parm ../tz2.ortho.parm7
 trajin ../tz2.ortho.nc
 avgbox MyBox out box.ortho.dat
 EOF
-RunCpptraj "Average box test"
+RunCpptraj "Average box test, orthogonal"
 DoTest box.ortho.dat.save box.ortho.dat
+
+cat > box.in <<EOF
+parm ../tz2.truncoct.parm7
+trajin ../tz2.truncoct.nc
+avgbox MyBox out box.truncoct.dat
+EOF
+RunCpptraj "Average box test, truncated octahedron"
+DoTest box.truncoct.dat.save box.truncoct.dat
 
 EndTest
 exit 0
