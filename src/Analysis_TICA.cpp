59eec9034545568694d1d6533a1583e2c85ace60
Daniel R. Roe <daniel.roe@nih.gov>
2023-05-22 13:04:24 -0400

    Calc vector quantities

--- src/Analysis_TICA.cpp
+++ src/Analysis_TICA.cpp
@@ -58,13 +58,24 @@ Analysis::RetType Analysis_TICA::Analyze() {
     mprinterr("Error: No atoms selected by mask '%s'\n", mask1_.MaskString());
     return Analysis::ERR;
   }
-  // Allocate frame
-  Frame frame0;
-  frame0.SetupFrameFromMask( mask1_, TgtTraj_->Top().Atoms(), TgtTraj_->CoordsInfo() );
+  // Allocate frames
+  Frame coords0;
+  coords0.SetupFrameFromMask( mask1_, TgtTraj_->Top().Atoms(), TgtTraj_->CoordsInfo() );
+  Frame coords1 = coords0;
+  // Diagonal vectors
+  typedef std::vector<Vec3> Varray;
+  Varray vect(mask1_.Nselected(), Vec3(0.0));
+  Varray vect2(mask1_.Nselected(), Vec3(0.0));
   // Loop over frames
   for (unsigned int frm0 = 0; frm0 < Nframes; frm0++) {
     mprintf("DEBUG: Frame %i\n", frm0);
-    TgtTraj_->GetFrame(frm0, frame0, mask1_);
+    TgtTraj_->GetFrame(frm0, coords0, mask1_);
+    // Covariance
+    for (int idx2 = 0; idx2 < mask1_.Nselected(); idx2++) {
+      Vec3 XYZj( coords0.XYZ(mask1_[idx2]) );
+      vect[idx2] += XYZj;
+      vect2[idx2] += XYZj.Squared();
+    } // END outer loop over idx2
   }
   return Analysis::OK;
 }
