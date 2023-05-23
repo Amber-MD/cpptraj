c2233bd0129db9933edf945243f322adcfc281b5
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-23 13:54:58 -0400

    Add mass

--- test/Test_TICA/RunTest.sh
+++ test/Test_TICA/RunTest.sh
@@ -10,9 +10,13 @@ cat > tica.in <<EOF
 parm ../tz2.parm7
 loadcrd ../tz2.crd name TZ2
 
-crdaction TZ2 matrix name M1 covar out M1.dat @CA
+set MASK = :1-3@CA
 
-runanalysis tica crdset TZ2 mask @CA lag 1
+crdaction TZ2 matrix name M1 mwcovar out M1.dat \$MASK
+
+runanalysis tica crdset TZ2 mask \$MASK lag 1 mass
+
+list dataset
 EOF
 RunCpptraj "TICA test."
 
