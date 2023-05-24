0f031324d36210f178abf68ccdaae16756437509
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-24 10:15:44 -0400

    Add tordiff test

---
+++ test/Test_TorDiff/RunTest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles tordiff.in frc.dat tor.dat
+
+TESTNAME='Toroidal-view-preserving diffusion calculation tests'
+Requires netcdf
+
+INPUT='-i tordiff.in'
+
+UNITNAME='Basic toroidal-view-preserving diffusion calculation test'
+cat > tordiff.in <<EOF
+parm ../tz2.ortho.parm7
+trajin ../tz2.ortho.nc
+
+diffusion FRC :WAT@O out frc.dat
+tordiff TOR :WAT@O out tor.dat
+EOF
+RunCpptraj "$UNITNAME"
+DoTest tor.dat.save tor.dat
+
+EndTest
+exit 0
