7ed15737f6d59b2b7b4da45ca3d36542e76532ff
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-24 12:56:24 -0400

    Add Info and Init routines

--- src/DiffusionResults.cpp
+++ src/DiffusionResults.cpp
@@ -2,6 +2,9 @@
 #include "CpptrajStdio.h"
 #include "DataSet.h"
 #include "DataSet_1D.h"
+#include "DataSetList.h"
+#include "DataFileList.h"
+#include "ArgList.h"
 
 using namespace Cpptraj;
 /** CONSTRUCTOR */
@@ -14,6 +17,56 @@ DiffusionResults::DiffusionResults() :
   diffout_(0)
 {}
 
+/** Process arguments and create sets. */
+int DiffusionResults::InitDiffusionResults(DataSetList& DSL, DataFileList& DFL,
+                                           ArgList& actionArgs, std::string const& dsname_)
+{
+  diffout_ = DFL.AddDataFile(actionArgs.GetStringKey("diffout"));
+  MetaData::tsType ts = MetaData::NOT_TS;
+  diffConst_ = DSL.AddSet(DataSet::DOUBLE, MetaData(dsname_, "D", ts));
+  diffLabel_ = DSL.AddSet(DataSet::STRING, MetaData(dsname_, "Label", ts));
+  diffSlope_ = DSL.AddSet(DataSet::DOUBLE, MetaData(dsname_, "Slope", ts));
+  diffInter_ = DSL.AddSet(DataSet::DOUBLE, MetaData(dsname_, "Intercept", ts));
+  diffCorrl_ = DSL.AddSet(DataSet::DOUBLE, MetaData(dsname_, "Corr", ts));
+  if (diffConst_ == 0 || diffLabel_ == 0 || diffSlope_ == 0 || diffInter_ == 0 ||
+      diffCorrl_ == 0)
+  {
+    mprinterr("Error: Could not allocate 1 or more diffusion constant sets.\n");
+    return 1;
+  }
+# ifdef MPI
+  // No sync needed since these are not time series
+  diffConst_->SetNeedsSync( false );
+  diffLabel_->SetNeedsSync( false );
+  diffSlope_->SetNeedsSync( false );
+  diffInter_->SetNeedsSync( false );
+  diffCorrl_->SetNeedsSync( false );
+# endif
+  if (diffout_ != 0) {
+    diffout_->AddDataSet( diffConst_ );
+    diffout_->AddDataSet( diffSlope_ );
+    diffout_->AddDataSet( diffInter_ );
+    diffout_->AddDataSet( diffCorrl_ );
+    diffout_->AddDataSet( diffLabel_ );
+  }
+  Dimension Ddim( 1, 1, "Set" );
+  diffConst_->SetDim(Dimension::X, Ddim);
+  diffLabel_->SetDim(Dimension::X, Ddim);
+  diffSlope_->SetDim(Dimension::X, Ddim);
+  diffInter_->SetDim(Dimension::X, Ddim);
+  diffCorrl_->SetDim(Dimension::X, Ddim);
+  return 0;
+}
+
+/** Print info to stdout. */
+void DiffusionResults::Info() const {
+  if (diffout_ != 0)
+    mprintf("\tDiffusion constant output to '%s'\n", diffout_->DataFilename().full());
+  else
+    mprintf("\tDiffusion constant output to STDOUT.\n");
+}
+
+/** Calculate diffusion constant from slope of MSD vs time data. */
 void DiffusionResults::CalcDiffusionConst(unsigned int& set, DataSet* ds, int Ndim,
                                           std::string const& label) const
 { // TODO check 1D
--- src/DiffusionResults.h
+++ src/DiffusionResults.h
@@ -4,11 +4,18 @@
 // Fwd declare
 class DataSet;
 class DataFile;
+class DataSetList;
+class DataFileList;
+class ArgList;
 namespace Cpptraj {
 /// Calculate and hold calculated diffusion constants from MSD data
 class DiffusionResults {
   public:
     DiffusionResults();
+
+    int InitDiffusionResults(DataSetList&, DataFileList&, ArgList& actionArgs, std::string const&);
+
+    void Info() const;
 
     void CalcDiffusionConst(unsigned int&, DataSet*, int, std::string const&) const;
   private:
