a4fd99cce9bc41140d62b2d6d720597e865f47ef
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-24 08:47:00 -0400

    Start toroidal diffusion action

---
+++ src/Action_ToroidalDiffusion.cpp
@@ -0,0 +1,90 @@
+#include "Action_ToroidalDiffusion.h"
+#include "CpptrajStdio.h"
+
+// Action_ToroidalDiffusion::Help()
+void Action_ToroidalDiffusion::Help() const {
+  mprintf("\t[mask <mask>]\n");
+}
+
+// Action_ToroidalDiffusion::Init()
+Action::RetType Action_ToroidalDiffusion::Init(ArgList& actionArgs, ActionInit& init, int debugIn)
+{
+  if (mask1_.SetMaskString( actionArgs.GetStringKey("mask") )) {
+    mprinterr("Error: Invalid mask string.\n");
+    return Action::ERR;
+  }
+
+  mprintf("    TORDIFF: Toroidal-view-preserving diffusion calculation.\n");
+  mprintf("\tCalculating diffusion for molecules selected by mask '%s'\n", mask1_.MaskString());
+  mprintf("# Citation: Bullerjahn, von Bulow, Heidari, Henin, and Hummer.\n"
+          "#           \"Unwrapping NPT Simulations to Calculate Diffusion Coefficients.\n"
+          "#           https://arxiv.org/abs/2303.09418\n");
+
+  return Action::OK;
+}
+
+/** \return Array of masks selecting molecules to track. */
+Action_ToroidalDiffusion::Marray Action_ToroidalDiffusion::setup_entities(Topology const& topIn)
+const
+{
+  Marray entitiesOut;
+  std::vector<bool> molSelected(topIn.Nmol(), false);
+
+  unsigned int molIdx = 0;
+  for (Topology::mol_iterator mol = topIn.MolStart(); mol != topIn.MolEnd(); ++mol, ++molIdx)
+  {
+    for (Unit::const_iterator seg = mol->MolUnit().segBegin(); seg != mol->MolUnit().segEnd(); ++seg)
+    {
+      for (int at = seg->Begin(); at != seg->End(); at++)
+      {
+        if (mask1_.AtomInCharMask( at )) {
+          if (!molSelected[molIdx]) {
+            entitiesOut.push_back( AtomMask() );
+            molSelected[molIdx] = true;
+          }
+          entitiesOut.back().AddAtom( at );
+        }
+      } // END loop over atoms in segment
+    } // END loop over segments in molecule
+  } // END loop over molecules
+
+  mprintf("DEBUG: Entities selected by '%s'\n", mask1_.MaskString());
+  for (Marray::const_iterator it = entitiesOut.begin(); it != entitiesOut.end(); ++it) {
+    mprintf("\t");
+    for (AtomMask::const_iterator at = it->begin(); at != it->end(); ++at)
+      mprintf(" %i", *at + 1);
+    mprintf("\n");
+  }
+
+  return entitiesOut;
+}
+
+// Action_ToroidalDiffusion::Setup()
+Action::RetType Action_ToroidalDiffusion::Setup(ActionSetup& setup)
+{
+  if (setup.Top().SetupCharMask( mask1_ )) {
+    mprinterr("Error: Could not set up mask '%s'\n", mask1_.MaskString());
+    return Action::ERR;
+  }
+  mask1_.MaskInfo();
+  if (mask1_.None()) {
+    mprintf("Warning: No atoms selected by '%s'\n", mask1_.MaskString());
+    return Action::SKIP;
+  }
+  if (entities_.empty()) {
+    // First time set up.
+    entities_ = setup_entities( setup.Top() );
+    if (entities_.empty()) {
+      mprinterr("Error: No entities to calculate diffusion for.\n");
+      return Action::ERR;
+    }
+  }
+
+  return Action::OK;
+}
+
+// Action_ToroidalDiffusion::DoAction()
+Action::RetType Action_ToroidalDiffusion::DoAction(int frameNum, ActionFrame& frm)
+{
+
+}
---
+++ src/Action_ToroidalDiffusion.h
@@ -0,0 +1,26 @@
+#ifndef INC_ACTION_TOROIDALDIFFUSION_H
+#define INC_ACTION_TOROIDALDIFFUSION_H
+#include "Action.h"
+#include "CharMask.h"
+/// Implements the Toroidal-view-preserving scheme of Hummer et al. for calculating diffusion
+class Action_ToroidalDiffusion : public Action {
+  public:
+    Action_ToroidalDiffusion() {}
+    DispatchObject* Alloc() const { return (DispatchObject*)new Action_ToroidalDiffusion(); }
+    void Help() const;
+  private:
+    Action::RetType Init(ArgList&, ActionInit&, int);
+    Action::RetType Setup(ActionSetup&);
+    Action::RetType DoAction(int, ActionFrame&);
+    void Print() {}
+
+    typedef std::vector<Vec3> Varray;
+    typedef std::vector<AtomMask> Marray;
+
+    Marray setup_entities(Topology const&) const;
+
+    Varray torPositions_; ///< Current positions of each entity in the toroidal scheme.
+    Marray entities_;     ///< Masks selecting each entity to track diffusion of.
+    CharMask mask1_;      ///< Mask selecting entities.
+};
+#endif
