64ac6a2f189bfc8b397ee4d8978d97a01ebf78e3
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-25 10:18:11 -0400

    Add toroidal scheme unwrap test

--- test/Test_Unwrap/RunTest.sh
+++ test/Test_Unwrap/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles ptraj.in unwrap.crd unwrap.ortho.crd tz2.ortho.wato.dat
+CleanFiles ptraj.in unwrap.crd unwrap.ortho.crd tz2.ortho.wato.dat tor.dat
 TESTNAME='Unwrap tests'
 Requires netcdf notparallel
 
@@ -36,6 +36,14 @@ run
 EOF
 RunCpptraj "Unwrap with average box correction test"
 DoTest tz2.ortho.wato.dat.save tz2.ortho.wato.dat
+
+cat > ptraj.in <<EOF
+trajin ../tz2.ortho.nc
+unwrap scheme tor
+diffusion TOR :WAT@O out tor.dat noimage
+EOF
+RunCpptraj "Unwrap with toroidal-view-preserving test"
+DoTest ../Test_TorDiff/tor.dat.save tor.dat
 
 EndTest
 
