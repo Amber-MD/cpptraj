47b0132e564d1f2cd8a37034b7517a776a39c2b6
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-31 09:19:40 -0400

    Allow MPI in runanalysis

--- src/Exec_RunAnalysis.cpp
+++ src/Exec_RunAnalysis.cpp
@@ -20,14 +20,7 @@ Exec::RetType Exec_RunAnalysis::Execute(CpptrajState& State, ArgList& argIn) {
       return CpptrajState::ERR;
   }
   // Run specified analysis
-  int err = 0;
-# ifdef MPI
-  // Only master performs analyses currently.
-  if (Parallel::TrajComm().Size() > 1)
-    mprintf("Warning: Analysis does not currently use multiple MPI processes.\n");
-  if (Parallel::TrajComm().Master())
-# endif
-    err = DoRunAnalysis(State, argIn);
+  int err = DoRunAnalysis(State, argIn);
 # ifdef MPI
   if (Parallel::World().CheckError( err )) err = 1;
 # endif
@@ -45,19 +38,45 @@ int Exec_RunAnalysis::DoRunAnalysis(CpptrajState& State, ArgList& argIn) const {
   if (ana == 0) return 1;
   Timer total_time;
   total_time.Start();
-  CpptrajState::RetType err = CpptrajState::ERR;
+  CpptrajState::RetType stat = CpptrajState::ERR;
+# ifdef MPI
+  AnalysisSetup setup(State.DSL(), State.DFL(), Parallel::World());
+# else
   AnalysisSetup setup(State.DSL(), State.DFL());
+# endif
   if ( ana->Setup( analyzeargs, setup, State.Debug() ) == Analysis::OK )
   {
     analyzeargs.CheckForMoreArgs();
-    if (ana->Analyze() != Analysis::ERR) {
-      err = CpptrajState::OK;
+    Analysis::RetType ret;
+#   ifdef MPI
+    if (ana->IsParallel()) {
+      ret = ana->Analyze();
+    } else {
+      mprintf("Warning: Analysis '%s' does not currently use multiple MPI processes.\n", analyzeargs.Command());
+      if ( Parallel::World().Master() )
+        ret = ana->Analyze();
+      Parallel::World().MasterBcast( &ret, 1, MPI_INT );
+    }
+    int err;
+    if (ret == Analysis::ERR) {
+      rprinterr("Error: In parallel, analysis '%s' failed.\n", analyzeargs.Command());
+      err = 1;
+    } else
+      err = 0;
+    if (Parallel::World().CheckError( err ))
+      ret = Analysis::ERR;
+#   else /* MPI */
+    ret = ana->Analyze();
+#   endif /* MPI */
+    if (ret != Analysis::ERR) {
+      rprintf("DEBUG: Analysis success!\n");
+      stat = CpptrajState::OK;
       State.MasterDataFileWrite();
     }
   }
   delete ana;
   total_time.Stop();
   mprintf("TIME: Total analysis execution time: %.4f seconds.\n", total_time.Total());
-  if (err != CpptrajState::OK) return 1;
+  if (stat != CpptrajState::OK) return 1;
   return 0;
 }
