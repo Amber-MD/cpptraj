1d1efc0140cd210bc196909730a2e42403baf992
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-31 10:23:24 -0400

    Parallelize test

--- test/Test_CalcDiffusion/RunTest.sh
+++ test/Test_CalcDiffusion/RunTest.sh
@@ -2,21 +2,47 @@
 
 . ../MasterTest.sh
 
-CleanFiles diff.in
+CleanFiles diff.in unwrap.out unwrap.dcd tz2.diff.dat
 
-INPUT='diff.in'
+INPUT='-i diff.in'
 
 TESTNAME='Diffusion analysis tests'
-Requires netcdf maxthreads 1 
+Requires netcdf
 
-cat > diff.in <<EOF
+UNITNAME='Diffusion analysis calculation test'
+if [ -z "$DO_PARALLEL" ] ; then
+  CheckFor maxthreads 1
+  if [ $? -eq 0 ] ; then
+    cat > diff.in <<EOF
 parm ../tz2.ortho.parm7
 loadcrd ../tz2.ortho.nc name TZ2 1 10 
 crdaction TZ2 unwrap bymol
 runanalysis calcdiffusion crdset TZ2 out tz2.diff.dat :WAT@O
 EOF
-RunCpptraj "Diffusion analysis calculation test"
-DoTest tz2.diff.dat.save tz2.diff.dat
+    RunCpptraj "$UNITNAME"
+    DoTest tz2.diff.dat.save tz2.diff.dat
+  fi
+else
+  CheckFor maxthreads 6
+  if [ $? -eq 0 ] ; then
+    # First pass, unwrap in serial
+    cat > diff.in <<EOF
+parm ../tz2.ortho.parm7
+trajin ../tz2.ortho.nc
+unwrap bymol
+trajout unwrap.dcd
+EOF
+    $CPPTRAJ -i diff.in -o unwrap.out
+    # Second pass, calc diffusion
+    cat > diff.in <<EOF
+parm ../tz2.ortho.parm7
+loadcrd unwrap.dcd name TZ2 1 10
+runanalysis calcdiffusion crdset TZ2 out tz2.diff.dat :WAT@O
+EOF
+    RunCpptraj "$UNITNAME"
+    DoTest tz2.diff.dat.save tz2.diff.dat
+  fi
+fi
 
 EndTest
 exit 0
