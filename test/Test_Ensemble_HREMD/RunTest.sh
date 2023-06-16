177e19001fce9f5b4054825830aa83ae728318bd
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-06-16 10:06:33 -0400

    Start adding ensemble analysis test

--- test/Test_Ensemble_HREMD/RunTest.sh
+++ test/Test_Ensemble_HREMD/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles cpptraj.in sorted.remlog.crd.? sorted.crdidx.crd.?
+CleanFiles cpptraj.in sorted.remlog.crd.? sorted.crdidx.crd.? distances.dat.?
 
 TESTNAME='H-REMD sorting tests'
 Requires netcdf nthreads 4
@@ -34,5 +34,18 @@ DoTest sorted.remlog.crd.0.save sorted.crdidx.crd.0
 DoTest sorted.remlog.crd.1.save sorted.crdidx.crd.1
 DoTest sorted.remlog.crd.2.save sorted.crdidx.crd.2
 DoTest sorted.remlog.crd.3.save sorted.crdidx.crd.3
+
+UNITNAME='Ensemble analysis test'
+cat > cpptraj.in <<EOF
+parm ../tz2.nhe.parm7
+ensemblesize 4
+ensemble rem.crd.001 nosort
+distance EndToEnd :1 :12 out distances.dat
+EOF
+RunCpptraj "$UNITNAME"
+DoTest distances.dat.0.save distances.dat.0
+DoTest distances.dat.1.save distances.dat.1
+DoTest distances.dat.2.save distances.dat.2
+DoTest distances.dat.3.save distances.dat.3
 
 EndTest
