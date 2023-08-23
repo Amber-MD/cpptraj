7853f6e86b894f3e2630c19a335ab1bd3f0c01f4
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-08-23 09:05:32 -0400

    Add change mass from data set test

--- test/Test_Change/RunTest.sh
+++ test/Test_Change/RunTest.sh
@@ -3,14 +3,16 @@
 . ../MasterTest.sh
 
 CleanFiles change.in ala3.mod.pdb ala3.chain.pdb crdala3.chain.pdb \
-           AFV.zeroHmass.dat
+           AFV.zeroHmass.dat AFV.fluctMass.dat
 
 TESTNAME='Change command test'
-Requires maxthreads 1
 
 INPUT='-i change.in'
 
-cat > change.in <<EOF
+UNITNAME='Change atom and residue name tests'
+CheckFor maxthreads 1
+if [ $? -eq 0 ] ; then
+  cat > change.in <<EOF
 parm ../Test_Charmm/ala3.psf
 trajin ../Test_Charmm/ala3.dcd 1 1
 change parmindex 0 resname from :1 to NALA
@@ -18,35 +20,61 @@ change parmindex 0 resname from :3 to CALA
 change parmindex 0 atomname from @HN to H
 trajout ala3.mod.pdb chainid " "
 EOF
-RunCpptraj "Change atom and residue name tests"
-DoTest ala3.mod.pdb.save ala3.mod.pdb
+  RunCpptraj "$UNITNAME"
+  DoTest ala3.mod.pdb.save ala3.mod.pdb
+fi
 
-cat > change.in <<EOF
+UNITNAME='Change chain ID test'
+CheckFor maxthreads 1
+if [ $? -eq 0 ] ; then
+  cat > change.in <<EOF
 parm ../Test_Charmm/ala3.psf
 trajin ../Test_Charmm/ala3.dcd 1 1
 change parmindex 0 chainid of * to A
 trajout ala3.chain.pdb
 EOF
-RunCpptraj "Change chain ID test"
-DoTest ala3.chain.pdb.save ala3.chain.pdb
+  RunCpptraj "$UNITNAME"
+  DoTest ala3.chain.pdb.save ala3.chain.pdb
+fi
 
-cat > change.in <<EOF
+UNITNAME='Change chain ID of COORDS set test'
+CheckFor maxthreads 1
+if [ $? -eq 0 ] ; then
+  cat > change.in <<EOF
 parm ../Test_Charmm/ala3.psf
 loadcrd ../Test_Charmm/ala3.dcd 1 1 name MyCrd
 change crdset MyCrd chainid of * to A
 crdout MyCrd crdala3.chain.pdb
 EOF
-RunCpptraj "Change chain ID of COORDS set test"
-DoTest ala3.chain.pdb.save crdala3.chain.pdb
+  RunCpptraj "$UNITNAME"
+  DoTest ala3.chain.pdb.save crdala3.chain.pdb
+fi
 
-cat > change.in <<EOF
+UNITNAME='Change mass of atoms test'
+CheckFor maxthreads 1
+if [ $? -eq 0 ] ; then
+  cat > change.in <<EOF
 parm ../AFV.parm7
 change mass of @H= to 0.0
 atoms * out AFV.zeroHmass.dat
 #parmwrite out AFV.zeroHmass.parm7
 EOF
-RunCpptraj "Change mass of atoms test"
-DoTest AFV.zeroHmass.dat.save AFV.zeroHmass.dat 
+  RunCpptraj "$UNITNAME"
+  DoTest AFV.zeroHmass.dat.save AFV.zeroHmass.dat
+fi
+
+UNITNAME='Change mass from data set test'
+cat > change.in <<EOF
+parm ../AFV.parm7
+trajin ../AFV.nc
+align first !@H=
+atomicfluct MyFluct #out AFV.myfluct.dat
+run
+change mass fromset MyFluct
+atoms * out AFV.fluctMass.dat
+EOF
+RunCpptraj "$UNITNAME"
+DoTest AFV.fluctMass.dat.save AFV.fluctMass.dat
 
 EndTest
 exit 0
