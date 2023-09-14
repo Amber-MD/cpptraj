f0e5ef4d9e4d2c55dfff5ec2a443b86646ae5e2b
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-09-14 10:16:25 -0400

    Add initial rms calc when doing by brute force. Add crdtransform test.

--- test/Test_RefineTrajectory/RunTest.sh
+++ test/Test_RefineTrajectory/RunTest.sh
@@ -13,24 +13,33 @@ Requires netcdf
 cat > cpptraj.in <<EOF
 parm ../tz2.parm7
 trajin ../tz2.nc
+reference ../tz2.nc 1 [FIRST]
 
 # Initial average
 rms first !@H=
 average crdset R0
 run
 
+crdaction R0 rms Rinit ref [FIRST] out rms.dat !@H=
 # Loop over references
 for i=0;i<10;i++
   j = \$i + 1
   rms ref R\$i !@H=
   average crdset R\$j
   run
-  crdaction R\$j rms R\$i.\$j ref R\$i out rms.dat
+  crdaction R\$j rms R\$i.\$j ref R\$i out rms.dat !@H=
 done
 list
 EOF
+RunCpptraj "$TESTNAME, using loop"
 
-RunCpptraj "$TESTNAME"
+cat > cpptraj.in <<EOF
+parm ../tz2.parm7
+loadcrd ../tz2.nc name MyCrd
+
+crdtransform MyCrd mask !@H= 
+EOF
+RunCpptraj "$TESTNAME, using crdtransform"
 
 EndTest
 
