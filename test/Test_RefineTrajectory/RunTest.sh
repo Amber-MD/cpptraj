fc164886f96d80d9cb812b2bc1e73eeb40163300
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-09-18 09:00:27 -0400

    Actually test the brute-force refinement

--- test/Test_RefineTrajectory/RunTest.sh
+++ test/Test_RefineTrajectory/RunTest.sh
@@ -22,16 +22,19 @@ run
 
 crdaction R0 rms Rinit ref [FIRST] out rms.dat !@H=
 # Loop over references
-for i=0;i<10;i++
+for i=0;i<9;i++
   j = \$i + 1
   rms ref R\$i !@H=
   average crdset R\$j
+  trajout looprefined.crd
   run
   crdaction R\$j rms R\$i.\$j ref R\$i out rms.dat !@H=
 done
+#crdout R\$j looprefined.crd
 list
 EOF
 RunCpptraj "$TESTNAME, RMS refinement using loop"
+DoTest refinedcoords.crd.save looprefined.crd -a 0.002
 
 cat > cpptraj.in <<EOF
 parm ../tz2.parm7
