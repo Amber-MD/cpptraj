5074294e210093289b151cdf6154f7bc03db91dd
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-09-25 14:35:01 -0400

    Find next atom to set

--- src/Structure/Zmatrix.cpp
+++ src/Structure/Zmatrix.cpp
@@ -58,6 +58,13 @@ int Zmatrix::SetFromFrame(Frame const& frameIn, Topology const& topIn)
   return 0;
 }
 
+static inline void atomIsSet(int i, std::vector<bool>& isSet, unsigned int& Nset) {
+  if (!isSet[i]) {
+    isSet[i] = true;
+    Nset++;
+  }
+}
+
 /** Set Cartesian coordinates in Frame from internal coordinates. */
 int Zmatrix::SetToFrame(Frame& frameOut) const {
   if ((unsigned int)frameOut.Natom() != IC_.size()) {
@@ -65,10 +72,14 @@ int Zmatrix::SetToFrame(Frame& frameOut) const {
               frameOut.Natom(), IC_.size());
     return 1;
   }
+
+  std::vector<bool> isSet( IC_.size(), false );
+  unsigned int Nset = 0;
   // Set position of the first atom.
   if (seed0_ != InternalCoords::NO_ATOM) {
     //IC_[seed0_].ZeroXYZ();
     frameOut.SetXYZ(seed0_, Vec3(0.0));
+    atomIsSet(seed0_, isSet, Nset);
     // Set position of the second atom.
     if (seed1_ != InternalCoords::NO_ATOM) {
       if (IC_[seed1_].AtJ() != seed0_) {
@@ -78,6 +89,7 @@ int Zmatrix::SetToFrame(Frame& frameOut) const {
       double r1 = IC_[seed1_].Dist();
       //IC_[seed1_].SetXYZ( Vec3(r1, 0, 0) );
       frameOut.SetXYZ(seed1_, Vec3(r1, 0, 0));
+      atomIsSet(seed1_, isSet, Nset);
       // Set position of the third atom
       if (seed2_ != InternalCoords::NO_ATOM) {
         if (IC_[seed2_].AtJ() != seed1_) {
@@ -96,9 +108,41 @@ int Zmatrix::SetToFrame(Frame& frameOut) const {
 
         //IC_[seed2].SetXYZ( Vec3(r1 + x, y, 0) );
         frameOut.SetXYZ( seed2_, Vec3(r1 + x, y, 0) );
+        atomIsSet(seed2_, isSet, Nset);
       } // END seed atom 2
     } // END seed atom 1
   } // END seed atom 0
+
+  // Find the lowest unset atom
+  unsigned int lowestUnsetAtom = 0;
+  for (; lowestUnsetAtom < IC_.size(); ++lowestUnsetAtom)
+    if (!isSet[lowestUnsetAtom]) break;
+  mprintf("Lowest unset atom: %u\n", lowestUnsetAtom+1);
+
+  // Loop over remaining atoms
+  while (Nset < IC_.size()) {
+    // Find the next atom that is not yet set.
+    unsigned int idx = lowestUnsetAtom;
+    bool findNextAtom = true;
+    while (findNextAtom) {
+      while (idx < IC_.size() && isSet[idx]) idx++;
+      if (idx >= IC_.size()) {
+        mprinterr("Error: Could not find next atom to set.\n");
+        return 1;
+      }
+      // All 3 of the connecting atoms must be set
+      if (isSet[ IC_[idx].AtJ() ] &&
+          isSet[ IC_[idx].AtK() ] &&
+          isSet[ IC_[idx].AtL() ])
+      {
+        findNextAtom = false;
+      }
+    } // END loop finding next atom to set
+    mprintf("DEBUG: Next atom to set is %u\n", idx+1);
+    break; // DEBUG
+  } // END loop over internal coordinates
+
+
 
   return 0;
 }
