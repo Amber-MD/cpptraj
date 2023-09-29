043f0b6a21cbf5bbb8180ef6d3410a80a7eed493
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-09-29 09:12:22 -0400

    Start harder sugar test

--- test/Test_Zmatrix/RunTest.sh
+++ test/Test_Zmatrix/RunTest.sh
@@ -8,12 +8,25 @@ INPUT='-i cpptraj.in'
 
 TESTNAME='Zmatrix tests'
 
-cat > cpptraj.in <<EOF
+Basic() {
+  cat > cpptraj.in <<EOF
 parm ../Test_ReadPrep/test1.mol2.save
 loadcrd ../Test_ReadPrep/test1.mol2.save name MyCrd
 zmatrix MyCrd out zmatrix.dat
 EOF
-RunCpptraj "$TESTNAME"
-DoTest zmatrix.dat.save zmatrix.dat
+  RunCpptraj "$TESTNAME, Basic"
+  DoTest zmatrix.dat.save zmatrix.dat
+}
+
+Sugar() {
+  cat > cpptraj.in <<EOF
+parm 0SB.mol2
+loadcrd 0SB.mol2 name 0SB
+zmatrix 0SB out zmatrix.0SB.dat
+EOF
+  RunCpptraj "$TESTNAME, Sugar"
+}
+
+Basic
 
 EndTest
