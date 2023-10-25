a310d4a03c28b30c145f01d96654b56995688956
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-10-25 10:35:52 -0400

    Ensure we can read all the sections. Skip unknown ones

--- src/DataIO_AmberLib.cpp
+++ src/DataIO_AmberLib.cpp
@@ -53,16 +53,103 @@ int DataIO_AmberLib::ReadData(FileName const& fname, DataSetList& dsl, std::stri
   line = infile.GetLine();
   while (!line.empty() && line[0] != '!')
   {
-    UnitNames.push_back( line );
+    // Using ArgList here is a hacky way to get rid of the quotes
+    ArgList tmparg(line);
+    UnitNames.push_back( tmparg[0] );
     line = infile.GetLine();
   }
-  mprintf("DEBUG: Units:");
-  for (Sarray::const_iterator it = UnitNames.begin(); it != UnitNames.end(); ++it)
-    mprintf(" %s", it->c_str());
-  mprintf("\n");
-
   // Now should be at first unit
+  mprintf("DEBUG: Units:\n");
+  for (Sarray::const_iterator it = UnitNames.begin(); it != UnitNames.end(); ++it)
+  {
+    mprintf("DEBUG: Reading unit %s\n", it->c_str());
+    std::string entryColumn = "!entry." + *it + ".unit.atoms";
+    ArgList tmparg( line );
+    if (tmparg.Nargs() < 1 || tmparg[0] != entryColumn) {
+      mprinterr("Error: Expected '%s', got '%s'\n", entryColumn.c_str(), line.c_str());
+      return 1;
+    }
+    read_unit( infile, line, *it );
+  }
 
+
+  return 0;
+}
+
+/** Strings corresponding to SectionType */
+const char* DataIO_AmberLib::sectionStr_[] = {
+  "atoms", "atomspertinfo", "boundbox", "childsequence", "connect",
+  "connectivity", "hierarchy", "name", "positions", "residueconnect",
+  "residues", "solventcap", "velocities", 0 };
+
+/** \return Section type from entry line. */
+DataIO_AmberLib::SectionType DataIO_AmberLib::id_section(std::string const& line,
+                                                         std::string const& unitName)
+{
+  std::string entry = "!entry." + unitName + ".unit.";
+  for (int idx = 0; idx < (int)UNKNOWN_SECTION; idx++) {
+    std::string sectionName = entry + std::string(sectionStr_[idx]);
+    std::size_t found = line.find_first_of(" ");
+    if (found == std::string::npos) {
+      mprinterr("Error: Malformed entry line: %s\n", line.c_str());
+      break;
+    }
+    if (line.compare(0, found, sectionName) == 0)
+      return (SectionType)idx;
+  }
+  return UNKNOWN_SECTION;
+}
+
+/** Read a unit from OFF file. It is expected that the next line from
+  * infile is the first entry in the unit.atoms table.
+  */
+int DataIO_AmberLib::read_unit(BufferedLine& infile, std::string& Line,
+                               std::string const& unitName)
+const
+{
+  // Format: "Atom name" "Type" "Type index (unused)" "resnum" "flags" "sequence" "element" "charge"
+  char aname[16];
+  char atype[16];
+  int typex;
+  int resx;
+  int flags;
+  int seq;
+  int elt;
+  double charge;
+  
+  SectionType currentSection = id_section( Line, unitName );
+  if (currentSection == UNKNOWN_SECTION) {
+    mprinterr("Error: Could not ID first section: %s\n", Line.c_str());
+    return 1;
+  }
+  mprintf("DEBUG: First section is %s\n", sectionStr_[currentSection]);
+  bool readUnit = true;
+  while (readUnit) {
+    const char* lineptr = infile.Line();
+    if (lineptr == 0) {
+      readUnit = false;
+      break;
+    }
+    Line.assign(lineptr);
+    if (!Line.empty()) {
+      mprintf("DEBUG: Line: %s\n", Line.c_str());
+      //ArgList cols( Line, " \t\n" );
+      if (Line[0] == '!') {
+        // See if we are at another unit
+        ArgList tmparg( Line, ". " );
+        if (tmparg[2] == "unit" && tmparg[3] == "atoms" && tmparg[4] == "table") {
+          readUnit = false;
+          break;
+        }
+        currentSection = id_section( Line, unitName );
+        if (currentSection == UNKNOWN_SECTION) {
+          mprintf("Warning: Could not ID section: %s\n", Line.c_str());
+        } else
+          mprintf("DEBUG: Section is %s\n", sectionStr_[currentSection]);
+      }
+    }
+  }
+  
   return 0;
 }
 
--- src/DataIO_AmberLib.h
+++ src/DataIO_AmberLib.h
@@ -1,6 +1,7 @@
 #ifndef INC_DATAIO_AMBERLIB_H
 #define INC_DATAIO_AMBERLIB_H
 #include "DataIO.h"
+class BufferedLine;
 /// <Enter description of DataIO_AmberLib here>
 class DataIO_AmberLib : public DataIO {
   public:
@@ -13,5 +14,17 @@ class DataIO_AmberLib : public DataIO {
     int processWriteArgs(ArgList&);
     int WriteData(FileName const&, DataSetList const&);
     bool ID_DataFormat(CpptrajFile&);
+  private:
+    // Keep in sync with sectionStr_
+    enum SectionType { ATOMTABLE = 0, PERTINFO, BOUNDBOX, CHILDSEQUENCE, CONNECT,
+                       CONNECTIVITY, HIERARCHY, UNITNAME, POSITIONS, RESCONNECT,
+                       RESIDUES, SOLVENTCAP, VELOCITIES, UNKNOWN_SECTION };
+    /// Strings corresponding to section type
+    static const char* sectionStr_[];
+
+    /// ID OFF section from line
+    static inline SectionType id_section(std::string const&, std::string const&);
+    /// Read unit from OFF file
+    int read_unit(BufferedLine&, std::string&, std::string const&) const;
 };
 #endif
