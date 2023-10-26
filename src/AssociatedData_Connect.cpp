a5b1e8751398dda0b08419b79ffab3ff212b7bb2
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-10-26 19:34:55 -0400

    Fill out Ainfo function

--- src/AssociatedData_Connect.cpp
+++ src/AssociatedData_Connect.cpp
@@ -1,7 +1,14 @@
 #include "AssociatedData_Connect.h"
+#include "CpptrajStdio.h"
 
 const char* AssociatedData_Connect::HelpText = "";
 
 void AssociatedData_Connect::Ainfo() const {
+  if (!connect_.empty()) {
+    mprintf(" (Connect Atoms:");
+    for (Iarray::const_iterator it = connect_.begin(); it != connect_.end(); ++it)
+      mprintf(" %i", *it + 1);
+    mprintf("\n");
+  }
   return;
 }
