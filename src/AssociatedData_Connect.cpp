da9c3fa4f9ac75edd139fb3e7929aa4f186e9312
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-11-01 10:12:50 -0400

    Create arg routine for connect associated data

--- src/AssociatedData_Connect.cpp
+++ src/AssociatedData_Connect.cpp
@@ -1,9 +1,21 @@
 #include "AssociatedData_Connect.h"
 #include "CpptrajStdio.h"
+#include "ArgList.h"
 
 const char* AssociatedData_Connect::HelpText = "";
 
-int AssociatedData_Connect::ProcessAdataArgs(ArgList&) {
+int AssociatedData_Connect::ProcessAdataArgs(ArgList& argIn) {
+  // Expect user atom args to start from 1
+  int head = argIn.getKeyInt("head", 0) - 1;
+  int tail = argIn.getKeyInt("tail", 0) - 1;
+  if (head < 0) head = -1;
+  if (tail < 0) tail = -1;
+  if (head == -1 && tail == -1) {
+    mprinterr("Error: Either at least 'head' or 'tail' must be specified.\n");
+    return 1;
+  }
+  connect_.push_back( head );
+  connect_.push_back( tail );
   return 0;
 }
 
