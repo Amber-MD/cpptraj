089cbd3cbdef9d3acd0c5b71cbaf8add53bb14c4
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-11-01 09:55:49 -0400

    Test libset keyword

--- test/Test_Sequence/RunTest.sh
+++ test/Test_Sequence/RunTest.sh
@@ -14,8 +14,12 @@ readdata cph_nucleic_caps.lib name CAPS
 list
 sequence CAPS[MOC] A15[CNALA] name Mol
 crdout Mol Mol.mol2
+
+sequence libset A15 libset CAPS MOC CNALA name Mol2
+crdout Mol2 Mol2.mol2
 EOF
 RunCpptraj "$TESTNAME"
 DoTest Mol.mol2.save Mol.mol2
+DoTest Mol.mol2.save Mol2.mol2
 
 EndTest
