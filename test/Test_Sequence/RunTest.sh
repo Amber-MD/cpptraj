123b45c512fa763d3367c64744eb6e5a12a6d310
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-11-01 09:41:32 -0400

    Start sequence test

---
+++ test/Test_Sequence/RunTest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+TESTNAME='Sequence tests'
+
+CleanFiles cpptraj.in Mol.mol2
+
+INPUT='-i cpptraj.in'
+
+cat > cpptraj.in <<EOF
+readdata ../Test_ReadOFF/aminocn15ipq_10.0.lib name A15
+readdata cph_nucleic_caps.lib name CAPS
+list
+sequence CAPS[MOC] A15[CNALA] name Mol
+crdout Mol Mol.mol2
+EOF
+RunCpptraj "$TESTNAME"
+
+EndTest
