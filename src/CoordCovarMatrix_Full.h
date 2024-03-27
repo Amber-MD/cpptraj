2cbeb3ef381a11ab2667d965eae71d4c91d7ca2e
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-03-27 10:18:32 -0400

    Update docs

--- src/CoordCovarMatrix_Full.h
+++ src/CoordCovarMatrix_Full.h
@@ -9,6 +9,7 @@ class CoordCovarMatrix_Full : public CoordCovarMatrix {
     /// Finish calculating the matrix (normalize, calc <rirj> - <ri><rj>)
     int FinishMatrix();
     // ---------------------------------
+    /// Set up matrix for 2 masks
     int SetupMatrix(std::vector<Atom> const&, AtomMask const&,
                     std::vector<Atom> const&, AtomMask const&, bool);
     /// Add selected atoms in Frame to matrix
@@ -20,11 +21,11 @@ class CoordCovarMatrix_Full : public CoordCovarMatrix {
     /// Set up the covariance matrix for selected atoms
     int setupMat(std::vector<Atom> const&, AtomMask const&);
 
-    Varray vect_1_;
-    Varray vect_2_;
+    Varray vect_1_; ///< Hold average of elements in mask1
+    Varray vect_2_; ///< Hold average of elements in mask2
 //    Varray vect2_1_; ///< Array of average diagonal elements squared
 //    Varray vect2_2_;
-    Darray mass1_;
-    Darray mass2_;
+    Darray mass1_; ///< Hold masses of atoms in mask1
+    Darray mass2_; ///< Hold masses of atoms in mask2
 };
 #endif
