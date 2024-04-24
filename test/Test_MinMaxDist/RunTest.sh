2bcc0b7ade659a40c1f82e94680d97e402317d82
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-04-24 10:42:06 -0400

    Change byres test to use imaging and fewer residues

--- test/Test_MinMaxDist/RunTest.sh
+++ test/Test_MinMaxDist/RunTest.sh
@@ -17,17 +17,24 @@ maxdist name Max mask1 :2 mask2 :11 byatom out tz2.byatom.dat
 mindist name Min1 mask1 ^1 byatom out tz2.byatom.1.dat
 maxdist name Max1 mask1 ^1 byatom out tz2.byatom.1.dat
 
-mindist name ResMin1 mask1 ^1 byres out tz2.byres.1.dat
-maxdist name ResMax1 mask1 ^1 byres out tz2.byres.1.dat
 
 #nativecontacts :2 :11 savenonnative mindist out temp.dat
 #nativecontacts :2 :11 savenonnative maxdist out temp.dat
 #nativecontacts ^1 maxdist out temp.dat
 #precision temp.dat 8 3
 EOF
-RunCpptraj "Min/max distance test"
+RunCpptraj "Min/max distance by atom tests"
 DoTest tz2.byatom.dat.save tz2.byatom.dat
 DoTest tz2.byatom.1.dat.save tz2.byatom.1.dat
+
+cat > mmdist.in <<EOF
+parm ../tz2.truncoct.parm7
+trajin ../tz2.truncoct.crd
+
+mindist name ResMin1 mask1 :1-7 byres out tz2.byres.1.dat
+maxdist name ResMax1 mask1 :1-7 byres out tz2.byres.1.dat
+EOF
+RunCpptraj "Min/max distance by residue tests"
 DoTest tz2.byres.1.dat.save tz2.byres.1.dat
 
 EndTest
