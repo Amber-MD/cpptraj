fa825f9c1bba84ddcc42f9e5b1b4f00a53736bc5
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-04-24 10:27:38 -0400

    Add byatom single mask tests

--- test/Test_MinMaxDist/RunTest.sh
+++ test/Test_MinMaxDist/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles mmdist.in tz2.byatom.dat temp.dat
+CleanFiles mmdist.in tz2.byatom.dat temp.dat tz2.byatom.1.dat
 
 TESTNAME='Min/max distance tests'
 
@@ -13,13 +13,17 @@ trajin ../tz2.crd
 
 mindist name Min mask1 :2 mask2 :11 byatom out tz2.byatom.dat
 maxdist name Max mask1 :2 mask2 :11 byatom out tz2.byatom.dat
+mindist name Min1 mask1 ^1 byatom out tz2.byatom.1.dat
+maxdist name Max1 mask1 ^1 byatom out tz2.byatom.1.dat
 
 #nativecontacts :2 :11 savenonnative mindist out temp.dat
 #nativecontacts :2 :11 savenonnative maxdist out temp.dat
+#nativecontacts ^1 maxdist out temp.dat
 #precision temp.dat 8 3
 EOF
 RunCpptraj "Min/max distance test"
 DoTest tz2.byatom.dat.save tz2.byatom.dat
+DoTest tz2.byatom.1.dat.save tz2.byatom.1.dat
 
 EndTest
 exit 0
