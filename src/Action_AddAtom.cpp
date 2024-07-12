1741acd7c12b57b602be6d5a53cf0242ed86fd5e
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-07-12 09:03:26 -0400

    Start addatom

---
+++ src/Action_AddAtom.cpp
@@ -0,0 +1,57 @@
+#include "Action_AddAtom.h"
+#include "CpptrajStdio.h"
+
+/** CONSTRUCTOR */
+Action_AddAtom::Action_AddAtom() :
+  newParm_(0)
+{}
+
+/** DESTRUCTOR */
+Action_AddAtom::~Action_AddAtom() {
+  if (newParm_ != 0) delete newParm_;
+}
+
+// Action_AddAtom::Help()
+void Action_AddAtom::Help() const {
+
+}
+
+// Action_AddAtom::Init()
+Action::RetType Action_AddAtom::Init(ArgList& actionArgs, ActionInit& init, int debugIn)
+{
+  std::string aname = actionArgs.GetStringKey("aname");
+  if (aname.empty()) {
+    mprinterr("Error: Must specify atom name with 'aname'.\n");
+    return Action::ERR;
+  }
+  NameType atomName( aname );
+
+  std::string elt = actionArgs.GetStringKey("elt");
+  if (elt.empty())
+    elt.assign("H");
+  if (elt.size() > 2) {
+    mprinterr("Error: Element name '%s' is too big; should be 2 characters max.\n", elt.c_str());
+    return Action::ERR;
+  }
+
+  std::string rname = actionArgs.GetStringKey("rname");
+  if (rname.empty())
+    rname.assign("TMP");
+  NameType residueName( rname );
+
+
+  mprintf("    ADDATOM: Adding atom named '%s', element %s, residue name '%s'\n",
+          *atomName, elt.c_str(), *residueName);
+}
+
+// Action_AddAtom::Setup()
+Action::RetType Action_AddAtom::Setup(ActionSetup& setup)
+{
+
+}
+
+// Action_AddAtom::DoAction()
+Action::RetType Action_AddAtom::DoAction(int frameNum, ActionFrame& frm)
+{
+
+}
