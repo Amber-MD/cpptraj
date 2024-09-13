1d66d29500006991037319e8772483849122f928
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-09-13 11:12:50 -0400

    Start energy decomp action

---
+++ src/Action_EneDecomp.cpp
@@ -0,0 +1,25 @@
+#include "Action_EneDecomp.h"
+#include "CpptrajStdio.h"
+
+// Action_EneDecomp::Help()
+void Action_EneDecomp::Help() const {
+
+}
+
+// Action_EneDecomp::Init()
+Action::RetType Action_EneDecomp::Init(ArgList& actionArgs, ActionInit& init, int debugIn)
+{
+
+}
+
+// Action_EneDecomp::Setup()
+Action::RetType Action_EneDecomp::Setup(ActionSetup& setup)
+{
+
+}
+
+// Action_EneDecomp::DoAction()
+Action::RetType Action_EneDecomp::DoAction(int frameNum, ActionFrame& frm)
+{
+
+}
