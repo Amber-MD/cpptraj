d77bfd2d94656810a58fdbff05d7f34d170720ec
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-09-23 13:41:38 -0400

    Add C6 param and LJ self energy calcs

--- src/Energy/EwaldParams_LJPME.h
+++ src/Energy/EwaldParams_LJPME.h
@@ -13,8 +13,12 @@ class EwaldParams_LJPME : public EwaldParams_PME {
     /// \return LJ Ewald coefficient
     double LJ_EwaldCoeff() const { return lw_coeff_; }
 
+    /// \return LJPME self energy
+    double Self6() const;
   private:
-   double lw_coeff_;                  ///< LJ Ewald coefficient     
+   Darray Cparam_;   ///< Hold selected atomic C6 coefficients for LJ PME
+   double lw_coeff_; ///< LJ Ewald coefficient
+   double ljpme_self_; ///< LJ PME self energy calculated from C6 parameters and LJ Ewald coeff.
 };
 }
 }
