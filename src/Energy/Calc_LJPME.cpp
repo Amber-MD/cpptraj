2cd111f911b2627e85af09bd63a460952a33c0bf
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-09-23 14:16:54 -0400

    Add Init routine

--- src/Energy/Calc_LJPME.cpp
+++ src/Energy/Calc_LJPME.cpp
@@ -1,4 +1,6 @@
 #include "Calc_LJPME.h"
+#include "../CpptrajStdio.h"
+#include "../EwaldOptions.h"
 
 using namespace Cpptraj::Energy;
 
@@ -6,4 +8,21 @@ Calc_LJPME::Calc_LJPME() :
   Recip_(PME_Recip::COULOMB),
   LJrecip_(PME_Recip::LJ)
 {}
+
+/** Set up LJPME parameters. */
+int Calc_LJPME::Init(Box const& boxIn, EwaldOptions const& pmeOpts, int debugIn)
+{
+  if (NBengine_.ModifyEwaldParams().InitEwald(boxIn, pmeOpts, debugIn)) {
+    mprinterr("Error: LJPME calculation init failed.\n");
+    return 1;
+  }
+  if (pairList_.InitPairList(NBengine_.EwaldParams().Cutoff(), pmeOpts.SkinNB(), debugIn))
+    return 1;
+  if (pairList_.SetupPairList( boxIn ))
+    return 1;
+  Recip_.SetDebug( debugIn );
+  LJrecip_.SetDebug( debugIn );
+
+  return 0;
+}
 
