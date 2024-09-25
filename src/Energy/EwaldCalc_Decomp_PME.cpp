503db3dcb5f897aa83693d4328513d796b8246ee
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-09-25 10:25:00 -0400

    Do engine init

--- src/Energy/EwaldCalc_Decomp_PME.cpp
+++ src/Energy/EwaldCalc_Decomp_PME.cpp
@@ -11,10 +11,10 @@ EwaldCalc_Decomp_PME::EwaldCalc_Decomp_PME() :
 /** Set up PME parameters. */
 int EwaldCalc_Decomp_PME::Init(Box const& boxIn, EwaldOptions const& pmeOpts, int debugIn)
 {
-  //if (NBengine_.ModifyEwaldParams().InitEwald(boxIn, pmeOpts, debugIn)) {
-  //  mprinterr("Error: PME calculation init failed.\n");
-  //  return 1;
-  //}
+  if (NBengine_.ModifyEwaldParams().InitEwald(boxIn, pmeOpts, debugIn)) {
+    mprinterr("Error: Decomposable PME calculation init failed.\n");
+    return 1;
+  }
   if (pairList_.InitPairList(NBengine_.EwaldParams().Cutoff(), pmeOpts.SkinNB(), debugIn))
     return 1;
   if (pairList_.SetupPairList( boxIn ))
