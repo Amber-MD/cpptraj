34a47cc8ae37176d9af14c1205115e513c4eeb1f
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-10-15 11:21:19 -0400

    Use PME_RecipParams class

--- src/Energy/PME_Recip.cpp
+++ src/Energy/PME_Recip.cpp
@@ -1,14 +1,13 @@
 #include "PME_Recip.h"
+#include "PME_RecipParams.h"
 #include "../Box.h"
 #include "../CpptrajStdio.h"
-#include "../EwaldOptions.h"
 
 typedef helpme::Matrix<double> Mat;
 
 using namespace Cpptraj::Energy;
 
-PME_Recip::PME_Recip(Type typeIn) :
-  debug_(0)
+PME_Recip::PME_Recip(Type typeIn)
 {
   switch (typeIn) {
     case COULOMB:
@@ -20,103 +19,6 @@ PME_Recip::PME_Recip(Type typeIn) :
       scaleFac_ = -1.0;
       break;
   }
-}
-
-/** Init */
-int PME_Recip::InitRecip(EwaldOptions const& pmeOpts, int debugIn) {
-  debug_ = debugIn;
-  nfft_[0] = pmeOpts.Nfft1();
-  nfft_[1] = pmeOpts.Nfft2();
-  nfft_[2] = pmeOpts.Nfft3();
-  order_ = pmeOpts.SplineOrder();
-  // Set defaults if necessary
-  if (order_ < 1) order_ = 6;
-  PrintRecipOpts();
-  return 0;
-}
-
-/** Print options to stdout. */
-void PME_Recip::PrintRecipOpts() const {
-  mprintf("\tRecip opts (distance kernel exponent= %i\n", distKernelExponent_);
-  mprintf("\t  Bspline order= %i\n", order_);
-  mprintf("\t ");
-  for (int i = 0; i != 3; i++)
-    if (nfft_[i] == -1)
-      mprintf(" NFFT%i=auto", i+1);
-    else
-      mprintf(" NFFT%i=%i", i+1, nfft_[i]);
-  mprintf("\n");
-}
-
-/** \return true if given number is a product of powers of 2, 3, or 5. */
-bool PME_Recip::check_prime_factors(int nIn) {
-  if (nIn == 1) return true;
-  int NL = nIn;
-  int NQ;
-  // First divide down by 2
-  while (NL > 0) {
-    NQ = NL / 2;
-    if (NQ * 2 != NL) break;
-    if (NQ == 1) return true;
-    NL = NQ;
-  }
-  // Next try 3
-  while (NL > 0) {
-    NQ = NL / 3;
-    if (NQ * 3 != NL) break;
-    if (NQ == 1) return true;
-    NL = NQ;
-  }
-  // Last try 5
-  while (NL > 0) {
-    NQ = NL / 5;
-    if (NQ * 5 != NL) break;
-    if (NQ == 1) return true;
-    NL = NQ;
-  }
-  return false;
-}
-
-/** Compute the ceiling of len that is also a product of powers of 2, 3, 5.
-  * Use check_prime_factors to get the smallest integer greater or equal
-  * than len which is decomposable into powers of 2, 3, 5.
-  */
-int PME_Recip::ComputeNFFT(double len) {
-  int mval = (int)len - 1;
-  for (int i = 0; i < 100; i++) {
-    mval += 1;
-    // Sanity check
-    if (mval < 1) {
-      mprinterr("Error: Bad box length %g, cannot get NFFT value.\n", len);
-      return 0;
-    }
-    if (check_prime_factors(mval))
-      return mval;
-  }
-  mprinterr("Error: Failed to get good FFT array size for length %g Ang.\n", len);
-  return 0;
-}
-
-/** Given a box, determine number of FFT grid points in each dimension. */
-int PME_Recip::DetermineNfft(int& nfft1, int& nfft2, int& nfft3, Box const& boxIn) const
-{
-   if (nfft1 < 1) {
-    // Need even dimension for X direction
-    nfft1 = ComputeNFFT( (boxIn.Param(Box::X) + 1.0) * 0.5 );
-    nfft1 *= 2;
-  }
-  if (nfft2 < 1)
-    nfft2 = ComputeNFFT( boxIn.Param(Box::Y) );
-  if (nfft3 < 1)
-    nfft3 = ComputeNFFT( boxIn.Param(Box::Z) );
-
-  if (nfft1 < 1 || nfft2 < 1 || nfft3 < 1) {
-    mprinterr("Error: Bad NFFT values: %i %i %i\n", nfft1, nfft2, nfft3);
-    return 1;
-  }
-  if (debug_ > 0) mprintf("DEBUG: NFFTs: %i %i %i\n", nfft1, nfft2, nfft3);
-
-  return 0;
 }
 
 /** \return 1 if the box type is invalid for helPME */
@@ -137,17 +39,17 @@ int PME_Recip::set_lattice(PMEInstanceD::LatticeType& lattice, Box const& boxIn)
 
 /** \return Reciprocal space part of PME energy calc. */
 // TODO currently helPME needs the coords/charge arrays to be non-const, need to fix that
-double PME_Recip::Recip_ParticleMesh(Darray& coordsDin, Box const& boxIn, Darray& ChargeIn, double ew_coeffIn)
+double PME_Recip::Recip_ParticleMesh(PME_RecipParams const& recipParams,
+                                     Darray& coordsDin, Box const& boxIn,
+                                     Darray& ChargeIn, double ew_coeffIn)
 {
   t_recip_.Start();
   // This essentially makes coordsD and chargesD point to arrays.
   Mat coordsD(&coordsDin[0], ChargeIn.size(), 3);
   Mat chargesD(&ChargeIn[0], ChargeIn.size(), 1);
-  int nfft1 = nfft_[0];
-  int nfft2 = nfft_[1];
-  int nfft3 = nfft_[2];
+  int nfft1, nfft2, nfft3;
 
-  if ( DetermineNfft(nfft1, nfft2, nfft3, boxIn) ) {
+  if ( recipParams.DetermineNfft(nfft1, nfft2, nfft3, boxIn) ) {
     mprinterr("Error: Could not determine FFT grid spacing.\n");
     return 0.0;
   }
@@ -163,7 +65,7 @@ double PME_Recip::Recip_ParticleMesh(Darray& coordsDin, Box const& boxIn, Darray
   // NOTE: Scale factor for Charmm is 332.0716
   // NOTE: The electrostatic constant has been baked into the Charge_ array already.
   //auto pme_object = std::unique_ptr<PMEInstanceD>(new PMEInstanceD());
-  pme_object_.setup(distKernelExponent_, ew_coeffIn, order_, nfft1, nfft2, nfft3, scaleFac_, 0);
+  pme_object_.setup(distKernelExponent_, ew_coeffIn, recipParams.Order(), nfft1, nfft2, nfft3, scaleFac_, 0);
   // Check the unit cell vectors
   PMEInstanceD::LatticeType lattice;
   if (set_lattice(lattice, boxIn)) return 0;
@@ -187,24 +89,25 @@ double PME_Recip::Recip_ParticleMesh(Darray& coordsDin, Box const& boxIn, Darray
 
 /** \return Reciprocal space part of PME energy calc. */
 // TODO currently helPME needs the coords/charge arrays to be non-const, need to fix that
-double PME_Recip::Recip_Decomp(Darray& atom_recip,
-                               Darray& coordsDin, Box const& boxIn, Darray& ChargeIn, double ew_coeffIn)
+double PME_Recip::Recip_Decomp(PME_RecipParams const& recipParams,
+                               Darray& atom_recip,
+                               Darray& coordsDin, Box const& boxIn,
+                               Darray& ChargeIn, double ew_coeffIn)
 {
   t_recip_.Start();
   atom_recip.resize( ChargeIn.size() );
   // This essentially makes coordsD and chargesD point to arrays.
   Mat coordsD(&coordsDin[0], ChargeIn.size(), 3);
   Mat chargesD(&ChargeIn[0], ChargeIn.size(), 1);
-  int nfft1 = nfft_[0];
-  int nfft2 = nfft_[1];
-  int nfft3 = nfft_[2];
+  int nfft1, nfft2, nfft3;
 
-  if ( DetermineNfft(nfft1, nfft2, nfft3, boxIn) ) {
+  if ( recipParams.DetermineNfft(nfft1, nfft2, nfft3, boxIn) ) {
     mprinterr("Error: Could not determine FFT grid spacing.\n");
     return 0.0;
   }
+
   // Instantiate double precision PME object
-  pme_object_.setup(distKernelExponent_, ew_coeffIn, order_, nfft1, nfft2, nfft3, scaleFac_, 0);
+  pme_object_.setup(distKernelExponent_, ew_coeffIn, recipParams.Order(), nfft1, nfft2, nfft3, scaleFac_, 0);
   // Check the unit cell vectors
   PMEInstanceD::LatticeType lattice;
   if (set_lattice(lattice, boxIn)) return 0;
--- src/Energy/PME_Recip.h
+++ src/Energy/PME_Recip.h
@@ -3,9 +3,9 @@
 #include "../helpme_standalone.h"
 #include "../Timer.h"
 class Box;
-class EwaldOptions;
 namespace Cpptraj {
 namespace Energy {
+class PME_RecipParams;
 /// Do the reciprocal part of a PME calculation
 class PME_Recip {
     typedef std::vector<double> Darray;
@@ -13,29 +13,18 @@ class PME_Recip {
     enum Type { COULOMB = 0, LJ };
 
     PME_Recip(Type);
-    /// Initialize
-    int InitRecip(EwaldOptions const& pmeOpts, int);
 
-    /// Print options to stdout
-    void PrintRecipOpts() const;
-
-    double Recip_ParticleMesh(Darray&, Box const&, Darray&, double);
-    double Recip_Decomp(Darray&, Darray&, Box const&, Darray&, double);
+    double Recip_ParticleMesh(PME_RecipParams const&, Darray&, Box const&, Darray&, double);
+    double Recip_Decomp(PME_RecipParams const&, Darray&, Darray&, Box const&, Darray&, double);
 
     Timer const& Timing_Total() const { return t_recip_; }
     //Timer const& Timing_Calc() const { return t_calc_; }
   private:
-    static bool check_prime_factors(int);
-    static int ComputeNFFT(double);
-    int DetermineNfft(int&, int&, int&, Box const&) const;
     static int set_lattice(PMEInstanceD::LatticeType&, Box const&);
 
     PMEInstanceD pme_object_;
     Timer t_recip_; ///< Recip calc timer
     //Timer t_calc_;
-    int nfft_[3]; ///< Number of grid points for FFT in X, Y, Z
-    int order_; ///< B-Spline order
-    int debug_;
     int distKernelExponent_; ///< Exponent of the distance kernel: 1 for Coulomb, 6 for LJ
     double scaleFac_; ///< scale factor to be applied to all computed energies and derivatives thereof
 };
