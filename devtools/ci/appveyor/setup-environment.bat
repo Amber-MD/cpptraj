6b48b4daa6ad6e87bc679157974ccd8552ea5d72
Daniel R. Roe <daniel.roe@nih.gov>
2025-09-23 10:30:42 -0400

    Add refresh and sysupgrade options to pacman

--- devtools/ci/appveyor/setup-environment.bat
+++ devtools/ci/appveyor/setup-environment.bat
@@ -33,7 +33,7 @@ if %BUILD_TYPE% equ cmake-vs (
 	set CXX=%MINGWPREFIX%-g++.exe
 	set FC=%MINGWPREFIX%-gfortran.exe
         sh -lc "pacman-key --keyserver hkp://keyserver.ubuntu.com --refresh-keys"
-	sh -lc "pacman -S --noconfirm --needed mingw-w64-x86_64-netcdf mingw-w64-x86_64-openblas mingw-w64-x86_64-arpack mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-ncurses mingw-w64-x86_64-readline diffutils mingw-w64-x86_64-fftw"
+	sh -lc "pacman -Syu --noconfirm --needed mingw-w64-x86_64-netcdf mingw-w64-x86_64-openblas mingw-w64-x86_64-arpack mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-ncurses mingw-w64-x86_64-readline diffutils mingw-w64-x86_64-fftw"
 
 	rem build NetCDF (we need our own version since the one in Pacman has an unwanted dependency on HDF5 and Termcap)
 	rem sh -lc "if [ ! -f /usr/local/lib/libnetcdf.a ]; then curl -fsS -o netcdf-c-4.7.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-4.7.3.zip  && 7z x netcdf-c-4.7.3.zip -o$APPVEYOR_BUILD_FOLDER > /dev/null && cd $APPVEYOR_BUILD_FOLDER/netcdf-c-4.7.3 && exec 0</dev/null && ./configure --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap && make -r install; else echo 'Have Cached NetCDF'; fi"
