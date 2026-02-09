4f49da81b6c9fd9c86f6d395ac7e76316e283645
Daniel R. Roe <daniel.r.roe@gmail.com>
2026-02-08 20:10:24 -0500

    Get coords

--- src/Energy/CMAP.cpp
+++ src/Energy/CMAP.cpp
@@ -12,4 +12,12 @@ double CMAP::Ene_CMAP(CmapArray const& Cmaps, Frame const& frameIn) {
   for (CmapArray::const_iterator cmap = Cmaps.begin();
                                  cmap != Cmaps.end(); ++cmap
   {
-    
+    const double* ixyz = frameIn.XYZ( cmap->A1() );
+    const double* jxyz = frameIn.XYZ( cmap->A2() );
+    const double* kxyz = frameIn.XYZ( cmap->A3() );
+    const double* lxyz = frameIn.XYZ( cmap->A4() );
+    const double* mxyz = frameIn.XYZ( cmap->A5() );
+
+  }
+  return ene_cmap;
+}
