4de2184d7a99101a76a3afb1780fff3441b3623e
Daniel R. Roe <daniel.r.roe@gmail.com>
2026-02-09 11:28:41 -0500

    Add to build framework, add missing depends

--- src/Energy/CMAP.cpp
+++ src/Energy/CMAP.cpp
@@ -1,4 +1,8 @@
 #include "CMAP.h"
+#include "../CpptrajStdio.h" // DEBUG
+#include "../Frame.h"
+#include "../ParameterTypes.h"
+#include "../TorsionRoutines.h"
 
 using namespace Cpptraj::Energy;
 
@@ -6,17 +10,30 @@ using namespace Cpptraj::Energy;
 CMAP::CMAP() {}
 
 /** Calculate CMAP energy */
-double CMAP::Ene_CMAP(CmapArray const& Cmaps, Frame const& frameIn) {
+double CMAP::Ene_CMAP(CmapArray const& Cmaps, Frame const& frameIn)
+const
+{
   double ene_cmap = 0.0;
 
   for (CmapArray::const_iterator cmap = Cmaps.begin();
-                                 cmap != Cmaps.end(); ++cmap
+                                 cmap != Cmaps.end(); ++cmap)
   {
     const double* ixyz = frameIn.XYZ( cmap->A1() );
     const double* jxyz = frameIn.XYZ( cmap->A2() );
     const double* kxyz = frameIn.XYZ( cmap->A3() );
     const double* lxyz = frameIn.XYZ( cmap->A4() );
     const double* mxyz = frameIn.XYZ( cmap->A5() );
+
+    // Calculate the dihedral angle (phi) and the derivatives of the
+    // four coordinates with respect to phi. Remember this subroutine is
+    // operating in radians.
+    Vec3 dA, dB, dC, dD;
+    double cosphi_ijkl, sinphi_ijkl;
+    Torsion_and_part_deriv( ixyz, jxyz, kxyz, lxyz,
+                            dA, dB, dC, dD,
+                            cosphi_ijkl, sinphi_ijkl );
+    double phi = copysign(acos(cosphi_ijkl),sinphi_ijkl) * Constants::RADDEG;
+    mprintf("DEBUG: Dihedral 1 %i %i %i %i = %g deg.\n", cmap->A1()+1, cmap->A2()+1, cmap->A3()+1, cmap->A4()+1, phi);
 
   }
   return ene_cmap;
--- src/Energy/CMAP.h
+++ src/Energy/CMAP.h
@@ -1,11 +1,16 @@
 #ifndef INC_ENERGY_CMAP_H
 #define INC_ENERGY_CMAP_H
+class CmapArray;
+class Frame;
 namespace Cpptraj {
 namespace Energy {
 /// Implement CMAP energy term
 class CMAP {
   public:
     CMAP();
+
+    /// Calculate CMAP energy
+    double Ene_CMAP(CmapArray const&, Frame const&) const;
   private:
 };
 }
