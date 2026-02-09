f58ea7294608f89df47ab364bb237159c4ae4dad
Daniel R. Roe <daniel.r.roe@gmail.com>
2026-02-09 11:44:47 -0500

    Calc psi

--- src/Energy/CMAP.cpp
+++ src/Energy/CMAP.cpp
@@ -27,14 +27,24 @@ const
     // Calculate the dihedral angle (phi) and the derivatives of the
     // four coordinates with respect to phi. Remember this subroutine is
     // operating in radians.
-    Vec3 dA, dB, dC, dD;
+    Vec3 dAphi, dBphi, dCphi, dDphi;
     double cosphi_ijkl, sinphi_ijkl;
     Torsion_and_part_deriv( ixyz, jxyz, kxyz, lxyz,
-                            dA, dB, dC, dD,
+                            dAphi, dBphi, dCphi, dDphi,
                             cosphi_ijkl, sinphi_ijkl );
     double phi = copysign(acos(cosphi_ijkl),sinphi_ijkl) * Constants::RADDEG;
     mprintf("DEBUG: Dihedral 1 %i %i %i %i = %g deg.\n", cmap->A1()+1, cmap->A2()+1, cmap->A3()+1, cmap->A4()+1, phi);
 
+    // Calculate the dihedral angle (psi) and the derivatives of the
+    // four coordinates with respect to psi. Remember this subroutine is
+    // operating in radians.
+    Vec3 dApsi, dBpsi, dCpsi, dDpsi;
+    double cospsi_jklm, sinpsi_jklm;
+    Torsion_and_part_deriv( jxyz, kxyz, lxyz, mxyz,
+                            dApsi, dBpsi, dCpsi, dDpsi,
+                            cospsi_jklm, sinpsi_jklm );
+    double psi = copysign(acos(cospsi_jklm),sinpsi_jklm) * Constants::RADDEG;
+    mprintf("DEBUG: Dihedral 2 %i %i %i %i = %g deg.\n", cmap->A2()+1, cmap->A3()+1, cmap->A4()+1, cmap->A5()+1, psi);
   }
   return ene_cmap;
 }
