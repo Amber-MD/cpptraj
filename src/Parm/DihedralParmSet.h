3f431606e3326b43276eb87e9d50d60c6513bc1c
Daniel R. Roe <daniel.r.roe@gmail.com>
2026-02-24 14:28:13 -0500

    Start the DihedralParmSet class

---
+++ src/Parm/DihedralParmSet.h
@@ -0,0 +1,83 @@
+#ifndef INC_PARM_DIHEDRALPARMSET_H
+#define INC_PARM_DIHEDRALPARMSET_H
+#include <map>
+#include "ParmEnum.h"
+#include "../ParameterTypes.h"
+#include "../TypeNameHolder.h"
+namespace Cpptraj {
+namespace Parm {
+/// Used for reading in dihedral parameters.
+/** When dihedral parameters are read in one at a time it is not
+  * guaranteed that parameters for all multiplcities will be
+  * present. This allows dihedral parameters to be read in
+  * one multiplicit at a time and then be checked for
+  * consistency afterwards.
+  */
+class DihedralParmSet {
+    typedef std::pair<TypeNameHolder, DihedralParmArray> Dpair;
+    typedef std::map<TypeNameHolder, DihedralParmArray> Dmap;
+  public:
+    DihedralParmSet() {}
+    /// Add (or update) a single dihedral parameter for given atom types.
+    RetType AddDihParm(TypeNameHolder const& types, DihedralParmType const& dp, bool allowUpdate) {
+      // Check if dihedral parm for these types exist
+      Dmap::iterator it0 = dihparm_.lower_bound( types );
+      if (it0 == dihparm_.end() || it0->first != types) {
+        // Brand new dihedral for these types
+        it0 = dihparm_.insert(it0, Dpair(types, DihedralParmArray(1, dp)));
+      } else {
+        // If we are here types match - check multiplicity
+        DihedralParmArray::iterator it1 = it0->second.begin();
+        for (; it1 != it0->second.end(); ++it1)
+        {
+          if (FEQ(it1->Pn(), dp.Pn()))
+            break;
+        }
+        if (it1 == it0->second.end()) {
+          // Brand new multiplicity for this dihedral.
+          //mprintf("DEBUG: Dihedral new mult: %s %s %s %s pk=%12.4f pn=%12.4f pp=%12.4f\n",
+          //        *types[0], *types[1], *types[2], *types[3], dp.Pk(), dp.Pn(), dp.Phase());
+          if (it0->second.empty())
+            it0->second.push_back( dp );
+          else if (dp.Pn() > it0->second.back().Pn())
+            it0->second.push_back( dp );
+          else {
+            // Try to keep multiplicities in order.
+            DihedralParmArray sorted;
+            bool isInserted = false;
+            for (DihedralParmArray::const_iterator jt = it0->second.begin(); jt != it0->second.end(); ++jt) {
+              if (!isInserted) {
+                if (dp.Pn() < jt->Pn()) {
+                  sorted.push_back( dp );
+                  isInserted = true;
+                }
+              }
+              sorted.push_back( *jt );
+            }
+            it0->second = sorted;
+          }
+        } else {
+          // Multiplicity already exists for this dihedral.
+          if (dp < *it1 || *it1 < dp) {
+            //mprintf("DEBUG: Attempt dihedral update mult (allow=%i): %s %s %s %s pk=%6.2f pn=%3.1f pp=%6.3f (orig pk=%6.2f pn=%3.1f pp=%6.3f )\n",
+            //        (int)allowUpdate, *types[0], *types[1], *types[2], *types[3], dp.Pk(), dp.Pn(), dp.Phase(), it1->Pk(), it1->Pn(), it1->Phase());
+            if (allowUpdate) {
+              previousParm_ = *it1;
+              *it1 = dp;
+              return UPDATED;
+            } else {
+              return ERR;
+            }
+          } else
+            return SAME;
+        }
+      }
+      return ADDED;
+    }
+  private:
+    Dmap dihparm_;
+    DihedralParmType previousParm_; ///< When parameter is updated, store previous value.
+};
+}
+}
+#endif
