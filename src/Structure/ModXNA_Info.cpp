f85eac9a9b11d34cb9315c4c0ac28feb4f0dc427
Daniel R. Roe <daniel.roe@nih.gov>
2026-03-05 08:43:26 -0500

    Start modxna info class

---
+++ src/Structure/ModXNA_Info.cpp
@@ -0,0 +1,48 @@
+#include "ModXNA_Info.h"
+#include "../ArgList.h"
+#include "../CpptrajStdio.h"
+
+using namespace Cpptraj::Structure;
+
+/** CONSTRUCTOR */
+ModXNA_Info::ModXNA_Info() {}
+
+/** Parse ModXNA info from given string. */
+ModXNA_Info::StatType ModXNA_Info::ParseModxnaStr(std::string const& line) {
+  if (line.empty()) return NOT_MODXNA;
+  ArgList args(line, ":");
+
+  if (args.GetStringNext() != "modXNA-fragment") return NOT_MODXNA;
+
+  if (args.Nargs() < 2) {
+    mprinterr("Error: modXNA string is too short.\n"
+              "Error: '%s'\n", line.c_str());
+    return ERR;
+  }
+  fragmentName_ = args[1];
+
+  if (args.Nargs() >= 4) head_ = args[3];
+  if (args.Nargs() >= 6) headStrip_ = args[5];
+  if (args.Nargs() >= 8) tail_ = args[7];
+  if (args.Nargs() >= 10) tailStrip_ = args[9];
+  if (args.Nargs() >= 12) anchor_ = args[11];
+  if (args.Nargs() >= 14) anchorStrip_ = args[13];
+
+  return OK;
+}
+
+static inline void printmxna(const char* desc, std::string const& str) {
+  if (!str.empty()) mprintf("%s: %s\n", desc, str.c_str());
+}
+
+/** Print info to stdout */
+void ModXNA_Info::PrintModxna() const {
+  if (!HasModxna()) return;
+  mprintf("\tFragment: %s\n", fragmentName_.c_str());
+  printmxna("\tHead        ", head_);
+  printmxna("\tHeadStrip   ", headStrip_);
+  printmxna("\tTail        ", tail_);
+  printmxna("\tTailStrip   ", tailStrip_);
+  printmxna("\tAnchor      ", anchor_);
+  printmxna("\tAnchorStrip ", anchorStrip_);
+}
---
+++ src/Structure/ModXNA_Info.h
@@ -0,0 +1,39 @@
+#ifndef INC_STRUCTURE_MODXNA_INFO_H
+#define INC_STRUCTURE_MODXNA_INFO_H
+#include <string>
+namespace Cpptraj {
+namespace Structure {
+/// Hold info used by the modXNA FF for joining fragments
+class ModXNA_Info {
+  public:
+    /// CONSTRUCTOR
+    ModXNA_Info();
+
+    enum StatType { OK = 0, NOT_MODXNA, ERR };
+
+    /// Parse the given modXNA title string
+    StatType ParseModxnaStr(std::string const&);
+    /// \return true if there is modXNA info
+    bool HasModxna() const { return (!fragmentName_.empty()); }
+    /// Print to stdout
+    void PrintModxna() const;
+
+    std::string const& FragmentName() const { return fragmentName_; }
+    std::string const& Head()         const { return head_; }
+    std::string const& HeadStrip()    const { return headStrip_; }
+    std::string const& Tail()         const { return tail_; }
+    std::string const& TailStrip()    const { return tailStrip_; }
+    std::string const& Anchor()       const { return anchor_; }
+    std::string const& AnchorStrip()  const { return anchorStrip_; }
+  private:
+    std::string fragmentName_;
+    std::string head_;
+    std::string headStrip_;
+    std::string tail_;
+    std::string tailStrip_;
+    std::string anchor_;
+    std::string anchorStrip_;
+};
+}
+}
+#endif
