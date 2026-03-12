5c24239004b99047fbeafd483407d9c9a60e7a7a
Daniel R. Roe <daniel.roe@nih.gov>
2026-03-12 08:08:53 -0400

    Start adding back the old Builder class for modXNA

---
+++ src/Structure/OldBuilder.h
@@ -0,0 +1,25 @@
+#ifndef INC_STRUCTURE_OLDBUILDER_H
+#define INC_STRUCTURE_OLDBUILDER_H
+#include <vector>
+class Topology;
+class Frame;
+namespace Cpptraj {
+namespace Structure {
+/// This is the old (<v7) builder used to attach different topology/frame combos using internal coordinates.
+/** Retaining this class for backwards compatibility with modXNA. */
+class OldBuilder {
+  public:
+    /// CONSTRUCTOR
+    OldBuilder();
+    /// Combine second fragment into first fragment and bond
+    int Combine(Topology&, Frame&, Topology const&, Frame const&, int, int);
+    /// Set debug
+    void SetDebug(int d) { debug_ = d; }
+  private:
+    typedef std::vector<bool> Barray;
+
+    int debug_;
+};
+}
+}
+#endif
