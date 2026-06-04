5d5747968b43e3d3391ba5376f4027eba2501605
Daniel R. Roe <daniel.roe@nih.gov>
2026-06-04 09:31:36 -0400

    Use Frame::SetFrame() instead of copying; previously only coordinate information was being properly copied.

--- src/Action_SetVelocity.cpp
+++ src/Action_SetVelocity.cpp
@@ -140,10 +140,7 @@ Action::RetType Action_SetVelocity::Setup(ActionSetup& setup) {
 
 // Action_SetVelocity::DoAction()
 Action::RetType Action_SetVelocity::DoAction(int frameNum, ActionFrame& frm) {
-  std::copy( frm.Frm().xAddress(), frm.Frm().xAddress() + frm.Frm().size(), newFrame_.xAddress() );
-  if (frm.Frm().HasVelocity())
-    std::copy( frm.Frm().vAddress(), frm.Frm().vAddress() + frm.Frm().size(),
-               newFrame_.vAddress() );
+  newFrame_.SetFrame( frm.Frm() );
   if (mode_ == ZERO) {
     for (AtomMask::const_iterator atom = Mask_.begin(); atom != Mask_.end(); ++atom)
     {
